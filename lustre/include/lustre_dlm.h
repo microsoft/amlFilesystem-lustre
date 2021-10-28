lustre/lustre-release

commit 2250e072c37855d611aa64027945981fe2c8f4d7

LU-12017 ldlm: DoM truncate deadlock
setxattr takes inode lock and sends reint to MDS.
truncate takes MDS_INODELOCK_DOM lock and wants
to acquire inode lock.

MDS locks are for different bits
MDS_INODELOCK_UPDATE|MDS_INODELOCK_XATTR vs
MDS_INODELOCK_DOM but they blocks each other if
some blocking lock was present earlier.

If IBITS waiting lock has no conflicts with any lock in the
granted queue or any lock ahead in the waiting queue then
it can be granted.

Use separate waiting lists for each ibit to eliminate full
lr_waiting list scan.

Cray-bug-id: LUS-6970
Change-Id: I95b2ed0b1a0063b7ece5277a5ee06e2511d44e5f
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://review.whamcloud.com/35057
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_dlm.h
@@ -651,6 +651,19 @@ struct ldlm_interval_tree {
 	struct interval_node	*lit_root; /* actual ldlm_interval */
 };
 
+/**
+ * Lists of waiting locks for each inodebit type.
+ * A lock can be in several liq_waiting lists and it remains in lr_waiting.
+ */
+struct ldlm_ibits_queues {
+	struct list_head	liq_waiting[MDS_INODELOCK_NUMBITS];
+};
+
+struct ldlm_ibits_node {
+	struct list_head	lin_link[MDS_INODELOCK_NUMBITS];
+	struct ldlm_lock	*lock;
+};
+
 /** Whether to track references to exports by LDLM locks. */
 #define LUSTRE_TRACKS_LOCK_EXP_REFS (0)
 
@@ -747,9 +760,12 @@ struct ldlm_lock {
 	 */
 	struct list_head	l_res_link;
 	/**
-	 * Tree node for ldlm_extent.
+	 * Internal structures per lock type..
 	 */
-	struct ldlm_interval	*l_tree_node;
+	union {
+		struct ldlm_interval	*l_tree_node;
+		struct ldlm_ibits_node  *l_ibits_node;
+	};
 	/**
 	 * Per export hash of locks.
 	 * Protected by per-bucket exp->exp_lock_hash locks.
@@ -1005,10 +1021,14 @@ struct ldlm_resource {
 	/** Resource name */
 	struct ldlm_res_id	lr_name;
 
-	/**
-	 * Interval trees (only for extent locks) for all modes of this resource
-	 */
-	struct ldlm_interval_tree *lr_itree;
+	union {
+		/**
+		 * Interval trees (only for extent locks) for all modes of
+		 * this resource
+		 */
+		struct ldlm_interval_tree *lr_itree;
+		struct ldlm_ibits_queues *lr_ibits_queues;
+	};
 
 	union {
 		/**
@@ -1274,6 +1294,12 @@ typedef int (*ldlm_processing_policy)(struct ldlm_lock *lock, __u64 *flags,
 				      enum ldlm_error *err,
 				      struct list_head *work_list);
 
+typedef int (*ldlm_reprocessing_policy)(struct ldlm_resource *res,
+					struct list_head *queue,
+					struct list_head *work_list,
+					enum ldlm_process_intention intention,
+					struct ldlm_lock *hint);
+
 /**
  * Return values for lock iterators.
  * Also used during deciding of lock grants and cancellations.
@@ -1369,6 +1395,8 @@ struct ldlm_lock *ldlm_request_lock(struct ptlrpc_request *req);
 /* ldlm_lock.c */
 #ifdef HAVE_SERVER_SUPPORT
 ldlm_processing_policy ldlm_get_processing_policy(struct ldlm_resource *res);
+ldlm_reprocessing_policy
+ldlm_get_reprocessing_policy(struct ldlm_resource *res);
 #endif
 void ldlm_register_intent(struct ldlm_namespace *ns, ldlm_res_policy arg);
 void ldlm_lock2handle(const struct ldlm_lock *lock,
@@ -1520,7 +1548,7 @@ enum ldlm_mode ldlm_revalidate_lock_handle(const struct lustre_handle *lockh,
 					   __u64 *bits);
 void ldlm_lock_mode_downgrade(struct ldlm_lock *lock, enum ldlm_mode new_mode);
 void ldlm_lock_cancel(struct ldlm_lock *lock);
-void ldlm_reprocess_all(struct ldlm_resource *res);
+void ldlm_reprocess_all(struct ldlm_resource *res, struct ldlm_lock *hint);
 void ldlm_reprocess_recovery_done(struct ldlm_namespace *ns);
 void ldlm_lock_dump_handle(int level, const struct lustre_handle *lockh);
 void ldlm_unlink_lock_skiplist(struct ldlm_lock *req);

lustre/ldlm/ldlm_flock.c
@@ -581,7 +581,7 @@ reprocess:
 restart:
 				ldlm_reprocess_queue(res, &res->lr_waiting,
 						     &rpc_list,
-						     LDLM_PROCESS_RESCAN);
+						     LDLM_PROCESS_RESCAN, NULL);
 
                                 unlock_res_and_lock(req);
                                 rc = ldlm_run_ast_work(ns, &rpc_list,

lustre/ldlm/ldlm_inodebits.c
@@ -59,6 +59,88 @@
 #ifdef HAVE_SERVER_SUPPORT
 
 /**
+ * It should iterate through all waiting locks on a given resource queue and
+ * attempt to grant them. An optimization is to check only heads waitintg
+ * locks for each inodebit type.
+ *
+ * Must be called with resource lock held.
+ */
+int ldlm_reprocess_inodebits_queue(struct ldlm_resource *res,
+				   struct list_head *queue,
+				   struct list_head *work_list,
+				   enum ldlm_process_intention intention,
+				   struct ldlm_lock *hint)
+{
+	__u64 flags;
+	int rc = LDLM_ITER_CONTINUE;
+	enum ldlm_error err;
+	struct list_head bl_ast_list = LIST_HEAD_INIT(bl_ast_list);
+	struct ldlm_ibits_queues *queues = res->lr_ibits_queues;
+	int i;
+
+	ENTRY;
+
+	check_res_locked(res);
+
+	LASSERT(res->lr_type == LDLM_IBITS);
+	LASSERT(intention == LDLM_PROCESS_RESCAN ||
+		intention == LDLM_PROCESS_RECOVERY);
+
+	if (intention == LDLM_PROCESS_RECOVERY)
+		return ldlm_reprocess_queue(res, queue, work_list, intention,
+					    NULL);
+
+restart:
+	CDEBUG(D_DLMTRACE, "--- Reprocess resource "DLDLMRES" (%p)\n",
+	       PLDLMRES(res), res);
+
+	for (i = 0; i < MDS_INODELOCK_NUMBITS; i++) {
+		struct list_head rpc_list = LIST_HEAD_INIT(rpc_list);
+		struct list_head *head = &queues->liq_waiting[i];
+		struct ldlm_lock *pending;
+		struct ldlm_ibits_node *node;
+
+		if (list_empty(head))
+			continue;
+		if (hint && !(hint->l_policy_data.l_inodebits.bits & (1 << i)))
+			continue;
+
+		node = list_entry(head->next, struct ldlm_ibits_node,
+				  lin_link[i]);
+
+		pending = node->lock;
+		LDLM_DEBUG(pending, "Reprocessing lock from queue %d", i);
+
+		flags = 0;
+		rc = ldlm_process_inodebits_lock(pending, &flags, intention,
+						 &err, &rpc_list);
+		if (ldlm_is_granted(pending)) {
+			list_splice(&rpc_list, work_list);
+			/* Try to grant more locks from current queue */
+			i--;
+		} else {
+			list_splice(&rpc_list, &bl_ast_list);
+		}
+	}
+
+	if (!list_empty(&bl_ast_list)) {
+		unlock_res(res);
+
+		rc = ldlm_run_ast_work(ldlm_res_to_ns(res), &bl_ast_list,
+				       LDLM_WORK_BL_AST);
+
+		lock_res(res);
+		if (rc == -ERESTART)
+			GOTO(restart, rc);
+	}
+
+	if (!list_empty(&bl_ast_list))
+		ldlm_discard_bl_list(&bl_ast_list);
+
+	RETURN(rc);
+}
+
+/**
  * Determine if the lock is compatible with all locks on the queue.
  *
  * If \a work_list is provided, conflicting locks are linked there.
@@ -427,4 +509,56 @@ int ldlm_cli_dropbits(struct ldlm_lock *lock, __u64 drop_bits)
 exit:
 	LDLM_DEBUG(lock, "client lock convert END");
 	return rc;
+}
+
+
+int ldlm_inodebits_alloc_lock(struct ldlm_lock *lock)
+{
+	if (ldlm_is_ns_srv(lock)) {
+		int i;
+
+		OBD_SLAB_ALLOC_PTR(lock->l_ibits_node, ldlm_inodebits_slab);
+		if (lock->l_ibits_node == NULL)
+			return -ENOMEM;
+		for (i = 0; i < MDS_INODELOCK_NUMBITS; i++)
+			INIT_LIST_HEAD(&lock->l_ibits_node->lin_link[i]);
+		lock->l_ibits_node->lock = lock;
+	} else {
+		lock->l_ibits_node = NULL;
+	}
+	return 0;
+}
+
+void ldlm_inodebits_add_lock(struct ldlm_resource *res, struct list_head *head,
+			     struct ldlm_lock *lock)
+{
+	int i;
+
+	if (!ldlm_is_ns_srv(lock))
+		return;
+
+	if (head == &res->lr_waiting) {
+		for (i = 0; i < MDS_INODELOCK_NUMBITS; i++) {
+			if (lock->l_policy_data.l_inodebits.bits & (1 << i))
+				list_add_tail(&lock->l_ibits_node->lin_link[i],
+					&res->lr_ibits_queues->liq_waiting[i]);
+		}
+	} else if (head == &res->lr_granted && lock->l_ibits_node != NULL) {
+		for (i = 0; i < MDS_INODELOCK_NUMBITS; i++)
+			LASSERT(list_empty(&lock->l_ibits_node->lin_link[i]));
+		OBD_SLAB_FREE_PTR(lock->l_ibits_node, ldlm_inodebits_slab);
+		lock->l_ibits_node = NULL;
+	}
+}
+
+void ldlm_inodebits_unlink_lock(struct ldlm_lock *lock)
+{
+	int i;
+
+	ldlm_unlink_lock_skiplist(lock);
+	if (!ldlm_is_ns_srv(lock))
+		return;
+
+	for (i = 0; i < MDS_INODELOCK_NUMBITS; i++)
+		list_del_init(&lock->l_ibits_node->lin_link[i]);
 }

lustre/ldlm/ldlm_internal.h
@@ -120,6 +120,7 @@ extern unsigned int ldlm_enqueue_min;
 /* ldlm_resource.c */
 extern struct kmem_cache *ldlm_resource_slab;
 extern struct kmem_cache *ldlm_lock_slab;
+extern struct kmem_cache *ldlm_inodebits_slab;
 extern struct kmem_cache *ldlm_interval_tree_slab;
 
 void ldlm_resource_insert_lock_after(struct ldlm_lock *original,
@@ -156,7 +157,8 @@ void ldlm_add_ast_work_item(struct ldlm_lock *lock, struct ldlm_lock *new,
 #ifdef HAVE_SERVER_SUPPORT
 int ldlm_reprocess_queue(struct ldlm_resource *res, struct list_head *queue,
 			 struct list_head *work_list,
-			 enum ldlm_process_intention intention);
+			 enum ldlm_process_intention intention,
+			 struct ldlm_lock *hint);
 int ldlm_handle_conflict_lock(struct ldlm_lock *lock, __u64 *flags,
 			      struct list_head *rpc_list);
 void ldlm_discard_bl_list(struct list_head *bl_list);
@@ -202,13 +204,24 @@ int ldlm_process_inodebits_lock(struct ldlm_lock *lock, __u64 *flags,
 				enum ldlm_process_intention intention,
 				enum ldlm_error *err,
 				struct list_head *work_list);
+int ldlm_reprocess_inodebits_queue(struct ldlm_resource *res,
+				   struct list_head *queue,
+				   struct list_head *work_list,
+				   enum ldlm_process_intention intention,
+				   struct ldlm_lock *hint);
 /* ldlm_extent.c */
 int ldlm_process_extent_lock(struct ldlm_lock *lock, __u64 *flags,
 			     enum ldlm_process_intention intention,
 			     enum ldlm_error *err, struct list_head *work_list);
 #endif
+int ldlm_extent_alloc_lock(struct ldlm_lock *lock);
 void ldlm_extent_add_lock(struct ldlm_resource *res, struct ldlm_lock *lock);
 void ldlm_extent_unlink_lock(struct ldlm_lock *lock);
+
+int ldlm_inodebits_alloc_lock(struct ldlm_lock *lock);
+void ldlm_inodebits_add_lock(struct ldlm_resource *res, struct list_head *head,
+			     struct ldlm_lock *lock);
+void ldlm_inodebits_unlink_lock(struct ldlm_lock *lock);
 
 /* ldlm_flock.c */
 int ldlm_process_flock_lock(struct ldlm_lock *req, __u64 *flags,
@@ -235,7 +248,6 @@ struct ldlm_state {
 extern struct kmem_cache *ldlm_interval_slab; /* slab cache for ldlm_interval */
 extern void ldlm_interval_attach(struct ldlm_interval *n, struct ldlm_lock *l);
 extern struct ldlm_interval *ldlm_interval_detach(struct ldlm_lock *l);
-extern struct ldlm_interval *ldlm_interval_alloc(struct ldlm_lock *lock);
 extern void ldlm_interval_free(struct ldlm_interval *node);
 /* this function must be called with res lock held */
 static inline struct ldlm_extent *

lustre/ldlm/ldlm_lock.c
@@ -149,6 +149,19 @@ ldlm_processing_policy ldlm_get_processing_policy(struct ldlm_resource *res)
         return ldlm_processing_policy_table[res->lr_type];
 }
 EXPORT_SYMBOL(ldlm_get_processing_policy);
+
+static ldlm_reprocessing_policy ldlm_reprocessing_policy_table[] = {
+	[LDLM_PLAIN]	= ldlm_reprocess_queue,
+	[LDLM_EXTENT]	= ldlm_reprocess_queue,
+	[LDLM_FLOCK]	= ldlm_reprocess_queue,
+	[LDLM_IBITS]	= ldlm_reprocess_inodebits_queue,
+};
+
+ldlm_reprocessing_policy ldlm_get_reprocessing_policy(struct ldlm_resource *res)
+{
+	return ldlm_reprocessing_policy_table[res->lr_type];
+}
+
 #endif /* HAVE_SERVER_SUPPORT */
 
 void ldlm_register_intent(struct ldlm_namespace *ns, ldlm_res_policy arg)
@@ -203,8 +216,6 @@ void ldlm_lock_put(struct ldlm_lock *lock)
                 lprocfs_counter_decr(ldlm_res_to_ns(res)->ns_stats,
                                      LDLM_NSS_LOCKS);
                 lu_ref_del(&res->lr_reference, "lock", lock);
-                ldlm_resource_putref(res);
-                lock->l_resource = NULL;
                 if (lock->l_export) {
                         class_export_lock_put(lock->l_export, lock);
                         lock->l_export = NULL;
@@ -213,7 +224,15 @@ void ldlm_lock_put(struct ldlm_lock *lock)
                 if (lock->l_lvb_data != NULL)
                         OBD_FREE_LARGE(lock->l_lvb_data, lock->l_lvb_len);
 
-                ldlm_interval_free(ldlm_interval_detach(lock));
+		if (res->lr_type == LDLM_EXTENT) {
+			ldlm_interval_free(ldlm_interval_detach(lock));
+		} else if (res->lr_type == LDLM_IBITS) {
+			if (lock->l_ibits_node != NULL)
+				OBD_SLAB_FREE_PTR(lock->l_ibits_node,
+						  ldlm_inodebits_slab);
+		}
+		ldlm_resource_putref(res);
+		lock->l_resource = NULL;
                 lu_ref_fini(&lock->l_reference);
 		OBD_FREE_RCU(lock, sizeof(*lock), &lock->l_handle);
         }
@@ -1666,11 +1685,18 @@ struct ldlm_lock *ldlm_lock_create(struct ldlm_namespace *ns,
 		lock->l_glimpse_ast = cbs->lcs_glimpse;
 	}
 
-	lock->l_tree_node = NULL;
-	/* if this is the extent lock, allocate the interval tree node */
-	if (type == LDLM_EXTENT)
-		if (ldlm_interval_alloc(lock) == NULL)
-			GOTO(out, rc = -ENOMEM);
+	switch (type) {
+	case LDLM_EXTENT:
+		rc = ldlm_extent_alloc_lock(lock);
+		break;
+	case LDLM_IBITS:
+		rc = ldlm_inodebits_alloc_lock(lock);
+		break;
+	default:
+		rc = 0;
+	}
+	if (rc)
+		GOTO(out, rc);
 
 	if (lvb_len) {
 		lock->l_lvb_len = lvb_len;
@@ -1699,9 +1725,10 @@ static enum ldlm_error ldlm_lock_enqueue_helper(struct ldlm_lock *lock,
 	enum ldlm_error rc = ELDLM_OK;
 	struct list_head rpc_list = LIST_HEAD_INIT(rpc_list);
 	ldlm_processing_policy policy;
+
 	ENTRY;
 
-	policy = ldlm_processing_policy_table[res->lr_type];
+	policy = ldlm_get_processing_policy(res);
 restart:
 	policy(lock, flags, LDLM_PROCESS_ENQUEUE, &rc, &rpc_list);
 	if (rc == ELDLM_OK && lock->l_granted_mode != lock->l_req_mode &&
@@ -1880,7 +1907,8 @@ out:
  */
 int ldlm_reprocess_queue(struct ldlm_resource *res, struct list_head *queue,
 			 struct list_head *work_list,
-			 enum ldlm_process_intention intention)
+			 enum ldlm_process_intention intention,
+			 struct ldlm_lock *hint)
 {
 	struct list_head *tmp, *pos;
 	ldlm_processing_policy policy;
@@ -1888,11 +1916,12 @@ int ldlm_reprocess_queue(struct ldlm_resource *res, struct list_head *queue,
 	int rc = LDLM_ITER_CONTINUE;
 	enum ldlm_error err;
 	struct list_head bl_ast_list = LIST_HEAD_INIT(bl_ast_list);
+
 	ENTRY;
 
 	check_res_locked(res);
 
-	policy = ldlm_processing_policy_table[res->lr_type];
+	policy = ldlm_get_processing_policy(res);
 	LASSERT(policy);
 	LASSERT(intention == LDLM_PROCESS_RESCAN ||
 		intention == LDLM_PROCESS_RECOVERY);
@@ -2287,20 +2316,23 @@ out:
  * if anything could be granted as a result of the cancellation.
  */
 static void __ldlm_reprocess_all(struct ldlm_resource *res,
-				 enum ldlm_process_intention intention)
+				 enum ldlm_process_intention intention,
+				 struct ldlm_lock *hint)
 {
 	struct list_head rpc_list;
 #ifdef HAVE_SERVER_SUPPORT
+	ldlm_reprocessing_policy reprocess;
 	struct obd_device *obd;
-        int rc;
-        ENTRY;
+	int rc;
+
+	ENTRY;
 
 	INIT_LIST_HEAD(&rpc_list);
-        /* Local lock trees don't get reprocessed. */
-        if (ns_is_client(ldlm_res_to_ns(res))) {
-                EXIT;
-                return;
-        }
+	/* Local lock trees don't get reprocessed. */
+	if (ns_is_client(ldlm_res_to_ns(res))) {
+		EXIT;
+		return;
+	}
 
 	/* Disable reprocess during lock replay stage but allow during
 	 * request replay stage.
@@ -2311,7 +2343,8 @@ static void __ldlm_reprocess_all(struct ldlm_resource *res,
 		RETURN_EXIT;
 restart:
 	lock_res(res);
-	ldlm_reprocess_queue(res, &res->lr_waiting, &rpc_list, intention);
+	reprocess = ldlm_get_reprocessing_policy(res);
+	reprocess(res, &res->lr_waiting, &rpc_list, intention, hint);
 	unlock_res(res);
 
 	rc = ldlm_run_ast_work(ldlm_res_to_ns(res), &rpc_list,
@@ -2321,21 +2354,21 @@ restart:
 		goto restart;
 	}
 #else
-        ENTRY;
+	ENTRY;
 
 	INIT_LIST_HEAD(&rpc_list);
-        if (!ns_is_client(ldlm_res_to_ns(res))) {
-                CERROR("This is client-side-only module, cannot handle "
-                       "LDLM_NAMESPACE_SERVER resource type lock.\n");
-                LBUG();
-        }
+	if (!ns_is_client(ldlm_res_to_ns(res))) {
+		CERROR("This is client-side-only module, cannot handle "
+		       "LDLM_NAMESPACE_SERVER resource type lock.\n");
+		LBUG();
+	}
 #endif
-        EXIT;
+	EXIT;
 }
 
-void ldlm_reprocess_all(struct ldlm_resource *res)
+void ldlm_reprocess_all(struct ldlm_resource *res, struct ldlm_lock *hint)
 {
-	__ldlm_reprocess_all(res, LDLM_PROCESS_RESCAN);
+	__ldlm_reprocess_all(res, LDLM_PROCESS_RESCAN, hint);
 }
 EXPORT_SYMBOL(ldlm_reprocess_all);
 
@@ -2345,7 +2378,7 @@ static int ldlm_reprocess_res(struct cfs_hash *hs, struct cfs_hash_bd *bd,
 	struct ldlm_resource *res = cfs_hash_object(hs, hnode);
 
 	/* This is only called once after recovery done. LU-8306. */
-	__ldlm_reprocess_all(res, LDLM_PROCESS_RECOVERY);
+	__ldlm_reprocess_all(res, LDLM_PROCESS_RECOVERY, NULL);
 	return 0;
 }
 
@@ -2494,7 +2527,7 @@ static void ldlm_cancel_lock_for_export(struct obd_export *exp,
 	ldlm_lvbo_update(res, lock, NULL, 1);
 	ldlm_lock_cancel(lock);
 	if (!exp->exp_obd->obd_stopping)
-		ldlm_reprocess_all(res);
+		ldlm_reprocess_all(res, lock);
 	ldlm_resource_putref(res);
 
 	ecl->ecl_loop++;
@@ -2657,7 +2690,7 @@ void ldlm_lock_mode_downgrade(struct ldlm_lock *lock, enum ldlm_mode new_mode)
 	ldlm_grant_lock(lock, NULL);
 	unlock_res_and_lock(lock);
 
-	ldlm_reprocess_all(lock->l_resource);
+	ldlm_reprocess_all(lock->l_resource, lock);
 
 	EXIT;
 #endif

microsoft/amlFilesystem-lustre

commit ba7ab7531d2fd4e401c5293b2bccb5fcbbf70f94

LU-13456 ldlm: fix reprocessing of locks with more bits
Reprocessing check queues should be extended
with just granted lock inodebits.

ldlm_reprocess_all() should be called on BL AST race.

Change-Id: Ifd232062068481c1c62fa2f2a14c7778d4402260
Fixes: 2250e072c3785 ("LU-12017 ldlm: DoM truncate deadlock")
HPE-bug-id: LUS-8224
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>

lustre/include/lustre_dlm.h
@@ -1361,7 +1361,7 @@ typedef int (*ldlm_reprocessing_policy)(struct ldlm_resource *res,
 					struct list_head *queue,
 					struct list_head *work_list,
 					enum ldlm_process_intention intention,
-					struct ldlm_lock *hint);
+					__u64 hint);
 
 /**
  * Return values for lock iterators.
@@ -1612,7 +1612,7 @@ enum ldlm_mode ldlm_revalidate_lock_handle(const struct lustre_handle *lockh,
 					   __u64 *bits);
 void ldlm_lock_mode_downgrade(struct ldlm_lock *lock, enum ldlm_mode new_mode);
 void ldlm_lock_cancel(struct ldlm_lock *lock);
-void ldlm_reprocess_all(struct ldlm_resource *res, struct ldlm_lock *hint);
+void ldlm_reprocess_all(struct ldlm_resource *res, __u64 hint);
 void ldlm_reprocess_recovery_done(struct ldlm_namespace *ns);
 void ldlm_lock_dump_handle(int level, const struct lustre_handle *lockh);
 void ldlm_unlink_lock_skiplist(struct ldlm_lock *req);

lustre/ldlm/ldlm_flock.c
@@ -604,7 +604,7 @@ reprocess:
 restart:
 				ldlm_reprocess_queue(res, &res->lr_waiting,
 						     &rpc_list,
-						     LDLM_PROCESS_RESCAN, NULL);
+						     LDLM_PROCESS_RESCAN, 0);
 
 				unlock_res_and_lock(req);
 				rc = ldlm_run_ast_work(ns, &rpc_list,

lustre/ldlm/ldlm_inodebits.c
@@ -68,7 +68,7 @@ int ldlm_reprocess_inodebits_queue(struct ldlm_resource *res,
 				   struct list_head *queue,
 				   struct list_head *work_list,
 				   enum ldlm_process_intention intention,
-				   struct ldlm_lock *hint)
+				   __u64 mask)
 {
 	__u64 flags;
 	int rc = LDLM_ITER_CONTINUE;
@@ -87,11 +87,15 @@ int ldlm_reprocess_inodebits_queue(struct ldlm_resource *res,
 
 	if (intention == LDLM_PROCESS_RECOVERY)
 		return ldlm_reprocess_queue(res, queue, work_list, intention,
-					    NULL);
+					    0);
 
 restart:
 	CDEBUG(D_DLMTRACE, "--- Reprocess resource "DLDLMRES" (%p)\n",
 	       PLDLMRES(res), res);
+	if (mask)
+		CDEBUG(D_DLMTRACE, "Hint %llx\n", mask);
+	else
+		mask = MDS_INODELOCK_FULL;
 
 	for (i = 0; i < MDS_INODELOCK_NUMBITS; i++) {
 		LIST_HEAD(rpc_list);
@@ -99,9 +103,7 @@ restart:
 		struct ldlm_lock *pending;
 		struct ldlm_ibits_node *node;
 
-		if (list_empty(head))
-			continue;
-		if (hint && !(hint->l_policy_data.l_inodebits.bits & BIT(i)))
+		if (list_empty(head) || !(mask & (1 << i)))
 			continue;
 
 		node = list_entry(head->next, struct ldlm_ibits_node,
@@ -115,8 +117,8 @@ restart:
 						 &err, &rpc_list);
 		if (ldlm_is_granted(pending)) {
 			list_splice(&rpc_list, work_list);
-			/* Try to grant more locks from current queue */
-			i--;
+			mask |= pending->l_policy_data.l_inodebits.bits;
+			i = ffs(pending->l_policy_data.l_inodebits.bits) - 2;
 		} else {
 			list_splice(&rpc_list, &bl_ast_list);
 		}
@@ -129,8 +131,10 @@ restart:
 				       LDLM_WORK_BL_AST);
 
 		lock_res(res);
-		if (rc == -ERESTART)
+		if (rc == -ERESTART) {
+			mask = 0;
 			GOTO(restart, rc);
+		}
 	}
 
 	if (!list_empty(&bl_ast_list))

lustre/ldlm/ldlm_internal.h
@@ -143,8 +143,7 @@ void ldlm_add_ast_work_item(struct ldlm_lock *lock, struct ldlm_lock *new,
 #ifdef HAVE_SERVER_SUPPORT
 int ldlm_reprocess_queue(struct ldlm_resource *res, struct list_head *queue,
 			 struct list_head *work_list,
-			 enum ldlm_process_intention intention,
-			 struct ldlm_lock *hint);
+			 enum ldlm_process_intention intention, __u64 hint);
 int ldlm_handle_conflict_lock(struct ldlm_lock *lock, __u64 *flags,
 			      struct list_head *rpc_list);
 void ldlm_discard_bl_list(struct list_head *bl_list);
@@ -195,7 +194,7 @@ int ldlm_reprocess_inodebits_queue(struct ldlm_resource *res,
 				   struct list_head *queue,
 				   struct list_head *work_list,
 				   enum ldlm_process_intention intention,
-				   struct ldlm_lock *hint);
+				   __u64 hint);
 /* ldlm_extent.c */
 int ldlm_process_extent_lock(struct ldlm_lock *lock, __u64 *flags,
 			     enum ldlm_process_intention intention,

lustre/ldlm/ldlm_lock.c
@@ -1935,8 +1935,7 @@ out:
  */
 int ldlm_reprocess_queue(struct ldlm_resource *res, struct list_head *queue,
 			 struct list_head *work_list,
-			 enum ldlm_process_intention intention,
-			 struct ldlm_lock *hint)
+			 enum ldlm_process_intention intention, __u64 hint)
 {
 	struct list_head *tmp, *pos;
 	ldlm_processing_policy policy;
@@ -2038,7 +2037,7 @@ int ldlm_handle_conflict_lock(struct ldlm_lock *lock, __u64 *flags,
 		class_fail_export(lock->l_export);
 
 	if (rc == -ERESTART)
-		ldlm_reprocess_all(res, NULL);
+		ldlm_reprocess_all(res, 0);
 
 	lock_res(res);
 	if (rc == -ERESTART) {
@@ -2349,7 +2348,7 @@ out:
  */
 static void __ldlm_reprocess_all(struct ldlm_resource *res,
 				 enum ldlm_process_intention intention,
-				 struct ldlm_lock *hint)
+				 __u64 hint)
 {
 	LIST_HEAD(rpc_list);
 #ifdef HAVE_SERVER_SUPPORT
@@ -2382,6 +2381,7 @@ restart:
 			       LDLM_WORK_CP_AST);
 	if (rc == -ERESTART) {
 		LASSERT(list_empty(&rpc_list));
+		hint = 0;
 		goto restart;
 	}
 #else
@@ -2396,7 +2396,7 @@ restart:
 	EXIT;
 }
 
-void ldlm_reprocess_all(struct ldlm_resource *res, struct ldlm_lock *hint)
+void ldlm_reprocess_all(struct ldlm_resource *res, __u64 hint)
 {
 	__ldlm_reprocess_all(res, LDLM_PROCESS_RESCAN, hint);
 }
@@ -2408,7 +2408,7 @@ static int ldlm_reprocess_res(struct cfs_hash *hs, struct cfs_hash_bd *bd,
 	struct ldlm_resource *res = cfs_hash_object(hs, hnode);
 
 	/* This is only called once after recovery done. LU-8306. */
-	__ldlm_reprocess_all(res, LDLM_PROCESS_RECOVERY, NULL);
+	__ldlm_reprocess_all(res, LDLM_PROCESS_RECOVERY, 0);
 	return 0;
 }
 
@@ -2555,7 +2555,7 @@ static void ldlm_cancel_lock_for_export(struct obd_export *exp,
 	ldlm_lvbo_update(res, lock, NULL, 1);
 	ldlm_lock_cancel(lock);
 	if (!exp->exp_obd->obd_stopping)
-		ldlm_reprocess_all(res, lock);
+		ldlm_reprocess_all(res, lock->l_policy_data.l_inodebits.bits);
 	ldlm_resource_putref(res);
 
 	ecl->ecl_loop++;
@@ -2720,7 +2720,8 @@ void ldlm_lock_mode_downgrade(struct ldlm_lock *lock, enum ldlm_mode new_mode)
 	ldlm_grant_lock(lock, NULL);
 	unlock_res_and_lock(lock);
 
-	ldlm_reprocess_all(lock->l_resource, lock);
+	ldlm_reprocess_all(lock->l_resource,
+			   lock->l_policy_data.l_inodebits.bits);
 
 	EXIT;
 #endif

lustre/ldlm/ldlm_lockd.c
@@ -1200,7 +1200,7 @@ int ldlm_glimpse_locks(struct ldlm_resource *res,
 	rc = ldlm_run_ast_work(ldlm_res_to_ns(res), gl_work_list,
 			       LDLM_WORK_GL_AST);
 	if (rc == -ERESTART)
-		ldlm_reprocess_all(res, NULL);
+		ldlm_reprocess_all(res, 0);
 
 	RETURN(rc);
 }
@@ -1548,12 +1548,13 @@ retry:
 				ldlm_lock_destroy_nolock(lock);
 				unlock_res_and_lock(lock);
 			}
-			ldlm_reprocess_all(lock->l_resource, lock);
+			ldlm_reprocess_all(lock->l_resource, lock->l_policy_data.l_inodebits.bits);
 		}
 
 		if (!err && !ldlm_is_cbpending(lock) &&
 		    dlm_req->lock_desc.l_resource.lr_type != LDLM_FLOCK)
-			ldlm_reprocess_all(lock->l_resource, lock);
+			ldlm_reprocess_all(lock->l_resource,
+					   lock->l_policy_data.l_inodebits.bits);
 
 		LDLM_LOCK_RELEASE(lock);
 	}
@@ -1659,7 +1660,10 @@ int ldlm_handle_convert0(struct ptlrpc_request *req,
 		ldlm_clear_blocking_data(lock);
 		unlock_res_and_lock(lock);
 
-		ldlm_reprocess_all(lock->l_resource, NULL);
+		/* All old bits should be reprocessed to send new BL AST if
+		 * it wasn't sent earlier due to LDLM_FL_AST_SENT bit set.
+		 * */
+		ldlm_reprocess_all(lock->l_resource, bits);
 	}
 
 	dlm_rep->lock_handle = lock->l_remote_handle;
@@ -1733,7 +1737,7 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 		 */
 		if (res != pres) {
 			if (pres != NULL) {
-				ldlm_reprocess_all(pres, NULL);
+				ldlm_reprocess_all(pres, 0);
 				LDLM_RESOURCE_DELREF(pres);
 				ldlm_resource_putref(pres);
 			}
@@ -1764,7 +1768,7 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 		LDLM_LOCK_PUT(lock);
 	}
 	if (pres != NULL) {
-		ldlm_reprocess_all(pres, NULL);
+		ldlm_reprocess_all(pres, 0);
 		LDLM_RESOURCE_DELREF(pres);
 		ldlm_resource_putref(pres);
 	}
@@ -2693,14 +2697,17 @@ static int ldlm_revoke_lock_cb(struct cfs_hash *hs, struct cfs_hash_bd *bd,
 
 void ldlm_revoke_export_locks(struct obd_export *exp)
 {
+	int rc;
 	LIST_HEAD(rpc_list);
-
 	ENTRY;
 
 	cfs_hash_for_each_nolock(exp->exp_lock_hash,
 				 ldlm_revoke_lock_cb, &rpc_list, 0);
-	ldlm_run_ast_work(exp->exp_obd->obd_namespace, &rpc_list,
+	rc = ldlm_run_ast_work(exp->exp_obd->obd_namespace, &rpc_list,
 			  LDLM_WORK_REVOKE_AST);
+
+	if (rc == -ERESTART)
+		ldlm_reprocess_recovery_done(exp->exp_obd->obd_namespace);
 
 	EXIT;
 }

lustre/ldlm/ldlm_reclaim.c
@@ -181,6 +181,7 @@ static void ldlm_reclaim_res(struct ldlm_namespace *ns, int *count,
 {
 	struct ldlm_reclaim_cb_data	data;
 	int				idx, type, start;
+	int				rc;
 	ENTRY;
 
 	LASSERT(*count != 0);
@@ -216,7 +217,10 @@ static void ldlm_reclaim_res(struct ldlm_namespace *ns, int *count,
 	LASSERTF(*count >= data.rcd_added, "count:%d, added:%d\n", *count,
 		 data.rcd_added);
 
-	ldlm_run_ast_work(ns, &data.rcd_rpc_list, LDLM_WORK_REVOKE_AST);
+	rc  = ldlm_run_ast_work(ns, &data.rcd_rpc_list, LDLM_WORK_REVOKE_AST);
+	if (rc == -ERESTART)
+		ldlm_reprocess_recovery_done(ns);
+
 	*count -= data.rcd_added;
 	EXIT;
 }

lustre/ldlm/ldlm_request.c
@@ -236,9 +236,8 @@ int ldlm_completion_ast_async(struct ldlm_lock *lock, __u64 flags, void *data)
 		RETURN(ldlm_completion_tail(lock, data));
 	}
 
-	LDLM_DEBUG(lock,
-		   "client-side enqueue returned a blocked lock, going forward");
-	ldlm_reprocess_all(lock->l_resource, NULL);
+	LDLM_DEBUG(lock, "client-side enqueue returned a blocked lock, going forward");
+	ldlm_reprocess_all(lock->l_resource, 0);
 	RETURN(0);
 }
 EXPORT_SYMBOL(ldlm_completion_ast_async);
@@ -1255,7 +1254,8 @@ static __u64 ldlm_cli_cancel_local(struct ldlm_lock *lock)
 		}
 		LDLM_DEBUG(lock, "server-side local cancel");
 		ldlm_lock_cancel(lock);
-		ldlm_reprocess_all(lock->l_resource, lock);
+		ldlm_reprocess_all(lock->l_resource,
+				   lock->l_policy_data.l_inodebits.bits);
 	}
 
 	RETURN(rc);

lustre/mdt/mdt_open.c
@@ -2025,7 +2025,8 @@ out_unlock:
 	}
 
 out_reprocess:
-	ldlm_reprocess_all(lease->l_resource, lease);
+	ldlm_reprocess_all(lease->l_resource,
+			   lease->l_policy_data.l_inodebits.bits);
 	LDLM_LOCK_PUT(lease);
 
 	ma->ma_valid = 0;
@@ -2235,7 +2236,8 @@ out_obj:
 		/* the 2nd object has been used, and not swapped */
 		mdt_object_put(info->mti_env, o2);
 
-	ldlm_reprocess_all(lease->l_resource, lease);
+	ldlm_reprocess_all(lease->l_resource,
+			   lease->l_policy_data.l_inodebits.bits);
 
 out_lease:
 	LDLM_LOCK_PUT(lease);
@@ -2356,7 +2358,8 @@ out_unlock:
 		OBD_FREE_PTR_ARRAY(resync_ids, resync_count);
 
 out_reprocess:
-	ldlm_reprocess_all(lease->l_resource, lease);
+	ldlm_reprocess_all(lease->l_resource,
+			   lease->l_policy_data.l_inodebits.bits);
 	LDLM_LOCK_PUT(lease);
 
 	ma->ma_valid = 0;

lustre/mdt/mdt_reint.c
@@ -2179,7 +2179,8 @@ static int mdd_migrate_close(struct mdt_thread_info *info,
 	 * cancelled, it's okay to cancel it now as we've held mot_open_sem.
 	 */
 	ldlm_lock_cancel(lease);
-	ldlm_reprocess_all(lease->l_resource, lease);
+	ldlm_reprocess_all(lease->l_resource,
+			   lease->l_policy_data.l_inodebits.bits);
 	LDLM_LOCK_PUT(lease);
 
 close: