lustre/lustre-release

commit 7fa0fd41577001629f7ecc418cfe814a46207148

LU-11441 obdclass: make mod rpc slot wait queue FIFO
Relatively big load may cause a process to spin for a long time
without successful grabbing a free mod rpc slot. It has been observed
a process spinning more than 100 seconds when there were 72 mdtest-s
and 8 IOR-s.

Make mod rpc slot wait queue to run FIFO so that waiting thread got
free mod rpc slot in order they entered the queue.

Cray-bug-id: LUS-6380
Change-Id: I4ef2cf529cf1bb4c8500d7a2b181a05ca080223e
Signed-off-by: Alexander Zarochentsev <c17826@cray.com>
Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Reviewed-on: https://review.whamcloud.com/33282
Tested-by: Jenkins
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/genops.c
@@ -2395,8 +2395,9 @@ __u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc,
 		       "opc %u, max %hu\n",
 		       cli->cl_import->imp_obd->obd_name, opc, max);
 
-		l_wait_event(cli->cl_mod_rpcs_waitq,
-			     obd_mod_rpc_slot_avail(cli, close_req), &lwi);
+		l_wait_event_exclusive(cli->cl_mod_rpcs_waitq,
+				       obd_mod_rpc_slot_avail(cli, close_req),
+				       &lwi);
 	} while (true);
 }
 EXPORT_SYMBOL(obd_get_mod_rpc_slot);

microsoft/amlFilesystem-lustre

commit bdce2d4f5658b2aa2675e4ed88558d13275bb58a

LU-13993 ptlrpc: Close request can't be sent
There is a slot for close request but close
thread can't be woken up due to exclusive wait.

Make close wait as non-exclusive, so it will be
woken up with each call.

Change-Id: Ib38924550269e61479847e1d4e169c945516d8b5
HPE-bug-id: LUS-9344
Fixes: 7fa0fd4157 ("LU-11441 obdclass: make mod rpc slot wait queue FIFO")
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Vladimir Saveliev <c17830@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/obdclass/genops.c
@@ -2305,9 +2305,13 @@ __u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc)
 		       "opc %u, max %hu\n",
 		       cli->cl_import->imp_obd->obd_name, opc, max);
 
-		wait_event_idle_exclusive(cli->cl_mod_rpcs_waitq,
-					  obd_mod_rpc_slot_avail(cli,
-								 close_req));
+		if (close_req) {
+			wait_event_idle(cli->cl_mod_rpcs_waitq,
+					obd_mod_rpc_slot_avail(cli, true));
+		} else {
+			wait_event_idle_exclusive(cli->cl_mod_rpcs_waitq,
+					obd_mod_rpc_slot_avail(cli, false));
+		}
 	} while (true);
 }
 EXPORT_SYMBOL(obd_get_mod_rpc_slot);