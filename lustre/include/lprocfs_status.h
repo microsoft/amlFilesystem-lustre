lustre/lustre-release

commit ea2cd3af7bfabfa6876727ee44495f4c331bea8e

LU-11407 obdclass: add start time to stats files
When the stats files are initialized or reset, store the current
timestamp with the stats.  That allows computing average IO and
RPC rates over the accumulated stats lifetime, in addition to the
normal incremental operation rates found by comparing successive
values read from the stats file with the read interval.

Any stats that currently print the "snapshot_time:" header will
now also print "start_time:" and "elapsed_time:" fields as well.
Consolodate this printing into a helper function instead of
duplicating very similar code in many different functions.  Output
can't be exactly the same for all callers, because these fields are
embedded into different types of output files, but it is very close.

Change struct rename_stats and brw_stats to use a common name prefix.

Change the obd_job_stats timestamps to ktime_t so that we can use the
common helper function for printing the header.  It is easier to store
ojs_cleanup_interval internally as 1/2 of the maximum stats age, since
since division is more easily done when the value is initially set as
seconds compared to when it is ktime_t.  This may also be a tiny bit
more efficient since we don't do a divide/shift on each access.

Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Iacefa17def455ef53a28fd14b6d4c670463ebbe5
Reviewed-on: https://review.whamcloud.com/33201
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Ben Evans <beevans@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lprocfs_status.h
@@ -121,7 +121,8 @@ enum {
 };
 
 struct brw_stats {
-        struct obd_histogram hist[BRW_LAST];
+	ktime_t			bs_init;
+	struct obd_histogram	bs_hist[BRW_LAST];
 };
 
 enum {
@@ -132,7 +133,8 @@ enum {
 };
 
 struct rename_stats {
-        struct obd_histogram hist[RENAME_LAST];
+	ktime_t			rs_init;
+	struct obd_histogram	rs_hist[RENAME_LAST];
 };
 
 /* An lprocfs counter can be configured using the enum bit masks below.
@@ -230,6 +232,7 @@ struct lprocfs_stats {
 	/* 1 + the biggest cpu # whose ls_percpu slot has been allocated */
 	unsigned short			ls_biggest_alloc_num;
 	enum lprocfs_stats_flags	ls_flags;
+	ktime_t				ls_init;
 	/* Lock used when there are no percpu stats areas; For percpu stats,
 	 * it is used to protect ls_biggest_alloc_num change */
 	spinlock_t			ls_lock;
@@ -427,8 +430,8 @@ struct obd_job_stats {
 	struct cfs_hash	       *ojs_hash;	/* hash of jobids */
 	struct list_head	ojs_list;	/* list of job_stat structs */
 	rwlock_t		ojs_lock;	/* protect ojs_list/js_list */
-	unsigned int		ojs_cleanup_interval;/* seconds before expiry */
-	time64_t		ojs_last_cleanup; /* previous cleanup time */
+	ktime_t			ojs_cleanup_interval;/* 1/2 expiry seconds */
+	ktime_t			ojs_cleanup_last;/* previous cleanup time */
 	cntr_init_callback	ojs_cntr_init_fn;/* lprocfs_stats initializer */
 	unsigned short		ojs_cntr_num;	/* number of stats in struct */
 	bool			ojs_cleaning;	/* currently expiring stats */
@@ -570,6 +573,9 @@ extern int lprocfs_seq_create(struct proc_dir_entry *parent, const char *name,
 extern int lprocfs_obd_seq_create(struct obd_device *obd, const char *name,
 				  mode_t mode, const struct proc_ops *seq_fops,
 				  void *data);
+extern void lprocfs_stats_header(struct seq_file *seq, ktime_t now,
+				 ktime_t ts_init, int width, const char *colon,
+				 bool show_units);
 
 /* Generic callbacks */
 extern int lprocfs_uuid_seq_show(struct seq_file *m, void *data);

lustre/include/lustre_osc.h
@@ -105,6 +105,7 @@ struct osc_device {
 
 	/* Write stats is actually protected by client_obd's lock. */
 	struct osc_stats {
+		ktime_t		os_init;
 		uint64_t	os_lockless_writes;    /* by bytes */
 		uint64_t	os_lockless_reads;     /* by bytes */
 	} od_stats;

lustre/include/obd.h
@@ -280,6 +280,7 @@ struct client_obd {
 	u32			cl_max_pages_per_rpc;
 	u32			cl_max_rpcs_in_flight;
 	u32			cl_max_short_io_bytes;
+	ktime_t			cl_stats_init;
 	struct obd_histogram	cl_read_rpc_hist;
 	struct obd_histogram	cl_write_rpc_hist;
 	struct obd_histogram	cl_read_page_hist;
@@ -334,9 +335,10 @@ struct client_obd {
 	__u16			 cl_close_rpcs_in_flight;
 	wait_queue_head_t	 cl_mod_rpcs_waitq;
 	unsigned long		*cl_mod_tag_bitmap;
+	ktime_t			 cl_mod_rpcs_init;
 	struct obd_histogram	 cl_mod_rpcs_hist;
 
-        /* mgc datastruct */
+	/* mgc datastruct */
 	struct mutex		  cl_mgc_mutex;
 	struct local_oid_storage *cl_mgc_los;
 	struct dt_object	 *cl_mgc_configs_dir;

lustre/llite/llite_internal.h
@@ -581,14 +581,15 @@ struct ra_io_arg {
 #define LL_HIST_START 12 /* buckets start at 2^12 = 4k */
 #define LL_PROCESS_HIST_MAX 10
 struct per_process_info {
-        pid_t pid;
-        struct obd_histogram pp_r_hist;
-        struct obd_histogram pp_w_hist;
+	pid_t pid;
+	struct obd_histogram pp_r_hist;
+	struct obd_histogram pp_w_hist;
 };
 
 /* pp_extents[LL_PROCESS_HIST_MAX] will hold the combined process info */
 struct ll_rw_extents_info {
-        struct per_process_info pp_extents[LL_PROCESS_HIST_MAX + 1];
+	ktime_t pp_init;
+	struct per_process_info pp_extents[LL_PROCESS_HIST_MAX + 1];
 };
 
 #define LL_OFFSET_HIST_MAX 100
@@ -698,16 +699,18 @@ struct ll_sb_info {
 
 	struct lu_site           *ll_site;
 	struct cl_device         *ll_cl;
+
 	/* Statistics */
 	struct ll_rw_extents_info ll_rw_extents_info;
-	int                       ll_extent_process_count;
+	int			  ll_extent_process_count;
+	unsigned int		  ll_offset_process_count;
+	ktime_t			  ll_process_stats_init;
 	struct ll_rw_process_info ll_rw_process_info[LL_PROCESS_HIST_MAX];
-	unsigned int              ll_offset_process_count;
 	struct ll_rw_process_info ll_rw_offset_info[LL_OFFSET_HIST_MAX];
-	unsigned int              ll_rw_offset_entry_count;
-	int                       ll_stats_track_id;
+	unsigned int		  ll_rw_offset_entry_count;
+	int			  ll_stats_track_id;
 	enum stats_track_type     ll_stats_track_type;
-	int                       ll_rw_stats_on;
+	int			  ll_rw_stats_on;
 
 	/* metadata stat-ahead */
 	unsigned int		  ll_sa_running_max;/* max concurrent

lustre/llite/lproc_llite.c
@@ -1954,23 +1954,19 @@ static void ll_display_extents_info(struct ll_rw_extents_info *io_extents,
 
 static int ll_rw_extents_stats_pp_seq_show(struct seq_file *seq, void *v)
 {
-	struct timespec64 now;
 	struct ll_sb_info *sbi = seq->private;
 	struct ll_rw_extents_info *io_extents = &sbi->ll_rw_extents_info;
 	int k;
-
-	ktime_get_real_ts64(&now);
 
 	if (!sbi->ll_rw_stats_on) {
 		seq_puts(seq, "disabled\n write anything to this file to activate, then '0' or 'disable' to deactivate\n");
 		return 0;
 	}
-	seq_printf(seq, "snapshot_time:         %llu.%09lu (secs.nsecs)\n",
-		   (s64)now.tv_sec, now.tv_nsec);
-        seq_printf(seq, "%15s %19s       | %20s\n", " ", "read", "write");
-        seq_printf(seq, "%13s   %14s %4s %4s  | %14s %4s %4s\n",
-                   "extents", "calls", "%", "cum%",
-                   "calls", "%", "cum%");
+	lprocfs_stats_header(seq, ktime_get(), io_extents->pp_init, 25, ":", 1);
+	seq_printf(seq, "%15s %19s       | %20s\n", " ", "read", "write");
+	seq_printf(seq, "%13s   %14s %4s %4s  | %14s %4s %4s\n",
+		   "extents", "calls", "%", "cum%",
+		   "calls", "%", "cum%");
 	spin_lock(&sbi->ll_pp_extent_lock);
 	for (k = 0; k < LL_PROCESS_HIST_MAX; k++) {
 		if (io_extents->pp_extents[k].pid != 0) {
@@ -2005,6 +2001,7 @@ static ssize_t ll_rw_extents_stats_pp_seq_write(struct file *file,
 		sbi->ll_rw_stats_on = 1;
 
 	spin_lock(&sbi->ll_pp_extent_lock);
+	io_extents->pp_init = ktime_get();
 	for (i = 0; i < LL_PROCESS_HIST_MAX; i++) {
 		io_extents->pp_extents[i].pid = 0;
 		lprocfs_oh_clear(&io_extents->pp_extents[i].pp_r_hist);
@@ -2018,18 +2015,15 @@ LDEBUGFS_SEQ_FOPS(ll_rw_extents_stats_pp);
 
 static int ll_rw_extents_stats_seq_show(struct seq_file *seq, void *v)
 {
-	struct timespec64 now;
 	struct ll_sb_info *sbi = seq->private;
 	struct ll_rw_extents_info *io_extents = &sbi->ll_rw_extents_info;
-
-	ktime_get_real_ts64(&now);
 
 	if (!sbi->ll_rw_stats_on) {
 		seq_puts(seq, "disabled\n write anything to this file to activate, then '0' or 'disable' to deactivate\n");
 		return 0;
 	}
-	seq_printf(seq, "snapshot_time:         %llu.%09lu (secs.nsecs)\n",
-		   (s64)now.tv_sec, now.tv_nsec);
+
+	lprocfs_stats_header(seq, ktime_get(), io_extents->pp_init, 25, ":", 1);
 
 	seq_printf(seq, "%15s %19s       | %20s\n", " ", "read", "write");
 	seq_printf(seq, "%13s   %14s %4s %4s  | %14s %4s %4s\n",
@@ -2063,6 +2057,7 @@ static ssize_t ll_rw_extents_stats_seq_write(struct file *file,
 		sbi->ll_rw_stats_on = 1;
 
 	spin_lock(&sbi->ll_pp_extent_lock);
+	io_extents->pp_init = ktime_get();
 	for (i = 0; i <= LL_PROCESS_HIST_MAX; i++) {
 		io_extents->pp_extents[i].pid = 0;
 		lprocfs_oh_clear(&io_extents->pp_extents[i].pp_r_hist);
@@ -2180,22 +2175,20 @@ void ll_rw_stats_tally(struct ll_sb_info *sbi, pid_t pid,
 
 static int ll_rw_offset_stats_seq_show(struct seq_file *seq, void *v)
 {
-	struct timespec64 now;
 	struct ll_sb_info *sbi = seq->private;
 	struct ll_rw_process_info *offset = sbi->ll_rw_offset_info;
 	struct ll_rw_process_info *process = sbi->ll_rw_process_info;
 	int i;
 
-	ktime_get_real_ts64(&now);
-
 	if (!sbi->ll_rw_stats_on) {
 		seq_puts(seq, "disabled\n write anything to this file to activate, then '0' or 'disable' to deactivate\n");
 		return 0;
 	}
-	spin_lock(&sbi->ll_process_lock);
 
-	seq_printf(seq, "snapshot_time:         %llu.%09lu (secs.nsecs)\n",
-		   (s64)now.tv_sec, now.tv_nsec);
+	spin_lock(&sbi->ll_process_lock);
+	lprocfs_stats_header(seq, ktime_get(), sbi->ll_process_stats_init, 25,
+			     ":", true);
+
 	seq_printf(seq, "%3s %10s %14s %14s %17s %17s %14s\n",
 		   "R/W", "PID", "RANGE START", "RANGE END",
 		   "SMALLEST EXTENT", "LARGEST EXTENT", "OFFSET");
@@ -2255,6 +2248,7 @@ static ssize_t ll_rw_offset_stats_seq_write(struct file *file,
 	spin_lock(&sbi->ll_process_lock);
 	sbi->ll_offset_process_count = 0;
 	sbi->ll_rw_offset_entry_count = 0;
+	sbi->ll_process_stats_init = ktime_get();
 	memset(process_info, 0, sizeof(struct ll_rw_process_info) *
 	       LL_PROCESS_HIST_MAX);
 	memset(offset_info, 0, sizeof(struct ll_rw_process_info) *

microsoft/amlFilesystem-lustre

commit 29d9ebca20310de3495bad5931b48d4bb02611d2

Correct lproc stats to present in wallclock rather than uptime
This fixes a bug introduced in change ea2cd3af7b, which resulted in
presentation of lproc stat timestamps in time since boot rather than
wallclock time.

lustre/include/lprocfs_status.h
@@ -206,7 +206,7 @@ struct lprocfs_stats {
 	/* 1 + the biggest cpu # whose ls_percpu slot has been allocated */
 	unsigned short			ls_biggest_alloc_num;
 	enum lprocfs_stats_flags	ls_flags;
-	ktime_t				ls_init;
+	struct timespec64		ls_init;
 	/* Lock used when there are no percpu stats areas; For percpu stats,
 	 * it is used to protect ls_biggest_alloc_num change */
 	spinlock_t			ls_lock;
@@ -408,7 +408,7 @@ struct brw_stats_props {
 };
 
 struct brw_stats {
-	ktime_t			bs_init;
+	struct timespec64	bs_init;
 	struct obd_hist_pcpu	bs_hist[BRW_RW_STATS_NUM];
 	struct brw_stats_props	bs_props[BRW_RW_STATS_NUM / 2];
 };
@@ -588,8 +588,8 @@ extern int lprocfs_seq_create(struct proc_dir_entry *parent, const char *name,
 extern int lprocfs_obd_seq_create(struct obd_device *obd, const char *name,
 				  mode_t mode, const struct proc_ops *seq_fops,
 				  void *data);
-extern void lprocfs_stats_header(struct seq_file *seq, ktime_t now,
-				 ktime_t ts_init, int width, const char *colon,
+extern void lprocfs_stats_header(struct seq_file *seq, struct timespec64 now,
+				 struct timespec64 ts_init, int width, const char *colon,
 				 bool show_units);
 
 /* Generic callbacks */

lustre/include/lustre_osc.h
@@ -103,9 +103,9 @@ struct osc_device {
 
 	/* Write stats is actually protected by client_obd's lock. */
 	struct osc_stats {
-		ktime_t		os_init;
-		uint64_t	os_lockless_writes;    /* by bytes */
-		uint64_t	os_lockless_reads;     /* by bytes */
+		struct timespec64	os_init;
+		uint64_t		os_lockless_writes;    /* by bytes */
+		uint64_t		os_lockless_reads;     /* by bytes */
 	} od_stats;
 
 	/* configuration item(s) */

lustre/include/obd.h
@@ -279,7 +279,7 @@ struct client_obd {
 	u32			cl_max_pages_per_rpc;
 	u32			cl_max_rpcs_in_flight;
 	u32			cl_max_short_io_bytes;
-	ktime_t			cl_stats_init;
+	struct timespec64	cl_stats_init;
 	struct obd_histogram	cl_read_rpc_hist;
 	struct obd_histogram	cl_write_rpc_hist;
 	struct obd_histogram	cl_read_page_hist;
@@ -334,7 +334,7 @@ struct client_obd {
 	__u16			 cl_close_rpcs_in_flight;
 	wait_queue_head_t	 cl_mod_rpcs_waitq;
 	unsigned long		*cl_mod_tag_bitmap;
-	ktime_t			 cl_mod_rpcs_init;
+	struct timespec64	 cl_mod_rpcs_init;
 	struct obd_histogram	 cl_mod_rpcs_hist;
 
 	/* mgc datastruct */

lustre/ldlm/ldlm_lib.c
@@ -482,6 +482,8 @@ int client_obd_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 
 	cli->cl_max_short_io_bytes = OBD_DEF_SHORT_IO_BYTES;
 
+	ktime_get_real_ts64(&cli->cl_stats_init);
+
 	/*
 	 * set cl_chunkbits default value to PAGE_SHIFT,
 	 * it will be updated at OSC connection time.
@@ -510,6 +512,7 @@ int client_obd_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 	cli->cl_close_rpcs_in_flight = 0;
 	init_waitqueue_head(&cli->cl_mod_rpcs_waitq);
 	cli->cl_mod_tag_bitmap = NULL;
+	ktime_get_real_ts64(&cli->cl_mod_rpcs_init);
 
 	INIT_LIST_HEAD(&cli->cl_chg_dev_linkage);
 

lustre/llite/llite_internal.h
@@ -621,7 +621,7 @@ struct per_process_info {
 
 /* pp_extents[LL_PROCESS_HIST_MAX] will hold the combined process info */
 struct ll_rw_extents_info {
-	ktime_t pp_init;
+	struct timespec64 pp_init;
 	struct per_process_info pp_extents[LL_PROCESS_HIST_MAX + 1];
 };
 
@@ -742,7 +742,7 @@ struct ll_sb_info {
 	unsigned int		  ll_offset_process_count;
 	struct ll_rw_process_info *ll_rw_process_info;
 	struct ll_rw_process_info *ll_rw_offset_info;
-	ktime_t			  ll_process_stats_init;
+	struct timespec64	  ll_process_stats_init;
 	unsigned int		  ll_rw_offset_entry_count;
 	int			  ll_stats_track_id;
 	enum stats_track_type	  ll_stats_track_type;

lustre/llite/lproc_llite.c
@@ -2050,6 +2050,7 @@ static int ll_rw_extents_stats_pp_seq_show(struct seq_file *seq, void *v)
 {
 	struct ll_sb_info *sbi = seq->private;
 	struct ll_rw_extents_info *rw_extents = sbi->ll_rw_extents_info;
+	struct timespec64 now;
 	int k;
 
 	if (!sbi->ll_rw_stats_on || !rw_extents) {
@@ -2057,8 +2058,9 @@ static int ll_rw_extents_stats_pp_seq_show(struct seq_file *seq, void *v)
 		return 0;
 	}
 
+	ktime_get_real_ts64(&now);
 	spin_lock(&sbi->ll_pp_extent_lock);
-	lprocfs_stats_header(seq, ktime_get(), rw_extents->pp_init, 25, ":", 1);
+	lprocfs_stats_header(seq, now, rw_extents->pp_init, 25, ":", 1);
 	seq_printf(seq, "%15s %19s       | %20s\n", " ", "read", "write");
 	seq_printf(seq, "%13s   %14s %4s %4s  | %14s %4s %4s\n",
 		   "extents", "calls", "%", "cum%", "calls", "%", "cum%");
@@ -2089,6 +2091,7 @@ static int alloc_rw_stats_info(struct ll_sb_info *sbi)
 		spin_lock_init(&rw_extents->pp_extents[i].pp_r_hist.oh_lock);
 		spin_lock_init(&rw_extents->pp_extents[i].pp_w_hist.oh_lock);
 	}
+	ktime_get_real_ts64(&rw_extents->pp_init);
 
 	spin_lock(&sbi->ll_pp_extent_lock);
 	if (!sbi->ll_rw_extents_info)
@@ -2110,6 +2113,7 @@ static int alloc_rw_stats_info(struct ll_sb_info *sbi)
 		sbi->ll_rw_process_info = process;
 	if (!sbi->ll_rw_offset_info)
 		sbi->ll_rw_offset_info = offset;
+	ktime_get_real_ts64(&sbi->ll_process_stats_init);
 	spin_unlock(&sbi->ll_process_lock);
 
 	/* another writer allocated the structs before we got the lock */
@@ -2174,7 +2178,7 @@ static ssize_t ll_rw_extents_stats_pp_seq_write(struct file *file,
 	spin_lock(&sbi->ll_pp_extent_lock);
 	rw_extents = sbi->ll_rw_extents_info;
 	if (rw_extents) {
-		rw_extents->pp_init = ktime_get();
+		ktime_get_real_ts64(&rw_extents->pp_init);
 		for (i = 0; i < LL_PROCESS_HIST_MAX; i++) {
 			rw_extents->pp_extents[i].pid = 0;
 			lprocfs_oh_clear(&rw_extents->pp_extents[i].pp_r_hist);
@@ -2192,14 +2196,16 @@ static int ll_rw_extents_stats_seq_show(struct seq_file *seq, void *v)
 {
 	struct ll_sb_info *sbi = seq->private;
 	struct ll_rw_extents_info *rw_extents = sbi->ll_rw_extents_info;
+	struct timespec64 now;
 
 	if (!sbi->ll_rw_stats_on || !rw_extents) {
 		seq_puts(seq, "disabled\n write anything to this file to activate, then '0' or 'disable' to deactivate\n");
 		return 0;
 	}
 
+	ktime_get_real_ts64(&now);
 	spin_lock(&sbi->ll_lock);
-	lprocfs_stats_header(seq, ktime_get(), rw_extents->pp_init, 25, ":", 1);
+	lprocfs_stats_header(seq, now, rw_extents->pp_init, 25, ":", 1);
 
 	seq_printf(seq, "%15s %19s       | %20s\n", " ", "read", "write");
 	seq_printf(seq, "%13s   %14s %4s %4s  | %14s %4s %4s\n",
@@ -2242,7 +2248,7 @@ static ssize_t ll_rw_extents_stats_seq_write(struct file *file,
 	spin_lock(&sbi->ll_pp_extent_lock);
 	rw_extents = sbi->ll_rw_extents_info;
 	if (rw_extents) {
-		rw_extents->pp_init = ktime_get();
+		ktime_get_real_ts64(&rw_extents->pp_init);
 		for (i = 0; i <= LL_PROCESS_HIST_MAX; i++) {
 			rw_extents->pp_extents[i].pid = 0;
 			lprocfs_oh_clear(&rw_extents->pp_extents[i].pp_r_hist);
@@ -2373,6 +2379,7 @@ static int ll_rw_offset_stats_seq_show(struct seq_file *seq, void *v)
 	struct ll_sb_info *sbi = seq->private;
 	struct ll_rw_process_info *offset;
 	struct ll_rw_process_info *process;
+	struct timespec64 now;
 	int i;
 
 	if (!sbi->ll_rw_stats_on) {
@@ -2380,8 +2387,9 @@ static int ll_rw_offset_stats_seq_show(struct seq_file *seq, void *v)
 		return 0;
 	}
 
+	ktime_get_real_ts64(&now);
 	spin_lock(&sbi->ll_process_lock);
-	lprocfs_stats_header(seq, ktime_get(), sbi->ll_process_stats_init, 25,
+	lprocfs_stats_header(seq, now, sbi->ll_process_stats_init, 25,
 			     ":", true);
 	seq_printf(seq, "%3s %10s %14s %14s %17s %17s %14s\n",
 		   "R/W", "PID", "RANGE START", "RANGE END",
@@ -2449,7 +2457,7 @@ static ssize_t ll_rw_offset_stats_seq_write(struct file *file,
 	spin_lock(&sbi->ll_process_lock);
 	sbi->ll_offset_process_count = 0;
 	sbi->ll_rw_offset_entry_count = 0;
-	sbi->ll_process_stats_init = ktime_get();
+	ktime_get_real_ts64(&sbi->ll_process_stats_init);
 	if (sbi->ll_rw_process_info)
 		memset(sbi->ll_rw_process_info, 0,
 		       sizeof(struct ll_rw_process_info) * LL_PROCESS_HIST_MAX);

lustre/mdc/lproc_mdc.c
@@ -408,6 +408,7 @@ static ssize_t mdc_rpc_stats_seq_write(struct file *file,
 	struct client_obd *cli = &obd->u.cli;
 
 	lprocfs_oh_clear(&cli->cl_mod_rpcs_hist);
+	ktime_get_real_ts64(&cli->cl_mod_rpcs_init);
 
 	lprocfs_oh_clear(&cli->cl_read_rpc_hist);
 	lprocfs_oh_clear(&cli->cl_write_rpc_hist);
@@ -517,8 +518,10 @@ static int mdc_stats_seq_show(struct seq_file *seq, void *v)
 {
 	struct obd_device *obd = seq->private;
 	struct osc_stats *stats = &obd2osc_dev(obd)->od_stats;
+	struct timespec64 now;
 
-	lprocfs_stats_header(seq, ktime_get(), stats->os_init, 25, ":", true);
+	ktime_get_real_ts64(&now);
+	lprocfs_stats_header(seq, now, stats->os_init, 25, ":", true);
 	seq_printf(seq, "lockless_write_bytes\t\t%llu\n",
 		   stats->os_lockless_writes);
 	seq_printf(seq, "lockless_read_bytes\t\t%llu\n",
@@ -535,7 +538,7 @@ static ssize_t mdc_stats_seq_write(struct file *file,
 	struct osc_stats *stats = &obd2osc_dev(obd)->od_stats;
 
 	memset(stats, 0, sizeof(*stats));
-	stats->os_init = ktime_get();
+	ktime_get_real_ts64(&stats->os_init);
 
 	return len;
 }

lustre/mdc/mdc_dev.c
@@ -1610,6 +1610,8 @@ static struct lu_device *mdc_device_alloc(const struct lu_env *env,
 		RETURN(ERR_PTR(rc));
 	}
 	od->od_exp = obd->obd_self_export;
+	ktime_get_real_ts64(&od->od_stats.os_init);
+
 	RETURN(d);
 }
 

lustre/mdt/mdt_lproc.c
@@ -112,9 +112,11 @@ static void display_rename_stats(struct seq_file *seq, char *name,
 static void rename_stats_show(struct seq_file *seq,
 			      struct rename_stats *rename_stats)
 {
+	struct timespec64 now;
 	/* this sampling races with updates */
 	seq_puts(seq, "rename_stats:\n- ");
-	lprocfs_stats_header(seq, ktime_get(), rename_stats->rs_init, 15, ":",
+	ktime_get_real_ts64(&now);
+	lprocfs_stats_header(seq, now, rename_stats->rs_init, 15, ":",
 			     false);
 
 	display_rename_stats(seq, "same_dir",
@@ -144,7 +146,7 @@ mdt_rename_stats_seq_write(struct file *file, const char __user *buf,
 
 	for (i = 0; i < RENAME_LAST; i++)
 		lprocfs_oh_clear(&mdt->mdt_rename_stats.rs_hist[i]);
-	mdt->mdt_rename_stats.rs_init = ktime_get();
+	ktime_get_real_ts64(&mdt->mdt_rename_stats.rs_init);
 
 	return len;
 }
@@ -156,6 +158,7 @@ static int lproc_mdt_attach_rename_seqstat(struct mdt_device *mdt)
 
 	for (i = 0; i < RENAME_LAST; i++)
 		spin_lock_init(&mdt->mdt_rename_stats.rs_hist[i].oh_lock);
+	ktime_get_real_ts64(&mdt->mdt_rename_stats.rs_init);
 
 	return lprocfs_obd_seq_create(mdt2obd_dev(mdt), "rename_stats", 0644,
 				      &mdt_rename_stats_fops, mdt);

lustre/obdclass/genops.c
@@ -2197,10 +2197,12 @@ int obd_mod_rpc_stats_seq_show(struct client_obd *cli,
 			       struct seq_file *seq)
 {
 	unsigned long mod_tot = 0, mod_cum;
+	struct timespec64 now;
 	int i;
 
+	ktime_get_real_ts64(&now);
 	spin_lock(&cli->cl_mod_rpcs_lock);
-	lprocfs_stats_header(seq, ktime_get(), cli->cl_mod_rpcs_init, 25,
+	lprocfs_stats_header(seq, now, cli->cl_mod_rpcs_init, 25,
 			     ":", true);
 	seq_printf(seq, "modify_RPCs_in_flight:  %hu\n",
 		   cli->cl_mod_rpcs_in_flight);

lustre/obdclass/lprocfs_jobstats.c
@@ -66,8 +66,8 @@ struct job_stat {
 	struct list_head	js_list;	/* on ojs_list, with ojs_lock */
 	atomic_t		js_refcount;	/* num users of this struct */
 	char			js_jobid[LUSTRE_JOBID_SIZE]; /* job name + NUL*/
-	ktime_t			js_time_init;	/* time of initial stat*/
-	ktime_t			js_time_latest;	/* time of most recent stat*/
+	struct timespec64	js_time_init;	/* time in epoch time of initial stat*/
+	ktime_t			js_time_latest;	/* time since boot of most recent stat*/
 	struct lprocfs_stats	*js_stats;	/* per-job statistics */
 	struct obd_job_stats	*js_jobstats;	/* for accessing ojs_lock */
 };
@@ -263,8 +263,8 @@ static struct job_stat *job_alloc(char *jobid, struct obd_job_stats *jobs)
 	jobs->ojs_cntr_init_fn(job->js_stats, 0);
 
 	memcpy(job->js_jobid, jobid, sizeof(job->js_jobid));
-	job->js_time_init = ktime_get();
-	job->js_time_latest = job->js_time_init;
+	ktime_get_real_ts64(&job->js_time_init);
+	job->js_time_latest = ktime_get();
 	job->js_jobstats = jobs;
 	INIT_HLIST_NODE(&job->js_hash);
 	INIT_LIST_HEAD(&job->js_list);
@@ -442,6 +442,7 @@ static int lprocfs_jobstats_seq_show(struct seq_file *p, void *v)
 	char escaped[LUSTRE_JOBID_SIZE * 4] = "";
 	char *quote = "", *c, *end;
 	int i, joblen = 0;
+	struct timespec64 now;
 
 	if (v == SEQ_START_TOKEN) {
 		seq_printf(p, "job_stats:\n");
@@ -468,7 +469,8 @@ static int lprocfs_jobstats_seq_show(struct seq_file *p, void *v)
 
 	seq_printf(p, "- %-16s %s%*s%s\n",
 		   "job_id:", quote, joblen, escaped, quote);
-	lprocfs_stats_header(p, job->js_time_latest, job->js_time_init, 16,
+	ktime_get_real_ts64(&now);
+	lprocfs_stats_header(p, now, job->js_time_init, 16,
 			     ":", true);
 
 	s = job->js_stats;

lustre/obdclass/lprocfs_status.c
@@ -1241,6 +1241,8 @@ struct lprocfs_stats *lprocfs_alloc_stats(unsigned int num,
 				goto fail;
 	}
 
+	ktime_get_real_ts64(&stats->ls_init);
+
 	return stats;
 
 fail:
@@ -1326,6 +1328,8 @@ void lprocfs_clear_stats(struct lprocfs_stats *stats)
 		}
 	}
 
+	ktime_get_real_ts64(&stats->ls_init);
+
 	lprocfs_stats_unlock(stats, LPROCFS_GET_NUM_CPU, &flags);
 }
 EXPORT_SYMBOL(lprocfs_clear_stats);
@@ -1360,19 +1364,17 @@ static void *lprocfs_stats_seq_next(struct seq_file *p, void *v, loff_t *pos)
 	return lprocfs_stats_seq_start(p, pos);
 }
 
-void lprocfs_stats_header(struct seq_file *seq, ktime_t now, ktime_t ts_init,
+void lprocfs_stats_header(struct seq_file *seq, struct timespec64 now, struct timespec64 ts_init,
 			  int width, const char *colon, bool show_units)
 {
 	const char *units = show_units ? " secs.nsecs" : "";
 	struct timespec64 ts;
 
-	ts = ktime_to_timespec64(now);
 	seq_printf(seq, "%-*s%s %llu.%09lu%s\n", width,
-		   "snapshot_time", colon, (s64)ts.tv_sec, ts.tv_nsec, units);
-	ts = ktime_to_timespec64(ts_init);
+		   "snapshot_time", colon, (s64)now.tv_sec, now.tv_nsec, units);
 	seq_printf(seq, "%-*s%s %llu.%09lu%s\n", width,
-		   "start_time", colon, (s64)ts.tv_sec, ts.tv_nsec, units);
-	ts = ktime_to_timespec64(ktime_sub(now, ts_init));
+		   "start_time", colon, (s64)ts_init.tv_sec, ts_init.tv_nsec, units);
+	ts = timespec64_sub(now, ts_init);
 	seq_printf(seq, "%-*s%s %llu.%09lu%s\n", width,
 		   "elapsed_time", colon, (s64)ts.tv_sec, ts.tv_nsec, units);
 }
@@ -1384,10 +1386,13 @@ static int lprocfs_stats_seq_show(struct seq_file *p, void *v)
 	struct lprocfs_stats *stats = p->private;
 	struct lprocfs_counter_header *hdr;
 	struct lprocfs_counter ctr;
+	struct timespec64 now;
 	int idx = *(loff_t *)v;
 
-	if (idx == 0)
-		lprocfs_stats_header(p, ktime_get(), stats->ls_init, 25, "", 1);
+	if (idx == 0) {
+		ktime_get_real_ts64(&now);
+		lprocfs_stats_header(p, now, stats->ls_init, 25, "", 1);
+	}
 
 	hdr = &stats->ls_cnt_header[idx];
 	lprocfs_stats_collect(stats, idx, &ctr);
@@ -1620,6 +1625,7 @@ void lprocfs_init_ldlm_stats(struct lprocfs_stats *ldlm_stats)
 			     LPROCFS_TYPE_REQS, "ldlm_cp_callback");
 	lprocfs_counter_init(ldlm_stats, LDLM_GL_CALLBACK - LDLM_FIRST_OPC,
 			     LPROCFS_TYPE_REQS, "ldlm_gl_callback");
+	ktime_get_real_ts64(&ldlm_stats->ls_init);
 }
 EXPORT_SYMBOL(lprocfs_init_ldlm_stats);
 

lustre/obdclass/lprocfs_status_server.c
@@ -773,10 +773,12 @@ static const struct brw_stats_props brw_props[] = {
 static int brw_stats_seq_show(struct seq_file *seq, void *v)
 {
 	struct brw_stats *brw_stats = seq->private;
+	struct timespec64 now;
 	int i;
 
 	/* this sampling races with updates */
-	lprocfs_stats_header(seq, ktime_get(), brw_stats->bs_init, 25, ":", 1);
+	ktime_get_real_ts64(&now);
+	lprocfs_stats_header(seq, now, brw_stats->bs_init, 25, ":", 1);
 
 	for (i = 0; i < ARRAY_SIZE(brw_stats->bs_props); i++) {
 		if (!brw_stats->bs_props[i].bsp_name)
@@ -802,6 +804,7 @@ static ssize_t brw_stats_seq_write(struct file *file,
 
 	for (i = 0; i < BRW_RW_STATS_NUM; i++)
 		lprocfs_oh_clear_pcpu(&brw_stats->bs_hist[i]);
+	ktime_get_real_ts64(&brw_stats->bs_init);
 
 	return len;
 }
@@ -838,7 +841,6 @@ void ldebugfs_register_osd_stats(struct dentry *parent,
 	int i;
 
 	LASSERT(brw_stats);
-	brw_stats->bs_init = ktime_get();
 	for (i = 0; i < BRW_RW_STATS_NUM; i++) {
 		struct brw_stats_props *props = brw_stats->bs_props;
 
@@ -848,6 +850,7 @@ void ldebugfs_register_osd_stats(struct dentry *parent,
 			props[i / 2].bsp_scale = brw_props[i / 2].bsp_scale;
 		}
 	}
+	ktime_get_real_ts64(&brw_stats->bs_init);
 
 	if (!parent)
 		return;

lustre/osc/lproc_osc.c
@@ -708,12 +708,13 @@ static int osc_rpc_stats_seq_show(struct seq_file *seq, void *v)
 {
 	struct obd_device *obd = seq->private;
 	struct client_obd *cli = &obd->u.cli;
+	struct timespec64 now;
 	unsigned long read_tot = 0, write_tot = 0, read_cum, write_cum;
 	int i;
 
+	ktime_get_real_ts64(&now);
 	spin_lock(&cli->cl_loi_list_lock);
-
-	lprocfs_stats_header(seq, ktime_get(), cli->cl_stats_init, 25, ":", 1);
+	lprocfs_stats_header(seq, now, cli->cl_stats_init, 25, ":", 1);
 	seq_printf(seq, "read RPCs in flight:  %d\n",
 		   cli->cl_r_in_flight);
 	seq_printf(seq, "write RPCs in flight: %d\n",
@@ -811,7 +812,7 @@ static ssize_t osc_rpc_stats_seq_write(struct file *file,
 	lprocfs_oh_clear(&cli->cl_write_page_hist);
 	lprocfs_oh_clear(&cli->cl_read_offset_hist);
 	lprocfs_oh_clear(&cli->cl_write_offset_hist);
-	cli->cl_stats_init = ktime_get();
+	ktime_get_real_ts64(&cli->cl_stats_init);
 
 	return len;
 }
@@ -821,8 +822,10 @@ static int osc_stats_seq_show(struct seq_file *seq, void *v)
 {
 	struct obd_device *obd = seq->private;
 	struct osc_stats *stats = &obd2osc_dev(obd)->od_stats;
+	struct timespec64 now;
 
-	lprocfs_stats_header(seq, ktime_get(), stats->os_init, 25, ":", true);
+	ktime_get_real_ts64(&now);
+	lprocfs_stats_header(seq, now, stats->os_init, 25, ":", true);
 	seq_printf(seq, "lockless_write_bytes\t\t%llu\n",
 		   stats->os_lockless_writes);
 	seq_printf(seq, "lockless_read_bytes\t\t%llu\n",
@@ -839,7 +842,7 @@ static ssize_t osc_stats_seq_write(struct file *file,
 	struct osc_stats *stats = &obd2osc_dev(obd)->od_stats;
 
 	memset(stats, 0, sizeof(*stats));
-	stats->os_init = ktime_get();
+	ktime_get_real_ts64(&stats->os_init);
 
 	return len;
 }
@@ -891,6 +894,7 @@ int osc_tunables_init(struct obd_device *obd)
 	rc = lprocfs_obd_setup(obd, false);
 	if (rc)
 		return rc;
+
 #ifdef CONFIG_PROC_FS
 	/* If the basic OSC proc tree construction succeeded then
 	 * lets do the rest.

lustre/osc/osc_dev.c
@@ -224,6 +224,8 @@ static struct lu_device *osc_device_alloc(const struct lu_env *env,
                 RETURN(ERR_PTR(rc));
         }
         od->od_exp = obd->obd_self_export;
+        ktime_get_real_ts64(&od->od_stats.os_init);
+
         RETURN(d);
 }
 