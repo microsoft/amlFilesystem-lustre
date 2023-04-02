81c65579b045e691678af6434a13e430c039f94c
Shaun Tancheff <shaun.tancheff@hpe.com>
2023-04-02 06:45:49 -0500

    LU-16327 llite: read_folio, release_folio, filler_t
    Linux commit v5.18-rc5-221-gb7446e7cf15f
      fs: Remove aop flags parameter from grab_cache_page_write_begin()
    flags have been dropped from write_begin() and
    grab_cache_page_write_begin()
    
    Linux commit v5.18-rc5-241-g5efe7448a142
      fs: Introduce aops->read_folio
    Provide a ll_read_folio handler around ll_readpage
    
    Linux commit v5.18-rc5-280-ge9b5b23e957e
      fs: Change the type of filler_t
    Affects read_cache_page, provides a wrapper for read_cache_page
    and wrappers for filler functions
    
    Linux commit v5.18-rc5-282-gfa29000b6b26
      fs: Add aops->release_folio
    Provide an ll_release_folio function based on ll_releasepage
    
    Test-Parameters: trivial
    HPE-bug-id: LUS-11357
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: Ibd4ec1133c80cd0eb8400c4cd07b50e421dd35c5

--- lustre/llite/rw.c
+++ lustre/llite/rw.c
@@ -2021,7 +2021,7 @@ int ll_readpage(struct file *file, struct page *vmpage)
 	} else {
 		unlock_page(vmpage);
 		result = PTR_ERR(page);
-        }
+	}
 
 out:
 	if (ra.cra_release != NULL)
@@ -2037,3 +2037,10 @@ out:
 
 	RETURN(result);
 }
+
+#ifdef HAVE_AOPS_READ_FOLIO
+int ll_read_folio(struct file *file, struct folio *folio)
+{
+	return ll_readpage(file, folio_page(folio, 0));
+}
+#endif
