lustre/lustre-release

commit 39f63cf54c624d89439b5b473035c1afe35e10fa

LU-4017 quota: add setting/getting project id function
Extend Attr RPC to support project ID attribute, new
ioctl is introduced to get/set project id, it is kept
same ioctl number as VFS, you could use:

lsattr -p <file>
chattr -p <file>

Redefine LL_MAXQUOTAS as 3, change
@qsd_acct_failed into qqi, otherwise osd-zfs
could not use USR/GRP quota normally.

Remove osd_quota_migration(), it only output
some messages in case upgrading from 1.8 which
give us extra efforts for project quota, remove
it indead.

also fix a typo bug in osp_attr_set().

Change-Id: I4b537be9efa29fda730592b6d5e41c6b60fb6c0d
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/26202
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
Tested-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/llite/vvp_object.c
@@ -100,6 +100,7 @@ static int vvp_attr_get(const struct lu_env *env, struct cl_object *obj,
 	attr->cat_blocks = inode->i_blocks;
 	attr->cat_uid = from_kuid(&init_user_ns, inode->i_uid);
 	attr->cat_gid = from_kgid(&init_user_ns, inode->i_gid);
+	attr->cat_projid = ll_i2info(inode)->lli_projid;
 	/* KMS is not known by this layer */
 	return 0; /* layers below have to fill in the rest */
 }
@@ -121,8 +122,10 @@ static int vvp_attr_update(const struct lu_env *env, struct cl_object *obj,
 		inode->i_ctime.tv_sec = attr->cat_ctime;
 	if (0 && valid & CAT_SIZE)
 		i_size_write(inode, attr->cat_size);
+	if (valid & CAT_PROJID)
+		ll_i2info(inode)->lli_projid = attr->cat_projid;
 	/* not currently necessary */
-	if (0 && valid & (CAT_UID|CAT_GID|CAT_SIZE))
+	if (0 && valid & (CAT_UID|CAT_GID|CAT_SIZE|CAT_PROJID))
 		mark_inode_dirty(inode);
 	return 0;
 }

lustre/contrib/wireshark/packet-lustre.c
@@ -549,7 +549,7 @@ static int hf_lustre_mdt_rec_setattr_sa_ctime = -1;
 static int hf_lustre_mdt_rec_setattr_sa_attr_flags = -1;
 static int hf_lustre_mdt_rec_setattr_sa_mode = -1;
 static int hf_lustre_mdt_rec_setattr_sa_padding_2 = -1;
-static int hf_lustre_mdt_rec_setattr_sa_padding_3 = -1;
+static int hf_lustre_mdt_rec_setattr_sa_projid = -1;
 static int hf_lustre_mdt_rec_setattr_sa_padding_4 = -1;
 static int hf_lustre_mdt_rec_setattr_sa_padding_5 = -1;
 
@@ -3198,7 +3198,7 @@ lustre_dissect_struct_mdt_body(tvbuff_t *tvb _U_, int offset _U_,
 /* IDL: 	uint32 sa_attr_flags; */
 /* IDL: 	uint32 sa_mode; */
 /* IDL: 	uint32 sa_padding_2; */
-/* IDL: 	uint32 sa_padding_3; */
+/* IDL: 	uint32 sa_projid; */
 /* IDL: 	uint32 sa_padding_4; */
 /* IDL: 	uint32 sa_padding_5; */
 /* IDL: } */
@@ -3393,9 +3393,9 @@ lustre_dissect_element_mdt_rec_setattr_sa_padding_2(tvbuff_t *tvb _U_, int offse
 }
 
 static int
-lustre_dissect_element_mdt_rec_setattr_sa_padding_3(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_)
+lustre_dissect_element_mdt_rec_setattr_sa_projid(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_)
 {
-  offset=dissect_uint32(tvb, offset, pinfo, tree,  hf_lustre_mdt_rec_setattr_sa_padding_3);
+  offset=dissect_uint32(tvb, offset, pinfo, tree,  hf_lustre_mdt_rec_setattr_sa_projid);
 
   return offset;
 }
@@ -3479,7 +3479,7 @@ lustre_dissect_struct_mdt_rec_setattr(tvbuff_t *tvb _U_, int offset _U_, packet_
 
   offset=lustre_dissect_element_mdt_rec_setattr_sa_padding_2(tvb, offset, pinfo, tree);
 
-  offset=lustre_dissect_element_mdt_rec_setattr_sa_padding_3(tvb, offset, pinfo, tree);
+  offset=lustre_dissect_element_mdt_rec_setattr_sa_projid(tvb, offset, pinfo, tree);
 
   offset=lustre_dissect_element_mdt_rec_setattr_sa_padding_4(tvb, offset, pinfo, tree);
 
@@ -9909,8 +9909,8 @@ void proto_register_dcerpc_lustre(void)
       { "Sa Mode", "lustre.mdt_rec_setattr.sa_mode", FT_UINT32, BASE_OCT, NULL, 0, "", HFILL }},
     { &hf_lustre_mdt_rec_setattr_sa_padding_2,
       { "Sa Padding 2", "lustre.mdt_rec_setattr.sa_padding_2", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
-    { &hf_lustre_mdt_rec_setattr_sa_padding_3,
-      { "Sa Padding 3", "lustre.mdt_rec_setattr.sa_padding_3", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
+    { &hf_lustre_mdt_rec_setattr_sa_projid,
+      { "Sa Padding 3", "lustre.mdt_rec_setattr.sa_projid", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
     { &hf_lustre_mdt_rec_setattr_sa_padding_4,
       { "Sa Padding 4", "lustre.mdt_rec_setattr.sa_padding_4", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
     { &hf_lustre_mdt_rec_setattr_sa_padding_5,

lustre/include/cl_object.h
@@ -166,6 +166,9 @@ struct cl_attr {
 
 	/* nlink of the directory */
 	__u64  cat_nlink;
+
+	/* Project identifier for quota purpose. */
+	__u32  cat_projid;
 };
 
 /**
@@ -179,7 +182,8 @@ enum cl_attr_valid {
         CAT_CTIME  = 1 << 5,
         CAT_BLOCKS = 1 << 6,
         CAT_UID    = 1 << 7,
-        CAT_GID    = 1 << 8
+	CAT_GID    = 1 << 8,
+	CAT_PROJID = 1 << 9
 };
 
 /**

lustre/include/lustre/lustre_idl.h
@@ -1744,7 +1744,7 @@ struct mdt_rec_setattr {
         __u32           sa_attr_flags;
         __u32           sa_mode;
 	__u32           sa_bias;      /* some operation flags */
-        __u32           sa_padding_3;
+	__u32		sa_projid;
         __u32           sa_padding_4;
         __u32           sa_padding_5;
 };

lustre/include/lustre/lustre_user.h
@@ -65,7 +65,7 @@
  * Should be removed as soon as system header is updated.
  */
 #undef LL_MAXQUOTAS
-#define LL_MAXQUOTAS 2
+#define LL_MAXQUOTAS 3
 #undef INITQFNAMES
 #define INITQFNAMES { \
     "user",	/* USRQUOTA */ \
@@ -361,6 +361,25 @@ struct ll_futimes_3 {
 #define LL_IOC_FID2MDTIDX		_IOWR('f', 248, struct lu_fid)
 #define LL_IOC_GETPARENT		_IOWR('f', 249, struct getparent)
 #define LL_IOC_LADVISE			_IOR('f', 250, struct llapi_lu_ladvise)
+
+#ifdef	FS_IOC_FSGETXATTR
+#define LL_IOC_FSGETXATTR		FS_IOC_FSGETXATTR
+#define LL_IOC_FSSETXATTR		FS_IOC_FSSETXATTR
+#else
+/*
+ * Structure for FS_IOC_FSGETXATTR and FS_IOC_FSSETXATTR.
+*/
+struct fsxattr {
+	__u32           fsx_xflags;     /* xflags field value (get/set) */
+	__u32           fsx_extsize;    /* extsize field value (get/set)*/
+	__u32           fsx_nextents;   /* nextents field value (get)   */
+	__u32           fsx_projid;     /* project identifier (get/set) */
+	unsigned char   fsx_pad[12];
+};
+#define LL_IOC_FSGETXATTR		_IOR('X', 31, struct fsxattr)
+#define LL_IOC_FSSETXATTR		_IOW('X', 32, struct fsxattr)
+#endif
+
 
 /* Lease types for use as arg and return of LL_IOC_{GET,SET}_LEASE ioctl. */
 enum ll_lease_type {

lustre/include/lustre/lustreapi.h
@@ -43,7 +43,7 @@
 #include <lustre/lustre_user.h>
 
 #ifndef LL_MAXQUOTAS
-#define LL_MAXQUOTAS 2
+#define LL_MAXQUOTAS 3
 #endif
 
 extern bool liblustreapi_initialized;

lustre/include/lustre_quota.h
@@ -48,7 +48,7 @@
 #endif
 
 #ifndef LL_MAXQUOTAS
-#define LL_MAXQUOTAS 2
+#define LL_MAXQUOTAS 3
 #endif
 
 struct lquota_id_info;

lustre/include/obd.h
@@ -836,6 +836,7 @@ struct md_op_data {
 
 	/* default stripe offset */
 	__u32			op_default_stripe_offset;
+	__u32			op_projid;
 };
 
 struct md_callback {

microsoft/amlFilesystem-lustre

commit 2e42912fccc7b8538e678bb0f8858981a6e12291

LU-17556 llite: removed dead branches
A few cases disabled branches:
   if (0 && ...

Code disabled for many years should be removed.

Fixes: 39f63cf54c6 ("LU-4476 kernel: support process namespace containers")
Fixes: 99727c7a1a4 ("LU-4017 quota: add setting/getting project id function")
Fixes: c3e10ade1ee ("Moved IAM code from ldiskfs to OSD.")
Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I8f7ba09881d66845acea9fdf24f499fb7b5366fa

lustre/llite/vvp_object.c
@@ -112,13 +112,8 @@ static int vvp_attr_update(const struct lu_env *env, struct cl_object *obj,
 		inode_set_mtime(inode, attr->cat_mtime, 0);
 	if (valid & CAT_CTIME)
 		inode_set_ctime(inode, attr->cat_ctime, 0);
-	if (0 && valid & CAT_SIZE)
-		i_size_write(inode, attr->cat_size);
 	if (valid & CAT_PROJID)
 		ll_i2info(inode)->lli_projid = attr->cat_projid;
-	/* not currently necessary */
-	if (0 && valid & (CAT_UID|CAT_GID|CAT_SIZE|CAT_PROJID))
-		mark_inode_dirty(inode);
 	return 0;
 }
 

lustre/osd-ldiskfs/osd_iam_lvar.c
@@ -333,18 +333,6 @@ static int n_invariant(const struct iam_leaf *leaf)
 			BREAKPOINT();
 			return 0;
 		}
-		if (0 && nexthash < starthash) {
-			/*
-			 * Unfortunately this useful invariant cannot be
-			 * reliably checked as parent node is not necessarily
-			 * locked.
-			 */
-			n_print(leaf);
-			CERROR("invalid hash value less than start hash: %#x < %#x\n",
-				nexthash, starthash);
-			dump_stack();
-			return 0;
-		}
 		if (nexthash < hash) {
 			BREAKPOINT();
 			return 0;