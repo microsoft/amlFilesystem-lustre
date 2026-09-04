lustre/lustre-release

commit 4cb897ca4a970a35ef239de6148f487a9683ba2f

LU-18878 ptlrpc: improve ping evictor and recovery timeout
Let's re-use the logic used for extending the ldlm prolong timeout for
the ping evictor timeout and for recovery timer timeout - use the AT
instead of hard-coded values.

As the AT-based ping timeout is extended on different value each time,
moving an updated export to the end of the timed list is able to make
it not well sorted. Change the list of timed exports, to the rbtree.

Partially revert LUS-11054, tunable ping_evict_timeout_multiplier is
not needed anymore

A minor cleanup of exp_timed flag on connect/reconnect.

Fixes: 8e66f061c ("LU-16002 ptlrpc: adds configurable ping interval”)
HPE-bug-id: LUS-11723
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: I97b9795746ccd4242f9798c8192abd4d7a57bbac
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162935
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58620
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ldlm/ldlm_lib.c
@@ -980,6 +980,7 @@ int rev_import_init(struct obd_export *export)
 {
 	struct obd_device *obd = export->exp_obd;
 	struct obd_import *revimp;
+	int rc = 0;
 
 	LASSERT(export->exp_imp_reverse == NULL);
 
@@ -997,13 +998,22 @@ int rev_import_init(struct obd_export *export)
 	spin_unlock(&export->exp_lock);
 	class_import_put(revimp);
 
-	if (!export->exp_not_timed) {
-		spin_lock(&obd->obd_dev_lock);
-		list_add_tail(&export->exp_obd_chain_timed,
-			      &obd->obd_exports_timed);
-		spin_unlock(&obd->obd_dev_lock);
+	if (export->exp_timed) {
+		void *data;
+
+		rc = obd_export_timed_init(export, &data);
+		if (rc == 0) {
+			spin_lock(&obd->obd_dev_lock);
+			/* At the beginning, there is no AT stats yet, use
+			 * previous approach for the ping evictor timeout */
+			export->exp_deadline =
+				PING_EVICT_TIMEOUT + ktime_get_real_seconds();
+			obd_export_timed_add(export, &data);
+			spin_unlock(&obd->obd_dev_lock);
+			obd_export_timed_fini(export, &data);
+		}
 	}
-	return 0;
+	return rc;
 }
 EXPORT_SYMBOL(rev_import_init);
 
@@ -1497,7 +1507,7 @@ dont_check_exports:
 			 * should be called to cleanup stuff
 			 */
 			spin_lock(&target->obd_dev_lock);
-			list_del_init(&export->exp_obd_chain_timed);
+			obd_export_timed_del(export);
 			spin_unlock(&target->obd_dev_lock);
 
 			class_export_get(export);
@@ -2430,29 +2440,8 @@ static void handle_recovery_req(struct ptlrpc_thread *thread,
 		 * Add request @timeout to the recovery time so next request from
 		 * this client may come in recovery time
 		 */
-		if (!obd_at_off(obd)) {
-			struct ptlrpc_service_part *svcpt;
-			timeout_t est_timeout;
-
-			svcpt = req->rq_rqbd->rqbd_svcpt;
-			/*
-			 * If the server sent early reply for this request,
-			 * the client will recalculate the timeout according to
-			 * current server estimate service time, so we will
-			 * use the maxium timeout here for waiting the client
-			 * sending the next req
-			 */
-			est_timeout = obd_at_get(obd, &svcpt->scp_at_estimate);
-			timeout = max_t(timeout_t, at_est2timeout(est_timeout),
-					lustre_msg_get_timeout(req->rq_reqmsg));
-			/*
-			 * Add 2 net_latency, one for balance rq_deadline
-			 * (see ptl_send_rpc), one for resend the req to server,
-			 * Note: client will pack net_latency in replay req
-			 * (see ptlrpc_replay_req)
-			 */
-			timeout += 2 * lustre_msg_get_service_timeout(req->rq_reqmsg);
-		}
+		if (!obd_at_off(obd))
+			timeout = ptlrpc_export_prolong_timeout(req, true);
 		extend_recovery_timer(class_exp2obd(req->rq_export), timeout,
 				      true);
 	}
@@ -2843,7 +2832,7 @@ static int target_recovery_thread(void *arg)
 		 * so we need refresh the last_request_time, to avoid the
 		 * export is being evicted
 		 */
-		ptlrpc_update_export_timer(req->rq_export, 0);
+		ptlrpc_update_export_timer(req);
 	}
 
 	/*

lustre/obdclass/genops.c
@@ -378,7 +378,7 @@ struct obd_device *class_newdev(const char *type_name, const char *name,
 	newdev->obd_grant_check_threshold = 100;
 	INIT_LIST_HEAD(&newdev->obd_unlinked_exports);
 	INIT_LIST_HEAD(&newdev->obd_delayed_exports);
-	INIT_LIST_HEAD(&newdev->obd_exports_timed);
+	newdev->obd_exports_timed.rb_node = NULL;
 	INIT_LIST_HEAD(&newdev->obd_nid_stats);
 	spin_lock_init(&newdev->obd_nid_lock);
 	spin_lock_init(&newdev->obd_dev_lock);
@@ -982,7 +982,7 @@ static struct obd_export *__class_new_export(struct obd_device *obd,
 	spin_lock_init(&export->exp_bl_list_lock);
 	INIT_LIST_HEAD(&export->exp_bl_list);
 	INIT_LIST_HEAD(&export->exp_stale_list);
-	INIT_LIST_HEAD(&export->exp_obd_chain_timed);
+	INIT_LIST_HEAD(&export->exp_timed_chain);
 	INIT_WORK(&export->exp_zombie_work, obd_zombie_exp_cull);
 
 	export->exp_sp_peer = LUSTRE_SP_ANY;
@@ -1040,6 +1040,118 @@ struct obd_export *class_new_export_self(struct obd_device *obd,
 	return __class_new_export(obd, uuid, true);
 }
 
+struct rb_node_exp_deadline {
+	struct rb_node	  ned_node;
+	struct list_head  ned_head;
+	time64_t	  ned_deadline;
+};
+
+static inline bool ptlrpc_exp_deadline_less(struct rb_node *ln,
+					    const struct rb_node *rn)
+{
+	struct rb_node_exp_deadline *left, *right;
+
+	left = rb_entry(ln, struct rb_node_exp_deadline, ned_node);
+	right = rb_entry(rn, struct rb_node_exp_deadline, ned_node);
+
+	return left->ned_deadline < right->ned_deadline;
+}
+
+static inline int ptlrpc_exp_deadline_cmp(const void *key,
+					  const struct rb_node *node)
+{
+	struct rb_node_exp_deadline *ned;
+	time64_t *time = (time64_t *)key;
+
+	ned = rb_entry(node, struct rb_node_exp_deadline, ned_node);
+	return (*time < ned->ned_deadline ? -1 :
+		*time > ned->ned_deadline ?  1 : 0);
+}
+
+int obd_export_timed_init(struct obd_export *exp, void **data)
+
+{
+	OBD_ALLOC(*data, sizeof(struct rb_node_exp_deadline));
+	return data == NULL ? -ENOMEM : 0;
+}
+EXPORT_SYMBOL(obd_export_timed_init);
+
+void obd_export_timed_fini(struct obd_export *exp, void **data)
+{
+	if (*data) {
+		OBD_FREE(*data, sizeof(struct rb_node_exp_deadline));
+		*data = NULL;
+	}
+}
+EXPORT_SYMBOL(obd_export_timed_fini);
+
+void obd_export_timed_add(struct obd_export *exp, void **data)
+{
+	struct rb_node_exp_deadline *ned = *data;
+	struct rb_node *node;
+
+	node = rb_find(&exp->exp_deadline, &exp->exp_obd->obd_exports_timed,
+		       ptlrpc_exp_deadline_cmp);
+
+	if (node == NULL) {
+		LASSERT(ned != NULL);
+		INIT_LIST_HEAD(&ned->ned_head);
+		RB_CLEAR_NODE(&ned->ned_node);
+		ned->ned_deadline = exp->exp_deadline;
+		*data = NULL;
+
+		rb_add(&ned->ned_node, &exp->exp_obd->obd_exports_timed,
+		       ptlrpc_exp_deadline_less);
+	} else {
+		ned = rb_entry(node, struct rb_node_exp_deadline, ned_node);
+		LASSERT(!list_empty(&ned->ned_head));
+	}
+
+	list_add_tail(&exp->exp_timed_chain, &ned->ned_head);
+}
+EXPORT_SYMBOL(obd_export_timed_add);
+
+void obd_export_timed_del(struct obd_export *exp)
+{
+	struct rb_node_exp_deadline *ned;
+
+	if (list_empty(&exp->exp_timed_chain))
+		return;
+
+	ned = rb_entry(rb_find(&exp->exp_deadline,
+			       &exp->exp_obd->obd_exports_timed,
+			       ptlrpc_exp_deadline_cmp),
+		       struct rb_node_exp_deadline, ned_node);
+	LASSERT(!list_empty(&ned->ned_head));
+	LASSERT(ned->ned_deadline == exp->exp_deadline);
+	list_del_init(&exp->exp_timed_chain);
+
+	if (list_empty(&ned->ned_head)) {
+		rb_erase(&ned->ned_node, &exp->exp_obd->obd_exports_timed);
+		OBD_FREE_PTR(ned);
+	}
+}
+EXPORT_SYMBOL(obd_export_timed_del);
+
+struct obd_export *obd_export_timed_get(struct obd_device *obd, bool last)
+{
+	struct rb_node_exp_deadline *ned;
+	struct rb_node *node;
+
+	node = last ? rb_last(&obd->obd_exports_timed) :
+		rb_first(&obd->obd_exports_timed);
+
+	if (node == NULL)
+		return NULL;
+
+	ned = rb_entry(node, struct rb_node_exp_deadline, ned_node);
+	LASSERT(!list_empty(&ned->ned_head));
+
+	return list_first_entry(&ned->ned_head, struct obd_export,
+				exp_timed_chain);
+}
+EXPORT_SYMBOL(obd_export_timed_get);
+
 void class_unlink_export(struct obd_export *exp)
 {
 	class_handle_unhash(&exp->exp_handle);
@@ -1071,7 +1183,7 @@ void class_unlink_export(struct obd_export *exp)
 #endif /* HAVE_SERVER_SUPPORT */
 
 	list_move(&exp->exp_obd_chain, &exp->exp_obd->obd_unlinked_exports);
-	list_del_init(&exp->exp_obd_chain_timed);
+	obd_export_timed_del(exp);
 	exp->exp_obd->obd_num_exports--;
 	spin_unlock(&exp->exp_obd->obd_dev_lock);
 

lustre/include/lustre_dlm.h
@@ -1515,7 +1515,6 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 
 void ldlm_revoke_export_locks(struct obd_export *exp);
 timeout_t ldlm_bl_timeout(struct ldlm_lock *lock);
-timeout_t ldlm_bl_timeout_by_rpc(struct ptlrpc_request *req);
 #endif
 int ldlm_del_waiting_lock(struct ldlm_lock *lock);
 int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, timeout_t timeout);

lustre/include/lustre_export.h
@@ -196,7 +196,7 @@ struct obd_export {
 	 * order
 	 * protected by obd_dev_lock
 	 */
-	struct list_head	exp_obd_chain_timed;
+	struct list_head	exp_timed_chain;
 	/** Obd device of this export */
 	struct obd_device      *exp_obd;
 	/**
@@ -223,6 +223,7 @@ struct obd_export {
 	__u64			exp_last_committed;
 	/** When was last request received */
 	time64_t		exp_last_request_time;
+	time64_t		exp_deadline;
 	/** On replay all requests waiting for replay are linked here */
 	struct list_head	exp_req_replay_queue;
 	/**
@@ -260,7 +261,7 @@ struct obd_export {
 				 */
 				exp_old_falloc:1,
 				exp_hashed:1,
-				exp_not_timed:1;
+				exp_timed:1;
 	/* also protected by exp_lock */
 	enum lustre_sec_part	exp_sp_peer;
 	struct sptlrpc_flavor	exp_flvr;		/* current */

lustre/include/lustre_net.h
@@ -2261,8 +2261,9 @@ int ptlrpc_service_health_check(struct ptlrpc_service *service);
 void ptlrpc_server_drop_request(struct ptlrpc_request *req);
 void ptlrpc_request_change_export(struct ptlrpc_request *req,
 				  struct obd_export *export);
-void ptlrpc_update_export_timer(struct obd_export *exp,
-				time64_t extra_delay);
+void ptlrpc_update_export_timer(struct ptlrpc_request *req);
+timeout_t ptlrpc_export_prolong_timeout(struct ptlrpc_request *req,
+					bool recovery);
 
 int ptlrpc_hr_init(void);
 void ptlrpc_hr_fini(void);

lustre/include/obd.h
@@ -683,7 +683,7 @@ struct obd_device {
 	struct obd_export       *obd_self_export;
 	struct obd_export	*obd_lwp_export;
 	/* list of exports in LRU order, for ping evictor, with obd_dev_lock */
-	struct list_head	obd_exports_timed;
+	struct rb_root		obd_exports_timed;
 	time64_t		obd_eviction_timer;	/* for ping evictor */
 
 	atomic_t                obd_max_recoverable_clients;

lustre/include/obd_class.h
@@ -406,6 +406,12 @@ void class_import_put(struct obd_import *);
 struct obd_import *class_new_import(struct obd_device *obd);
 void class_destroy_import(struct obd_import *exp);
 
+int obd_export_timed_init(struct obd_export *exp, void **data);
+void obd_export_timed_fini(struct obd_export *exp, void **data);
+void obd_export_timed_add(struct obd_export *exp, void **data);
+void obd_export_timed_del(struct obd_export *exp);
+struct obd_export *obd_export_timed_get(struct obd_device *obd, bool last);
+
 #ifdef HAVE_SERVER_SUPPORT
 struct obd_type *class_search_type(const char *name);
 struct obd_type *class_get_type(const char *name);

lustre/include/obd_support.h
@@ -41,7 +41,6 @@ extern unsigned int obd_lbug_on_eviction;
 extern unsigned int obd_timeout;          /* seconds */
 extern unsigned int ldlm_timeout;         /* seconds */
 extern unsigned int ping_interval;        /* seconds */
-extern unsigned int ping_evict_timeout_multiplier;
 extern unsigned int obd_timeout_set;
 extern unsigned int ldlm_timeout_set;
 extern unsigned int bulk_timeout;
@@ -90,7 +89,7 @@ extern bool obd_enable_fname_encoding;
  * and there's no urgent need to evict a client just because it's idle, we
  * should be very conservative here.
  */
-#define PING_EVICT_TIMEOUT (PING_INTERVAL * ping_evict_timeout_multiplier)
+#define PING_EVICT_TIMEOUT (PING_INTERVAL * 6)
 #define DISK_TIMEOUT 50          /* Beyond this we warn about disk speed */
 #define CONNECTION_SWITCH_MIN 5U /* Connection switching rate limiter */
  /* Max connect interval for nonresponsive servers; ~50s to avoid building up

microsoft/amlFilesystem-lustre

commit 5baad8ff9ff57c34cfcfaa8740931e14dfdf0ef7

LU-17000 obdclass: handle export timer allocation failure
obd_export_timed_init() tested the void ** parameter instead of the
pointer OBD_ALLOC() wrote through it, so it always returned 0.  On
OOM both callers then ran obd_export_timed_add() with a NULL node,
with obd_dev_lock held, and tripped its ned != NULL assertion.  That
assertion stays: it is now unreachable, and a caller that has already
unlinked the export cannot roll back a failure there.

Allocate in rev_import_init() before exp_imp_reverse is published,
since the reconnect path calls rev_import_init() only while that is
NULL and a failure after it is set would strand the export outside
obd_exports_timed, invisible to the ping evictor.

Test-Parameters: trivial
Fixes: 4cb897ca4a97 ("LU-18878 ptlrpc: improve ping evictor and recovery timeout")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Ie28b19be3c5e7b681184e67131d1f90fe037c2e8

lustre/ldlm/ldlm_lib.c
@@ -1057,13 +1057,26 @@ int rev_import_init(struct obd_export *export)
 {
 	struct obd_device *obd = export->exp_obd;
 	struct obd_import *revimp;
-	int rc = 0;
+	void *data = NULL;
+	int rc;
 
 	LASSERT(export->exp_imp_reverse == NULL);
 
+	/* allocate before exp_imp_reverse is published: once it is set the
+	 * reconnect path skips rev_import_init(), so a later failure would
+	 * leave the export out of obd_exports_timed for good
+	 */
+	if (export->exp_timed) {
+		rc = obd_export_timed_init(export, &data);
+		if (rc)
+			return rc;
+	}
+
 	revimp = class_new_import(obd);
-	if (revimp == NULL)
+	if (!revimp) {
+		obd_export_timed_fini(export, &data);
 		return -ENOMEM;
+	}
 
 	revimp->imp_remote_handle.cookie = 0ULL;
 	revimp->imp_client = &obd->obd_ldlm_client;
@@ -1075,22 +1088,18 @@ int rev_import_init(struct obd_export *export)
 	spin_unlock(&export->exp_lock);
 	class_import_put(revimp);
 
-	if (export->exp_timed) {
-		void *data;
-
-		rc = obd_export_timed_init(export, &data);
-		if (rc == 0) {
-			spin_lock(&obd->obd_dev_lock);
-			/* At the beginning, there is no AT stats yet, use
-			 * previous approach for the ping evictor timeout */
-			export->exp_deadline =
-				PING_EVICT_TIMEOUT + ktime_get_real_seconds();
-			obd_export_timed_add(export, &data);
-			spin_unlock(&obd->obd_dev_lock);
-			obd_export_timed_fini(export, &data);
-		}
+	if (data) {
+		spin_lock(&obd->obd_dev_lock);
+		/* At the beginning, there is no AT stats yet, use
+		 * previous approach for the ping evictor timeout
+		 */
+		export->exp_deadline = PING_EVICT_TIMEOUT +
+				       ktime_get_real_seconds();
+		obd_export_timed_add(export, &data);
+		spin_unlock(&obd->obd_dev_lock);
+		obd_export_timed_fini(export, &data);
 	}
-	return rc;
+	return 0;
 }
 EXPORT_SYMBOL(rev_import_init);
 

lustre/obdclass/genops.c
@@ -1050,10 +1050,9 @@ static inline int ptlrpc_exp_deadline_cmp(const void *key,
 }
 
 int obd_export_timed_init(struct obd_export *exp, void **data)
-
 {
 	OBD_ALLOC(*data, sizeof(struct rb_node_exp_deadline));
-	return data == NULL ? -ENOMEM : 0;
+	return *data ? 0 : -ENOMEM;
 }
 EXPORT_SYMBOL(obd_export_timed_init);
 