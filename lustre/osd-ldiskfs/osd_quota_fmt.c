lustre/lustre-release

commit 57df6353ca590f32a32c0f0dc4a1d10a86a027ec

LU-1182 ldiskfs-osd: space accounting support
Add space accounting support to ldiskfs OSD.

This patch also sets initial attributes in do_create().
mdd_attr_set_internal() from mdd_object_initialize() is kept until
EDQUOT is returned in lquota itself.
Attributes of new inodes are now initialized in osd_object_create().
All LA_MODE bits are now passed to ldiskfs_create_inode().
(original patch from LiWei, see ORI-46)

Signed-off-by: Johann Lombardi <johann@whamcloud.com>
Change-Id: I77a621c76343c2633810bb3cef9859ee30b7b23a
Reviewed-on: http://review.whamcloud.com/3160
Reviewed-by: Niu Yawei <niu@whamcloud.com>
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/osd-ldiskfs/osd_quota_fmt.c
@@ -1,0 +1,284 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2011, 2012, Whamcloud, Inc.
+ * Use is subject to license terms.
+ *
+ * Lustre administrative quota format.
+ * from linux/fs/quota_v2.c
+ */
+
+#include "osd_internal.h"
+#include "osd_quota_fmt.h"
+
+typedef char *dqbuf_t;
+
+static const union
+{
+	struct lustre_disk_dqblk_v2 r1;
+} emptydquot = { .r1 = { 0 } };
+
+static inline dqbuf_t getdqbuf(void)
+{
+	dqbuf_t buf = cfs_alloc(LUSTRE_DQBLKSIZE, CFS_ALLOC_IO);
+	if (!buf)
+		CWARN("Not enough memory for quota buffers.\n");
+	return buf;
+}
+
+static inline void freedqbuf(dqbuf_t buf)
+{
+	cfs_free(buf);
+}
+
+/**
+ * Read the \a blk into \a buf.
+ *
+ * TODO Will support enforcement quota later.
+ */
+static ssize_t quota_read_blk(const struct lu_env *env,
+			      struct osd_object *obj,
+			      int type, uint blk, dqbuf_t buf)
+{
+	ssize_t ret;
+	struct super_block *sb = obj->oo_inode->i_sb;
+
+	ENTRY;
+
+	memset(buf, 0, LUSTRE_DQBLKSIZE);
+	ret = sb->s_op->quota_read(sb, type, buf, LUSTRE_DQBLKSIZE,
+				   blk << LUSTRE_DQBLKSIZE_BITS);
+
+	/* Reading past EOF just returns a block of zeros */
+	if (ret == -EBADR)
+		ret = 0;
+
+	RETURN(ret);
+}
+
+/**
+ * Find entry in block by given \a dqid in the leaf block \a blk
+ *
+ * \retval +ve, the offset of the entry in file
+ * \retval   0, entry not found
+ * \retval -ve, unexpected failure
+ */
+static loff_t find_block_dqentry(const struct lu_env *env,
+				 struct osd_object *obj, int type,
+				 qid_t dqid, uint blk,
+				 struct osd_it_quota *it)
+{
+	dqbuf_t				 buf = getdqbuf();
+	loff_t				 ret;
+	int				 i;
+	struct lustre_disk_dqblk_v2	*ddquot;
+	int				 dqblk_sz;
+
+	ENTRY;
+
+	ddquot = (struct lustre_disk_dqblk_v2 *)GETENTRIES(buf);
+	dqblk_sz = sizeof(struct lustre_disk_dqblk_v2);
+	if (!buf)
+		RETURN(-ENOMEM);
+	ret = quota_read_blk(env, obj, type, blk, buf);
+	if (ret < 0) {
+		CERROR("Can't read quota tree block %u.\n", blk);
+		GOTO(out_buf, ret);
+	}
+
+	if (dqid) {
+		for (i = 0; i < LUSTRE_DQSTRINBLK &&
+			    le32_to_cpu(ddquot[i].dqb_id) != dqid; i++)
+			continue;
+	} else { /* ID 0 as a bit more complicated searching... */
+		for (i = 0; i < LUSTRE_DQSTRINBLK; i++)
+			if (!le32_to_cpu(ddquot[i].dqb_id) &&
+			    memcmp((char *)&emptydquot, (char *)&ddquot[i],
+				   dqblk_sz))
+				break;
+	}
+	if (i == LUSTRE_DQSTRINBLK) {
+		CDEBUG(D_QUOTA, "Quota for id %u not found.\n", dqid);
+		ret = 0;
+		GOTO(out_buf, ret);
+	} else {
+		ret = (blk << LUSTRE_DQBLKSIZE_BITS) +
+		      sizeof(struct lustre_disk_dqdbheader) + i * dqblk_sz;
+
+		if (it) {
+			it->oiq_blk[LUSTRE_DQTREEDEPTH - 1] = blk;
+			it->oiq_offset = ret;
+			it->oiq_id = dqid;
+		} else {
+			ret = 0;
+		}
+	}
+out_buf:
+	freedqbuf(buf);
+	RETURN(ret);
+}
+
+/**
+ * Find entry for given \a dqid in the tree block \a blk
+ *
+ * \retval +ve, the offset of the entry in file
+ * \retval   0, entry not found
+ * \retval -ve, unexpected failure
+ */
+loff_t find_tree_dqentry(const struct lu_env *env,
+			 struct osd_object *obj, int type,
+			 qid_t dqid, uint blk, int depth,
+			 struct osd_it_quota *it)
+{
+	dqbuf_t	 buf = getdqbuf();
+	loff_t	 ret;
+	u32	*ref = (u32 *) buf;
+
+	ENTRY;
+
+	if (!buf)
+		RETURN(-ENOMEM);
+	ret = quota_read_blk(env, obj, 0, blk, buf);
+	if (ret < 0) {
+		CERROR("Can't read quota tree block %u.\n", blk);
+		GOTO(out_buf, ret);
+	}
+	ret = 0;
+	blk = le32_to_cpu(ref[GETIDINDEX(dqid, depth)]);
+	if (!blk)               /* No reference? */
+		GOTO(out_buf, ret);
+
+	if (depth < LUSTRE_DQTREEDEPTH - 1)
+		ret = find_tree_dqentry(env, obj, type, dqid, blk,
+					depth + 1, it);
+	else
+		ret = find_block_dqentry(env, obj, type, dqid, blk, it);
+
+	if (it && ret > 0) /* Entry found */
+		it->oiq_blk[depth] = blk;
+out_buf:
+	freedqbuf(buf);
+	RETURN(ret);
+}
+
+/**
+ * Search from \a index within the leaf block \a blk, and fill the \a it with
+ * the first valid entry.
+ *
+ * \retval +ve, no valid entry found
+ * \retval   0, entry found
+ * \retval -ve, unexpected failure
+ */
+int walk_block_dqentry(const struct lu_env *env, struct osd_object *obj,
+		       int type, uint blk, uint index,
+		       struct osd_it_quota *it)
+{
+	dqbuf_t				 buf = getdqbuf();
+	loff_t				 ret = 0;
+	struct lustre_disk_dqdbheader	*dqhead;
+	int				 i, dqblk_sz;
+	struct lustre_disk_dqblk_v2	*ddquot;
+
+	ENTRY;
+
+	dqhead = (struct lustre_disk_dqdbheader *)buf;
+	dqblk_sz = sizeof(struct lustre_disk_dqblk_v2);
+	if (!buf)
+		RETURN(-ENOMEM);
+	ret = quota_read_blk(env, obj, type, blk, buf);
+	if (ret < 0) {
+		CERROR("Can't read quota tree block %u.\n", blk);
+		GOTO(out_buf, ret);
+	}
+	ret = 1;
+
+	if (!le32_to_cpu(dqhead->dqdh_entries))
+		GOTO(out_buf, ret);
+
+	ddquot = (struct lustre_disk_dqblk_v2 *)GETENTRIES(buf);
+	LASSERT(index < LUSTRE_DQSTRINBLK);
+	for (i = index; i < LUSTRE_DQSTRINBLK; i++) {
+		/* skip empty entry */
+		if (!memcmp((char *)&emptydquot,
+			    (char *)&ddquot[i], dqblk_sz))
+			continue;
+
+		it->oiq_blk[LUSTRE_DQTREEDEPTH - 1] = blk;
+		it->oiq_id = le32_to_cpu(ddquot[i].dqb_id);
+		it->oiq_offset = (blk << LUSTRE_DQBLKSIZE_BITS) +
+				  sizeof(struct lustre_disk_dqdbheader) +
+				  i * dqblk_sz;
+		ret = 0;
+		break;
+	}
+
+out_buf:
+	freedqbuf(buf);
+	RETURN(ret);
+}
+
+/**
+ * Search from \a index within the tree block \a blk, and fill the \a it
+ * with the first valid entry.
+ *
+ * \retval +ve, no valid entry found
+ * \retval   0, entry found
+ * \retval -ve, unexpected failure
+ */
+int walk_tree_dqentry(const struct lu_env *env, struct osd_object *obj,
+		      int type, uint blk, int depth, uint index,
+		      struct osd_it_quota *it)
+{
+	dqbuf_t	 buf = getdqbuf();
+	loff_t	 ret;
+	u32	*ref = (u32 *) buf;
+
+	ENTRY;
+
+	if (!buf)
+		RETURN(-ENOMEM);
+	ret = quota_read_blk(env, obj, type, blk, buf);
+	if (ret < 0) {
+		CERROR("Can't read quota tree block %u.\n", blk);
+		goto out_buf;
+	}
+	ret = 1;
+
+	for (; index <= 0xff && ret > 0; index++) {
+		blk = le32_to_cpu(ref[index]);
+		if (!blk)       /* No reference */
+			continue;
+
+		if (depth < LUSTRE_DQTREEDEPTH - 1)
+			ret = walk_tree_dqentry(env, obj, type, blk,
+						depth + 1, 0, it);
+		else
+			ret = walk_block_dqentry(env, obj, type, blk, 0, it);
+	}
+
+	if (ret == 0) /* Entry found */
+		it->oiq_blk[depth] = blk;
+out_buf:
+	freedqbuf(buf);
+	RETURN(ret);
+}

ldiskfs/kernel_patches/patches/ext4-quota-dont-update-cmtime.patch
@@ -1,0 +1,92 @@
+commit 21f976975cbecbdaf23ceeacc1cab2b1c05a028e
+Author: Jan Kara <jack@suse.cz>
+Date:   Mon Apr 4 15:33:39 2011 -0400
+
+    ext4: remove unnecessary [cm]time update of quota file
+
+    It is not necessary to update [cm]time of quota file on each quota
+    file write and it wastes journal space and IO throughput with inode
+    writes. So just remove the updating from ext4_quota_write() and only
+    update times when quotas are being turned off. Userspace cannot get
+    anything reliable from quota files while they are used by the kernel
+    anyway.
+
+    Signed-off-by: Jan Kara <jack@suse.cz>
+    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
+
+Index: linux-stage/fs/ext4/ext4_jbd2.h
+===================================================================
+--- linux-stage.orig/fs/ext4/ext4_jbd2.h	2012-06-26 11:26:25.000000000 +0200
++++ linux-stage/fs/ext4/ext4_jbd2.h	2012-06-26 11:35:31.025105000 +0200
+@@ -88,8 +88,8 @@
+ 
+ #ifdef CONFIG_QUOTA
+ /* Amount of blocks needed for quota update - we know that the structure was
+- * allocated so we need to update only inode+data */
+-#define EXT4_QUOTA_TRANS_BLOCKS(sb) (test_opt(sb, QUOTA) ? 2 : 0)
++ * allocated so we need to update only data block */
++#define EXT4_QUOTA_TRANS_BLOCKS(sb) (test_opt(sb, QUOTA) ? 1 : 0)
+ /* Amount of blocks needed for quota insert/delete - we do some block writes
+  * but inode, sb and group updates are done only once */
+ #define EXT4_QUOTA_INIT_BLOCKS(sb) (test_opt(sb, QUOTA) ? (DQUOT_INIT_ALLOC*\
+Index: linux-stage/fs/ext4/super.c
+===================================================================
+--- linux-stage.orig/fs/ext4/super.c	2012-06-26 11:35:09.000000000 +0200
++++ linux-stage/fs/ext4/super.c	2012-06-26 11:37:30.905374000 +0200
+@@ -4582,6 +4582,7 @@ static int ext4_quota_on(struct super_bl
+ static int ext4_quota_off(struct super_block *sb, int type, int remount)
+ {
+ 	struct quota_info *dqopt = sb_dqopt(sb);
++	int                cnt;
+ 
+ 	mutex_lock(&dqopt->dqonoff_mutex);
+ 	if (!sb_any_quota_loaded(sb)) {
+@@ -4598,6 +4599,37 @@ static int ext4_quota_off(struct super_b
+ 		up_read(&sb->s_umount);
+ 	}
+ 
++	for (cnt = 0; cnt < MAXQUOTAS; cnt++) {
++		struct inode      *inode;
++		handle_t          *handle;
++
++		if (type != -1 && cnt != type)
++			continue;
++
++		mutex_lock(&dqopt->dqonoff_mutex);
++		inode = dqopt->files[cnt];
++		if (!sb_has_quota_loaded(sb, cnt) || !inode) {
++			mutex_unlock(&dqopt->dqonoff_mutex);
++			continue;
++		}
++
++		inode = igrab(inode);
++		mutex_unlock(&dqopt->dqonoff_mutex);
++
++		if (!inode)
++			continue;
++
++		/* Update modification times of quota files when userspace can
++		 * start looking at them */
++		handle = ext4_journal_start(inode, 1);
++		if (!IS_ERR(handle)) {
++			inode->i_mtime = inode->i_ctime = CURRENT_TIME;
++			ext4_mark_inode_dirty(handle, inode);
++			ext4_journal_stop(handle);
++		}
++		iput(inode);
++	}
++
+ 	return vfs_quota_off(sb, type, remount);
+ }
+ 
+@@ -4696,9 +4728,8 @@ out:
+ 	if (inode->i_size < off + len) {
+ 		i_size_write(inode, off + len);
+ 		EXT4_I(inode)->i_disksize = inode->i_size;
++		ext4_mark_inode_dirty(handle, inode);
+ 	}
+-	inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+-	ext4_mark_inode_dirty(handle, inode);
+ 	mutex_unlock(&inode->i_mutex);
+ 	return len;
+ }

ldiskfs/kernel_patches/patches/ext4-quota-first-class.patch
@@ -1,0 +1,399 @@
+From: Aditya Kali <adityakali@google.com>
+
+This patch is an attempt towards supporting quotas as first class
+feature in ext4. It is based on the proposal at:
+https://ext4.wiki.kernel.org/index.php/Design_For_1st_Class_Quota_in_Ext4
+This patch introduces a new feature - EXT4_FEATURE_RO_COMPAT_QUOTA which, when
+turned on, enables quota accounting at mount time iteself. Also, the
+quota inodes are stored in two additional superblock fields.
+Some changes introduced by this patch that should be pointed out are:
+1) Two new ext4-superblock fields - s_usr_quota_inum and s_grp_quota_inum
+   for storing the quota inodes in use.
+2) If the QUOTA feature and corresponding quota inodes are set in superblock,
+   Quotas are turned on at mount time irrespective of the quota mount options.
+   Thus the mount options 'quota', 'usrquota' and 'grpquota' are completely
+   ignored with the new QUOTA feature flag.
+3) Default quota inodes are: inode#3 for tracking userquota and inode#4 for
+   tracking group quota. The superblock fields can be set to use other inodes
+   as well.
+4) mke2fs or tune2fs will initialize these inodes when quota feature is
+   being set. The default reserved inodes will not be visible to user as
+   regular files.
+5) Once quotas are turned on, they cannot be turned off while the FS is
+   mounted. This is because we do not want to let the quota get inconsistent.
+6) With the QUOTA feature set, since the quota inodes are hidden, some of the
+   utilities from quota-tools will no longer work correctly. Instead, e2fsprogs
+   will include support for fixing the quota files.
+7) Support is only for the new V2 quota file format.
+
+Signed-off-by: Aditya Kali <adityakali@google.com>
+---
+Index: linux-stage/fs/ext4/ext4.h
+===================================================================
+--- linux-stage.orig/fs/ext4/ext4.h	2012-06-26 11:26:23.345235745 +0200
++++ linux-stage/fs/ext4/ext4.h	2012-06-26 11:37:38.250355000 +0200
+@@ -162,6 +162,8 @@ typedef struct ext4_io_end {
+  */
+ #define	EXT4_BAD_INO		 1	/* Bad blocks inode */
+ #define EXT4_ROOT_INO		 2	/* Root inode */
++#define EXT4_USR_QUOTA_INO       3      /* User quota inode */
++#define EXT4_GRP_QUOTA_INO       4      /* Group quota inode */
+ #define EXT4_BOOT_LOADER_INO	 5	/* Boot loader inode */
+ #define EXT4_UNDEL_DIR_INO	 6	/* Undelete directory inode */
+ #define EXT4_RESIZE_INO		 7	/* Reserved group descriptors inode */
+@@ -1016,7 +1018,9 @@ struct ext4_super_block {
+ 	__u8	s_last_error_func[32];	/* function where the error happened */
+ #define EXT4_S_ERR_END offsetof(struct ext4_super_block, s_mount_opts)
+ 	__u8	s_mount_opts[64];
+-	__le32	s_reserved[112];        /* Padding to the end of the block */
++	__le32	s_usr_quota_inum;	/* inode for tracking user quota */
++	__le32	s_grp_quota_inum;	/* inode for tracking group quota */
++	__le32	s_reserved[110];        /* Padding to the end of the block */
+ };
+ 
+ #ifdef __KERNEL__
+@@ -1090,6 +1094,7 @@ struct ext4_sb_info {
+ #ifdef CONFIG_QUOTA
+ 	char *s_qf_names[MAXQUOTAS];		/* Names of quota files with journalled quota */
+ 	int s_jquota_fmt;			/* Format of quota to use */
++	unsigned long s_qf_inums[MAXQUOTAS];    /* Quota file inodes */
+ #endif
+ 	unsigned int s_want_extra_isize; /* New inodes should reserve # bytes */
+ 	struct rb_root system_blks;
+@@ -1189,6 +1194,8 @@ static inline struct timespec ext4_curre
+ static inline int ext4_valid_inum(struct super_block *sb, unsigned long ino)
+ {
+ 	return ino == EXT4_ROOT_INO ||
++		ino == EXT4_USR_QUOTA_INO ||
++		ino == EXT4_GRP_QUOTA_INO ||
+ 		ino == EXT4_JOURNAL_INO ||
+ 		ino == EXT4_RESIZE_INO ||
+ 		(ino >= EXT4_FIRST_INO(sb) &&
+@@ -1293,6 +1300,7 @@ EXT4_INODE_BIT_FNS(state, state_flags)
+ #define EXT4_FEATURE_RO_COMPAT_GDT_CSUM		0x0010
+ #define EXT4_FEATURE_RO_COMPAT_DIR_NLINK	0x0020
+ #define EXT4_FEATURE_RO_COMPAT_EXTRA_ISIZE	0x0040
++#define EXT4_FEATURE_RO_COMPAT_QUOTA		0x0100
+ 
+ #define EXT4_FEATURE_INCOMPAT_COMPRESSION	0x0001
+ #define EXT4_FEATURE_INCOMPAT_FILETYPE		0x0002
+@@ -1325,7 +1333,8 @@ EXT4_INODE_BIT_FNS(state, state_flags)
+ 					 EXT4_FEATURE_RO_COMPAT_DIR_NLINK | \
+ 					 EXT4_FEATURE_RO_COMPAT_EXTRA_ISIZE | \
+ 					 EXT4_FEATURE_RO_COMPAT_BTREE_DIR |\
+-					 EXT4_FEATURE_RO_COMPAT_HUGE_FILE)
++					 EXT4_FEATURE_RO_COMPAT_HUGE_FILE| \
++					 EXT4_FEATURE_RO_COMPAT_QUOTA)
+ 
+ /*
+  * Default values for user and/or group using reserved blocks
+Index: linux-stage/fs/ext4/ext4_jbd2.h
+===================================================================
+--- linux-stage.orig/fs/ext4/ext4_jbd2.h	2012-06-26 11:35:31.025105000 +0200
++++ linux-stage/fs/ext4/ext4_jbd2.h	2012-06-26 11:37:38.250631000 +0200
+@@ -89,14 +89,20 @@
+ #ifdef CONFIG_QUOTA
+ /* Amount of blocks needed for quota update - we know that the structure was
+  * allocated so we need to update only data block */
+-#define EXT4_QUOTA_TRANS_BLOCKS(sb) (test_opt(sb, QUOTA) ? 1 : 0)
++#define EXT4_QUOTA_TRANS_BLOCKS(sb) ((test_opt(sb, QUOTA) ||\
++		EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) ?\
++		1 : 0)
+ /* Amount of blocks needed for quota insert/delete - we do some block writes
+  * but inode, sb and group updates are done only once */
+-#define EXT4_QUOTA_INIT_BLOCKS(sb) (test_opt(sb, QUOTA) ? (DQUOT_INIT_ALLOC*\
+-		(EXT4_SINGLEDATA_TRANS_BLOCKS(sb)-3)+3+DQUOT_INIT_REWRITE) : 0)
++#define EXT4_QUOTA_INIT_BLOCKS(sb) ((test_opt(sb, QUOTA) ||\
++		EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) ?\
++		(DQUOT_INIT_ALLOC*(EXT4_SINGLEDATA_TRANS_BLOCKS(sb)-3)\
++		 +3+DQUOT_INIT_REWRITE) : 0)
+ 
+-#define EXT4_QUOTA_DEL_BLOCKS(sb) (test_opt(sb, QUOTA) ? (DQUOT_DEL_ALLOC*\
+-		(EXT4_SINGLEDATA_TRANS_BLOCKS(sb)-3)+3+DQUOT_DEL_REWRITE) : 0)
++#define EXT4_QUOTA_DEL_BLOCKS(sb) ((test_opt(sb, QUOTA) ||\
++		EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) ?\
++		(DQUOT_DEL_ALLOC*(EXT4_SINGLEDATA_TRANS_BLOCKS(sb)-3)\
++		 +3+DQUOT_DEL_REWRITE) : 0)
+ #else
+ #define EXT4_QUOTA_TRANS_BLOCKS(sb) 0
+ #define EXT4_QUOTA_INIT_BLOCKS(sb) 0
+Index: linux-stage/fs/ext4/super.c
+===================================================================
+--- linux-stage.orig/fs/ext4/super.c	2012-06-26 11:37:30.905374000 +0200
++++ linux-stage/fs/ext4/super.c	2012-06-26 11:38:30.997488000 +0200
+@@ -86,6 +86,11 @@ wait_queue_head_t aio_wq[WQ_HASH_SZ];
+ 
+ static int bigendian_extents;
+ 
++#ifdef CONFIG_QUOTA
++static int ext4_acct_on(struct super_block *sb);
++static int ext4_acct_off(struct super_block *sb);
++#endif
++
+ ext4_fsblk_t ext4_block_bitmap(struct super_block *sb,
+ 			       struct ext4_group_desc *bg)
+ {
+@@ -670,6 +675,12 @@ static void ext4_put_super(struct super_
+ 
+ 	ext4_unregister_li_request(sb);
+ 
++#ifdef CONFIG_QUOTA
++	/* disable usage tracking which was enabled at mount time */
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA))
++		ext4_acct_off(sb);
++#endif
++
+ 	flush_workqueue(sbi->dio_unwritten_wq);
+ 	destroy_workqueue(sbi->dio_unwritten_wq);
+ 
+@@ -2142,14 +2153,22 @@ static void ext4_orphan_cleanup(struct s
+ #ifdef CONFIG_QUOTA
+ 	/* Needed for iput() to work correctly and not trash data */
+ 	sb->s_flags |= MS_ACTIVE;
+-	/* Turn on quotas so that they are updated correctly */
+-	for (i = 0; i < MAXQUOTAS; i++) {
+-		if (EXT4_SB(sb)->s_qf_names[i]) {
+-			int ret = ext4_quota_on_mount(sb, i);
+-			if (ret < 0)
+-				ext4_msg(sb, KERN_ERR,
+-					"Cannot turn on journaled "
+-					"quota: error %d", ret);
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) {
++		int ret;
++		ret = ext4_acct_on(sb);
++		if (ret)
++			ext4_msg(sb, KERN_ERR, "Failed to turn on usage "
++				 "tracking for quota: error %d", ret);
++	} else {
++		/* Turn on quotas so that they are updated correctly */
++		for (i = 0; i < MAXQUOTAS; i++) {
++			if (EXT4_SB(sb)->s_qf_names[i]) {
++				int ret = ext4_quota_on_mount(sb, i);
++				if (ret < 0)
++					ext4_msg(sb, KERN_ERR,
++						"Cannot turn on journaled "
++						"quota: error %d", ret);
++			}
+ 		}
+ 	}
+ #endif
+@@ -2193,10 +2212,14 @@ static void ext4_orphan_cleanup(struct s
+ 		ext4_msg(sb, KERN_INFO, "%d truncate%s cleaned up",
+ 		       PLURAL(nr_truncates));
+ #ifdef CONFIG_QUOTA
+-	/* Turn quotas off */
+-	for (i = 0; i < MAXQUOTAS; i++) {
+-		if (sb_dqopt(sb)->files[i])
+-			vfs_quota_off(sb, i, 0);
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) {
++		ext4_acct_off(sb);
++	} else {
++		/* Turn quotas off */
++		for (i = 0; i < MAXQUOTAS; i++) {
++			if (sb_dqopt(sb)->files[i])
++				vfs_quota_off(sb, i, 0);
++		}
+ 	}
+ #endif
+ 	sb->s_flags = s_flags; /* Restore MS_RDONLY status */
+@@ -3395,6 +3418,15 @@ static int ext4_fill_super(struct super_
+ #ifdef CONFIG_QUOTA
+ 	sb->s_qcop = &ext4_qctl_operations;
+ 	sb->dq_op = &ext4_quota_operations;
++
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) {
++		/* Use new qctl operations with quota on function that does not
++		 * require user specified quota file path. */
++		sb->s_qcop = &ext4_qctl_operations;
++
++		sbi->s_qf_inums[USRQUOTA] = es->s_usr_quota_inum;
++		sbi->s_qf_inums[GRPQUOTA] = es->s_grp_quota_inum;
++	}
+ #endif
+ 	INIT_LIST_HEAD(&sbi->s_orphan); /* unlinked but open files */
+ 	mutex_init(&sbi->s_orphan_lock);
+@@ -3622,8 +3654,31 @@ no_journal:
+ 	} else
+ 		descr = "out journal";
+ 
+-	ext4_msg(sb, KERN_INFO, "mounted filesystem with%s. "
+-		 "Opts: %s%s", descr, sbi->s_es->s_mount_opts,
++#ifdef CONFIG_QUOTA
++	/* Enable space tracking during mount, enforcement can be enabled/disable
++	 * later with quota_on/off */
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA) &&
++	    !(sb->s_flags & MS_RDONLY)) {
++		ret = ext4_acct_on(sb);
++		if (ret) {
++			ext4_msg(sb, KERN_ERR, "Can't enable usage tracking on "
++			         "a filesystem with the QUOTA feature set");
++			goto failed_mount4;
++		}
++	}
++#else
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA) &&
++	    !(sb->s_flags & MS_RDONLY))
++		ext4_msg(sb, KERN_WARNING, "Mounting a filesystem with the "
++		         "QUOTA feature set whereas the kernel does not "
++			 "support quota, e2fsck will be required to fix usage "
++			 "information");
++
++#endif  /* CONFIG_QUOTA */
++
++	ext4_msg(sb, KERN_INFO, "mounted filesystem with%s. quota=%s. "
++		 "Opts: %s%s", descr, sb_any_quota_loaded(sb) ? "on" : "off",
++		 sbi->s_es->s_mount_opts,
+ 		 *sbi->s_es->s_mount_opts ? "; " : "");
+ 
+ 	lock_kernel();
+@@ -3981,6 +4036,12 @@ static int ext4_commit_super(struct supe
+ 					&EXT4_SB(sb)->s_freeblocks_counter));
+ 	es->s_free_inodes_count = cpu_to_le32(percpu_counter_sum_positive(
+ 					&EXT4_SB(sb)->s_freeinodes_counter));
++#ifdef CONFIG_QUOTA
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) {
++		es->s_usr_quota_inum = EXT4_SB(sb)->s_qf_inums[USRQUOTA];
++		es->s_grp_quota_inum = EXT4_SB(sb)->s_qf_inums[GRPQUOTA];
++	}
++#endif
+ 	sb->s_dirt = 0;
+ 	BUFFER_TRACE(sbh, "marking dirty");
+ 	mark_buffer_dirty(sbh);
+@@ -4531,6 +4592,22 @@ static int ext4_quota_on(struct super_bl
+ 	int err;
+ 	struct path path;
+ 
++	/* When QUOTA feature is set, quota on enables enforcement, accounting
++	 * being already enabled at mount time */
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA)) {
++		struct inode *qf_inode;
++
++	        if (!EXT4_SB(sb)->s_qf_inums[type])
++			return -EINVAL;
++		qf_inode = ext4_iget(sb, EXT4_SB(sb)->s_qf_inums[type]);
++		if (IS_ERR(qf_inode))
++			return PTR_ERR(qf_inode);
++		err = vfs_quota_enable(qf_inode, type, QFMT_VFS_V1,
++		                       DQUOT_LIMITS_ENABLED);
++		iput(qf_inode);
++		return err;
++	}
++
+ 	if (!test_opt(sb, QUOTA))
+ 		return -EINVAL;
+ 	/* When remounting, no checks are needed and in fact, name is NULL */
+@@ -4630,9 +4707,114 @@ static int ext4_quota_off(struct super_b
+ 		iput(inode);
+ 	}
+ 
++	/* When QUOTA feature is set, quota off just disables enforcement but
++	 * leaves accounting on */
++	if (EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA))
++		return vfs_quota_disable(sb, type, DQUOT_LIMITS_ENABLED);
++
+ 	return vfs_quota_off(sb, type, remount);
+ }
+ 
++/*
++ * New quota_on function that is used to turn accounting on when QUOTA
++ * feature is set.
++ */
++static int ext4_acct_on(struct super_block *sb)
++{
++	struct inode *qf_inode[MAXQUOTAS];
++	int           rc;
++
++	if (!EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA) ||
++	    !EXT4_SB(sb)->s_qf_inums[USRQUOTA] ||
++	    !EXT4_SB(sb)->s_qf_inums[GRPQUOTA])
++		return -EINVAL;
++
++	qf_inode[USRQUOTA] = ext4_iget(sb, EXT4_SB(sb)->s_qf_inums[USRQUOTA]);
++	if (IS_ERR(qf_inode[USRQUOTA])) {
++		EXT4_SB(sb)->s_qf_inums[USRQUOTA] = 0;
++		return PTR_ERR(qf_inode[USRQUOTA]);
++	}
++	qf_inode[GRPQUOTA] = ext4_iget(sb, EXT4_SB(sb)->s_qf_inums[GRPQUOTA]);
++	if (IS_ERR(qf_inode[GRPQUOTA])) {
++		iput(qf_inode[USRQUOTA]);
++		EXT4_SB(sb)->s_qf_inums[GRPQUOTA] = 0;
++		return PTR_ERR(qf_inode[GRPQUOTA]);
++	}
++
++	/*
++	 * When we journal data on quota file, we have to flush journal to see
++	 * all updates to the file when we bypass pagecache...
++	 */
++	if (EXT4_SB(sb)->s_journal) {
++		/*
++		 * We don't need to lock updates but journal_flush() could
++		 * otherwise be livelocked...
++		 */
++		jbd2_journal_lock_updates(EXT4_SB(sb)->s_journal);
++		rc = jbd2_journal_flush(EXT4_SB(sb)->s_journal);
++		jbd2_journal_unlock_updates(EXT4_SB(sb)->s_journal);
++		if (rc) {
++			iput(qf_inode[USRQUOTA]);
++			iput(qf_inode[GRPQUOTA]);
++			return rc;
++		}
++	}
++
++	/* only enable quota accounting by default */
++	rc = vfs_quota_enable(qf_inode[USRQUOTA], USRQUOTA, QFMT_VFS_V1,
++		              DQUOT_USAGE_ENABLED);
++	iput(qf_inode[USRQUOTA]);
++	if (rc) {
++		iput(qf_inode[GRPQUOTA]);
++		return rc;
++	}
++	rc = vfs_quota_enable(qf_inode[GRPQUOTA], GRPQUOTA, QFMT_VFS_V1,
++			      DQUOT_USAGE_ENABLED);
++	iput(qf_inode[GRPQUOTA]);
++	return rc;
++}
++
++/*
++ * New quota_on function that is used to turn off accounting when QUOTA feature
++ * is set.
++ */
++static int ext4_acct_off(struct super_block *sb)
++{
++	int type, rc = 0;
++
++	if (!EXT4_HAS_RO_COMPAT_FEATURE(sb, EXT4_FEATURE_RO_COMPAT_QUOTA))
++		return -EINVAL;
++
++	for (type = 0; type < MAXQUOTAS; type++) {
++		struct inode *inode = sb_dqopt(sb)->files[type];
++		handle_t     *handle;
++
++		if (!inode)
++			continue;
++		/* Update modification times of quota files when userspace can
++		 * start looking at them */
++		handle = ext4_journal_start(inode, 1);
++		if (IS_ERR(handle))
++			goto out;
++
++		inode->i_mtime = inode->i_ctime = CURRENT_TIME;
++		ext4_mark_inode_dirty(handle, inode);
++		ext4_journal_stop(handle);
++	}
++
++out:
++	for (type = 0; type < MAXQUOTAS; type++) {
++		int ret;
++		ret = vfs_quota_disable(sb, type,
++				    DQUOT_USAGE_ENABLED | DQUOT_LIMITS_ENABLED);
++		if (!rc && ret)
++			rc = ret;
++	}
++	return rc;
++}
++
++
++
+ /* Read data from quotafile - avoid pagecache and such because we cannot afford
+  * acquiring the locks... As quota files are never truncated and quota code
+  * itself serializes the operations (and noone else should touch the files)

microsoft/amlFilesystem-lustre

commit 81b1dd220e468e1af376574097c933f4513a71e8

LU-17000 osd-ldiskfs: widen quota block offsets before shifting
The quota tree walker turns a block number into a file offset with
"blk << LUSTRE_DQBLKSIZE_BITS".  blk is an unsigned int read straight
off disk with le32_to_cpu(), so the shift is evaluated in 32 bits and
wraps for any block at or past 4 GiB into the quota file, and the
truncated value is then widened to the loff_t it is stored in.

Cast blk to loff_t before shifting.  Only the quota_read() offset in
quota_read_blk() has an observable effect today: a wrapped offset there
reads a different, live block of the quota file.  The other two sites
are corrected for consistency but cannot be seen - osd_it_quota::
oiq_offset has had no readers since 8fdb46e476 ("LU-6617 quota: remove
quota migration code"), and find_block_dqentry()'s return value is only
tested for its sign by osd_it_acct_get(), which the truncation cannot
change because the 32-bit product is zero-extended.

Test-Parameters: trivial
Fixes: 57df6353ca59 ("LU-1182 ldiskfs-osd: space accounting support")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Iebc7b7decf179362dfd0d15b93955fc89ee6dca4

lustre/osd-ldiskfs/osd_quota_fmt.c
@@ -61,7 +61,7 @@ static ssize_t quota_read_blk(const struct lu_env *env,
 		 "type=%d\n", type);
 
 	ret = sb->s_op->quota_read(sb, type, buf, LUSTRE_DQBLKSIZE,
-				   blk << LUSTRE_DQBLKSIZE_BITS);
+				   (loff_t)blk << LUSTRE_DQBLKSIZE_BITS);
 
 	/* Reading past EOF just returns a block of zeros */
 	if (ret == -EBADR)
@@ -126,7 +126,7 @@ static loff_t find_block_dqentry(const struct lu_env *env,
 		ret = 0;
 		GOTO(out_buf, ret);
 	} else {
-		ret = (blk << LUSTRE_DQBLKSIZE_BITS) +
+		ret = ((loff_t)blk << LUSTRE_DQBLKSIZE_BITS) +
 		      sizeof(struct lustre_disk_dqdbheader) + i * dqblk_sz;
 
 		if (it) {
@@ -259,7 +259,7 @@ int walk_block_dqentry(const struct lu_env *env, struct osd_object *obj,
 
 		it->oiq_blk[LUSTRE_DQTREEDEPTH] = blk;
 		it->oiq_id = le32_to_cpu(ddquot[i].dqb_id);
-		it->oiq_offset = (blk << LUSTRE_DQBLKSIZE_BITS) +
+		it->oiq_offset = ((loff_t)blk << LUSTRE_DQBLKSIZE_BITS) +
 				  sizeof(struct lustre_disk_dqdbheader) +
 				  i * dqblk_sz;
 		it->oiq_index[LUSTRE_DQTREEDEPTH] = i;