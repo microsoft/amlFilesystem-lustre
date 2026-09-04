lustre/lustre-release

commit 1fd63fcb045c91aa259068d3077b481652da0270

LU-12090 utils: lfs rmfid
a new RPC_REINT_RMFID has been introduced by the patch.
it's supposed to be used with corresponding llapi_rmfid()
to unlink a batch of MDS files by their FIDs. the caller
has to have permission to modify parent dir(s) and the objects
themselves.

Change-Id: I50421d85babc74d448842acea489321a5d40052d
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34449
Reviewed-by: Li Xi <lixi@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/dir.c
@@ -1229,6 +1229,54 @@ out:
         RETURN(rc);
 }
 
+int ll_rmfid(struct file *file, void __user *arg)
+{
+	const struct fid_array __user *ufa = arg;
+	struct fid_array *lfa = NULL;
+	size_t size;
+	unsigned nr;
+	int i, rc, *rcs = NULL;
+	ENTRY;
+
+	if (!cfs_capable(CFS_CAP_DAC_READ_SEARCH) &&
+	    !(ll_i2sbi(file_inode(file))->ll_flags & LL_SBI_USER_FID2PATH))
+		RETURN(-EPERM);
+	/* Only need to get the buflen */
+	if (get_user(nr, &ufa->fa_nr))
+		RETURN(-EFAULT);
+	/* DoS protection */
+	if (nr > OBD_MAX_FIDS_IN_ARRAY)
+		RETURN(-E2BIG);
+
+	size = offsetof(struct fid_array, fa_fids[nr]);
+	OBD_ALLOC(lfa, size);
+	if (!lfa)
+		RETURN(-ENOMEM);
+	OBD_ALLOC(rcs, sizeof(int) * nr);
+	if (!rcs)
+		GOTO(free_lfa, rc = -ENOMEM);
+
+	if (copy_from_user(lfa, arg, size))
+		GOTO(free_rcs, rc = -EFAULT);
+
+	/* Call mdc_iocontrol */
+	rc = md_rmfid(ll_i2mdexp(file_inode(file)), lfa, rcs, NULL);
+	if (!rc) {
+		for (i = 0; i < nr; i++)
+			if (rcs[i])
+				lfa->fa_fids[i].f_ver = rcs[i];
+		if (copy_to_user(arg, lfa, size))
+			rc = -EFAULT;
+	}
+
+free_rcs:
+	OBD_FREE(rcs, sizeof(int) * nr);
+free_lfa:
+	OBD_FREE(lfa, size);
+
+	RETURN(rc);
+}
+
 /* This function tries to get a single name component,
  * to send to the server. No actual path traversal involved,
  * so we limit to NAME_MAX */
@@ -1603,6 +1651,8 @@ out_rmdir:
                         ll_putname(filename);
 		RETURN(rc);
 	}
+	case LL_IOC_RMFID:
+		RETURN(ll_rmfid(file, (void __user *)arg));
 	case LL_IOC_LOV_SWAP_LAYOUTS:
 		RETURN(-EPERM);
 	case IOC_OBD_STATFS:

lustre/doc/lfs-rmfid.1
@@ -1,0 +1,25 @@
+.TH LFS-RMFID 1 2017-07-25 "Lustre" "Lustre Utilities"
+.SH NAME
+lfs rmfid \- remove file by FID
+.SH SYNOPSIS
+.B lfs rmfid
+<\fIdirectory\fR> <\fIFID1\fR> [<\fIFID2\fR>...]
+.SH DESCRIPTION
+This command removes file(s) specified by the \fIFID\fR.
+.br
+In some cases it is more optimal to remove files by their FIDs.
+The \fBdirectory\fR specifies a mountpoint of Lustre filesystem where given
+\fBFIDs\fR are stored. The mountpoint should be mounted with
+\fBuser_fid2path\fR mount option and the caller has to have a right to
+modify the given files. rmfid will be trying to remove all hardlinks of the
+given file. FIDs can be wrapped with square brackets.
+.SH EXAMPLES
+.TP
+.B lfs rmfid /mnt/lustre [0x200000400:0x1:0x0] [0x200000402:0x20:0x0]
+Remove files with FIDs [0x200000400:0x1:0x0] [0x200000402:0x20:0x0]
+.SH AUTHOR
+The \fBlfs rmfid\fR command is part of the Lustre filesystem.
+.SH SEE ALSO
+.BR lfs (1),
+.BR lfs-path2fid (1),
+.BR lfs-fid2path (1)

lustre/doc/llapi_rmfid.3
@@ -1,0 +1,50 @@
+.TH llapi_rmfid 3 "2014 Oct 13" "Lustre User API"
+.SH NAME
+llapi_rmfid \- Remove files by their FIDs in Lustre.
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_rmfid(const char *" path ", struct fid_array *" fa ");
+
+.sp
+.fi
+.SH DESCRIPTION
+.PP
+.BR llapi_rmfid()
+tries to remove
+.I fa->fa_nr
+Lustre files by FIDs stored in
+.I fa->fa_fids
+All file's hardlinks are subject to removal. This functionality is available
+only for root or regular users on filesystems mounted with
+.I user_fid2path
+mount option to delete files that they own and are in a directory in which
+they have write permission.
+
+.SH RETURN VALUES
+.LP
+.B llapi_rmfid()
+return 0 on success or a negative errno value on failure. Result for each file
+is stored in the corresponding
+.I fa->fa_fid[N].f_ver
+.SH ERRORS
+.TP 15
+.TP
+.SM -ENOENT
+.I file
+does not exist.
+.TP
+.SM -EBUSY
+file is open and can't be removed
+.TP
+.SM -EPERM
+The file cannot be open by user or CAP_DAC_READ_SEARCH is not granted.
+.TP
+.SM -EINVAL
+Invalid FID is passed
+.TP
+.SM -ENOMEM
+Not enough memory to process the request
+.SH "SEE ALSO"
+.BR lustreapi (7)

lustre/include/lustre/lustreapi.h
@@ -409,6 +409,7 @@ int llapi_path2parent(const char *path, unsigned int linkno,
 		      struct lu_fid *parent_fid, char *name, size_t name_size);
 int llapi_fd2parent(int fd, unsigned int linkno, struct lu_fid *parent_fid,
 		    char *name, size_t name_size);
+int llapi_rmfid(const char *path, struct fid_array *fa);
 int llapi_chomp_string(char *buf);
 int llapi_open_by_fid(const char *dir, const struct lu_fid *fid,
 		      int open_flags);

lustre/include/lustre_req_layout.h
@@ -179,6 +179,7 @@ extern struct req_format RQF_QUOTA_DQACQ;
 extern struct req_format RQF_MDS_SWAP_LAYOUTS;
 extern struct req_format RQF_MDS_REINT_MIGRATE;
 extern struct req_format RQF_MDS_REINT_RESYNC;
+extern struct req_format RQF_MDS_RMFID;
 /* MDS hsm formats */
 extern struct req_format RQF_MDS_HSM_STATE_GET;
 extern struct req_format RQF_MDS_HSM_STATE_SET;
@@ -256,6 +257,7 @@ extern struct req_msg_field RMF_IDX_INFO;
 extern struct req_msg_field RMF_CLOSE_DATA;
 extern struct req_msg_field RMF_FILE_SECCTX_NAME;
 extern struct req_msg_field RMF_FILE_SECCTX;
+extern struct req_msg_field RMF_FID_ARRAY;
 
 /*
  * connection handle received in MDS_CONNECT request.

lustre/include/obd.h
@@ -1156,6 +1156,8 @@ struct md_ops {
 				  struct lu_fid *fid);
 	int (*m_unpackmd)(struct obd_export *exp, struct lmv_stripe_md **plsm,
 			  const union lmv_mds_md *lmv, size_t lmv_size);
+	int (*m_rmfid)(struct obd_export *exp, struct fid_array *fa, int *rcs,
+		       struct ptlrpc_request_set *set);
 };
 
 static inline struct md_open_data *obd_mod_alloc(void)

lustre/include/obd_class.h
@@ -1783,6 +1783,18 @@ static inline int md_unpackmd(struct obd_export *exp,
 	return MDP(exp->exp_obd, unpackmd)(exp, plsm, lmm, lmm_size);
 }
 
+static inline int md_rmfid(struct obd_export *exp, struct fid_array *fa,
+			   int *rcs, struct ptlrpc_request_set *set)
+{
+	int rc;
+
+	rc = exp_check_ops(exp);
+	if (rc)
+		return rc;
+
+	return MDP(exp->exp_obd, rmfid)(exp, fa, rcs, set);
+}
+
 /* OBD Metadata Support */
 
 extern int obd_init_caches(void);

lustre/include/obd_support.h
@@ -251,6 +251,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_MDS_LOV_CREATE_RACE	 0x163
 #define OBD_FAIL_MDS_HSM_CDT_DELAY	 0x164
 #define OBD_FAIL_MDS_ORPHAN_DELETE	 0x165
+#define OBD_FAIL_MDS_RMFID_NET		 0x166
 
 /* layout lock */
 #define OBD_FAIL_MDS_NO_LL_GETATTR	 0x170

microsoft/amlFilesystem-lustre

commit b4ca119fde73531a7dbf55a7c442e2c940c5c0fe

LU-17000 llite: fix memory safety in ll_rmfid()
ll_rmfid() reads the caller's fa_nr with get_user() and sizes both
the lfa FID array and the rcs[] status array from that value, then
copy_from_user()s the header again.  lfa->fa_nr is a second read of
the same user field, and every buffer downstream is sized from it
instead of from the value the allocations used.  Return -EAGAIN if
it changed between the two reads, since that cannot happen under
normal usage.

Reject nr == 0 up front as well.  OBD_ALLOC_PTR_ARRAY(rcs, 0) hands
back ZERO_SIZE_PTR rather than NULL, so an empty array got past the
!rcs check and fell into the "nothing is visible" branch, which
returns rcs[nr - 1] and so reads far past the allocation.

That same branch leaks on a fileset mount, where lfa is reassigned
to lfa_new and lfa_flag makes free_lfa: skip it, leaving
free_lfa_new: as the only site that frees the buffer.  The branch
jumps past that label to free_rcs:, so send it to free_lfa_new:
instead.  'lfs rmfid <subdir mount> <fid outside the fileset>'
reaches it, since every FID that fails __ll_fid2path() goes to the
hidden tail of the array and leaves lfa_new->fa_nr at 0.

Test-Parameters: trivial
Fixes: 1fd63fcb045c ("LU-12090 utils: lfs rmfid")
Fixes: 9a72c073d33b ("LU-16494 fileset: check fileset for operations by fid")
Fixes: 3c3615856606 ("LU-17000 coverity: Fix Resource Leak(2)")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I2e92f41fb8f3bff69764bfab2ff2c306671d5662

lustre/llite/dir.c
@@ -1857,6 +1857,8 @@ static int ll_rmfid(struct file *file, void __user *arg)
 	/* DoS protection */
 	if (nr > OBD_MAX_FIDS_IN_ARRAY)
 		RETURN(-E2BIG);
+	if (nr == 0)
+		RETURN(0);
 
 	size = offsetof(struct fid_array, fa_fids[nr]);
 	OBD_ALLOC(lfa, size);
@@ -1868,6 +1870,9 @@ static int ll_rmfid(struct file *file, void __user *arg)
 
 	if (copy_from_user(lfa, arg, size))
 		GOTO(free_rcs, rc = -EFAULT);
+	/* fa_nr was just fetched a second time, it shouldn't change */
+	if (lfa->fa_nr != nr)
+		GOTO(free_rcs, rc = -EAGAIN);
 
 	/* In case of subdirectory mount, we need to make sure all the files
 	 * for which we want to remove FID are visible in the namespace.
@@ -1942,7 +1947,7 @@ static int ll_rmfid(struct file *file, void __user *arg)
 		lfa = lfa_new;
 	}
 	if (lfa->fa_nr == 0)
-		GOTO(free_rcs, rc = rcs[nr - 1]);
+		GOTO(free_lfa_new, rc = rcs[nr - 1]);
 
 	/* Call mdc_iocontrol */
 	rc = md_rmfid(ll_i2mdexp(file_inode(file)), lfa, rcs, NULL);