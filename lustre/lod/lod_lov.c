lustre/lustre-release

commit 8fa3532b1ee887be378adbf9432707b2d8a2d814

LU-17204 lod: don't panic on short LOVEA
when we request LOVEA and find the existing buffer is not enough,
we ask for LOVEA's size and reallocate the buffer. but LOVEA can
shrink in parallel (e.g. new default striping), so our expectation
that the size must be greater than size of the existing buffer is
not correct. replace the corresponding assertion with a simple
repeat + extra check for a livelock.

Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I26ad5091228bf78858f8538478dbcbdb235cddf4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52727
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/lod/lod_lov.c
@@ -458,7 +458,6 @@ int lod_ea_store_resize(struct lod_thread_info *info, size_t size)
 
 	if (info->lti_ea_store) {
 		LASSERT(info->lti_ea_store_size);
-		LASSERT(info->lti_ea_store_size < round);
 		CDEBUG(D_INFO, "EA store size %d is not enough, need %d\n",
 		       info->lti_ea_store_size, round);
 		OBD_FREE_LARGE(info->lti_ea_store, info->lti_ea_store_size);
@@ -1042,7 +1041,7 @@ int lod_get_ea(const struct lu_env *env, struct lod_object *lo,
 {
 	struct lod_thread_info	*info = lod_env_info(env);
 	struct dt_object	*next = dt_object_child(&lo->ldo_obj);
-	int			rc;
+	int rc, count = 0;
 	ENTRY;
 
 	LASSERT(info);
@@ -1070,6 +1069,11 @@ repeat:
 			RETURN(rc);
 
 		LASSERT(rc > 0);
+		if (rc <= info->lti_ea_store_size) {
+			/* sometimes LOVEA can shrink in parallel */
+			LASSERT(count++ < 10);
+			goto repeat;
+		}
 		rc = lod_ea_store_resize(info, rc);
 		if (rc)
 			RETURN(rc);

microsoft/amlFilesystem-lustre

commit cb9f8c01515f46d1661048f1a74867e3697a4972

LU-20559 lod: bound the lod_get_ea() retry for real
lod_get_ea() bounds its retry with LASSERT(count++ < 10), so with
--disable-libcfs-assert the counter never advances and the retry
after a racing LOVEA shrink is unbounded.

Make the bound real C. This changes the return contract: eleven
consecutive shrinks used to LBUG() the MDS and now return -EAGAIN,
which lod_striping_load() passes through to mdd_xattr_set(),
mdd_layout_change() and mdd_migrate_object(), all of which already
restart on it. The kernel doc is corrected to match what the
function has always returned on success, a positive EA size.

Fixes: 8fa3532b1ee8 ("LU-17204 lod: don't panic on short LOVEA")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Ia08a0baee7c4cf27a2c53bea000b6b4a81adab63

lustre/lod/lod_lov.c
@@ -1049,8 +1049,9 @@ out:
  * EA. The buffer is reallocated if the value doesn't fit.
  *
  * Return:
- * * %0 if EA is fetched successfully
- * * %0 if EA is empty
+ * * %positive size of the EA that was fetched
+ * * %0 if EA is empty or absent
+ * * %-EAGAIN if the EA kept shrinking under a racing writer
  * * %negative error number on failure
  */
 int lod_get_ea(const struct lu_env *env, struct lod_object *lo,
@@ -1058,6 +1059,7 @@ int lod_get_ea(const struct lu_env *env, struct lod_object *lo,
 {
 	struct lod_thread_info	*info = lod_env_info(env);
 	struct dt_object	*next = dt_object_child(&lo->ldo_obj);
+	struct lod_device	*lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
 	int rc, count = 0;
 	ENTRY;
 
@@ -1086,9 +1088,17 @@ repeat:
 
 		LASSERT(rc > 0);
 		if (rc <= info->lti_ea_store_size) {
+			int size = rc;
+
 			/* sometimes LOVEA can shrink in parallel */
-			LASSERT(count++ < 10);
-			goto repeat;
+			if (count++ < 10)
+				goto repeat;
+
+			rc = -EAGAIN;
+			CERROR("%s: "DFID" %s kept shrinking to %d: rc = %d\n",
+			       lod2obd(lod)->obd_name, PFID(lod_object_fid(lo)),
+			       name, size, rc);
+			RETURN(rc);
 		}
 		rc = lod_ea_store_resize(info, rc);
 		if (rc)