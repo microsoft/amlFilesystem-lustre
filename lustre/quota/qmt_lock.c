lustre/lustre-release

commit c606081b062af5541631d9ac106400b62c36582e

LU-19537 quota: fix race of lqe_glbl_lgd and qmt_sarr
When the "qmt_sarr" is updated by processing the new connection
from the QSD, the "lqe_glbl_data" allocated in qmt_lvbo_init
could still use the old slv_nr of the "qmt_sarr", which could
cause the slv_nr is less than the value stored in "qmt_sarr" when
qmt_seed_glbe_all is called and causes the ASSERTION.

Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: I25e7e34e93bc51227c8731a9bb77491a1839f10f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62144
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Vitaliy Kuznetsov <vkuznetsov@ddn.com>

lustre/quota/qmt_lock.c
@@ -12,6 +12,7 @@
 
 #define DEBUG_SUBSYSTEM S_LQUOTA
 
+#include <linux/delay.h>
 #include <linux/kthread.h>
 #include <linux/workqueue.h>
 
@@ -197,6 +198,7 @@ int qmt_lvbo_init(struct lu_device *ld, struct ldlm_resource *res)
 		struct qmt_pool_info	*pool;
 		struct lquota_entry	*lqe;
 		struct lqe_glbl_data	*lgd;
+		int wait = 0;
 
 		pool = qmt_pool_lookup_glb(env, qmt, pool_type);
 		if (IS_ERR(pool))
@@ -210,21 +212,49 @@ int qmt_lvbo_init(struct lu_device *ld, struct ldlm_resource *res)
 			GOTO(out, rc = PTR_ERR(lqe));
 		}
 
+again:
+		mutex_lock(&lqe->lqe_glbl_data_lock);
+		/* Is the old lqe_glbl_data still waiting to be freed in
+		 * qmt_lvbo_free_wq?
+		 */
+		if (lqe->lqe_glbl_data) {
+			mutex_unlock(&lqe->lqe_glbl_data_lock);
+
+			wait++;
+			/* wait one second */
+			if (wait < 1000) {
+				msleep_interruptible(1);
+				goto again;
+			}
+
+			LQUOTA_ERROR(lqe, "the lvb is held by qmt_wq: %p\n",
+				     res->lr_lvb_data);
+			lqe_putref(lqe);
+			GOTO(out_put_qpi, rc = -EBUSY);
+		}
+
 		/* TODO: need something like qmt_extend_lqe_gd that has
-		 * to be calledeach time when qpi_slv_nr is incremented */
+		 * to be called each time when qpi_slv_nr is incremented
+		 */
 		lgd = qmt_alloc_lqe_gd(pool, qtype);
 		if (!lgd) {
+			mutex_unlock(&lqe->lqe_glbl_data_lock);
 			lqe_putref(lqe);
-			qpi_putref(env, pool);
-			GOTO(out, rc = -ENOMEM);
+			GOTO(out_put_qpi, rc = -ENOMEM);
 		}
 
 		qmt_setup_lqe_gd(env, qmt, lqe, lgd, pool_type);
+		lqe->lqe_glbl_data = lgd;
+		mutex_unlock(&lqe->lqe_glbl_data_lock);
+
+		qmt_id_lock_notify(qmt, lqe);
 
 		/* store reference to lqe in lr_lvb_data */
 		res->lr_lvb_data = lqe;
-		qpi_putref(env, pool);
 		LQUOTA_DEBUG(lqe, "initialized res lvb");
+
+out_put_qpi:
+		qpi_putref(env, pool);
 	} else {
 		struct dt_object	*obj;
 

lustre/quota/qmt_entry.c
@@ -1311,6 +1311,8 @@ void qmt_setup_lqe_gd(const struct lu_env *env, struct qmt_device *qmt,
 	bool edquot;
 	int i;
 
+	LASSERT(mutex_is_locked(&lqe->lqe_glbl_data_lock));
+
 	qunit = lqe->lqe_qunit;
 	edquot = lqe->lqe_edquot;
 
@@ -1331,17 +1333,5 @@ void qmt_setup_lqe_gd(const struct lu_env *env, struct qmt_device *qmt,
 	qmt_pool_lqes_lookup_spec(env, qmt, pool_type,
 				  lqe_qtype(lqe), &lqe->lqe_id);
 	qmt_seed_glbe(env, lgd, false);
-
-	mutex_lock(&lqe->lqe_glbl_data_lock);
-	if (lqe->lqe_glbl_data == NULL) {
-		lqe->lqe_glbl_data = lgd;
-		lgd = NULL;
-	}
-	mutex_unlock(&lqe->lqe_glbl_data_lock);
-	if (lgd)
-		qmt_free_lqe_gd(lgd);
-
-	qmt_id_lock_notify(qmt, lqe);
-
 	qti_lqes_fini(env);
 }

lustre/quota/qmt_pool.c
@@ -691,58 +691,55 @@ static int qmt_lgd_extend_cb(struct cfs_hash *hs, struct cfs_hash_bd *bd,
 	rc = 0;
 
 	CDEBUG(D_QUOTA, "lgd %px\n", lqe->lqe_glbl_data);
+	old_lqeg_arr = NULL;
+	mutex_lock(&lqe->lqe_glbl_data_lock);
 	if (lqe->lqe_glbl_data) {
 		struct lqe_glbl_data *lgd;
+		struct qmt_pool_info *qpi = lqe2qpi(lqe);
+		int sarr_cnt = qmt_sarr_count(qpi);
 
-		old_lqeg_arr = NULL;
-		mutex_lock(&lqe->lqe_glbl_data_lock);
-		if (lqe->lqe_glbl_data) {
-			struct qmt_pool_info *qpi = lqe2qpi(lqe);
-			int sarr_cnt = qmt_sarr_count(qpi);
+		lgd = lqe->lqe_glbl_data;
+		if (lgd->lqeg_num_alloc < sarr_cnt) {
+			LASSERT((lgd->lqeg_num_alloc + 1) == sarr_cnt);
 
-			lgd = lqe->lqe_glbl_data;
-			if (lgd->lqeg_num_alloc < sarr_cnt) {
-				LASSERT((lgd->lqeg_num_alloc + 1) == sarr_cnt);
-
-				OBD_ALLOC(lqeg_arr,
-					  sizeof(struct lqe_glbl_entry) *
-					  (lgd->lqeg_num_alloc + 16));
-				if (lqeg_arr) {
-					memcpy(lqeg_arr, lgd->lqeg_arr,
-					       sizeof(struct lqe_glbl_entry) *
-					       (lgd->lqeg_num_alloc));
-					old_lqeg_arr = lgd->lqeg_arr;
-					old_num = lgd->lqeg_num_alloc;
-					lgd->lqeg_arr = lqeg_arr;
-					lgd->lqeg_num_alloc += 16;
-					CDEBUG(D_QUOTA,
-					       "extend lqeg_arr:%px from %d to %d\n",
-					       lgd, old_num,
-					       lgd->lqeg_num_alloc);
-				} else {
-					CERROR("%s: cannot allocate new lqeg_arr: rc = %d\n",
-					       qpi->qpi_qmt->qmt_svname,
-					       -ENOMEM);
-					GOTO(out, rc = -ENOMEM);
-				}
+			OBD_ALLOC(lqeg_arr,
+				  sizeof(struct lqe_glbl_entry) *
+				  (lgd->lqeg_num_alloc + 16));
+			if (lqeg_arr) {
+				memcpy(lqeg_arr, lgd->lqeg_arr,
+				       sizeof(struct lqe_glbl_entry) *
+				       (lgd->lqeg_num_alloc));
+				old_lqeg_arr = lgd->lqeg_arr;
+				old_num = lgd->lqeg_num_alloc;
+				lgd->lqeg_arr = lqeg_arr;
+				lgd->lqeg_num_alloc += 16;
+				CDEBUG(D_QUOTA,
+				       "extend lqeg_arr:%px from %d to %d\n",
+				       lgd, old_num,
+				       lgd->lqeg_num_alloc);
+			} else {
+				CERROR("%s: cannot allocate new lqeg_arr: rc = %d\n",
+				       qpi->qpi_qmt->qmt_svname,
+				       -ENOMEM);
+				GOTO(out, rc = -ENOMEM);
 			}
-			lgd->lqeg_arr[lgd->lqeg_num_used].lge_idx =
-				qmt_sarr_get_idx(qpi, sarr_cnt - 1);
-			lgd->lqeg_arr[lgd->lqeg_num_used].lge_edquot =
-				lqe->lqe_edquot;
-			lgd->lqeg_arr[lgd->lqeg_num_used].lge_qunit =
-				lqe->lqe_qunit;
-			lgd->lqeg_arr[lgd->lqeg_num_used].lge_edquot_nu = 0;
-			lgd->lqeg_arr[lgd->lqeg_num_used].lge_qunit_nu = 0;
-			LQUOTA_DEBUG(lqe, "add tgt idx:%d used %d alloc %d\n",
-				     lgd->lqeg_arr[lgd->lqeg_num_used].lge_idx,
-				     lgd->lqeg_num_used, lgd->lqeg_num_alloc);
-			lgd->lqeg_num_used++;
 		}
-out:
-		mutex_unlock(&lqe->lqe_glbl_data_lock);
-		OBD_FREE(old_lqeg_arr, old_num * sizeof(struct lqe_glbl_entry));
+		lgd->lqeg_arr[lgd->lqeg_num_used].lge_idx =
+			qmt_sarr_get_idx(qpi, sarr_cnt - 1);
+		lgd->lqeg_arr[lgd->lqeg_num_used].lge_edquot =
+			lqe->lqe_edquot;
+		lgd->lqeg_arr[lgd->lqeg_num_used].lge_qunit =
+			lqe->lqe_qunit;
+		lgd->lqeg_arr[lgd->lqeg_num_used].lge_edquot_nu = 0;
+		lgd->lqeg_arr[lgd->lqeg_num_used].lge_qunit_nu = 0;
+		LQUOTA_DEBUG(lqe, "add tgt idx:%d used %d alloc %d\n",
+			     lgd->lqeg_arr[lgd->lqeg_num_used].lge_idx,
+			     lgd->lqeg_num_used, lgd->lqeg_num_alloc);
+		lgd->lqeg_num_used++;
 	}
+out:
+	mutex_unlock(&lqe->lqe_glbl_data_lock);
+	OBD_FREE(old_lqeg_arr, old_num * sizeof(struct lqe_glbl_entry));
 
 	return rc;
 }

microsoft/amlFilesystem-lustre

commit a3002471ebfc6591e05b16275380c441c5c6c018

LU-19537 quota: Fix error print
When we cannot get the lvb because it's held by something
else, print lqe_glbl_data because that's the important bit.
The lr_lvb_data is always 0 at that point.

Fixes: c606081b062a ("LU-19537 quota: fix race of lqe_glbl_lgd and qmt_sarr")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: Iedc4f629afa04948060f089fcc6952204dcedb5b

lustre/quota/qmt_lock.c
@@ -220,6 +220,7 @@ again:
 		 * qmt_lvbo_free_wq?
 		 */
 		if (lqe->lqe_glbl_data) {
+			lgd = lqe->lqe_glbl_data;
 			mutex_unlock(&lqe->lqe_glbl_data_lock);
 
 			wait++;
@@ -229,8 +230,7 @@ again:
 				goto again;
 			}
 
-			LQUOTA_ERROR(lqe, "the lvb is held by qmt_wq: %p\n",
-				     res->lr_lvb_data);
+			LQUOTA_ERROR(lqe, "lgd held by qmt_wq: %px\n", lgd);
 			lqe_putref(lqe);
 			GOTO(out_put_qpi, rc = -EBUSY);
 		}