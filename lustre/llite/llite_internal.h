lustre/lustre-release

commit 96a5daa0c08d7b42ec368080a2a7f0dfb110ef98

LU-163 MDS returns 32/64-bit dir name hash according to client type
1) liblustre client, 1.8.5 or older client do not support 64-bit dir name hash.
2) register Lustre version 'get_name()' for NFS processing.
3) 'll_dir_seek()' processes name hash-based dir seek operation.
4) re-define "DIR_END_OFF" as "0x7fffffffffffffffULL" to always return positive
   dir name hash for successful seek.
5) small cleanup for 'll_dops_init()' to drop unnecessary statahead process.

Signed-off-by: nasf <yong.fan@whamcloud.com>
Change-Id: I0e3177c98c6e8814c39551ab54edfa3f861cbc8c
Reviewed-on: http://review.whamcloud.com/432
Tested-by: Hudson
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_internal.h
@@ -78,6 +78,12 @@ extern struct file_operations ll_pgcache_seq_fops;
 /* remote client permission cache */
 #define REMOTE_PERM_HASHSIZE 16
 
+struct ll_getname_data {
+        char            *lgd_name;      /* points to a buffer with NAME_MAX+1 size */
+        struct lu_fid    lgd_fid;       /* target fid we are looking for */
+        int              lgd_found;     /* inode matched? */
+};
+
 /* llite setxid/access permission for user on remote client */
 struct ll_remote_perm {
         cfs_hlist_node_t        lrp_list;
@@ -581,6 +587,7 @@ extern struct file_operations ll_dir_operations;
 extern struct inode_operations ll_dir_inode_operations;
 struct page *ll_get_dir_page(struct file *filp, struct inode *dir, __u64 hash,
                              int exact, struct ll_dir_chain *chain);
+int ll_readdir(struct file *filp, void *cookie, filldir_t filldir);
 
 int ll_get_mdt_idx(struct inode *inode);
 /* llite/namei.c */
@@ -679,7 +686,7 @@ int ll_fid2path(struct obd_export *exp, void *arg);
 /**
  * protect race ll_find_aliases vs ll_revalidate_it vs ll_unhash_aliases
  */
-int ll_dops_init(struct dentry *de, int block);
+int ll_dops_init(struct dentry *de, int block, int init_sa);
 extern cfs_spinlock_t ll_lookup_lock;
 extern struct dentry_operations ll_d_ops;
 void ll_intent_drop_lock(struct lookup_intent *);

lustre/llite/llite_nfs.c
@@ -73,7 +73,7 @@ static struct inode *search_inode_for_lustre(struct super_block *sb,
         struct ptlrpc_request *req = NULL;
         struct inode          *inode = NULL;
         int                   eadatalen = 0;
-        unsigned long         hash = (unsigned long) cl_fid_build_ino(fid);
+        unsigned long         hash = (unsigned long) cl_fid_build_ino(fid, 0);
         struct  md_op_data    *op_data;
         int                   rc;
         ENTRY;
@@ -141,7 +141,7 @@ static struct dentry *ll_iget_for_nfs(struct super_block *sb,
         if (!result)
                 RETURN(ERR_PTR(-ENOMEM));
 
-        ll_dops_init(result, 1);
+        ll_dops_init(result, 1, 0);
 
         RETURN(result);
 }
@@ -182,6 +182,63 @@ static int ll_encode_fh(struct dentry *de, __u32 *fh, int *plen,
         *plen = sizeof(struct lustre_nfs_fid)/4;
 
         RETURN(LUSTRE_NFS_FID);
+}
+
+static int ll_nfs_get_name_filldir(void *cookie, const char *name, int namelen,
+                                   loff_t hash, u64 ino, unsigned type)
+{
+        /* It is hack to access lde_fid for comparison with lgd_fid.
+         * So the input 'name' must be part of the 'lu_dirent'. */
+        struct lu_dirent *lde = container_of0(name, struct lu_dirent, lde_name);
+        struct ll_getname_data *lgd = cookie;
+        struct lu_fid fid;
+
+        fid_le_to_cpu(&fid, &lde->lde_fid);
+        if (lu_fid_eq(&fid, &lgd->lgd_fid)) {
+                memcpy(lgd->lgd_name, name, namelen);
+                lgd->lgd_name[namelen] = 0;
+                lgd->lgd_found = 1;
+        }
+        return lgd->lgd_found;
+}
+
+static int ll_get_name(struct dentry *dentry, char *name,
+                       struct dentry *child)
+{
+        struct inode *dir = dentry->d_inode;
+        struct file *filp;
+        struct ll_getname_data lgd;
+        int rc;
+        ENTRY;
+
+        if (!dir || !S_ISDIR(dir->i_mode))
+                GOTO(out, rc = -ENOTDIR);
+
+        if (!dir->i_fop)
+                GOTO(out, rc = -EINVAL);
+
+        filp = ll_dentry_open(dget(dentry), NULL, O_RDONLY, current_cred());
+        if (IS_ERR(filp))
+                GOTO(out, rc = PTR_ERR(filp));
+
+        if (!filp->f_op->readdir)
+                GOTO(out_close, rc = -EINVAL);
+
+        lgd.lgd_name = name;
+        lgd.lgd_fid = ll_i2info(child->d_inode)->lli_fid;
+        lgd.lgd_found = 0;
+
+        cfs_mutex_lock(&dir->i_mutex);
+        rc = ll_readdir(filp, &lgd, ll_nfs_get_name_filldir);
+        cfs_mutex_unlock(&dir->i_mutex);
+        if (!rc && !lgd.lgd_found)
+                rc = -ENOENT;
+        EXIT;
+
+out_close:
+        fput(filp);
+out:
+        return rc;
 }
 
 #ifdef HAVE_FH_TO_DENTRY
@@ -290,6 +347,7 @@ static struct dentry *ll_get_parent(struct dentry *dchild)
 struct export_operations lustre_export_operations = {
        .get_parent = ll_get_parent,
        .encode_fh  = ll_encode_fh,
+       .get_name   = ll_get_name,
 #ifdef HAVE_FH_TO_DENTRY
         .fh_to_dentry = ll_fh_to_dentry,
         .fh_to_parent = ll_fh_to_parent,

ldiskfs/kernel_patches/patches/ext3-export-64bit-name-hash.patch
@@ -1,8 +1,20 @@
-Index: linux-2.6.18-194.17.1-ext3/fs/ext3/dir.c
+Index: linux-stage/fs/ext3/dir.c
 ===================================================================
---- linux-2.6.18-194.17.1-ext3.orig/fs/ext3/dir.c	2010-11-30 22:46:09.000000000 +0300
-+++ linux-2.6.18-194.17.1-ext3/fs/ext3/dir.c	2010-12-16 00:10:12.000000000 +0300
-@@ -240,19 +240,34 @@ out:
+--- linux-stage.orig/fs/ext3/dir.c	2011-04-19 01:39:47.000000000 +0800
++++ linux-stage/fs/ext3/dir.c	2011-04-19 01:44:19.000000000 +0800
+@@ -237,22 +237,50 @@
+ }
+
+ #ifdef CONFIG_EXT3_INDEX
++static inline int is_32bit_api(void)
++{
++#ifdef HAVE_IS_COMPAT_TASK
++        return is_compat_task();
++#else
++        return (BITS_PER_LONG == 32);
++#endif
++}
++
  /*
   * These functions convert from the major/minor hash to an f_pos
   * value.
@@ -16,128 +28,108 @@ Index: linux-2.6.18-194.17.1-ext3/fs/ext3/dir.c
 - * system call.  Worse yet, NFSv2 only allows for a 32-bit readdir
 - * cookie.  Sigh.
 + *
-+ * Whether 64-bit or 32-bit hash value is exported as file pos is
-+ * controlled by "64bithash" mount option.
++ * Up layer (OSD) should specify O_32BITHASH or O_64BITHASH explicitly.
++ * On the other hand, we allow ldiskfs to be mounted directly on both 32-bit
++ * and 64-bit nodes, under such case, neither O_32BITHASH nor O_64BITHASH is
++ * specified.
   */
 -#define hash2pos(major, minor)	(major >> 1)
 -#define pos2maj_hash(pos)	((pos << 1) & 0xffffffff)
 -#define pos2min_hash(pos)	(0)
-+
-+static inline loff_t hash2pos(struct super_block *sb, __u32 major, __u32 minor)
++static inline loff_t hash2pos(struct file *filp, __u32 major, __u32 minor)
 +{
-+	if (test_opt(sb, 64BITHASH))
-+		return (((__u64)(major >> 1) << 32) | (__u64)minor);
-+	else
++	if ((filp->f_flags & O_32BITHASH) ||
++	    (!(filp->f_flags & O_64BITHASH) && is_32bit_api()))
 +		return (major >> 1);
++	else
++		return (((__u64)(major >> 1) << 32) | (__u64)minor);
 +}
 +
-+static inline __u32 pos2maj_hash(struct super_block *sb, loff_t pos)
++static inline __u32 pos2maj_hash(struct file *filp, loff_t pos)
 +{
-+	if (test_opt(sb, 64BITHASH))
-+		return (((pos >> 32) << 1) & 0xffffffff);
-+	else
++	if ((filp->f_flags & O_32BITHASH) ||
++	    (!(filp->f_flags & O_64BITHASH) && is_32bit_api()))
 +		return ((pos << 1) & 0xffffffff);
++	else
++		return (((pos >> 32) << 1) & 0xffffffff);
 +}
 +
-+static inline __u32 pos2min_hash(struct super_block *sb, loff_t pos)
++static inline __u32 pos2min_hash(struct file *filp, loff_t pos)
 +{
-+	if (test_opt(sb, 64BITHASH))
-+		return (pos & 0xffffffff);
-+	else
++	if ((filp->f_flags & O_32BITHASH) ||
++	    (!(filp->f_flags & O_64BITHASH) && is_32bit_api()))
 +		return (0);
++	else
++		return (pos & 0xffffffff);
 +}
  
  /*
   * This structure holds the nodes of the red-black tree used to store
-@@ -314,7 +329,7 @@ static void free_rb_tree_fname(struct rb
+@@ -314,7 +342,7 @@
  }
  
  
 -static struct dir_private_info *create_dir_info(loff_t pos)
-+static struct dir_private_info *create_dir_info(struct super_block *sb, loff_t pos)
++static struct dir_private_info *create_dir_info(struct file* filp, loff_t pos)
  {
  	struct dir_private_info *p;
  
-@@ -325,8 +340,8 @@ static struct dir_private_info *create_d
+@@ -325,8 +353,8 @@
  	p->curr_node = NULL;
  	p->extra_fname = NULL;
  	p->last_pos = 0;
 -	p->curr_hash = pos2maj_hash(pos);
 -	p->curr_minor_hash = pos2min_hash(pos);
-+	p->curr_hash = pos2maj_hash(sb, pos);
-+	p->curr_minor_hash = pos2min_hash(sb, pos);
++	p->curr_hash = pos2maj_hash(filp, pos);
++	p->curr_minor_hash = pos2min_hash(filp, pos);
  	p->next_hash = 0;
  	return p;
  }
-@@ -422,7 +437,7 @@ static int call_filldir(struct file * fi
+@@ -422,7 +450,7 @@
  		printk("call_filldir: called with null fname?!?\n");
  		return 0;
  	}
 -	curr_pos = hash2pos(fname->hash, fname->minor_hash);
-+	curr_pos = hash2pos(sb, fname->hash, fname->minor_hash);
++	curr_pos = hash2pos(filp, fname->hash, fname->minor_hash);
  	while (fname) {
  		error = filldir(dirent, fname->name,
  				fname->name_len, curr_pos, 
-@@ -447,7 +462,7 @@ static int ext3_dx_readdir(struct file *
+@@ -447,7 +475,7 @@
  	int	ret;
  
  	if (!info) {
 -		info = create_dir_info(filp->f_pos);
-+		info = create_dir_info(inode->i_sb, filp->f_pos);
++		info = create_dir_info(filp, filp->f_pos);
  		if (!info)
  			return -ENOMEM;
  		filp->private_data = info;
-@@ -461,8 +476,8 @@ static int ext3_dx_readdir(struct file *
+@@ -461,8 +489,8 @@
  		free_rb_tree_fname(&info->root);
  		info->curr_node = NULL;
  		info->extra_fname = NULL;
 -		info->curr_hash = pos2maj_hash(filp->f_pos);
 -		info->curr_minor_hash = pos2min_hash(filp->f_pos);
-+		info->curr_hash = pos2maj_hash(inode->i_sb, filp->f_pos);
-+		info->curr_minor_hash = pos2min_hash(inode->i_sb, filp->f_pos);
++		info->curr_hash = pos2maj_hash(filp, filp->f_pos);
++		info->curr_minor_hash = pos2min_hash(filp, filp->f_pos);
  	}
  
  	/*
-Index: linux-2.6.18-194.17.1-ext3/fs/ext3/super.c
+Index: linux-stage/include/linux/ext3_fs.h
 ===================================================================
---- linux-2.6.18-194.17.1-ext3.orig/fs/ext3/super.c	2010-11-30 22:48:01.000000000 +0300
-+++ linux-2.6.18-194.17.1-ext3/fs/ext3/super.c	2010-12-16 00:11:59.000000000 +0300
-@@ -742,6 +742,7 @@ enum {
- 	Opt_grpquota,
- 	Opt_extents, Opt_noextents, Opt_bigendian_extents, Opt_extdebug,
- 	Opt_mballoc, Opt_nomballoc, Opt_stripe, Opt_maxdirsize, Opt_force_over_8tb,
-+	Opt_64bithash,
- };
- 
- static match_table_t tokens = {
-@@ -808,6 +809,7 @@ static match_table_t tokens = {
- 	{Opt_force_over_8tb, "force_over_8tb"},
- 	{Opt_resize, "resize"},
- 	{Opt_maxdirsize, "maxdirsize=%u"},
-+	{Opt_64bithash, "64bithash"},
- 	{Opt_err, NULL}
- };
- 
-@@ -1195,6 +1197,9 @@ clear_qf_name:
- 		case Opt_force_over_8tb:
- 			force_over_8tb = 1;
- 			break;
-+		case Opt_64bithash:
-+			set_opt(sbi->s_mount_opt, 64BITHASH);
-+			break;
- 		default:
- 			printk (KERN_ERR
- 				"EXT3-fs: Unrecognized mount option \"%s\" "
-Index: linux-2.6.18-194.17.1-ext3/include/linux/ext3_fs.h
-===================================================================
---- linux-2.6.18-194.17.1-ext3.orig/include/linux/ext3_fs.h	2010-11-30 22:52:58.000000000 +0300
-+++ linux-2.6.18-194.17.1-ext3/include/linux/ext3_fs.h	2010-12-16 00:12:45.000000000 +0300
-@@ -483,6 +483,8 @@ do {									       \
- #define EXT3_MOUNT_JOURNAL_ASYNC_COMMIT 0x20000000 /* Journal Async Commit */
+--- linux-stage.orig/include/linux/ext3_fs.h	2011-04-19 01:39:47.000000000 +0800
++++ linux-stage/include/linux/ext3_fs.h	2011-04-19 01:45:21.000000000 +0800
+@@ -54,6 +54,14 @@
+ #define ext3_debug(f, a...)	do {} while (0)
  #endif
  
-+#define EXT3_MOUNT_64BITHASH            0x40000000 /* export 64-bit name hash */
++#ifndef O_32BITHASH
++# define O_32BITHASH    0x10000000
++#endif
 +
- /* Compatibility, for having both ext2_fs.h and ext3_fs.h included at once */
- #ifndef clear_opt
- #define clear_opt(o, opt)		o &= ~EXT3_MOUNT_##opt
++#ifndef O_64BITHASH
++# define O_64BITHASH    0x20000000
++#endif
++
+ #define EXT3_MULTIBLOCK_ALLOCATOR	1
+
+ #define EXT3_MB_HINT_MERGE		1	/* prefer goal again. length */

ldiskfs/kernel_patches/patches/ext4-export-64bit-name-hash-rhel6.patch
@@ -1,141 +1,0 @@
-Index: linux-stage/fs/ext4/dir.c
-===================================================================
---- linux-stage.orig/fs/ext4/dir.c	2011-03-31 10:35:49.000000000 +0800
-+++ linux-stage/fs/ext4/dir.c	2011-04-01 09:33:58.706267179 +0800
-@@ -249,19 +249,32 @@
- /*
-  * These functions convert from the major/minor hash to an f_pos
-  * value.
-- *
-- * Currently we only use major hash numer.  This is unfortunate, but
-- * on 32-bit machines, the same VFS interface is used for lseek and
-- * llseek, so if we use the 64 bit offset, then the 32-bit versions of
-- * lseek/telldir/seekdir will blow out spectacularly, and from within
-- * the ext2 low-level routine, we don't know if we're being called by
-- * a 64-bit version of the system call or the 32-bit version of the
-- * system call.  Worse yet, NFSv2 only allows for a 32-bit readdir
-- * cookie.  Sigh.
-+ * Whether 64-bit or 32-bit hash value is exported as file pos is
-+ * controlled by "64bithash" mount option.
-  */
--#define hash2pos(major, minor)	(major >> 1)
--#define pos2maj_hash(pos)	((pos << 1) & 0xffffffff)
--#define pos2min_hash(pos)	(0)
-+static inline loff_t hash2pos(struct super_block *sb, __u32 major, __u32 minor)
-+{
-+	if (test_opt(sb, 64BITHASH))
-+		return (((__u64)(major >> 1) << 32) | (__u64)minor);
-+	else
-+		return (major >> 1);
-+}
-+
-+static inline __u32 pos2maj_hash(struct super_block *sb, loff_t pos)
-+{
-+	if (test_opt(sb, 64BITHASH))
-+		return (((pos >> 32) << 1) & 0xffffffff);
-+	else
-+		return ((pos << 1) & 0xffffffff);
-+}
-+
-+static inline __u32 pos2min_hash(struct super_block *sb, loff_t pos)
-+{
-+	if (test_opt(sb, 64BITHASH))
-+		return (pos  & 0xffffffff);
-+	else
-+		return (0);
-+}
- 
- /*
-  * This structure holds the nodes of the red-black tree used to store
-@@ -322,15 +335,16 @@
- }
- 
- 
--static struct dir_private_info *ext4_htree_create_dir_info(loff_t pos)
-+static struct dir_private_info *ext4_htree_create_dir_info(
-+        struct super_block *sb, loff_t pos)
- {
- 	struct dir_private_info *p;
- 
- 	p = kzalloc(sizeof(struct dir_private_info), GFP_KERNEL);
- 	if (!p)
- 		return NULL;
--	p->curr_hash = pos2maj_hash(pos);
--	p->curr_minor_hash = pos2min_hash(pos);
-+	p->curr_hash = pos2maj_hash(sb, pos);
-+	p->curr_minor_hash = pos2min_hash(sb, pos);
- 	return p;
- }
- 
-@@ -426,7 +440,7 @@
- 		       "null fname?!?\n");
- 		return 0;
- 	}
--	curr_pos = hash2pos(fname->hash, fname->minor_hash);
-+	curr_pos = hash2pos(sb, fname->hash, fname->minor_hash);
- 	while (fname) {
- 		error = filldir(dirent, fname->name,
- 				fname->name_len, curr_pos,
-@@ -451,7 +465,7 @@
- 	int	ret;
- 
- 	if (!info) {
--		info = ext4_htree_create_dir_info(filp->f_pos);
-+		info = ext4_htree_create_dir_info(inode->i_sb, filp->f_pos);
- 		if (!info)
- 			return -ENOMEM;
- 		filp->private_data = info;
-@@ -465,8 +479,8 @@
- 		free_rb_tree_fname(&info->root);
- 		info->curr_node = NULL;
- 		info->extra_fname = NULL;
--		info->curr_hash = pos2maj_hash(filp->f_pos);
--		info->curr_minor_hash = pos2min_hash(filp->f_pos);
-+		info->curr_hash = pos2maj_hash(inode->i_sb, filp->f_pos);
-+		info->curr_minor_hash = pos2min_hash(inode->i_sb, filp->f_pos);
- 	}
- 
- 	/*
-Index: linux-stage/fs/ext4/ext4.h
-===================================================================
---- linux-stage.orig/fs/ext4/ext4.h	2011-03-31 10:35:50.000000000 +0800
-+++ linux-stage/fs/ext4/ext4.h	2011-04-01 09:33:58.740267284 +0800
-@@ -785,6 +785,7 @@
- #define EXT4_MOUNT_JOURNAL_CHECKSUM	0x800000 /* Journal checksums */
- #define EXT4_MOUNT_JOURNAL_ASYNC_COMMIT	0x1000000 /* Journal Async Commit */
- #define EXT4_MOUNT_I_VERSION            0x2000000 /* i_version support */
-+#define EXT4_MOUNT_64BITHASH		0x4000000 /* export 64-bit name hash */
- #define EXT4_MOUNT_DELALLOC		0x8000000 /* Delalloc support */
- #define EXT4_MOUNT_DATA_ERR_ABORT	0x10000000 /* Abort on file data write */
- #define EXT4_MOUNT_BLOCK_VALIDITY	0x20000000 /* Block validity checking */
-Index: linux-stage/fs/ext4/super.c
-===================================================================
---- linux-stage.orig/fs/ext4/super.c	2011-03-31 10:35:50.000000000 +0800
-+++ linux-stage/fs/ext4/super.c	2011-04-01 09:35:00.251453404 +0800
-@@ -1540,7 +1540,7 @@
- 	Opt_inode_readahead_blks, Opt_journal_ioprio,
- 	Opt_discard, Opt_nodiscard,
- 	Opt_mballoc, Opt_bigendian_extents, Opt_force_over_16tb,
--	Opt_no_mbcache,
-+	Opt_no_mbcache, Opt_64bithash,
- 	Opt_extents, Opt_noextents,
- };
- 
-@@ -1614,6 +1614,7 @@
- 	{Opt_discard, "discard"},
- 	{Opt_nodiscard, "nodiscard"},
- 	{Opt_no_mbcache, "no_mbcache"},
-+	{Opt_64bithash, "64bithash"},
- 	{Opt_extents, "extents"},
- 	{Opt_noextents, "noextents"},
- 	{Opt_err, NULL},
-@@ -2092,6 +2093,9 @@
- 		case Opt_no_mbcache:
- 			set_opt(sbi->s_mount_opt, NO_MBCACHE);
- 			break;
-+		case Opt_64bithash:
-+			set_opt(sbi->s_mount_opt, 64BITHASH);
-+			break;
- 		default:
- 			ext4_msg(sb, KERN_ERR,
- 			       "Unrecognized mount option \"%s\" "

microsoft/amlFilesystem-lustre

commit 5746aa7bec58acd67b15c6905277f825ef7bd4db

LU-20501 llite: skip dot/dotdot in NFS get_name
sanityn test_102a intermittently fails on ZFS DNE with a spurious ESTALE
from open_by_handle_at() of a striped subdirectory:

  open_by_handle_at(subdir) error: Stale file handle

The handle decodes fine.  The ESTALE comes out of the NFS reconnect:
reconnect_one() finds the disconnected dentry's parent, calls
exportfs_get_name() (ll_get_name()) to get the child's name in that
parent, then lookup_one() on it.  ll_get_name() returned "..", so
lookup_one("..") failed -EACCES, and exportfs_decode_fh() converts any
decode error into -ESTALE.

ll_get_name() walks the parent's dirents with do_nfs_get_name_filldir(),
matching each lde_fid against the child FID.  It matches "." and ".."
like any other name, and it does not check LUDA_FID, which is what tells
it whether lde_fid is meaningful at all.

On ZFS the ".." entry of the filesystem root has no dependable FID:
osd-zfs osd_dir_it_rec() computes it with osd_find_parent_fid(), which
returns -ENOENT at the root, and that path left lde_fid untouched, so it
kept whatever the recycled dirent page held.  Instrumenting the root's
".." FID showed it varying per run between the correct root FID, zero,
wild values such as [0xfcaae7467e00fcaa:0xc834a400:0x450008], and real
FIDs of unrelated objects -- and when it happened to equal the child FID
being searched, ll_get_name() returned "..".

Skip "." and ".." outright.  A child is never reached through those
names, so they are never a valid answer here regardless of what lde_fid
holds.  The generic get_name() has the same guards and hit the same bug:
filldir_one() in fs/exportfs/expfs.c has long bounded the name at
NAME_MAX, and 9473c4450e9c ("exportfs: fix the fallback implementation
of the get_name export operation") added the dot/dotdot skip for exactly
this reason -- matching "." or ".." makes the following lookup fail.  It
is absent from the 6.8 tree here and present in 6.12.

Also skip any entry the server did not mark LUDA_FID.  lde_fid is only
defined when that flag is set, and mdd_dir_page_build() already tests it
before reading the field.  osd-zfs has a second path that leaves lde_fid
unwritten for an ordinary name: when osd_get_fid_by_oid() fails it marks
the entry LUDA_UNKNOWN and returns without setting the FID.  The name
check above does not cover that one, and matching its stale FID would
return a name the child does not have.  Skipping is the safe failure --
ll_get_name() reports "not found" rather than a wrong name.

Bound the name length while here.  do_nfs_get_name_filldir() memcpy()s
namelen bytes into lgd_name, which is ll_get_name()'s caller buffer --
exportfs_decode_fh_raw()'s char nbuf[NAME_MAX + 1] -- and lde_namelen
comes straight off the wire with nothing on the client bounding it, since
mdc_adjust_dirpages() only rewrites lde_reclen.  ll_getname_data gains
lgd_sbi so the report can name the device, as console messages are
expected to.

The check belongs here rather than in ll_dir_read().  There, namelen is
lde_namelen for every entry, and for an encrypted directory that is the
critically-encoded ciphertext length, which legitimately exceeds NAME_MAX
(osd_ldiskfs_filldir() stores critical_chars() of the name, and llcrypt
caps the ciphertext itself at NAME_MAX); bounding it at the source drops
those entries from readdir entirely.  By the time the filldir runs the
encrypted name has been decoded into a NAME_MAX buffer, so this branch
can only fire for a plain directory -- which is exactly the case where
nothing has bounded the value.

The server side is fixed separately by the companion osd-zfs patch,
which reports the OSD root instead of leaving lde_fid unwritten.  These
guards are worth keeping regardless: neither depends on the FID value,
so they also protect a new client talking to an unfixed server.

Validated on a ZFS DNE cluster (2 MDT, 2 OST) together with the osd-zfs
patch: sanityn test_102a fails 5/30 without the two patches and
passes 30/30 with them; full sanity-lfsck passes 73/73; sanity test_154f
passes.

Fixes: 96a5daa0c08d ("LU-163 MDS returns 32/64-bit dir name hash according to client type")

lustre/llite/llite_internal.h
@@ -71,6 +71,7 @@ static inline void set_lld_invalid(struct dentry *de, int flag)
 
 struct ll_getname_data {
 	struct dir_context	ctx;
+	struct ll_sb_info *lgd_sbi;	/* for error reporting */
 	char		*lgd_name;	/* points to buf with NAME_MAX+1 size */
 	struct lu_fid	lgd_fid;	/* target fid we are looking for */
 	struct lu_dirent *lgd_lde;	/* dirent currently passed to filldir */

lustre/llite/llite_nfs.c
@@ -275,6 +275,28 @@ do_nfs_get_name_filldir(struct ll_getname_data *lgd, const char *name,
 	struct lu_dirent *lde = lgd->lgd_lde;
 	struct lu_fid fid;
 
+	/* a child is never reached through "." or ".." */
+	if (name_is_dot_or_dotdot(name, namelen))
+		return 0;
+
+	/* lgd_name is NAME_MAX + 1, and for a plain directory namelen is
+	 * lde_namelen straight off the wire with nothing bounding it
+	 */
+	if (namelen > NAME_MAX) {
+		CERROR("%s: dirent namelen %d exceeds %d: rc = %d\n",
+		       lgd->lgd_sbi->ll_fsname, namelen, NAME_MAX,
+		       -EOVERFLOW);
+		return 0;
+	}
+
+	/* lde_fid is only meaningful when the server set LUDA_FID */
+	if (!(le32_to_cpu(lde->lde_attrs) & LUDA_FID)) {
+		CDEBUG(D_INODE, DNAME": no LUDA_FID, lde_attrs %#x\n",
+		       encode_fn_dname(namelen, name),
+		       le32_to_cpu(lde->lde_attrs));
+		return 0;
+	}
+
 	fid_le_to_cpu(&fid, &lde->lde_fid);
 	if (lu_fid_eq(&fid, &lgd->lgd_fid)) {
 		memcpy(lgd->lgd_name, name, namelen);
@@ -317,6 +339,8 @@ static int ll_get_name(struct dentry *dentry, char *name, struct dentry *child)
 
 	if (!dir || !S_ISDIR(dir->i_mode))
 		GOTO(out, rc = -ENOTDIR);
+
+	lgd.lgd_sbi = ll_i2sbi(dir);
 
 	if (!dir->i_fop)
 		GOTO(out, rc = -EINVAL);