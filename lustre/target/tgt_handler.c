lustre/lustre-release

commit 4c3864cf97711d73b12905fea720570cf814d179

LU-12600 tgt: shortio size should be unsigned
The short_io_size value is accepting unsigned values from
req_capsule_get_size, and so needs to be unsigned as well.

If it's not, it's possible for the short_io_size memcopy to
act on an incorrect value and cause memory corruption.

Reported-by: Alibaba Cloud <yunye.ry@alibaba-inc.com>
Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I043e314cd43a7b40519f951a605fa5a36ff91dcf
Reviewed-on: https://review.whamcloud.com/35653
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/target/tgt_handler.c
@@ -2388,7 +2388,7 @@ out_lock:
 EXPORT_SYMBOL(tgt_brw_read);
 
 static int tgt_shortio2pages(struct niobuf_local *local, int npages,
-			     unsigned char *buf, int size)
+			     unsigned char *buf, unsigned int size)
 {
 	int	i, off, len;
 	char	*ptr;
@@ -2586,7 +2586,7 @@ int tgt_brw_write(struct tgt_session_info *tsi)
 	if (rc < 0)
 		GOTO(out_lock, rc);
 	if (body->oa.o_flags & OBD_FL_SHORT_IO) {
-		int short_io_size;
+		unsigned int short_io_size;
 		unsigned char *short_io_buf;
 
 		short_io_size = req_capsule_get_size(&req->rq_pill,

microsoft/amlFilesystem-lustre

commit a7023c24ea58584a27b3f730658bc94cc57c1a81

LU-17000 target: handle short IO write copy properly
Verify lnb_bytes fits in RMF_SHORT_IO buffer in tgt_shortio2pages().
Reject the request if not, the way the read side tgt_pages2shortio()
already does, and use unsigned lengths while we are at it.
A malformed request cannot be fixed by resending it, so reply with
an error rather than dropping the reply, and log the lengths so that
a client sending one leaves a record on the server.

Test-Parameters: trivial clientversion=2.17 testlist=sanity
Fixes: 4c3864cf97 ("LU-12600 tgt: shortio size should be unsigned")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I5fa8f0c58a040f2c689fb74ca0efb6abd61f4053

lustre/target/tgt_handler.c
@@ -2644,8 +2644,9 @@ EXPORT_SYMBOL(tgt_brw_read);
 static int tgt_shortio2pages(struct niobuf_local *local, int npages,
 			     unsigned char *buf, unsigned int size)
 {
-	int	i, off, len;
-	char	*ptr;
+	unsigned int off, len;
+	char *ptr;
+	int i;
 
 	for (i = 0; i < npages; i++) {
 		off = local[i].lnb_page_offset & ~PAGE_MASK;
@@ -2654,10 +2655,13 @@ static int tgt_shortio2pages(struct niobuf_local *local, int npages,
 		if (len == 0)
 			continue;
 
-		CDEBUG(D_PAGE, "index %d offset = %d len = %d left = %d\n",
+		CDEBUG(D_PAGE, "index %d offset = %u len = %u left = %u\n",
 		       i, off, len, size);
 		ptr = lnb_kmap_local(&local[i]);
-		memcpy(ptr + off, buf, len < size ? len : size);
+		if (len > size)
+			return -EINVAL;
+
+		memcpy(ptr + off, buf, len);
 		kunmap_local(ptr);
 		buf += len;
 		size -= len;
@@ -2881,13 +2885,28 @@ int tgt_brw_write(struct tgt_session_info *tsi)
 						     RCL_CLIENT);
 		short_io_buf = req_capsule_client_get(&req->rq_pill,
 						      &RMF_SHORT_IO);
-		CDEBUG(D_INFO, "Client use short io for data transfer, size = %d\n",
+		CDEBUG(D_INFO, "Client use short io for data transfer, size = %u\n",
 			       short_io_size);
 
 		/* Copy short io buf to pages */
 		rc = tgt_shortio2pages(local_nb, npages, short_io_buf,
 				       short_io_size);
 		desc = NULL;
+		if (rc < 0) {
+			unsigned int declared = 0;
+
+			for (i = 0; i < niocount; i++)
+				declared += remote_nb[i].rnb_len;
+
+			CDEBUG_LIMIT(D_ERROR,
+				     "%s: short IO write from %s declares %u bytes but sends %u: rc = %d\n",
+				     obd_name, obd_export_nid2str(exp),
+				     declared, short_io_size, rc);
+			/* resending cannot fix a malformed request, so reply
+			 * with the error instead of dropping the reply
+			 */
+			GOTO(out_commitrw, rc);
+		}
 	} else {
 		desc = ptlrpc_prep_bulk_exp(req, npages, ioobj_max_brw_get(ioo),
 					    PTLRPC_BULK_GET_SINK,