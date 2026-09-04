lustre/lustre-release

commit 4d38566a004f6a636c37ec0c86f053be9b905bd7

LU-13717 sec: filename encryption
On client side, call the appropriate llcrypt primitives from llite,
to proceed with filename encryption before sending requests to servers
and filename decryption upon request receipt.
Note we need specific overlay functions to handle encoding and
decoding of encrypted filenames, as we do not want server side to deal
with binary names before they reach the backend file system layer.

On server side, mainly the OSD layer, we need to know the encryption
status of files being processed.
If an object belongs to an encrypted file, the filename has been
encoded by the client because it is binary, so it needs to be decoded
before being handed over to the backend file system layer.
And conversely, the filename of an encrypted file has to be encoded
before being sent over the wire.
Note server side is osd-ldiskfs only for now.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7ac9047f5a046b8bc63afdbbb1f28e78aa5c8c7e
Reviewed-on: https://review.whamcloud.com/43390
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/llite/dir.c
@@ -55,6 +55,7 @@
 #include <lustre_fid.h>
 #include <lustre_kernelcomm.h>
 #include <lustre_swab.h>
+#include <libcfs/libcfs_crypto.h>
 
 #include "llite_internal.h"
 
@@ -186,14 +187,21 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 		void *cookie, filldir_t filldir)
 {
 #endif
-	struct ll_sb_info    *sbi        = ll_i2sbi(inode);
-	__u64                 pos        = *ppos;
-	bool                  is_api32 = ll_need_32bit_api(sbi);
-	bool                  is_hash64 = sbi->ll_flags & LL_SBI_64BIT_HASH;
-	struct page          *page;
-	bool                  done = false;
-	int                   rc = 0;
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	__u64 pos = *ppos;
+	bool is_api32 = ll_need_32bit_api(sbi);
+	bool is_hash64 = sbi->ll_flags & LL_SBI_64BIT_HASH;
+	struct page *page;
+	bool done = false;
+	struct llcrypt_str lltr = LLTR_INIT(NULL, 0);
+	int rc = 0;
 	ENTRY;
+
+	if (IS_ENCRYPTED(inode)) {
+		rc = llcrypt_fname_alloc_buffer(inode, NAME_MAX, &lltr);
+		if (rc < 0)
+			RETURN(rc);
+	}
 
 	page = ll_get_dir_page(inode, op_data, pos);
 
@@ -238,9 +246,31 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 			 * for 'filldir()' must be part of the 'ent'. */
 #ifdef HAVE_DIR_CONTEXT
 			ctx->pos = lhash;
-			done = !dir_emit(ctx, ent->lde_name, namelen, ino,
-					 type);
+			if (!IS_ENCRYPTED(inode)) {
+				done = !dir_emit(ctx, ent->lde_name, namelen,
+						 ino, type);
+			} else {
+				/* Directory is encrypted */
+				int save_len = lltr.len;
+				struct llcrypt_str de_name =
+					LLTR_INIT(ent->lde_name, namelen);
+
+				rc = ll_fname_disk_to_usr(inode, 0, 0, &de_name,
+							  &lltr);
+				de_name = lltr;
+				lltr.len = save_len;
+				if (rc) {
+					done = 1;
+					break;
+				}
+				done = !dir_emit(ctx, de_name.name, de_name.len,
+						 ino, type);
+			}
 #else
+			/* HAVE_DIR_CONTEXT is defined from kernel 3.11, whereas
+			 * IS_ENCRYPTED is brought by kernel 4.14.
+			 * So there is no need to handle encryption case here.
+			 */
 			done = filldir(cookie, ent->lde_name, namelen, lhash,
 				       ino, type);
 #endif
@@ -277,6 +307,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 #else
 	*ppos = pos;
 #endif
+	llcrypt_fname_free_buffer(&lltr);
 	RETURN(rc);
 }
 
@@ -307,6 +338,12 @@ static int ll_readdir(struct file *filp, void *cookie, filldir_t filldir)
 	       "VFS Op:inode="DFID"(%p) pos/size%lu/%llu 32bit_api %d\n",
 	       PFID(ll_inode2fid(inode)),
 	       inode, (unsigned long)pos, i_size_read(inode), api32);
+
+	if (IS_ENCRYPTED(inode)) {
+		rc = llcrypt_get_encryption_info(inode);
+		if (rc && rc != -ENOKEY)
+			GOTO(out, rc);
+	}
 
 	if (pos == MDS_DIR_END_OFF)
 		/*

lustre/llite/llite_internal.h
@@ -1722,10 +1722,17 @@ static inline struct pcc_super *ll_info2pccs(struct ll_inode_info *lli)
 	return ll_i2pccs(ll_info2i(lli));
 }
 
-#ifdef HAVE_LUSTRE_CRYPTO
 /* crypto.c */
+int ll_setup_filename(struct inode *dir, const struct qstr *iname,
+		      int lookup, struct llcrypt_name *fname);
+int ll_fname_disk_to_usr(struct inode *inode,
+			 u32 hash, u32 minor_hash,
+			 struct llcrypt_str *iname, struct llcrypt_str *oname);
+int ll_revalidate_d_crypto(struct dentry *dentry, unsigned int flags);
+#ifdef HAVE_LUSTRE_CRYPTO
 extern const struct llcrypt_operations lustre_cryptops;
 #endif
+
 /* llite/llite_foreign.c */
 int ll_manage_foreign(struct inode *inode, struct lustre_md *lmd);
 bool ll_foreign_is_openable(struct dentry *dentry, unsigned int flags);

lustre/include/obd.h
@@ -840,6 +840,7 @@ enum md_op_flags {
 	MF_GETATTR_BY_FID	= BIT(5),
 	MF_QOS_MKDIR		= BIT(6),
 	MF_RR_MKDIR		= BIT(7),
+	MF_OPNAME_KMALLOCED	= BIT(8),
 };
 
 enum md_cli_flags {
@@ -857,6 +858,9 @@ enum md_op_code {
 	LUSTRE_OPC_MKNOD,
 	LUSTRE_OPC_CREATE,
 	LUSTRE_OPC_ANY,
+	LUSTRE_OPC_LOOKUP,
+	LUSTRE_OPC_OPEN,
+	LUSTRE_OPC_MIGR,
 };
 
 /**

lustre/llite/crypto.c
@@ -29,6 +29,7 @@
 #include "llite_internal.h"
 
 #ifdef HAVE_LUSTRE_CRYPTO
+#include <libcfs/libcfs_crypto.h>
 
 static int ll_get_context(struct inode *inode, void *ctx, size_t len)
 {
@@ -158,6 +159,150 @@ static bool ll_empty_dir(struct inode *inode)
 	return true;
 }
 
+/**
+ * ll_setup_filename() - overlay to llcrypt_setup_filename
+ * @dir: the directory that will be searched
+ * @iname: the user-provided filename being searched for
+ * @lookup: 1 if we're allowed to proceed without the key because it's
+ *	->lookup() or we're finding the dir_entry for deletion; 0 if we cannot
+ *	proceed without the key because we're going to create the dir_entry.
+ * @fname: the filename information to be filled in
+ *
+ * This overlay function is necessary to properly encode @fname after
+ * encryption, as it will be sent over the wire.
+ */
+int ll_setup_filename(struct inode *dir, const struct qstr *iname,
+		      int lookup, struct llcrypt_name *fname)
+{
+	int rc;
+
+	rc = llcrypt_setup_filename(dir, iname, lookup, fname);
+	if (rc)
+		return rc;
+
+	if (IS_ENCRYPTED(dir) &&
+	    !name_is_dot_or_dotdot(fname->disk_name.name,
+				   fname->disk_name.len)) {
+		int presented_len = critical_chars(fname->disk_name.name,
+						   fname->disk_name.len);
+		char *buf;
+
+		buf = kmalloc(presented_len + 1, GFP_NOFS);
+		if (!buf) {
+			rc = -ENOMEM;
+			goto out_free;
+		}
+
+		if (presented_len == fname->disk_name.len)
+			memcpy(buf, fname->disk_name.name, presented_len);
+		else
+			critical_encode(fname->disk_name.name,
+					fname->disk_name.len, buf);
+		buf[presented_len] = '\0';
+		kfree(fname->crypto_buf.name);
+		fname->crypto_buf.name = buf;
+		fname->crypto_buf.len = presented_len;
+		fname->disk_name.name = fname->crypto_buf.name;
+		fname->disk_name.len = fname->crypto_buf.len;
+	}
+
+	return rc;
+
+out_free:
+	llcrypt_free_filename(fname);
+	return rc;
+}
+
+/**
+ * ll_fname_disk_to_usr() - overlay to llcrypt_fname_disk_to_usr
+ * @inode: the inode to convert name
+ * @hash: major hash for inode
+ * @minor_hash: minor hash for inode
+ * @iname: the user-provided filename needing conversion
+ * @oname: the filename information to be filled in
+ *
+ * The caller must have allocated sufficient memory for the @oname string.
+ *
+ * This overlay function is necessary to properly decode @iname before
+ * decryption, as it comes from the wire.
+ */
+int ll_fname_disk_to_usr(struct inode *inode,
+			 u32 hash, u32 minor_hash,
+			 struct llcrypt_str *iname, struct llcrypt_str *oname)
+{
+	struct llcrypt_str lltr = LLTR_INIT(iname->name, iname->len);
+	char *buf = NULL;
+	int rc;
+
+	if (IS_ENCRYPTED(inode) &&
+	    !name_is_dot_or_dotdot(lltr.name, lltr.len) &&
+	    strnchr(lltr.name, lltr.len, '=')) {
+		/* Only proceed to critical decode if
+		 * iname contains espace char '='.
+		 */
+		int len = lltr.len;
+
+		buf = kmalloc(len, GFP_NOFS);
+		if (!buf)
+			return -ENOMEM;
+
+		len = critical_decode(lltr.name, len, buf);
+		lltr.name = buf;
+		lltr.len = len;
+	}
+
+	rc = llcrypt_fname_disk_to_usr(inode, hash, minor_hash, &lltr, oname);
+
+	kfree(buf);
+
+	return rc;
+}
+
+/* Copied from llcrypt_d_revalidate, as it is not exported */
+/*
+ * Validate dentries in encrypted directories to make sure we aren't potentially
+ * caching stale dentries after a key has been added.
+ */
+int ll_revalidate_d_crypto(struct dentry *dentry, unsigned int flags)
+{
+	struct dentry *dir;
+	int err;
+	int valid;
+
+	/*
+	 * Plaintext names are always valid, since llcrypt doesn't support
+	 * reverting to ciphertext names without evicting the directory's inode
+	 * -- which implies eviction of the dentries in the directory.
+	 */
+	if (!(dentry->d_flags & DCACHE_ENCRYPTED_NAME))
+		return 1;
+
+	/*
+	 * Ciphertext name; valid if the directory's key is still unavailable.
+	 *
+	 * Although llcrypt forbids rename() on ciphertext names, we still must
+	 * use dget_parent() here rather than use ->d_parent directly.  That's
+	 * because a corrupted fs image may contain directory hard links, which
+	 * the VFS handles by moving the directory's dentry tree in the dcache
+	 * each time ->lookup() finds the directory and it already has a dentry
+	 * elsewhere.  Thus ->d_parent can be changing, and we must safely grab
+	 * a reference to some ->d_parent to prevent it from being freed.
+	 */
+
+	if (flags & LOOKUP_RCU)
+		return -ECHILD;
+
+	dir = dget_parent(dentry);
+	err = llcrypt_get_encryption_info(d_inode(dir));
+	valid = !llcrypt_has_encryption_key(d_inode(dir));
+	dput(dir);
+
+	if (err < 0)
+		return err;
+
+	return valid;
+}
+
 const struct llcrypt_operations lustre_cryptops = {
 	.key_prefix		= "lustre:",
 	.get_context		= ll_get_context,
@@ -189,6 +334,24 @@ bool ll_sbi_has_encrypt(struct ll_sb_info *sbi)
 
 void ll_sbi_set_encrypt(struct ll_sb_info *sbi, bool set)
 {
+}
+
+int ll_setup_filename(struct inode *dir, const struct qstr *iname,
+		      int lookup, struct llcrypt_name *fname)
+{
+	return llcrypt_setup_filename(dir, iname, lookup, fname);
+}
+
+int ll_fname_disk_to_usr(struct inode *inode,
+			 u32 hash, u32 minor_hash,
+			 struct llcrypt_str *iname, struct llcrypt_str *oname)
+{
+	return llcrypt_fname_disk_to_usr(inode, hash, minor_hash, iname, oname);
+}
+
+int ll_revalidate_d_crypto(struct dentry *dentry, unsigned int flags)
+{
+	return 1;
 }
 #endif
 

lustre/llite/dcache.c
@@ -293,9 +293,14 @@ static int ll_revalidate_dentry(struct dentry *dentry,
 				unsigned int lookup_flags)
 {
 	struct inode *dir = dentry->d_parent->d_inode;
+	int rc;
 
 	CDEBUG(D_VFSTRACE, "VFS Op:name=%s, flags=%u\n",
 	       dentry->d_name.name, lookup_flags);
+
+	rc = ll_revalidate_d_crypto(dentry, lookup_flags);
+	if (rc != 1)
+		return rc;
 
 	/* If this is intermediate component path lookup and we were able to get
 	 * to this dentry, then its lock has not been revoked and the

lustre/llite/file.c
@@ -628,7 +628,7 @@ retry:
 	}
 
 	op_data = ll_prep_md_op_data(NULL, parent->d_inode, de->d_inode,
-				     name, len, 0, LUSTRE_OPC_ANY, NULL);
+				     name, len, 0, LUSTRE_OPC_OPEN, NULL);
 	if (IS_ERR(op_data)) {
 		kfree(name);
 		RETURN(PTR_ERR(op_data));
@@ -2283,7 +2283,7 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
                              struct ptlrpc_request **request)
 {
 	struct ll_sb_info *sbi = ll_i2sbi(inode);
-	struct mdt_body *body;
+	struct mdt_body  *body;
 	struct lov_mds_md *lmm = NULL;
 	struct ptlrpc_request *req = NULL;
 	struct md_op_data *op_data;
@@ -2305,8 +2305,8 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 	rc = md_getattr_name(sbi->ll_md_exp, op_data, &req);
 	ll_finish_md_op_data(op_data);
 	if (rc < 0) {
-		CDEBUG(D_INFO, "md_getattr_name failed "
-		       "on %s: rc %d\n", filename, rc);
+		CDEBUG(D_INFO, "md_getattr_name failed on %s: rc %d\n",
+		       filename, rc);
 		GOTO(out, rc);
 	}
 
@@ -4734,10 +4734,10 @@ int ll_get_fid_by_name(struct inode *parent, const char *name,
 		       int namelen, struct lu_fid *fid,
 		       struct inode **inode)
 {
-	struct md_op_data	*op_data = NULL;
-	struct mdt_body		*body;
-	struct ptlrpc_request	*req;
-	int			rc;
+	struct md_op_data *op_data = NULL;
+	struct mdt_body *body;
+	struct ptlrpc_request *req;
+	int rc;
 	ENTRY;
 
 	op_data = ll_prep_md_op_data(NULL, parent, NULL, name, namelen, 0,
@@ -4845,7 +4845,8 @@ int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
 	}
 
 	op_data = ll_prep_md_op_data(NULL, parent, NULL, name, namelen,
-				     child_inode->i_mode, LUSTRE_OPC_ANY, NULL);
+				     child_inode->i_mode, LUSTRE_OPC_MIGR,
+				     NULL);
 	if (IS_ERR(op_data))
 		GOTO(out_iput, rc = PTR_ERR(op_data));
 
@@ -4886,8 +4887,9 @@ again:
 		spin_unlock(&och->och_mod->mod_open_req->rq_lock);
 	}
 
-	rc = md_rename(ll_i2sbi(parent)->ll_md_exp, op_data, name, namelen,
-		       name, namelen, &request);
+	rc = md_rename(ll_i2sbi(parent)->ll_md_exp, op_data,
+		       op_data->op_name, op_data->op_namelen,
+		       op_data->op_name, op_data->op_namelen, &request);
 	if (rc == 0) {
 		LASSERT(request != NULL);
 		ll_update_times(request, parent);

lustre/llite/llite_lib.c
@@ -3104,6 +3104,9 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 				      __u32 mode, enum md_op_code opc,
 				      void *data)
 {
+	struct llcrypt_name fname = { 0 };
+	int rc;
+
 	LASSERT(i1 != NULL);
 
 	if (name == NULL) {
@@ -3128,7 +3131,6 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 
 	ll_i2gids(op_data->op_suppgids, i1, i2);
 	op_data->op_fid1 = *ll_inode2fid(i1);
-	op_data->op_code = opc;
 
 	if (S_ISDIR(i1->i_mode)) {
 		down_read_non_owner(&ll_i2info(i1)->lli_lsm_sem);
@@ -3160,8 +3162,46 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 	if (ll_need_32bit_api(ll_i2sbi(i1)))
 		op_data->op_cli_flags |= CLI_API32;
 
-	op_data->op_name = name;
-	op_data->op_namelen = namelen;
+	if (opc == LUSTRE_OPC_LOOKUP || opc == LUSTRE_OPC_CREATE) {
+		/* In case of lookup, ll_setup_filename() has already been
+		 * called in ll_lookup_it(), so just take provided name.
+		 */
+		fname.disk_name.name = (unsigned char *)name;
+		fname.disk_name.len = namelen;
+	} else if (name && namelen) {
+		struct qstr dname = QSTR_INIT(name, namelen);
+		struct inode *dir;
+		int lookup;
+
+		if (!S_ISDIR(i1->i_mode) && i2 && S_ISDIR(i2->i_mode)) {
+			/* special case when called from ll_link() */
+			dir = i2;
+			lookup = 0;
+		} else {
+			dir = i1;
+			lookup = (int)(opc == LUSTRE_OPC_ANY);
+		}
+		rc = ll_setup_filename(dir, &dname, lookup, &fname);
+		if (rc) {
+			ll_finish_md_op_data(op_data);
+			return ERR_PTR(rc);
+		}
+		if (fname.disk_name.name &&
+		    fname.disk_name.name != (unsigned char *)name)
+			/* op_data->op_name must be freed after use */
+			op_data->op_flags |= MF_OPNAME_KMALLOCED;
+	}
+
+	/* In fact LUSTRE_OPC_LOOKUP, LUSTRE_OPC_OPEN, LUSTRE_OPC_MIGR
+	 * are LUSTRE_OPC_ANY
+	 */
+	if (opc == LUSTRE_OPC_LOOKUP || opc == LUSTRE_OPC_OPEN ||
+	    opc == LUSTRE_OPC_MIGR)
+		op_data->op_code = LUSTRE_OPC_ANY;
+	else
+		op_data->op_code = opc;
+	op_data->op_name = fname.disk_name.name;
+	op_data->op_namelen = fname.disk_name.len;
 	op_data->op_mode = mode;
 	op_data->op_mod_time = ktime_get_real_seconds();
 	op_data->op_fsuid = from_kuid(&init_user_ns, current_fsuid());
@@ -3182,6 +3222,11 @@ void ll_finish_md_op_data(struct md_op_data *op_data)
 	ll_unlock_md_op_lsm(op_data);
 	ll_security_release_secctx(op_data->op_file_secctx,
 				   op_data->op_file_secctx_size);
+	if (op_data->op_flags & MF_OPNAME_KMALLOCED)
+		/* allocated via ll_setup_filename called
+		 * from ll_prep_md_op_data
+		 */
+		kfree(op_data->op_name);
 	llcrypt_free_ctx(op_data->op_file_encctx, op_data->op_file_encctx_size);
 	OBD_FREE_PTR(op_data);
 }

lustre/llite/namei.c
@@ -822,6 +822,7 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 	__u32 opc;
 	int rc;
 	char secctx_name[XATTR_NAME_MAX + 1];
+	struct llcrypt_name fname;
 
 	ENTRY;
 
@@ -850,12 +851,32 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 	if (it->it_op & IT_CREAT)
 		opc = LUSTRE_OPC_CREATE;
 	else
-		opc = LUSTRE_OPC_ANY;
+		opc = LUSTRE_OPC_LOOKUP;
 
-	op_data = ll_prep_md_op_data(NULL, parent, NULL, dentry->d_name.name,
-				     dentry->d_name.len, 0, opc, NULL);
-	if (IS_ERR(op_data))
-		GOTO(out, retval = ERR_CAST(op_data));
+	/* Here we should be calling llcrypt_prepare_lookup(). But it installs a
+	 * custom ->d_revalidate() method, so we lose ll_d_ops.
+	 * To workaround this, call ll_setup_filename() and do the rest
+	 * manually. Also make a copy of llcrypt_d_revalidate() (unfortunately
+	 * not exported function) and call it from ll_revalidate_dentry(), to
+	 * ensure we do not cache stale dentries after a key has been added.
+	 */
+	rc = ll_setup_filename(parent, &dentry->d_name, 1, &fname);
+	if ((!rc || rc == -ENOENT) && fname.is_ciphertext_name) {
+		spin_lock(&dentry->d_lock);
+		dentry->d_flags |= DCACHE_ENCRYPTED_NAME;
+		spin_unlock(&dentry->d_lock);
+	}
+	if (rc == -ENOENT)
+		RETURN(NULL);
+	if (rc)
+		RETURN(ERR_PTR(rc));
+
+	op_data = ll_prep_md_op_data(NULL, parent, NULL, fname.disk_name.name,
+				     fname.disk_name.len, 0, opc, NULL);
+	if (IS_ERR(op_data)) {
+		llcrypt_free_filename(&fname);
+		RETURN(ERR_CAST(op_data));
+	}
 
 	/* enforce umask if acl disabled or MDS doesn't support umask */
 	if (!IS_POSIXACL(parent) || !exp_connect_umask(ll_i2mdexp(parent)))
@@ -1083,6 +1104,7 @@ out:
 			op_data->op_file_encctx = NULL;
 			op_data->op_file_encctx_size = 0;
 		}
+		llcrypt_free_filename(&fname);
 		ll_finish_md_op_data(op_data);
 	}
 
@@ -1983,13 +2005,12 @@ static int ll_rename(struct inode *src, struct dentry *src_dchild,
 #endif
 		     )
 {
-	struct qstr *src_name = &src_dchild->d_name;
-	struct qstr *tgt_name = &tgt_dchild->d_name;
 	struct ptlrpc_request *request = NULL;
 	struct ll_sb_info *sbi = ll_i2sbi(src);
 	struct md_op_data *op_data;
 	ktime_t kstart = ktime_get();
 	umode_t mode = 0;
+	struct llcrypt_name foldname, fnewname;
 	int err;
 	ENTRY;
 
@@ -2036,9 +2057,20 @@ static int ll_rename(struct inode *src, struct dentry *src_dchild,
 	if (tgt_dchild->d_inode)
 		op_data->op_fid4 = *ll_inode2fid(tgt_dchild->d_inode);
 
+	err = ll_setup_filename(src, &src_dchild->d_name, 1, &foldname);
+	if (err)
+		RETURN(err);
+	err = ll_setup_filename(tgt, &tgt_dchild->d_name, 1, &fnewname);
+	if (err) {
+		llcrypt_free_filename(&foldname);
+		RETURN(err);
+	}
 	err = md_rename(sbi->ll_md_exp, op_data,
-			src_name->name, src_name->len,
-			tgt_name->name, tgt_name->len, &request);
+			foldname.disk_name.name, foldname.disk_name.len,
+			fnewname.disk_name.name, fnewname.disk_name.len,
+			&request);
+	llcrypt_free_filename(&foldname);
+	llcrypt_free_filename(&fnewname);
 	ll_finish_md_op_data(op_data);
 	if (!err) {
 		ll_update_times(request, src);

microsoft/amlFilesystem-lustre

commit eb95293bccd888b4f91569fe6030f5abd25791ab

LU-20501 llite: do not derive the dirent from the emitted name
ll_get_name() recovers the directory entry it is examining with
container_of() on the name pointer its filldir gets, which is only valid
while that name points into the lu_dirent.  ll_dir_read() has not
guaranteed that since encrypted directories were added: for those it
emits the name out of the separate llcrypt_fname_alloc_buffer()
allocation, so the derived "lu_dirent" starts 32 bytes before an
unrelated slab object and lde_fid is read out of bounds.

Tracing the two pointers on an encrypted directory shows the derived
entry landing at name - 0x20 for every entry, with the FID read back out
of the neighbouring allocation -- in one run
[0xffff998306137fe0:0x6137fe0:0xffff9983], a kernel pointer being
compared as a FID.  A plain directory in the same mount never diverges.

Pass the lu_dirent being emitted down from ll_dir_read() so the filldir
works from the entry itself rather than from pointer arithmetic on a name
it does not own.  Drop the (filldir_t) cast on .ctx.actor while
here: ll_nfs_get_name_filldir() already has that prototype under both
FILLDIR_TYPE variants, so the cast only hid the type check, and no other
.actor assignment in the tree casts.

This does not fix the failure LU-20501 was filed for.  ll_dir_read()
emits ent->lde_name for a plain directory, so container_of() already
recovered exactly the entry this patch passes down; only the
IS_ENCRYPTED() path changes behaviour, and sanityn test_102a (the existing
open-by-handle test, renamed here to make room for the new one) uses
unencrypted directories.  This fixes the out-of-bounds read only.

NFS export of an encrypted directory works after this only when the
parent inode is already warm on the mount doing the reconnect.
ll_get_name() calls ll_dir_read() without llcrypt_prepare_readdir(),
which ll_iterate() does before the same call, so the crypt info has to
have been loaded by something else -- search_inode_for_lustre() starts
with an ilookup5() and llcrypt_setup_filename() loads the info for every
lookup inside the directory.  Measured on a dummy-encrypted mount: with
the parent walked first, check_fhandle_syscalls passes 3/3; with
drop_caches and the LDLM LRU cleared so the inode is instantiated cold,
0/3, because ll_fname_disk_to_usr() then emits the no-key base64 form
that reconnect_path()'s lookup_one_len() cannot resolve.  Adding
llcrypt_prepare_readdir() there returns -ENODATA on a cold
NFS-instantiated inode, so the cold case needs its own fix.

The existing open-by-handle case is renamed test_102a so the new one can
be test_102b, which is how a variant is added here rather than putting a
102a beside a bare 102.  That does move the subtest name LU-20501's
existing failure history is keyed on, which is accepted deliberately.

sanityn test_102b covers it: it remounts both clients with
test_dummy_encryption, walks the directory on the second mount so the
parent's crypt info is loaded, and resolves a handle for a subdirectory
inside it.  It fails without this patch ("check_fhandle_syscalls on an
encrypted dir failed") and passes with it.  The cold-inode case is left
uncovered on purpose, since it still fails for the reason above.

Fixes: 4d38566a004f ("LU-13717 sec: filename encryption")

lustre/llite/dir.c
@@ -180,7 +180,8 @@ void ll_release_dir_folio(struct inode *inode, struct folio *folio,
 }
 
 int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
-		struct dir_context *ctx, int *partial_readdir_rc)
+		struct dir_context *ctx, int *partial_readdir_rc,
+		struct lu_dirent **dirent)
 {
 	struct ll_sb_info *sbi = ll_i2sbi(inode);
 	__u64 pos = *ppos;
@@ -240,10 +241,13 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 			fid_le_to_cpu(&fid, &ent->lde_fid);
 			ino = cl_fid_build_ino(&fid, is_api32);
 			type = S_DT(lu_dirent_type_get(ent));
-			/* For ll_nfs_get_name_filldir(), it will try to access
-			 * 'ent' through 'lde_name', so the parameter 'name'
-			 * for 'filldir()' must be part of the 'ent'.
+			/* ll_nfs_get_name_filldir() needs the entry itself,
+			 * which it cannot recover from the emitted name.
+			 * Only valid for the duration of the callback below:
+			 * the folio is unmapped once this loop is done.
 			 */
+			if (dirent)
+				*dirent = ent;
 			ctx->pos = lhash;
 			if (!IS_ENCRYPTED(inode)) {
 				done = !dir_emit(ctx, ent->lde_name, namelen,
@@ -377,7 +381,7 @@ static int ll_iterate(struct file *filp, struct dir_context *ctx)
 	op_data->op_fid3 = pfid;
 
 	ctx->pos = pos;
-	rc = ll_dir_read(inode, &pos, op_data, ctx, &partial_readdir_rc);
+	rc = ll_dir_read(inode, &pos, op_data, ctx, &partial_readdir_rc, NULL);
 	pos = ctx->pos;
 	lfd->lfd_pos = pos;
 	if (!lfd->fd_partial_readdir_rc)

lustre/llite/llite_internal.h
@@ -73,6 +73,7 @@ struct ll_getname_data {
 	struct dir_context	ctx;
 	char		*lgd_name;	/* points to buf with NAME_MAX+1 size */
 	struct lu_fid	lgd_fid;	/* target fid we are looking for */
+	struct lu_dirent *lgd_lde;	/* dirent currently passed to filldir */
 	int		lgd_found;	/* inode matched? */
 };
 
@@ -1298,7 +1299,8 @@ extern const struct file_operations ll_dir_operations;
 extern const struct inode_operations ll_dir_inode_operations;
 extern struct kmem_cache *quota_iter_slab;
 int ll_dir_read(struct inode *inode, __u64 *pos, struct md_op_data *op_data,
-		struct dir_context *ctx, int *partial_readdir_rc);
+		struct dir_context *ctx, int *partial_readdir_rc,
+		struct lu_dirent **dirent);
 int ll_get_mdt_idx(struct inode *inode);
 int ll_get_mdt_idx_by_fid(struct ll_sb_info *sbi, const struct lu_fid *fid);
 struct folio *ll_get_dir_folio(struct inode *dir, struct md_op_data *op_data,

lustre/llite/llite_nfs.c
@@ -272,19 +272,7 @@ static inline int
 do_nfs_get_name_filldir(struct ll_getname_data *lgd, const char *name,
 			int namelen, loff_t hash, u64 ino, unsigned int type)
 {
-	/*
-	 * It is hack to access lde_fid for comparison with lgd_fid.
-	 * So the input 'name' must be part of the 'lu_dirent', and
-	 * so must appear to be a non-const pointer to an empty array.
-	 */
-	char (*n)[0] = (void *)name;
-	/* NOTE: This should be container_of().  However container_of() in
-	 * kernels earlier than v4.13-rc1~37^2~94 cause this to generate a
-	 * warning, which fails when we compile with -Werror.  Those earlier
-	 * kernels don't have container_of_safe, calling that instead will use
-	 * the lustre-local version which doesn't generate the warning.
-	 */
-	struct lu_dirent *lde = container_of_safe(n, struct lu_dirent, lde_name);
+	struct lu_dirent *lde = lgd->lgd_lde;
 	struct lu_fid fid;
 
 	fid_le_to_cpu(&fid, &lde->lde_fid);
@@ -318,7 +306,7 @@ static int ll_get_name(struct dentry *dentry, char *name, struct dentry *child)
 	struct ll_getname_data lgd = {
 		.lgd_name = name,
 		.lgd_fid = ll_i2info(child->d_inode)->lli_fid,
-		.ctx.actor = (filldir_t)ll_nfs_get_name_filldir,
+		.ctx.actor = ll_nfs_get_name_filldir,
 		.lgd_found = 0,
 	};
 	struct md_op_data *op_data;
@@ -339,7 +327,7 @@ static int ll_get_name(struct dentry *dentry, char *name, struct dentry *child)
 		GOTO(out, rc = PTR_ERR(op_data));
 
 	inode_lock(dir);
-	rc = ll_dir_read(dir, &pos, op_data, &lgd.ctx, NULL);
+	rc = ll_dir_read(dir, &pos, op_data, &lgd.ctx, NULL, &lgd.lgd_lde);
 	inode_unlock(dir);
 	ll_finish_md_op_data(op_data);
 	if (!rc && !lgd.lgd_found)

lustre/tests/sanityn.sh
@@ -6667,7 +6667,7 @@ run_test 101c "Discard DoM data on close-unlink"
 # test to verify we can open by handle an unlinked file from > 1 client
 # This test opens the file normally on $DIR1, which is on one mount, and then
 # opens it by handle on $DIR2, which is on a different mount.
-test_102() {
+test_102a() {
 	(( "$MDS1_VERSION" >= $(version_code 2.11.57) )) ||
 		skip "Needs MDS version 2.11.57 or later"
 
@@ -6697,7 +6697,53 @@ test_102() {
 			error "check_fhandle_syscalls $tdir.stripe/subdir fail"
 	fi
 }
-run_test 102 "Test open by handle of unlinked file"
+run_test 102a "Test open by handle of unlinked file"
+
+test_102b() {
+	local encdir=$DIR/$tdir/encdir
+	local subdir=$encdir/sub
+
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	# arm the restores before touching either mount, so a failure part
+	# way through cannot leave a client unmounted for later subtests
+	local opts=$MOUNT_OPTS
+
+	stack_trap "mount_client $MOUNT2 $opts || error_noexit 'remount2'"
+	stack_trap "umount_client $MOUNT2 || true"
+	stack_trap "mount_client $MOUNT $opts || error_noexit 'remount'"
+	stack_trap "umount_client $MOUNT || true"
+
+	# both mounts need the key: the handle is resolved on $DIR2, and
+	# ll_get_name() there has to return the plaintext name
+	umount_client $MOUNT2 || error "umount $MOUNT2 failed"
+	umount_client $MOUNT || error "umount $MOUNT failed"
+	mount_client $MOUNT ${MOUNT_OPTS},test_dummy_encryption ||
+		error "mount $MOUNT with test_dummy_encryption failed"
+	mount_client $MOUNT2 ${MOUNT_OPTS},test_dummy_encryption ||
+		error "mount $MOUNT2 with test_dummy_encryption failed"
+
+	# registered last, so LIFO removes the encrypted tree while the key
+	# is still loaded
+	stack_trap "rm -rf $DIR/$tdir"
+
+	mkdir -p $subdir || error "mkdir $subdir failed"
+
+	cancel_lru_locks mdc
+
+	# ll_get_name() is only reached for a disconnected directory handle,
+	# and needs the parent's crypt info loaded on the mount doing the
+	# reconnect, so walk it there first
+	ls $DIR2/$tdir/encdir > /dev/null || error "cannot list encdir on $DIR2"
+
+	check_fhandle_syscalls $subdir $MOUNT2 ||
+		error "check_fhandle_syscalls on an encrypted dir failed"
+}
+run_test 102b "open by handle of a dir inside an encrypted dir"
 
 # Compare file size between first & second mount, ensuring the client correctly
 # glimpses even with unused speculative locks - LU-11670