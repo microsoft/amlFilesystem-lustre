lustre/lustre-release

commit 91063238818f1a10dce09fde02ab1af71f528fc8

LU-10499 pcc: Limit attach queue depth
The existing async attach code does not attempt to limit
the number of async attaches that can be requested at once.
This is a problem because we could theoretically create too
many kthreads and overwhelm the system.

When the attach queue depth is exceeded, we stop allowing
new items to be queued by switching over to sync attach.

Ideally we would rebuild the attach code to generate a
queue of attach requests and have the attach thread code
pull items from the queue until it's exhausted, but that's
a much more substantial change and is left for later.

It's really hard to remember what 'inode' and 'file' mean
when there's more than one in a function, so I've redone
some of the names here.

EX-6399 pcc: add tunable parameter for PCC attach thread

Add a tunable parameter llite.*.pcc_attach_thread_max to
tune the max number of kernel threads doing asynchronous
attach (1024 by default).

Was-Change-Id: Ic59c15af935dd8dff586fa6be3939d4322c136d5

Use variable names that are consistent with parameter name.
Add llite.pcc_attach_thread_max.4 man page for parameter.

Fixes: 5be466e87304 ("LU-10499 pcc: async attach in the background for PCC-RO file")
EX-bug-id: EX-5014 EX-6399
Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Signed-off-by: Qian Yingjin <qian@ddn.com>
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: Ib00dfb67f5245a28b722278d031ee8cdf5e190d6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54476
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

Documentation/man4/llite.pcc_attach_thread_max.4
@@ -1,0 +1,71 @@
+.TH LLITE.PCC_ATTACH_THREAD_MAX 4 2026-08-26 "Lustre" "Lustre Kernel Interfaces"
+.SH NAME
+llite.pcc_attach_thread_max \- limit number of PCC copy threads per mount
+.SH SYNOPSIS
+.SY
+.RI "lctl get_param llite." FSNAME *.pcc_attach_thread_max
+.SY
+.RI "lctl set_param llite." FSNAME *.pcc_attach_thread_max= THREAD_COUNT
+.YS
+.SS PROPERTIES
+.TP
+.B Access Permissions
+.br
+.BR 644 " | " -rw-r--r--
+.TP
+.B Scope
+.br
+Per local filesystem mountpoint.
+.TP
+.B Config
+.br
+.B pcc_attach_thread_max
+is always present for client mountpoints.
+.TP
+.B Default
+.br
+.RB pcc_attach_thread_max= 1024
+.TP
+.B Valid Range
+.br
+.RB pcc_attach_thread_max= 1
+.br
+.RB pcc_attach_thread_max= 16384
+.SH DESCRIPTION
+The Persistent Client Cache file attach process starts background file
+copy threads when a cache-eligible file is accessed to perform the
+.BR lfs-pcc-attach (1)
+operation.  The specified
+.I THREAD_COUNT
+is the maximum number of background threads started on each mountpoint.
+The default
+.B 1024
+limit is expected to be sufficient for most uses, but is tunable up or down.
+.SH MODULES
+This parameter is in the following modules:
+.EX
+.B llite.*.pcc_attach_thread_max
+.EE
+.SH EXAMPLES
+Permanently limit the number of background copy threads per mountpoint to 16:
+.RB "mgs# " "lctl set_param -P llite.*.pcc_attach_thread_max=16"
+.EE
+.PP
+Limit the number of background copy threads to 32 for the
+.B testfs
+filesystem on the local
+.BR client :
+.EX
+.RB "client# " "lctl set_param llite.testfs-*.enable_setstripe_gid=32"
+.EE
+.SH AVAILABILITY
+The
+.B llite.pcc_attach_thread_max
+parameter is part of the
+.BR lustre (7)
+filesystem package since release 2.18.0.
+.\" Added in commit 2.17.57-xx
+.SH SEE ALSO
+.BR lfs-pcc-attach (1),
+.BR lctl-get_param (8),
+.BR lctl-set_param (8)

lustre/llite/lproc_llite.c
@@ -881,14 +881,49 @@ static ssize_t pcc_async_affinity_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(pcc_async_affinity);
 
-static ssize_t pcc_mode_show(struct kobject *kobj, struct attribute *attr,
-			      char *buffer)
+static ssize_t
+pcc_attach_thread_max_show(struct kobject *kobj, struct attribute *attr,
+			       char *buffer)
 {
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);
 	struct pcc_super *super = &sbi->ll_pcc_super;
 
-	return sprintf(buffer, "0%o\n", super->pccs_mode);
+	return scnprintf(buffer, PAGE_SIZE, "%u\n",
+			 super->pccs_attach_thread_max);
+}
+
+static ssize_t
+pcc_attach_thread_max_store(struct kobject *kobj, struct attribute *attr,
+				const char *buffer, size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	if (val < 1 || val > PCC_ATTACH_THREAD_MAX_MAX)
+		return -EOVERFLOW;
+
+	super->pccs_attach_thread_max = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(pcc_attach_thread_max);
+
+static ssize_t pcc_mode_show(struct kobject *kobj, struct attribute *attr,
+			     char *buffer)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+
+	return scnprintf(buffer, PAGE_SIZE, "0%o\n", super->pccs_mode);
 
 }
 
@@ -2672,6 +2707,7 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_opencache_max_ms.attr,
 	&lustre_attr_parallel_dio.attr,
 	&lustre_attr_pcc_async_threshold.attr,
+	&lustre_attr_pcc_attach_thread_max.attr,
 	&lustre_attr_pcc_mode.attr,
 	&lustre_attr_pcc_async_affinity.attr,
 	&lustre_attr_read_ahead_async_file_threshold_mb.attr,

lustre/llite/pcc.c
@@ -115,6 +115,8 @@ int pcc_super_init(struct pcc_super *super)
 	super->pccs_generation = 1;
 	super->pccs_async_threshold = PCC_DEFAULT_ASYNC_THRESHOLD;
 	super->pccs_mode = 0400;
+	atomic_set(&super->pccs_attach_thread, 0);
+	super->pccs_attach_thread_max = PCC_ATTACH_THREAD_MAX_DEFAULT;
 
 	return 0;
 }
@@ -1780,6 +1782,7 @@ static struct pcc_attach_context *
 pcc_attach_context_alloc(struct file *file, struct inode *inode, __u32 id)
 {
 	struct pcc_attach_context *pccx;
+	struct pcc_super *super = ll_i2pccs(inode);
 
 	OBD_ALLOC_PTR(pccx);
 	if (!pccx)
@@ -1788,12 +1791,16 @@ pcc_attach_context_alloc(struct file *file, struct inode *inode, __u32 id)
 	pccx->pccx_file = get_file(file);
 	pccx->pccx_inode = inode;
 	pccx->pccx_attach_id = id;
+	atomic_inc(&super->pccs_attach_thread);
 
 	return pccx;
 }
 
 static inline void pcc_attach_context_free(struct pcc_attach_context *pccx)
 {
+	struct pcc_super *super = ll_i2pccs(pccx->pccx_inode);
+
+	atomic_dec(&super->pccs_attach_thread);
 	LASSERT(pccx->pccx_file != NULL);
 	fput(pccx->pccx_file);
 	OBD_FREE_PTR(pccx);
@@ -1910,16 +1917,33 @@ out:
 	RETURN(rc);
 }
 
-static int pcc_readonly_attach_sync(struct file *file,
-				    struct inode *inode, __u32 roid);
+static int pcc_readonly_attach_sync(struct file *file, struct inode *inode,
+				    u32 roid);
 
-static inline int pcc_do_readonly_attach(struct file *file,
-					 struct inode *inode, __u32 roid)
+static inline int pcc_do_readonly_attach(struct file *file, struct inode *inode,
+					 u32 roid)
 {
+	struct pcc_super *super = ll_i2pccs(inode);
+	bool async = true;
 	int rc;
 
-	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) >=
-	    ll_i2pccs(inode)->pccs_async_threshold) {
+	/* force sync if we're over the active attach limit, so this thread
+	 * can't keep increasing the thread count.  This lets us exceed the
+	 * number of active threads by $NUMTHREADS, but that should be fine
+	 * as long as we avoid unbounded numbers of kthreads.
+	 */
+	if (atomic_read(&super->pccs_attach_thread) >=
+	    super->pccs_attach_thread_max)
+		async = false;
+	/* if the file size is < the async threshold, don't do async */
+	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) <
+	    super->pccs_async_threshold) {
+		CDEBUG(D_CACHE, "%s: attach thread limit %u hit, using sync\n",
+		       ll_i2sbi(inode)->ll_fsname, super->pccs_attach_thread_max);
+		async = false;
+	}
+
+	if (async) {
 		rc = pcc_readonly_attach_async(file, inode, roid);
 		if (!rc || rc == -EINPROGRESS)
 			return rc;
@@ -3682,11 +3706,12 @@ static int pcc_filp_write(struct file *filp, const void *buf, ssize_t count,
 
 static ssize_t pcc_copy_data(struct file *src, struct file *dst)
 {
+	size_t buf_len = PCC_COPY_BUFFER_BYTES;
+	struct inode *inode = file_inode(src);
+	loff_t offset = 0;
 	ssize_t rc = 0;
 	ssize_t rc2;
-	loff_t pos, offset = 0;
-	size_t buf_len = 1048576;
-	struct inode *inode = file_inode(src);
+	loff_t pos;
 	void *buf;
 
 	ENTRY;
@@ -3735,10 +3760,10 @@ out_free:
 
 static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 				   struct pcc_dataset *dataset,
-				   struct dentry **dentry)
+				   struct dentry **pcc_dentry)
 {
 	const struct cred *old_cred;
-	struct file *pcc_filp;
+	struct file *pcc_file;
 	bool direct = false;
 	struct path path;
 	ssize_t ret;
@@ -3748,12 +3773,13 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 	ENTRY;
 
 	old_cred = override_creds(pcc_super_cred(inode->i_sb));
-	rc = __pcc_inode_create(dataset, &ll_i2info(inode)->lli_fid, dentry);
+	rc = __pcc_inode_create(dataset, &ll_i2info(inode)->lli_fid,
+				pcc_dentry);
 	if (rc)
 		GOTO(out_cred, rc);
 
 	path.mnt = dataset->pccd_path.mnt;
-	path.dentry = *dentry;
+	path.dentry = *pcc_dentry;
 	/* If the inode is encrypted, we want the PCC file to be synced to the
 	 * storage. This is necessary as we are going to decrypt the page cache
 	 * pages of the PCC inode later in pcc_file_read_iter(), but still we
@@ -3761,18 +3787,18 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 	 */
 	if (IS_ENCRYPTED(inode))
 		flags |= O_SYNC;
-	pcc_filp = dentry_open(&path, flags, current_cred());
-	if (IS_ERR_OR_NULL(pcc_filp)) {
-		rc = pcc_filp == NULL ? -EINVAL : PTR_ERR(pcc_filp);
+	pcc_file = dentry_open(&path, flags, current_cred());
+	if (IS_ERR_OR_NULL(pcc_file)) {
+		rc = pcc_file == NULL ? -EINVAL : PTR_ERR(pcc_file);
 		GOTO(out_dentry, rc);
 	}
 
-	rc = pcc_inode_reset_iattr(inode, *dentry, ATTR_UID | ATTR_GID,
+	rc = pcc_inode_reset_iattr(inode, *pcc_dentry, ATTR_UID | ATTR_GID,
 				   old_cred->uid, old_cred->gid, 0);
 	if (rc)
 		GOTO(out_fput, rc);
 
-	rc = pcc_file_reset_projid(dataset, pcc_filp,
+	rc = pcc_file_reset_projid(dataset, pcc_file,
 				    ll_i2info(inode)->lli_projid);
 	if (rc)
 		GOTO(out_fput, rc);
@@ -3791,26 +3817,29 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 		direct = true;
 	}
 
-	ret = pcc_copy_data(file, pcc_filp);
+	ret = pcc_copy_data(file, pcc_file);
 	if (direct)
 		file->f_flags |= O_DIRECT;
+
+	CDEBUG(D_CACHE, "Copied data from OSTs to PCC for %pd: rc = %llu\n",
+	       *pcc_dentry, (long long)ret);
+
 	if (ret < 0)
 		GOTO(out_fput, rc = ret);
-
 	/*
 	 * It must to truncate the PCC copy to the same size of the Lustre
 	 * copy after copy data. Otherwise, it may get wrong file size after
 	 * re-attach a file. See LU-13023 for details.
 	 */
-	rc = pcc_inode_reset_iattr(inode, *dentry,
+	rc = pcc_inode_reset_iattr(inode, *pcc_dentry,
 				   ATTR_SIZE | ATTR_MTIME | ATTR_MTIME_SET,
 				   KUIDT_INIT(0), KGIDT_INIT(0), ret);
 out_fput:
-	fput(pcc_filp);
+	fput(pcc_file);
 out_dentry:
 	if (rc) {
-		pcc_inode_remove(inode, *dentry);
-		dput(*dentry);
+		pcc_inode_remove(inode, *pcc_dentry);
+		dput(*pcc_dentry);
 	}
 out_cred:
 	revert_creds(old_cred);

Documentation/man1/lfs-pcc-attach.1
@@ -71,4 +71,5 @@ filesystem package since release 2.13.0.
 .BR lfs-pcc-pin (1),
 .BR lfs-pcc-state (1),
 .BR llapi_pcc_attach (3),
+.BR llite.pcc_attach_thread_max (4),
 .BR lctl-pcc (8)

lustre/llite/pcc.h
@@ -136,7 +136,20 @@ struct pcc_dataset {
 };
 
 #define PCC_DEFAULT_ASYNC_THRESHOLD	(256 << 20)
+/* buffer size for kernel buffered I/O for pcc attach
+ *
+ * performance barely varies for buffered I/O once size is more than a few
+ * hundred KiB, so just leave this at 1 MiB
+ */
+#define PCC_COPY_BUFFER_BYTES	(1 << 20) /* 1 MiB */
 
+/* after this many attaches are queued up, fall back to sync attach.  each
+ * attach creates a kthread, so we don't allow too many at once, but sync
+ * attach is very bad for applications, so we try to be generous.
+ */
+#define PCC_ATTACH_THREAD_MAX_DEFAULT	1024
+/* an unreasonably large number of threads, but systems always get larger */
+#define PCC_ATTACH_THREAD_MAX_MAX	16384
 struct pcc_super {
 	/* Protect pccs_datasets */
 	struct rw_semaphore	 pccs_rw_sem;
@@ -154,6 +167,8 @@ struct pcc_super {
 	__u64			 pccs_async_threshold;
 	bool			 pccs_async_affinity;
 	umode_t			 pccs_mode;
+	atomic_t		 pccs_attach_thread;
+	unsigned int		 pccs_attach_thread_max;
 };
 
 struct pcc_inode {

microsoft/amlFilesystem-lustre

commit 8b7124aeabc4db04a05e6ce55c1d37d2c2ffb13c

LU-10499 pcc: fix attach thread limit debug and man page
The CDEBUG that reports the attach thread limit forcing a
synchronous attach sits in the file-size branch of
pcc_do_readonly_attach(), so it prints for every file below
pccs_async_threshold and never for the case it describes.  Move
it into the thread-limit branch, which is the one an admin needs
to see when an attach silently becomes synchronous inside open(),
and print the active attach count next to the limit, since the
count can legitimately run past the limit by the number of
threads attaching concurrently.

pcc_attach_data_archive() prints the ssize_t returned by
pcc_copy_data() with %llu, so an error return renders as a huge
unsigned value instead of the negative errno.  Use %zd.

In llite.pcc_attach_thread_max.4, give the first example its
missing .EX, correct the second example to set
pcc_attach_thread_max rather than enable_setstripe_gid, resolve
the "Added in commit" placeholder, and say that the limit is
advisory and that attaches past it run synchronously inside
open().  Also align the continuation lines of the
pcc_attach_thread_max sysfs handlers with the open parenthesis.

Test-Parameters: trivial
Fixes: 91063238818f ("LU-10499 pcc: Limit attach queue depth")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I8144474943d581a4061d4741c703cdda4aae974a

Documentation/man4/llite.pcc_attach_thread_max.4
@@ -38,6 +38,16 @@ copy threads when a cache-eligible file is accessed to perform the
 operation.  The specified
 .I THREAD_COUNT
 is the maximum number of background threads started on each mountpoint.
+The limit is advisory: the running count is sampled before the attach is
+set up and is only incremented during that setup, so threads entering the
+attach path at the same time can each pass the check.  The number of
+running threads can exceed
+.I THREAD_COUNT
+by the number of such threads, for as long as their copies take.
+Once the limit is reached the attach is done synchronously inside
+.BR open (2)
+rather than in a background thread, so the opening process blocks until
+the copy finishes.
 The default
 .B 1024
 limit is expected to be sufficient for most uses, but is tunable up or down.
@@ -48,6 +58,7 @@ This parameter is in the following modules:
 .EE
 .SH EXAMPLES
 Permanently limit the number of background copy threads per mountpoint to 16:
+.EX
 .RB "mgs# " "lctl set_param -P llite.*.pcc_attach_thread_max=16"
 .EE
 .PP
@@ -56,7 +67,7 @@ Limit the number of background copy threads to 32 for the
 filesystem on the local
 .BR client :
 .EX
-.RB "client# " "lctl set_param llite.testfs-*.enable_setstripe_gid=32"
+.RB "client# " "lctl set_param llite.testfs-*.pcc_attach_thread_max=32"
 .EE
 .SH AVAILABILITY
 The
@@ -64,7 +75,7 @@ The
 parameter is part of the
 .BR lustre (7)
 filesystem package since release 2.18.0.
-.\" Added in commit 2.17.57-xx
+.\" Added in commit v2_17_57-117-g9106323881
 .SH SEE ALSO
 .BR lfs-pcc-attach (1),
 .BR lctl-get_param (8),

lustre/llite/lproc_llite.c
@@ -883,7 +883,7 @@ LUSTRE_RW_ATTR(pcc_async_affinity);
 
 static ssize_t
 pcc_attach_thread_max_show(struct kobject *kobj, struct attribute *attr,
-			       char *buffer)
+			   char *buffer)
 {
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);
@@ -895,7 +895,7 @@ pcc_attach_thread_max_show(struct kobject *kobj, struct attribute *attr,
 
 static ssize_t
 pcc_attach_thread_max_store(struct kobject *kobj, struct attribute *attr,
-				const char *buffer, size_t count)
+			    const char *buffer, size_t count)
 {
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);

lustre/llite/pcc.c
@@ -1925,6 +1925,7 @@ static inline int pcc_do_readonly_attach(struct file *file, struct inode *inode,
 {
 	struct pcc_super *super = ll_i2pccs(inode);
 	bool async = true;
+	int active;
 	int rc;
 
 	/* force sync if we're over the active attach limit, so this thread
@@ -1932,16 +1933,18 @@ static inline int pcc_do_readonly_attach(struct file *file, struct inode *inode,
 	 * number of active threads by $NUMTHREADS, but that should be fine
 	 * as long as we avoid unbounded numbers of kthreads.
 	 */
-	if (atomic_read(&super->pccs_attach_thread) >=
-	    super->pccs_attach_thread_max)
-		async = false;
-	/* if the file size is < the async threshold, don't do async */
-	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) <
-	    super->pccs_async_threshold) {
-		CDEBUG(D_CACHE, "%s: attach thread limit %u hit, using sync\n",
-		       ll_i2sbi(inode)->ll_fsname, super->pccs_attach_thread_max);
+	active = atomic_read(&super->pccs_attach_thread);
+	if (active >= super->pccs_attach_thread_max) {
+		CDEBUG(D_CACHE,
+		       "%s: %d attaches active, limit %u: using sync\n",
+		       ll_i2sbi(inode)->ll_fsname, active,
+		       super->pccs_attach_thread_max);
 		async = false;
 	}
+	/* if the file size is < the async threshold, don't do async */
+	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) <
+	    super->pccs_async_threshold)
+		async = false;
 
 	if (async) {
 		rc = pcc_readonly_attach_async(file, inode, roid);
@@ -3821,8 +3824,8 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 	if (direct)
 		file->f_flags |= O_DIRECT;
 
-	CDEBUG(D_CACHE, "Copied data from OSTs to PCC for %pd: rc = %llu\n",
-	       *pcc_dentry, (long long)ret);
+	CDEBUG(D_CACHE, "Copied data from OSTs to PCC for %pd: rc = %zd\n",
+	       *pcc_dentry, ret);
 
 	if (ret < 0)
 		GOTO(out_fput, rc = ret);