lustre/lustre-release

commit cb91f676ee19e10bd335f199bd5928cc2dd9a28b

LU-19276 llapi: add llapi_name_validate() helper
Add new llapi_name_validate() helper function to check if names
contain only alphanumeric and a few extra characters, and are
within the required length.  Use in mkfs_lustre.c::parse_opts(),
llapi_name_verify(), and llapi_pool_name_validate() to improve
argument validation and avoid duplication in lfs_arg_insane(),
lqa_name_insane(), and other places open-coding similar checks.
This also fixes various bugs in existing name validation.

The internal helper function llapi_name_verify() and its wrappers
llint_lqa_name_verify() and llint_pool_name_verify() check return
codes and prints an error message for the various typical returns.

Also validate nodemap names in lctl to print a better error message.

Add llapi_name_validate.3 and llapi_pool_name_validate.3 man pages.
Update commit for other pool-related man pages and sort in Makefile.
Improve generate-manpage.sh a bit while generating these man pages.

Test-Parameters: trivial
Fixes: ae7c99ac87a0 ("LU-18222 quota: add LQA lfs setquota/quota commands")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I732366b9972ba7c2d8ed5b49a405647e22300c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65994
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Reviewed-by: Xiao Yang <xyang@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

Documentation/man3/llapi_layout_pool_name_get.3
@@ -1,4 +1,4 @@
-.TH LLAPI_LAYOUT_POOL_NAME_GET 3 2024-08-27 "Lustre User API" "Lustre Library Functions"
+.TH LLAPI_LAYOUT_POOL_NAME_GET 3 2026-08-23 "Lustre User API" "Lustre Library Functions"
 .SH NAME
 llapi_layout_pool_name_get, llapi_layout_pool_name_set \- get or set the OST pool name of a Lustre file
 .SH SYNOPSIS
@@ -17,22 +17,29 @@ stores into
 .I pool_name
 up to
 .I n
-characters of the name of the pool of OSTS associated with
+characters of the name of the pool of OSTs associated with
 .IR layout .
 .PP
 .B llapi_layout_pool_name_set()
 sets the OST pool name of
-.I  layout
+.I layout
 to
-.IR pool_name .
-If
+.IR pool_name ,
+up to
+.B LOV_MAXPOOLNAME
+characters.  If
 .I pool_name
 uses "fsname.pool" notation to qualify the pool name
 with a filesystem name, the "fsname." portion will be silently
 discarded before storing the value.
 .PP
 .B llapi_layout_pool_name_set()
-does not validate that
+will validate that the
+.I pool_name
+is not longer than the maximum allowed name and only contains
+valid characters (per
+.I llapi_pool_name_validate (3)),
+but does not validate that
 .I pool_name
 names an existing non-empty pool, since it is not known a priori which
 filesystem
@@ -49,6 +56,9 @@ and
 return 0 on success, or -1 if an error occurred (in which case, errno is
 set appropriately).
 .SH ERRORS
+Errors from
+.IR llapi_pool_name_validate (3)
+will be returned on malformed pool names.
 .TP 15
 .B EINVAL
 An invalid argument was specified.
@@ -74,5 +84,6 @@ user application interface library since release 2.7.0
 .BR lctl (1),
 .BR llapi_layout_alloc (3),
 .BR llapi_layout_file_open (3),
+.BR llapi_pool_name_validate (3),
 .BR llapi_layout (7),
 .BR lustreapi (7)

Documentation/man3/llapi_name_validate.3
@@ -1,0 +1,78 @@
+.TH LLAPI_NAME_VALIDATE 3 2026-08-23 "Lustre User API" "Lustre Library Functions"
+.SH NAME
+llapi_name_validate() \- check string contains only valid characters
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_name_validate(const char *" name ", const char *" extra_chars \c
+.BI ", int " maxlen );
+.fi
+.SH DESCRIPTION
+.B llapi_name_validate()
+verifies that the characters in
+.I name
+contain only alphanumeric characters,
+plus a limited number of other characters in
+.IR extra_chars ,
+and is not longer than
+.I maxlen
+bytes in length (excluding NUL terminator).
+This ensures that
+.I name
+is suitable for use in filesystem identifiers such as the filesystem name,
+OST pool name, nodemap name, and similar.
+.SH RETURN VALUE
+.B llapi_name_validate()
+returns 0 on success, or a negative errno value on failure.
+.SH ERRORS
+.TP 15
+.SM EINVAL
+The supplied
+.B name
+contained an illegal character.
+.TP
+.SM ENAMETOOLONG
+The supplied
+.B name
+exceeded
+.B maxlen
+characters without a NUL terminator.
+.TP
+.SM ENXIO
+The
+.B name
+was NULL or empty.
+.SH EXAMPLES
+Check if
+.B fsname
+contains only alphanumeric, dash
+.RB (' - '),
+and underscore
+.RB (' _ ')
+characters and does not exceed the maximum filesystem name length:
+.PP
+.EX
+#include <string.h>
+#include <lustre/lustreapi.h>
+
+int main(int argc, char *argv[])
+{
+        char fsname[] = "newfs123";
+
+        rc = llapi_name_validate(fsname, "-_", LUSTRE_MAXFSNAME);
+        if (rc < 0) {
+                fprintf(stderr, "%s: fsname '%s' invalid: %s\\n",
+                        argv[1], fsname, strerror(errno));
+                return rc;
+        }
+}
+.EE
+.SH AVAILABILITY
+.B llapi_name_validate
+is part of the
+.BR lustre (7)
+filesystem package since release 2.18.0.
+.\" added in commit 2.17.52.xx
+.SH SEE ALSO
+.BR lustreapi (7)

Documentation/man3/llapi_pool_pin_file.3
@@ -1,4 +1,4 @@
-.TH LLAPI_POOL_PIN_FILE 3 2026-03-19 "Lustre User API" "Lustre Library Functions"
+.TH LLAPI_POOL_PIN_FILE 3 2026-08-23 "Lustre User API" "Lustre Library Functions"
 .SH NAME
 llapi_pool_pin_fd, llapi_pool_pin_file, llapi_pool_pin_fid \- pin a Lustre file to an OST pool
 .SH SYNOPSIS
@@ -82,14 +82,12 @@ On failure they return a negative errno value and set
 .B errno
 to the corresponding error code.
 .SH ERRORS
+Errors from
+.IR llapi_pool_name_validate (3)
+will be returned on malformed pool names.
 .TP 15
 .B EINVAL
-An invalid argument was specified,
-such as a NULL or overly long
-.I pool_name
-string.
-.PP
-.in +8n
+An invalid argument was specified.
 .SH EXAMPLES
 .EX
 #include <lustre/lustreapi.h>
@@ -109,10 +107,11 @@ The
 functions are part of the
 .BR lustre (7)
 user application interface library since release 2.18.0.
-.\" Added in commit v2_17-51-gxxx
+.\" Added in commit v2_17_52-151-g86b0c90666
 .SH SEE ALSO
-.BR llapi_pool_unpin_file (3),
 .BR llapi_pool_is_pinned_file (3),
+.BR llapi_pool_name_validate (3),
+.BR llapi_pool_unpin_file (3),
 .BR lustre (7),
 .BR lustreapi (7)
 

Documentation/man3/llapi_pool_unpin_file.3
@@ -1,4 +1,4 @@
-.TH LLAPI_POOL_UNPIN_FILE 3 2026-03-19 "Lustre User API" "Lustre Library Functions"
+.TH LLAPI_POOL_UNPIN_FILE 3 2026-08-23 "Lustre User API" "Lustre Library Functions"
 .SH NAME
 llapi_pool_unpin_fd, llapi_pool_unpin_file, llapi_pool_unpin_fid \- remove pool pinning from a Lustre file
 .SH SYNOPSIS
@@ -72,11 +72,12 @@ and set
 .B errno
 to the corresponding error code.
 .SH ERRORS
+Errors from
+.IR llapi_pool_name_validate (3)
+will be returned on malformed pool names.
 .TP 15
 .B EINVAL
-An invalid argument was specified, such as a NULL or overly long
-.I pool_name
-string.
+An invalid argument was specified.
 .TP 15
 .B ENOENT
 No pool entry for the specified
@@ -86,8 +87,6 @@ was found in the
 attribute when calling one of the
 .B llapi_pool_unpin_*()
 functions, or the attribute itself was missing.
-.PP
-.in +8n
 .SH EXAMPLES
 .EX
 #include <lustre/lustreapi.h>
@@ -111,10 +110,11 @@ and
 functions are part of the
 .BR lustre (7)
 user application interface library since release 2.18.0.
-.\" Added in commit v2_17-51-gxxx
+.\" Added in commit v2_17_52-151-g86b0c90666
 .SH SEE ALSO
-.BR llapi_pool_pin_file (3),
 .BR llapi_pool_is_pinned_file (3),
+.BR llapi_pool_name_validate (3),
+.BR llapi_pool_pin_file (3),
 .BR lustre (7),
 .BR lustreapi (7)
 

lustre/utils/liblustreapi_util.c
@@ -12,6 +12,7 @@
  * Author: Frank Zago <fzago@cray.com>
  */
 
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -20,7 +21,6 @@
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -701,4 +701,73 @@ int llapi_convert_mask2str(char *str, int size, __u64 mask,
 		str[size - 1] = '\0';
 
 	return len;
+}
+
+/** llapi_name_validate() - check string contains only valid characters
+ *
+ * Verify that the supplied @name contains only alphanumeric characters,
+ * plus a limited number of @extra_chars (e.g. '-' and '_'), and
+ * is no longer than @maxlen characters, so that it can be used as an
+ * filesystem identifier (fsname, OST pool, nodemap, etc.)
+ *
+ * @name        String to check for validity
+ * @extra_chars Extra characters to allow in @name
+ * @maxlen      Maximum number of characters in @name
+ * @return 0 if @name has only alphanumeric and extra chars,
+ *        -ve error number otherwise and sets @errno
+ */
+int llapi_name_validate(const char *name, const char *extra_chars,
+			unsigned int maxlen)
+{
+	int len = 0;
+	int rc;
+
+	if (!name || !*name) {
+		rc = -ENXIO;
+		goto out;
+	}
+
+	if (!extra_chars)
+		extra_chars = "";
+
+	while (*name && len++ < maxlen) {
+		if (!isalnum((unsigned char)*name) &&
+		    strchr(extra_chars, *name) == NULL) {
+			rc = -EINVAL;
+			goto out;
+		}
+		name++;
+	}
+	if (len > maxlen) {
+		rc = -ENAMETOOLONG;
+		goto out;
+	}
+
+	return 0;
+out:
+	errno = -rc;
+	return rc;
+}
+
+/* check @name and print error message if it is too long or has illegal chars */
+int llapi_name_verify(const char *name, const char *extra_chars,
+		      unsigned int maxlen, const char *type)
+{
+	int rc;
+
+	rc = llapi_name_validate(name, extra_chars, maxlen);
+	if (rc == -ENAMETOOLONG) {
+		llapi_error(LLAPI_MSG_ERROR, -rc,
+			    "%s name '%s' longer than maximum %u chars",
+			    type, name, maxlen);
+	} else if (rc == -ENXIO) {
+		llapi_error(LLAPI_MSG_ERROR, -rc,
+			    "%s name must be 1-%u characters",
+			    type, maxlen);
+	} else if (rc < 0) {
+		llapi_error(LLAPI_MSG_ERROR, -rc, "%s name '%s' not valid",
+			    type, name);
+	}
+
+	return rc;
 }

Documentation/man3/Makefile.am
@@ -90,6 +90,7 @@ LIBMAN = 					\
 	llapi_lov_get_uuids.3			\
 	llapi_lov_pattern_string.3		\
 	llapi_lov_string_pattern.3		\
+	llapi_name_validate.3			\
 	llapi_open_by_fid.3			\
 	llapi_open_by_fid_at.3			\
 	llapi_param_get_paths.3			\
@@ -109,15 +110,16 @@ LIBMAN = 					\
 	llapi_pcc_state_get_fd.3		\
 	llapi_pcc_clear.3			\
 	llapi_pcc_del.3				\
-	llapi_pool_pin_file.3			\
-	llapi_pool_pin_fd.3			\
-	llapi_pool_pin_fid.3			\
-	llapi_pool_unpin_file.3			\
-	llapi_pool_unpin_fd.3			\
-	llapi_pool_unpin_fid.3			\
-	llapi_pool_is_pinned_file.3		\
 	llapi_pool_is_pinned_fd.3		\
 	llapi_pool_is_pinned_fid.3		\
+	llapi_pool_is_pinned_file.3		\
+	llapi_pool_name_validate.3		\
+	llapi_pool_pin_fd.3			\
+	llapi_pool_pin_fid.3			\
+	llapi_pool_pin_file.3			\
+	llapi_pool_unpin_fd.3			\
+	llapi_pool_unpin_fid.3			\
+	llapi_pool_unpin_file.3			\
 	llapi_project_close.3			\
 	llapi_project_fgetnam.3			\
 	llapi_project_fgetprjid.3		\

Documentation/man3/llapi_pool_is_pinned_file.3
@@ -1,4 +1,4 @@
-.TH LLAPI_POOL_IS_PINNED_FILE 3 2026-03-19 "Lustre User API" "Lustre Library Functions"
+.TH LLAPI_POOL_IS_PINNED_FILE 3 2026-08-23 "Lustre User API" "Lustre Library Functions"
 .SH NAME
 llapi_pool_is_pinned_fd, llapi_pool_is_pinned_file, llapi_pool_is_pinned_fid \- test whether a Lustre file is pinned to an OST pool
 .SH SYNOPSIS
@@ -89,13 +89,18 @@ if not, and a negative error code
 on failure.
 .SH ERRORS
 .TP 15
-.B EINVAL
-An invalid argument was specified,
-such as a NULL or overly long
+.B ENAMETOOLONG
+The
 .I pool_name
-string.
-.PP
-.in +8n
+exceeds the maximum number of characters.
+.TP 15
+.B ENXIO
+The
+.I pool_name
+was not specified.
+.TP 15
+.B EINVAL
+An invalid argument was specified.
 .SH EXAMPLES
 .EX
 #include <lustre/lustreapi.h>
@@ -127,8 +132,9 @@ and
 functions are part of the
 .BR lustre (7)
 user application interface library since release 2.18.0.
-.\" Added in commit v2_17-51-gxxx
+.\" Added in commit v2_17_52-151-g86b0c90666
 .SH SEE ALSO
+.BR llapi_pool_name_validate (3),
 .BR llapi_pool_pin_file (3),
 .BR llapi_pool_unpin_file (3),
 .BR lustre (7),

Documentation/man3/llapi_pool_name_validate.3
@@ -1,0 +1,84 @@
+.TH LLAPI_POOL_NAME_VALIDATE 3 2026-08-23 "Lustre User API" "Lustre Library Functions"
+.SH NAME
+llapi_pool_name_validate() \- check pool name contains only valid characters
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_pool_name_validate(const char **" pool_name ");"
+.fi
+.SH DESCRIPTION
+.B llapi_pool_name_validate()
+verifies that the characters in
+.I pool_name
+contain only alphanumeric characters, '-', and '_', and is not longer than
+.I LOV_MAXPOOLNAME
+(15) bytes in length (excluding NUL terminator).
+If
+.I pool_name
+is of the form
+.IR fsname . pool_name
+then the leading
+.I fsname.
+part of the pool name is removed and the
+.I pool_name
+pointer will only point at the pool name upon return.
+This ensures that
+.I pool_name
+is suitable for use in filesystem pool identifiers.
+If the removal of the leading
+.I fsname.
+component of the pool name is undesirable, then
+.BR llapi_name_validate (3)
+should be used to verify the pool name.
+.SH RETURN VALUE
+.B llapi_pool_name_validate()
+returns 0 on success, or a negative errno value on failure.
+.SH ERRORS
+.TP 15
+.SM EINVAL
+The supplied
+.B name
+contained an illegal character.
+.TP
+.SM ENAMETOOLONG
+The supplied
+.B name
+exceeded
+.B LOV_MAXPOOLNAME
+characters without a NUL terminator.
+.TP
+.SM ENXIO
+The
+.B name
+was NULL or empty.
+.SH EXAMPLES
+Check if
+.B pool_name
+can be used as a pool identifier.
+.PP
+.EX
+#include <string.h>
+#include <lustre/lustreapi.h>
+
+int main(int argc, char *argv[])
+{
+        char pool_name[] = "newfs123.testpool";
+
+        rc = llapi_pool_name_validate(&pool_name);
+        if (rc < 0) {
+                fprintf(stderr, "%s: pool name '%s' invalid: %s\\n",
+                        argv[1], pool_name, strerror(errno));
+                return rc;
+        }
+}
+.EE
+.SH AVAILABILITY
+.B llapi_pool_name_validate
+is part of the
+.BR lustre (7)
+filesystem package since release 2.18.0.
+.\" added in commit v2_17_56
+.SH SEE ALSO
+.BR llapi_name_validate (3),
+.BR lustreapi (7)

microsoft/amlFilesystem-lustre

commit 08f88ad3ebd887dc52bfc0e71b95744900076bd0

LU-19276 llapi: fix llapi_name_validate() docs and signedness
The llapi_name_validate.3 synopsis still declares maxlen as "int",
while include/lustre/lustreapi.h declares it "unsigned int".  Sync
the man page with the header, and make the local length counter
unsigned as well so that it no longer mixes signedness with maxlen
in the loop and length comparisons.

The llapi_pool_name_validate(3) cross reference in
llapi_layout_pool_name_get.3 uses a bare ".I", which typesets a
stray space in front of the section number.  Use ".BR" there, and
for the remaining ".IR" cross references in that file, in
llapi_pool_pin_file.3 and in llapi_pool_unpin_file.3, to match the
man-pages(7) convention used by the rest of Documentation/man3.

Test-Parameters: trivial
Fixes: cb91f676ee19 ("LU-19276 llapi: add llapi_name_validate() helper")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Ib2c82131b0c2d06aeed8ebdc96064bd739b67240

Documentation/man3/llapi_layout_pool_name_get.3
@@ -38,7 +38,7 @@ will validate that the
 .I pool_name
 is not longer than the maximum allowed name and only contains
 valid characters (per
-.I llapi_pool_name_validate (3)),
+.BR llapi_pool_name_validate (3)),
 but does not validate that
 .I pool_name
 names an existing non-empty pool, since it is not known a priori which
@@ -57,7 +57,7 @@ return 0 on success, or -1 if an error occurred (in which case, errno is
 set appropriately).
 .SH ERRORS
 Errors from
-.IR llapi_pool_name_validate (3)
+.BR llapi_pool_name_validate (3)
 will be returned on malformed pool names.
 .TP 15
 .B EINVAL

Documentation/man3/llapi_name_validate.3
@@ -5,8 +5,8 @@ llapi_name_validate() \- check string contains only valid characters
 .nf
 .B #include <lustre/lustreapi.h>
 .PP
-.BI "int llapi_name_validate(const char *" name ", const char *" extra_chars \c
-.BI ", int " maxlen );
+.BI "int llapi_name_validate(const char *" name ", const char *" extra_chars ","
+.BI "                        unsigned int " maxlen ");"
 .fi
 .SH DESCRIPTION
 .B llapi_name_validate()

Documentation/man3/llapi_pool_pin_file.3
@@ -83,7 +83,7 @@ On failure they return a negative errno value and set
 to the corresponding error code.
 .SH ERRORS
 Errors from
-.IR llapi_pool_name_validate (3)
+.BR llapi_pool_name_validate (3)
 will be returned on malformed pool names.
 .TP 15
 .B EINVAL

Documentation/man3/llapi_pool_unpin_file.3
@@ -73,7 +73,7 @@ and set
 to the corresponding error code.
 .SH ERRORS
 Errors from
-.IR llapi_pool_name_validate (3)
+.BR llapi_pool_name_validate (3)
 will be returned on malformed pool names.
 .TP 15
 .B EINVAL

lustre/utils/liblustreapi_util.c
@@ -719,7 +719,7 @@ int llapi_convert_mask2str(char *str, int size, __u64 mask,
 int llapi_name_validate(const char *name, const char *extra_chars,
 			unsigned int maxlen)
 {
-	int len = 0;
+	unsigned int len = 0;
 	int rc;
 
 	if (!name || !*name) {