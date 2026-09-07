lustre/lustre-release

commit c413d2ede5dfe71a89878aed305a8d61f03a30a3

LU-19014 memcg: fix client hang in balance_dirty_pages()
Two nodes (at least) append write a shared file in Lustre with
memcg enabled.
The client randomly hung in balance_dirty_pages() with the
following call trace:
[<0>] balance_dirty_pages+0x2ee/0xd10
[<0>] balance_dirty_pages_ratelimited_flags+0x27a/0x380
[<0>] generic_perform_write+0x150/0x210
[<0>] vvp_io_write_start+0x516/0xc00 [lustre]
[<0>] cl_io_start+0x5a/0x110 [obdclass]
[<0>] cl_io_loop+0x97/0x1f0 [obdclass]
[<0>] ll_file_io_generic+0x4d2/0xe50 [lustre]
[<0>] do_file_write_iter+0x3e9/0x5d0 [lustre]
[<0>] vfs_write+0x2cb/0x410
[<0>] ksys_write+0x5f/0xe0
[<0>] do_syscall_64+0x5c/0xf0

After analyze the core dump of the hung system, we found that the
bdi_writeback data structure (wb) corresponded to the memcg has
pending dirty pages (in state WB_registered | WB_has_diry_io), but
can not write-out the dirty pages and loop in balance_dirty_pages
function.

This is a bug in Lustre memcg code. In OSC/MDC layer, it will stop
to flush dirty pages once found that there are no any unstable
pages.
However, there may be some dirty pages queued in the cache. In
this case, the client should still write back the dirty pages.
Thus the wb stat accounting will be updated and the write process
can continue instead of looping endless.

Moreover, there are some problem in the current Lustre CLIO engine.
When the system or a certain memcg is under memory pressure, the
client just queues the dirty page in page cache or in the current
active extent (OES_ACTIVE osc_extent) when vvp_io_write_commit()/
cl_io_commit_async() is called in ->write_end(). The queued pages
can not be written back even the kernel is trying to flush dirty
pages in writeback via ->ll_writepages().
The client is looping in the following call sequences:

loop:
->write_begin()
->write_end()
->balance_dirty_pages()
  ->Launch file writeback in background but cannot flush any
    dirty pages.
  ->The current process is paused a certain time (i.e. 200ms) as

lustre/osc/osc_cache.c
@@ -196,8 +196,7 @@ static int osc_extent_sanity_check0(struct osc_extent *ext,
 			GOTO(out, rc = 65);
 		fallthrough;
 	default:
-		if (atomic_read(&ext->oe_users) > 0)
-			GOTO(out, rc = 70);
+		break;
 	}
 
 	if (ext->oe_max_end < ext->oe_end || ext->oe_end < ext->oe_start)
@@ -564,10 +563,13 @@ static int osc_extent_merge(const struct lu_env *env, struct osc_extent *cur,
 /**
  * Drop user count of osc_extent, and unplug IO asynchronously.
  */
-void osc_extent_release(const struct lu_env *env, struct osc_extent *ext)
+void osc_extent_release(const struct lu_env *env, struct osc_extent *ext,
+			enum cl_io_priority prio)
 {
 	struct osc_object *obj = ext->oe_obj;
 	struct client_obd *cli = osc_cli(obj);
+	bool hp = cl_io_high_prio(prio);
+
 	ENTRY;
 
 	LASSERT(atomic_read(&ext->oe_users) > 0);
@@ -575,15 +577,26 @@ void osc_extent_release(const struct lu_env *env, struct osc_extent *ext)
 	LASSERT(ext->oe_grants > 0);
 
 	if (atomic_dec_and_lock(&ext->oe_users, &obj->oo_lock)) {
-		LASSERT(ext->oe_state == OES_ACTIVE);
 		if (ext->oe_trunc_pending) {
-			/* a truncate process is waiting for this extent.
+			/*
+			 * A truncate process is waiting for this extent.
 			 * This may happen due to a race, check
-			 * osc_cache_truncate_start(). */
+			 * osc_cache_truncate_start().
+			 */
+			if (ext->oe_state != OES_ACTIVE) {
+				int rc;
+
+				osc_object_unlock(obj);
+				rc = osc_extent_wait(env, ext, OES_INV);
+				if (rc < 0)
+					OSC_EXTENT_DUMP(D_ERROR, ext,
+							"error: %d.\n", rc);
+				osc_object_lock(obj);
+			}
 			osc_extent_state_set(ext, OES_TRUNC);
 			ext->oe_trunc_pending = 0;
 			osc_object_unlock(obj);
-		} else {
+		} else if (ext->oe_state == OES_ACTIVE) {
 			int grant = 0;
 
 			osc_extent_state_set(ext, OES_CACHE);
@@ -596,17 +609,16 @@ void osc_extent_release(const struct lu_env *env, struct osc_extent *ext)
 			if (osc_extent_merge(env, ext, next_extent(ext)) == 0)
 				grant += cli->cl_grant_extent_tax;
 
-			if (!ext->oe_rw && ext->oe_dlmlock) {
-				bool hp;
-
+			if (!hp && !ext->oe_rw && ext->oe_dlmlock) {
 				lock_res_and_lock(ext->oe_dlmlock);
 				hp = ldlm_is_cbpending(ext->oe_dlmlock);
 				unlock_res_and_lock(ext->oe_dlmlock);
-
-				/* HP extent should be written ASAP. */
-				if (hp)
-					ext->oe_hp = 1;
 			}
+
+
+			/* HP extent should be written ASAP. */
+			if (hp)
+				ext->oe_hp = 1;
 
 			if (ext->oe_hp)
 				list_move_tail(&ext->oe_link,
@@ -621,9 +633,14 @@ void osc_extent_release(const struct lu_env *env, struct osc_extent *ext)
 			osc_object_unlock(obj);
 			if (grant > 0)
 				osc_unreserve_grant(cli, 0, grant);
+		} else {
+			osc_object_unlock(obj);
 		}
 
-		osc_io_unplug_async(env, cli, obj);
+		if (unlikely(cl_io_high_prio(prio)))
+			osc_io_unplug(env, cli, obj);
+		else
+			osc_io_unplug_async(env, cli, obj);
 	}
 	osc_extent_put(env, ext);
 
@@ -916,7 +933,7 @@ static int osc_extent_wait(const struct lu_env *env, struct osc_extent *ext,
 	}
 	osc_object_unlock(obj);
 	if (rc == 1)
-		osc_extent_release(env, ext);
+		osc_extent_release(env, ext, IO_PRIO_NORMAL);
 
 	/* wait for the extent until its state becomes @state */
 	rc = wait_event_idle_timeout(ext->oe_waitq,
@@ -1160,6 +1177,9 @@ static int osc_extent_expand(struct osc_extent *ext, pgoff_t index,
 
 	LASSERT(ext->oe_max_end >= index && ext->oe_start <= index);
 	osc_object_lock(obj);
+	if (ext->oe_state != OES_ACTIVE)
+		GOTO(out, rc = -ESTALE);
+
 	LASSERT(sanity_check_nolock(ext) == 0);
 	end_chunk = ext->oe_end >> ppc_bits;
 	if (chunk > end_chunk + 1)
@@ -2342,7 +2362,10 @@ int osc_queue_async_io(const struct lu_env *env, struct cl_io *io,
 	 * 2. otherwise, a new extent will be allocated. */
 
 	ext = oio->oi_active;
-	if (ext != NULL && ext->oe_start <= index && ext->oe_max_end >= index) {
+	if (ext != NULL && ext->oe_state != OES_ACTIVE) {
+		need_release = 1;
+	} else if (ext != NULL && ext->oe_start <= index &&
+		   ext->oe_max_end >= index) {
 		/* one chunk plus extent overhead must be enough to write this
 		 * page */
 		grants = (1 << cli->cl_chunkbits) + cli->cl_grant_extent_tax;
@@ -2376,7 +2399,7 @@ int osc_queue_async_io(const struct lu_env *env, struct cl_io *io,
 		need_release = 1;
 	}
 	if (need_release) {
-		osc_extent_release(env, ext);
+		osc_extent_release(env, ext, IO_PRIO_NORMAL);
 		oio->oi_active = NULL;
 		ext = NULL;
 	}
@@ -2407,6 +2430,7 @@ int osc_queue_async_io(const struct lu_env *env, struct cl_io *io,
 				grants = tmp;
 		}
 
+restart_find:
 		tmp = grants;
 		if (rc == 0) {
 			ext = osc_extent_find(env, osc, index, &tmp);
@@ -2430,6 +2454,28 @@ int osc_queue_async_io(const struct lu_env *env, struct cl_io *io,
 		LASSERT((oap->oap_brw_flags & OBD_BRW_FROM_GRANT) != 0);
 
 		osc_object_lock(osc);
+		if (ext->oe_state != OES_ACTIVE) {
+			if (ext->oe_state == OES_CACHE) {
+				osc_extent_state_set(ext, OES_ACTIVE);
+				osc_update_pending(osc, OBD_BRW_WRITE,
+						   -ext->oe_nr_pages);
+				list_del_init(&ext->oe_link);
+			} else {
+				osc_object_unlock(osc);
+				osc_extent_get(ext);
+				osc_extent_release(env, ext, IO_PRIO_NORMAL);
+				oio->oi_active = NULL;
+
+				/* Waiting for IO finished.  */
+				rc = osc_extent_wait(env, ext, OES_INV);
+				osc_extent_put(env, ext);
+				if (rc < 0)
+					RETURN(rc);
+
+				GOTO(restart_find, rc);
+			}
+		}
+
 		if (ext->oe_nr_pages == 0)
 			ext->oe_srvlock = ops->ops_srvlock;
 		else
@@ -3097,14 +3143,18 @@ EXPORT_SYMBOL(osc_cache_wait_range);
  * Return how many pages will be issued, or error code if error occurred.
  */
 int osc_cache_writeback_range(const struct lu_env *env, struct osc_object *obj,
-			      pgoff_t start, pgoff_t end, int hp, int discard)
+			      pgoff_t start, pgoff_t end, int hp, int discard,
+			      enum cl_io_priority prio)
 {
 	struct osc_extent *ext;
 	LIST_HEAD(discard_list);
+	bool active_ext_check = false;
 	bool unplug = false;
 	int result = 0;
+
 	ENTRY;
 
+repeat:
 	osc_object_lock(obj);
 	ext = osc_extent_search(obj, start);
 	if (ext == NULL)
@@ -3176,6 +3226,16 @@ int osc_cache_writeback_range(const struct lu_env *env, struct osc_object *obj,
 			 * grants. We do this for the correctness of fsync. */
 			LASSERT(hp == 0 && discard == 0);
 			ext->oe_urgent = 1;
+
+			if (active_ext_check) {
+				osc_extent_state_set(ext, OES_CACHE);
+				list_move_tail(&ext->oe_link,
+					       &obj->oo_urgent_exts);
+				osc_update_pending(obj, OBD_BRW_WRITE,
+						   ext->oe_nr_pages);
+				unplug = true;
+			}
+
 			break;
 		case OES_TRUNC:
 			/* this extent is being truncated, can't do anything
@@ -3223,7 +3283,22 @@ int osc_cache_writeback_range(const struct lu_env *env, struct osc_object *obj,
 			result = rc;
 	}
 
-	OSC_IO_DEBUG(obj, "pageout [%lu, %lu], %d.\n", start, end, result);
+	OSC_IO_DEBUG(obj, "pageout [%lu, %lu] npages %lu: rc=%d.\n",
+		     start, end, obj->oo_npages, result);
+
+	/*
+	 * Try to flush the active I/O extents of the object.
+	 * Otherwise, the user process writing the file may be dirty exceeded
+	 * and waiting endless in balance_dirty_pages().
+	 */
+	if (result == 0 && prio == IO_PRIO_DIRTY_EXCEEDED &&
+	    !active_ext_check && atomic_read(&obj->oo_nr_ios) &&
+	    obj->oo_npages > 0) {
+		osc_extent_tree_dump(D_CACHE, obj);
+		active_ext_check = true;
+		GOTO(repeat, result);
+	}
+
 	RETURN(result);
 }
 EXPORT_SYMBOL(osc_cache_writeback_range);

config/lustre-core.m4
@@ -4406,6 +4406,29 @@ AC_DEFUN([LC_HAVE_FOLIO_BATCH_REINIT], [
 ]) # LC_HAVE_FOLIO_BATCH_REINIT
 
 #
+# LC_HAVE_INODE_ATTACH_WB_FOLIO
+#
+# linux kernel v6.2-rc4 commit: 9cfb816b1c6c99f4b3c1d4a0fb096162cd17ec71
+# mm/fs: convert inode_attach_wb() to take a folio
+#
+AC_DEFUN([LC_SRC_HAVE_INODE_ATTACH_WB_FOLIO], [
+	LB2_LINUX_TEST_SRC([inode_attach_wb_folio_arg], [
+		#include <linux/writeback.h>
+	],[
+		struct folio *folio = NULL;
+
+		inode_attach_wb(NULL, folio);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_INODE_ATTACH_WB_FOLIO], [
+	LB2_MSG_LINUX_TEST_RESULT([if 'inode_attach_wb()' takes folio],
+	[inode_attach_wb_folio_arg], [
+		AC_DEFINE(HAVE_INODE_ATTACH_WB_FOLIO, 1,
+			['inode_attach_wb()' takes folio])
+	])
+]) # LC_HAVE_INODE_ATTACH_WB_FOLIO
+
+#
 # LC_HAVE_IOV_ITER_IOVEC
 #
 # linux kernel v6.3-rc4-32-g6eb203e1a868
@@ -5399,6 +5422,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_LOCKS_LOCK_FILE_WAIT_IN_FILELOCK
 	LC_SRC_HAVE_U64_CAPABILITY
 	LC_SRC_HAVE_FOLIO_BATCH_REINIT
+	LC_SRC_HAVE_INODE_ATTACH_WB_FOLIO
 
 	# 6.4
 	LC_SRC_HAVE_IOV_ITER_IOVEC
@@ -5737,6 +5761,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_LOCKS_LOCK_FILE_WAIT_IN_FILELOCK
 	LC_HAVE_U64_CAPABILITY
 	LC_HAVE_FOLIO_BATCH_REINIT
+	LC_HAVE_INODE_ATTACH_WB_FOLIO
 
 	# 6.4
 	LC_HAVE_IOV_ITER_IOVEC

lustre/include/cl_object.h
@@ -1428,6 +1428,36 @@ static inline void cl_read_ahead_release(const struct lu_env *env,
 		ra->cra_release(env, ra);
 }
 
+enum cl_io_priority {
+	/* Normal I/O, usually just queue the pages in the client side cache. */
+	IO_PRIO_NORMAL	= 0,
+	/* I/O is urgent and should flush queued pages to OSTs ASAP. */
+	IO_PRIO_URGENT,
+	/* The memcg is under high memory pressure and the user write process
+	 * is dirty exceeded and under rate limiting in balance_dirty_pages().
+	 * It needs to flush dirty pages for the corresponding @wb ASAP.
+	 */
+	IO_PRIO_DIRTY_EXCEEDED,
+	/*
+	 * I/O is urgent and flushing pages are marked with OBD_BRW_SOFT_SYNC
+	 * flag and may trigger a soft sync on OSTs. Thus it can free unstable
+	 * pages much quickly.
+	 */
+	IO_PRIO_SOFT_SYNC,
+	/*
+	 * The system or a certain memcg is under high memory pressure. Need to
+	 * flush dirty pages to OSTs immediately and I/O RPC must wait the write
+	 * transcation commit on OSTs synchronously to release unstable pages.
+	 */
+	IO_PRIO_HARD_SYNC,
+	IO_PRIO_MAX,
+};
+
+static inline bool cl_io_high_prio(enum cl_io_priority prio)
+{
+	return prio >= IO_PRIO_URGENT;
+}
+
 /**
  * Per-layer io operations.
  * \see vvp_io_ops, lov_io_ops, lovsub_io_ops, osc_io_ops
@@ -1540,12 +1570,13 @@ struct cl_io_operations {
 	int  (*cio_commit_async)(const struct lu_env *env,
 				 const struct cl_io_slice *slice,
 				 struct cl_page_list *queue, int from, int to,
-				 cl_commit_cbt cb);
+				 cl_commit_cbt cb, enum cl_io_priority prio);
 	/**
 	 * Release active extent.
 	 */
 	void  (*cio_extent_release)(const struct lu_env *env,
-				    const struct cl_io_slice *slice);
+				    const struct cl_io_slice *slice,
+				    enum cl_io_priority prio);
 	/**
 	 * Decide maximum read ahead extent
 	 *
@@ -1833,13 +1864,14 @@ struct cl_io {
 			struct cl_page *ft_page;
 		} ci_fault;
 		struct cl_fsync_io {
-			loff_t             fi_start;
-			loff_t             fi_end;
+			loff_t			 fi_start;
+			loff_t			 fi_end;
 			/** file system level fid */
-			struct lu_fid     *fi_fid;
-			enum cl_fsync_mode fi_mode;
+			struct lu_fid	 	*fi_fid;
+			enum cl_fsync_mode	 fi_mode;
 			/* how many pages were written/discarded */
-			unsigned int       fi_nr_written;
+			unsigned int		 fi_nr_written;
+			enum cl_io_priority	 fi_prio;
 		} ci_fsync;
 		struct cl_ladvise_io {
 			__u64			 lio_start;
@@ -2389,8 +2421,9 @@ int   cl_io_submit_sync(const struct lu_env *env, struct cl_io *io,
 			long timeout);
 int   cl_io_commit_async(const struct lu_env *env, struct cl_io *io,
 			  struct cl_page_list *queue, int from, int to,
-			  cl_commit_cbt cb);
-void  cl_io_extent_release(const struct lu_env *env, struct cl_io *io);
+			  cl_commit_cbt cb, enum cl_io_priority prio);
+void  cl_io_extent_release(const struct lu_env *env, struct cl_io *io,
+			   enum cl_io_priority prio);
 int cl_io_lru_reserve(const struct lu_env *env, struct cl_io *io,
 		      loff_t pos, size_t bytes);
 int   cl_io_read_ahead(const struct lu_env *env, struct cl_io *io,

lustre/include/lustre_osc.h
@@ -563,7 +563,8 @@ int osc_cache_truncate_start(const struct lu_env *env, struct osc_object *obj,
 			     __u64 size, struct osc_extent **extp);
 void osc_cache_truncate_end(const struct lu_env *env, struct osc_extent *ext);
 int osc_cache_writeback_range(const struct lu_env *env, struct osc_object *obj,
-			      pgoff_t start, pgoff_t end, int hp, int discard);
+			      pgoff_t start, pgoff_t end, int hp, int discard,
+			      enum cl_io_priority prio);
 int osc_cache_wait_range(const struct lu_env *env, struct osc_object *obj,
 			 pgoff_t start, pgoff_t end);
 int osc_io_unplug0(const struct lu_env *env, struct client_obd *cli,
@@ -645,9 +646,10 @@ int osc_dio_submit(const struct lu_env *env, struct cl_io *io,
 int osc_io_commit_async(const struct lu_env *env,
 			const struct cl_io_slice *ios,
 			struct cl_page_list *qin, int from, int to,
-			cl_commit_cbt cb);
+			cl_commit_cbt cb, enum cl_io_priority prio);
 void osc_io_extent_release(const struct lu_env *env,
-			   const struct cl_io_slice *ios);
+			   const struct cl_io_slice *ios,
+			   enum cl_io_priority prio);
 int osc_io_iter_init(const struct lu_env *env, const struct cl_io_slice *ios);
 void osc_io_iter_fini(const struct lu_env *env,
 		      const struct cl_io_slice *ios);
@@ -731,6 +733,11 @@ static inline struct obd_export *osc_export(const struct osc_object *obj)
 static inline struct client_obd *osc_cli(const struct osc_object *obj)
 {
 	return &osc_export(obj)->exp_obd->u.cli;
+}
+
+static inline char *cli_name(struct client_obd *cli)
+{
+	return cli->cl_import->imp_obd->obd_name;
 }
 
 static inline struct osc_object *cl2osc(const struct cl_object *obj)

lustre/llite/file.c
@@ -5287,7 +5287,7 @@ static loff_t ll_file_seek(struct file *file, loff_t offset, int origin)
 
 		/* flush local cache first if any */
 		cl_sync_file_range(inode, offset, OBD_OBJECT_EOF,
-				   CL_FSYNC_LOCAL, 0);
+				   CL_FSYNC_LOCAL, 0, IO_PRIO_NORMAL);
 
 		retval = ll_lseek(file, offset, origin);
 		if (retval < 0)
@@ -5343,7 +5343,8 @@ static int ll_flush(struct file *file, fl_owner_t id)
  * Return how many pages have been written.
  */
 int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
-		       enum cl_fsync_mode mode, int ignore_layout)
+		       enum cl_fsync_mode mode, int ignore_layout,
+		       enum cl_io_priority prio)
 {
 	struct lu_env *env;
 	struct cl_io *io;
@@ -5373,6 +5374,7 @@ int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
 	fio->fi_fid = ll_inode2fid(inode);
 	fio->fi_mode = mode;
 	fio->fi_nr_written = 0;
+	fio->fi_prio = prio;
 
 	if (cl_io_init(env, io, CIT_FSYNC, io->ci_obj) == 0)
 		result = cl_io_loop(env, io);
@@ -5451,7 +5453,8 @@ int ll_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 		err = pcc_fsync(file, start, end, datasync, &cached);
 		if (!cached)
 			err = cl_sync_file_range(inode, start, end,
-						 CL_FSYNC_ALL, 0);
+						 CL_FSYNC_ALL, 0,
+						 IO_PRIO_NORMAL);
 		if (rc == 0 && err < 0)
 			rc = err;
 		if (rc < 0)

lustre/llite/llite_internal.h
@@ -1379,7 +1379,8 @@ int ll_read_folio(struct file *file, struct folio *folio);
 int ll_io_read_page(const struct lu_env *env, struct cl_io *io,
 			   struct cl_page *page, struct file *file);
 void ll_readahead_init(struct inode *inode, struct ll_readahead_state *ras);
-int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io);
+int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io,
+			enum cl_io_priority prio);
 
 enum lcc_type;
 void ll_cl_add(struct inode *inode, const struct lu_env *env, struct cl_io *io,
@@ -2002,7 +2003,8 @@ dentry_may_statahead(struct inode *dir, struct dentry *dentry)
 }
 
 int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
-		       enum cl_fsync_mode mode, int ignore_layout);
+		       enum cl_fsync_mode mode, int ignore_layout,
+		       enum cl_io_priority prio);
 
 static inline int ll_file_nolock(const struct file *file)
 {

lustre/llite/llite_lib.c
@@ -3265,7 +3265,8 @@ void ll_delete_inode(struct inode *inode)
 		 * unlink, so that file is not opened somewhere else
 		 */
 		cl_sync_file_range(inode, 0, OBD_OBJECT_EOF, inode->i_nlink ?
-				   CL_FSYNC_LOCAL : CL_FSYNC_DISCARD, 1);
+				   CL_FSYNC_LOCAL : CL_FSYNC_DISCARD, 1,
+				   IO_PRIO_NORMAL);
 	}
 
 	ll_truncate_inode_pages_final(inode);

lustre/llite/rw.c
@@ -1543,7 +1543,7 @@ int ll_writepage(struct page *vmpage, struct writeback_control *wbc)
 		 */
 		result = cl_sync_file_range(inode, offset,
 					    offset + PAGE_SIZE - 1,
-					    CL_FSYNC_LOCAL, 1);
+					    CL_FSYNC_LOCAL, 1, IO_PRIO_NORMAL);
 		if (result > 0) {
 			/* May have written more than one page. decreasing this
 			 * page because the caller will count it.
@@ -1570,6 +1570,7 @@ out:
 int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
 	struct inode *inode = mapping->host;
+	enum cl_io_priority prio = IO_PRIO_NORMAL;
 	loff_t start;
 	loff_t end;
 	enum cl_fsync_mode mode;
@@ -1611,8 +1612,11 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 		wb = inode_to_wb(inode);
 		if (wbc->for_background ||
 		    (wb->start_all_reason == WB_REASON_VMSCAN &&
-		     test_bit(WB_start_all, &wb->state)))
+		     test_bit(WB_start_all, &wb->state))) {
 			mode = CL_FSYNC_RECLAIM;
+			if (wb->dirty_exceeded)
+				prio = IO_PRIO_DIRTY_EXCEEDED;
+		}
 		spin_unlock(&inode->i_lock);
 #else
 		/*
@@ -1634,7 +1638,7 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 	 * inside the IO context of write, which will cause deadlock at
 	 * layout_conf since it waits for active IOs to complete.
 	 */
-	result = cl_sync_file_range(inode, start, end, mode, 1);
+	result = cl_sync_file_range(inode, start, end, mode, 1, prio);
 	if (result > 0) {
 		wbc->nr_to_write -= result;
 		result = 0;

microsoft/amlFilesystem-lustre

commit ad2e293051ca9e70f56521d62c5bbb9a3c0e59a4

LU-20078 osc: call osc_extent_tree_dump() without object lock held
The panic of next_extent() in osc_extent_tree_dump() is caused by:
- osc_extent_tree_dump() walks the extent rbtree without holding
  object lock: osc_object_lock().
- LU-19014 added a new debug call to osc_extent_tree_dump() in
  osc_cache_writeback_range() for the IO_PRIO_DIRTY_EXCEEDED path.
- next_extent() assumes the passed exctent is still in the tree
  and asserts on RB_EMPTY_NODE(); This is reasonable for normal
  tree walkers under the object lock, but unsafe for an unlocked
  debug dump.

Fix it by removing osc_extent_tree_dump() added by LU-19014.
Also fix similar osc_extent_tree_dump() call in osc_enter_cache()
by holding the object lock around it.

Fixes: c413d2ede5 ("LU-19014 memcg: fix client hang in balance_dirty_page()")
Signed-off-by: Yingjin Qian <qian@ddn.com>
Change-Id: I7802d951f1c01d92a9240e7b4d395104b52c8969

lustre/osc/osc_cache.c
@@ -1820,8 +1820,12 @@ static int osc_enter_cache(const struct lu_env *env, struct client_obd *cli,
 	} else if (remain == 0) {
 		OSC_DUMP_GRANT(D_CACHE, cli,
 			       "timeout, fall back to sync i/o");
+		spin_unlock(&cli->cl_loi_list_lock);
+		osc_object_lock(osc);
 		osc_extent_tree_dump(D_CACHE, osc);
+		osc_object_unlock(osc);
 		/* fall back to synchronous I/O */
+		RETURN(rc);
 	} else {
 		OSC_DUMP_GRANT(D_CACHE, cli,
 			       "no grant space, fall back to sync i/o");
@@ -3462,7 +3466,6 @@ repeat:
 	if (result == 0 && prio == IO_PRIO_DIRTY_EXCEEDED &&
 	    !active_ext_check && atomic_read(&obj->oo_nr_ios) &&
 	    obj->oo_npages > 0) {
-		osc_extent_tree_dump(D_CACHE, obj);
 		active_ext_check = true;
 		GOTO(repeat, result);
 	}