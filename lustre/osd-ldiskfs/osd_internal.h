lustre/lustre-release

commit d9b67be7148d89f5274a8b9299b143d86c174bb0

LU-17916 osd: prefer folio of order 0
Switch from struct page and page based API to struct folio
and the folio API while keeping the PAGE_SIZE logic by only
allocating folio(s) of order 0.

Provide lnb_fpgno to track the page number within an lnb_folio
where kmem_to_folio() in osd-zfs can be a large folio.

Propagate lnb_folio to the other osd implementations and
provide lnb_pgno(), lnb_kmap_local(), ll_lnb_kmap_local() and
lnb_folio_page() to manage the lnb_folio and lnb_page consistently.

osd_dio_get_folio in osd-zfs returns -ENOMEM on allocation failure
instead of LASSERT(). -ENOMEM will propagate up the call chain where
  osd_bufs_get() will free allocated memory
  dt_bufs_get() becomes responsible for any further handling.

HPE-bug-id: LUS-12384
Signed-off-by: Shaun Tancheff <shaun@tancheff.com>
Change-Id: I87b37ce94f2f717e8767785e3620e4a30bc22e88
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58458
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/osd-ldiskfs/osd_internal.h
@@ -510,8 +510,8 @@ struct osd_obj_declare {
 enum {
         LPROC_OSD_READ_BYTES    = 0,
         LPROC_OSD_WRITE_BYTES   = 1,
-        LPROC_OSD_GET_PAGE      = 2,
-        LPROC_OSD_NO_PAGE       = 3,
+        LPROC_OSD_GET_FOLIO     = 2,
+        LPROC_OSD_NO_FOLIO      = 3,
         LPROC_OSD_CACHE_ACCESS  = 4,
         LPROC_OSD_CACHE_HIT     = 5,
         LPROC_OSD_CACHE_MISS    = 6,

lustre/osd-ldiskfs/osd_io.c
@@ -409,7 +409,8 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 	struct block_device *bdev = sb->s_bdev;
 	struct bio *bio = NULL;
 	int bio_start_page_idx = 0;
-	struct page *page;
+	struct folio *folio;
+	unsigned int lnb_offset;
 	unsigned int page_offset;
 	sector_t sector;
 	int nblocks;
@@ -444,8 +445,9 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 		 * "blocks_left_page" is reduced), and the last page may
 		 * skip some blocks at the end (limited by "count").
 		 */
-		page = lnbs[page_idx]->lnb_page;
+		folio = lnbs[page_idx]->lnb_folio;
 		LASSERT(page_idx < iobuf->dr_npages);
+		lnb_offset = lnb_pgno(lnbs[page_idx]) << PAGE_SHIFT;
 
 		i = block_idx % blocks_per_page;
 		blocks_left_page = blocks_per_page - i;
@@ -472,7 +474,7 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 					 page_idx, block_idx, i,
 					 (unsigned long long)start_blocks,
 					 (unsigned long long)count, npages);
-				addr = kmap_local_page(page);
+				addr = lnb_kmap_local(lnbs[page_idx]);
 				memset(addr + page_offset, 0, blocksize);
 				kunmap_local(addr);
 				continue;
@@ -488,8 +490,8 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 				nblocks++;
 
 			if (bio && can_be_merged(bio, sector) &&
-			    bio_add_page(bio, page, blocksize * nblocks,
-					 page_offset) != 0)
+			    bio_add_folio(bio, folio, blocksize * nblocks,
+					  lnb_offset + page_offset) != 0)
 				continue;       /* added this frag OK */
 
 			rc = osd_submit_bio(osd, iobuf, bio);
@@ -516,8 +518,8 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 			if (rc)
 				goto out;
 
-			rc = bio_add_page(bio, page,
-					  blocksize * nblocks, page_offset);
+			rc = bio_add_folio(bio, folio, blocksize * nblocks,
+					   lnb_offset + page_offset);
 			LASSERT(rc != 0);
 		}
 	}
@@ -577,7 +579,8 @@ static int osd_map_remote_to_local(loff_t offset, ssize_t len, int *nrpages,
 		lnb->lnb_len = plen;
 		/* lnb->lnb_flags = rnb->rnb_flags; */
 		lnb->lnb_flags = 0;
-		lnb->lnb_page = NULL;
+		lnb->lnb_folio = NULL;
+		lnb->lnb_fpgno = 0;
 		lnb->lnb_rc = 0;
 		lnb->lnb_guard_rpc = 0;
 		lnb->lnb_guard_disk = 0;
@@ -595,8 +598,9 @@ static int osd_map_remote_to_local(loff_t offset, ssize_t len, int *nrpages,
 	RETURN(rc);
 }
 
-static struct page *osd_get_page(const struct lu_env *env, struct dt_object *dt,
-				 loff_t offset, gfp_t gfp_mask, bool cache)
+static struct folio *osd_get_folio(const struct lu_env *env,
+				  struct dt_object *dt, loff_t offset,
+				  gfp_t gfp_mask, bool cache)
 {
 	struct osd_thread_info *oti = osd_oti_get(env);
 	struct inode *inode = osd_dt_obj(dt)->oo_inode;
@@ -613,11 +617,11 @@ static struct page *osd_get_page(const struct lu_env *env, struct dt_object *dt,
 		if (!IS_ERR_OR_NULL(folio)) {
 			LASSERT(!folio_test_private_2(folio));
 			folio_wait_writeback(folio);
-			return fpgptr(folio);
 		} else {
-			lprocfs_counter_add(d->od_stats, LPROC_OSD_NO_PAGE, 1);
-			return NULL;
+			lprocfs_counter_add(d->od_stats, LPROC_OSD_NO_FOLIO, 1);
+			folio = NULL;
 		}
+		return folio;
 	}
 
 	if (inode->i_mapping->nrpages) {
@@ -627,7 +631,7 @@ static struct page *osd_get_page(const struct lu_env *env, struct dt_object *dt,
 					FGP_LOCK, gfp_mask);
 		if (!IS_ERR_OR_NULL(folio)) {
 			folio_wait_writeback(folio);
-			return fpgptr(folio);
+			return folio;
 		}
 	}
 
@@ -648,7 +652,7 @@ static struct page *osd_get_page(const struct lu_env *env, struct dt_object *dt,
 	folio->index = offset >> PAGE_SHIFT;
 	oti->oti_dio_pages_used++;
 
-	return fpgptr(folio);
+	return folio;
 }
 
 /*
@@ -695,25 +699,26 @@ static int osd_bufs_put(const struct lu_env *env, struct dt_object *dt,
 	ll_folio_batch_init(&fbatch);
 
 	for (i = 0; i < npages; i++) {
-		struct page *page = lnb[i].lnb_page;
+		struct folio *folio = lnb[i].lnb_folio;
 
-		if (page == NULL)
+		if (folio == NULL)
 			continue;
 
 		/* if the page isn't cached, then reset uptodate
 		 * to prevent reuse
 		 */
-		if (PagePrivate2(page)) {
+		if (folio_test_private_2(folio)) {
 			oti->oti_dio_pages_used--;
 			lnb[i].lnb_dio = 0;
 		} else {
 			if (lnb[i].lnb_locked)
-				unlock_page(page);
-			if (folio_batch_add_page(&fbatch, page) == 0)
+				folio_unlock(folio);
+			if (folio_batch_add(&fbatch, folio) == 0)
 				folio_batch_release(&fbatch);
 		}
 
-		lnb[i].lnb_page = NULL;
+		lnb[i].lnb_folio = NULL;
+		lnb[i].lnb_fpgno = 0;
 	}
 
 	LASSERTF(oti->oti_dio_pages_used == 0, "%d\n", oti->oti_dio_pages_used);
@@ -819,15 +824,18 @@ bypass_checks:
 	gfp_mask = rw & DT_BUFS_TYPE_LOCAL ? (GFP_NOFS | __GFP_HIGHMEM) :
 					     GFP_HIGHUSER;
 	for (i = 0; i < npages; i++, lnb++) {
-		lnb->lnb_page = osd_get_page(env, dt, lnb->lnb_file_offset,
-					     gfp_mask, cache);
-		if (lnb->lnb_page == NULL)
+		lnb->lnb_folio = osd_get_folio(env, dt, lnb->lnb_file_offset,
+					      gfp_mask, cache);
+		if (lnb->lnb_folio == NULL)
 			GOTO(cleanup, rc = -ENOMEM);
 
+		/* FUTURE: deal with multi-order folios */
+		LASSERT(folio_nr_pages(lnb->lnb_folio) == 1);
+		lnb->lnb_fpgno = 0;
 		lnb->lnb_locked = 1;
 		lnb->lnb_dio = !!cache;
 		if (cache)
-			mark_page_accessed(lnb->lnb_page);
+			folio_mark_accessed(lnb->lnb_folio);
 	}
 
 #if 0
@@ -943,7 +951,7 @@ static int osd_ldiskfs_map_inode_pages(struct inode *inode,
 {
 	int blocks_per_page = PAGE_SIZE >> inode->i_blkbits;
 	int rc = 0, i = 0, mapped_index = 0;
-	struct page *fp = NULL;
+	struct folio *fp = NULL;
 	int clen = 0;
 	pgoff_t max_page_index;
 	handle_t *handle = NULL;
@@ -960,7 +968,7 @@ static int osd_ldiskfs_map_inode_pages(struct inode *inode,
 	max_page_index = inode->i_sb->s_maxbytes >> PAGE_SHIFT;
 
 	CDEBUG(D_OTHER, "inode %llu: map %d pages from %lu\n",
-	       (u64)inode->i_ino, pages, folio_index_page((*lnbs)->lnb_page));
+	       (u64)inode->i_ino, pages, (*lnbs)->lnb_folio->index);
 
 	if (osd->od_extents_dense)
 		compressed = iobuf->dr_lnbs[0]->lnb_flags & OBD_BRW_COMPRESSED;
@@ -988,24 +996,23 @@ static int osd_ldiskfs_map_inode_pages(struct inode *inode,
 		ktime_t time;
 
 		if (fp == NULL) { /* start new extent */
-			fp = (*lnbs)->lnb_page;
+			fp = (*lnbs)->lnb_folio;
 			lnbs++;
 			clen = 1;
 			iobuf->dr_lextents++;
 			if (++i != pages)
 				continue;
-		} else if (folio_index_page(fp) + clen ==
-			   folio_index_page((*lnbs)->lnb_page)) {
+		} else if (fp->index + clen == (*lnbs)->lnb_folio->index) {
 			/* continue the extent */
 			lnbs++;
 			clen++;
 			if (++i != pages)
 				continue;
 		}
-		if (folio_index_page(fp) + clen > max_page_index)
+		if (fp->index + clen > max_page_index)
 			GOTO(cleanup, rc = -EFBIG);
 		/* process found extent */
-		map.m_lblk = folio_index_page(fp) * blocks_per_page;
+		map.m_lblk = fp->index * blocks_per_page;
 		map.m_len = blen = clen * blocks_per_page;
 
 		/*
@@ -1018,8 +1025,7 @@ static int osd_ldiskfs_map_inode_pages(struct inode *inode,
 		if (iobuf->dr_start_pg_wblks > 0) {
 			total = previous_total = start_blocks =
 				iobuf->dr_start_pg_wblks;
-			map.m_lblk = folio_index_page(fp) * blocks_per_page +
-				total;
+			map.m_lblk = fp->index * blocks_per_page + total;
 			map.m_len = blen - total;
 			iobuf->dr_start_pg_wblks = 0;
 		}
@@ -1134,8 +1140,7 @@ cont_map:
 			 */
 			osd_decay_extent_bytes(osd,
 				(total - previous_total) << inode->i_blkbits);
-			map.m_lblk = folio_index_page(fp) * blocks_per_page +
-				     total;
+			map.m_lblk = fp->index * blocks_per_page + total;
 			map.m_len = blen - total;
 			previous_total = total;
 			goto cont_map;
@@ -1194,17 +1199,18 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 		 * we'll set it uptodate once bulk is done. otherwise
 		 * subsequent reads can access non-stable data
 		 */
-		ClearPageUptodate(lnb[i].lnb_page);
+		folio_clear_uptodate(lnb[i].lnb_folio);
 
 		if (lnb[i].lnb_len == PAGE_SIZE)
 			continue;
 
-		if (maxidx >= folio_index_page(lnb[i].lnb_page)) {
+		if (maxidx >= lnb[i].lnb_folio->index) {
 			osd_iobuf_add_page(iobuf, &lnb[i]);
 		} else {
 			long off;
-			char *p = kmap_local_page(lnb[i].lnb_page);
+			char *p;
 
+			p = lnb_kmap_local(&lnb[i]);
 			off = lnb[i].lnb_page_offset;
 			if (off)
 				memset(p, 0, off);
@@ -1217,7 +1223,7 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 	}
 	end = ktime_get();
 	timediff = ktime_us_delta(end, start);
-	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_PAGE, timediff);
+	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_FOLIO, timediff);
 
 	if (iobuf->dr_npages) {
 		rc = osd_ldiskfs_map_inode_pages(inode, iobuf, osd, 0,
@@ -1502,9 +1508,9 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 		if (lnb[i].lnb_rc) { /* ENOSPC, network RPC error, etc. */
 			CDEBUG(D_INODE, "Skipping [%d] == %d\n", i,
 			       lnb[i].lnb_rc);
-			LASSERT(lnb[i].lnb_page);
+			LASSERT(lnb[i].lnb_folio);
 			generic_error_remove_folio(inode->i_mapping,
-						   page_folio(lnb[i].lnb_page));
+						   lnb[i].lnb_folio);
 			continue;
 		}
 
@@ -1514,17 +1520,17 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 		if (!(lnb[i].lnb_flags & OBD_BRW_MAPPED))
 			check_credits = 1;
 
-		LASSERT(PageLocked(lnb[i].lnb_page));
-		LASSERT(!PageWriteback(lnb[i].lnb_page));
+		LASSERT(folio_test_locked(lnb[i].lnb_folio));
+		LASSERT(!folio_test_writeback(lnb[i].lnb_folio));
 
 		/*
 		 * Since write and truncate are serialized by dd_sem, even
 		 * partial-page truncate should not leave dirty pages in the
 		 * page cache.
 		 */
-		LASSERT(!PageDirty(lnb[i].lnb_page));
+		LASSERT(!folio_test_dirty(lnb[i].lnb_folio));
 
-		SetPageUptodate(lnb[i].lnb_page);
+		folio_mark_uptodate(lnb[i].lnb_folio);
 
 		osd_iobuf_add_page(iobuf, &lnb[i]);
 	}
@@ -1551,12 +1557,12 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 	if (unlikely(rc != 0 && !thandle->th_restart_tran)) {
 		/* if write fails, we should drop pages from the cache */
 		for (i = 0; i < npages; i++) {
-			if (lnb[i].lnb_page == NULL)
+			if (lnb[i].lnb_folio == NULL)
 				continue;
-			if (!PagePrivate2(lnb[i].lnb_page)) {
-				LASSERT(PageLocked(lnb[i].lnb_page));
+			if (!folio_test_private_2(lnb[i].lnb_folio)) {
+				LASSERT(folio_test_locked(lnb[i].lnb_folio));
 				generic_error_remove_folio(inode->i_mapping,
-						page_folio(lnb[i].lnb_page));
+							   lnb[i].lnb_folio);
 			}
 		}
 	}
@@ -1600,11 +1606,11 @@ static int osd_read_prep(const struct lu_env *env, struct dt_object *dt,
 
 		/* Bypass disk read if fail_loc is set properly */
 		if (CFS_FAIL_CHECK_QUIET(OBD_FAIL_OST_FAKE_RW))
-			SetPageUptodate(lnb[i].lnb_page);
+			folio_mark_uptodate(lnb[i].lnb_folio);
 
-		if (PageUptodate(lnb[i].lnb_page)) {
+		if (folio_test_uptodate(lnb[i].lnb_folio)) {
 			cache_hits++;
-			unlock_page(lnb[i].lnb_page);
+			folio_unlock(lnb[i].lnb_folio);
 		} else {
 			cache_misses++;
 			osd_iobuf_add_page(iobuf, &lnb[i]);
@@ -1619,7 +1625,7 @@ static int osd_read_prep(const struct lu_env *env, struct dt_object *dt,
 	}
 	end = ktime_get();
 	timediff = ktime_us_delta(end, start);
-	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_PAGE, timediff);
+	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_FOLIO, timediff);
 
 	if (cache_hits != 0)
 		lprocfs_counter_add(osd->od_stats, LPROC_OSD_CACHE_HIT,
@@ -1641,10 +1647,11 @@ static int osd_read_prep(const struct lu_env *env, struct dt_object *dt,
 
 		/* early release to let others read data during the bulk */
 		for (i = 0; i < iobuf->dr_npages; i++) {
-			struct page *page = iobuf->dr_lnbs[i]->lnb_page;
-			LASSERT(PageLocked(page));
-			if (!PagePrivate2(page))
-				unlock_page(page);
+			struct folio *folio = iobuf->dr_lnbs[i]->lnb_folio;
+
+			LASSERT(folio_test_locked(folio));
+			if (!folio_test_private_2(folio))
+				folio_unlock(folio);
 		}
 	}
 

lustre/osd-ldiskfs/osd_lproc.c
@@ -94,11 +94,11 @@ static int osd_stats_init(struct osd_device *osd)
 					     osd->od_dt_dev.dd_debugfs_entry,
 					     0);
 	if (osd->od_stats) {
-		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_PAGE,
-				     LPROCFS_TYPE_LATENCY, "get_page");
-		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_PAGE,
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_FOLIO,
+				     LPROCFS_TYPE_LATENCY, "get_folio");
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_FOLIO,
 				     LPROCFS_CNTR_AVGMINMAX|LPROCFS_TYPE_REQS,
-				     "get_page_failures");
+				     "get_folio_failures");
 		lprocfs_counter_init(osd->od_stats, LPROC_OSD_CACHE_ACCESS,
 				     LPROCFS_CNTR_AVGMINMAX|LPROCFS_TYPE_PAGES,
 				     "cache_access");

lustre/osd-zfs/osd_internal.h
@@ -268,7 +268,7 @@ struct osd_thread_info {
 	uint64_t		oti_lastid_oid;
 
 	/* just for fake RW now */
-	struct page		**oti_dio_pages;
+	struct folio		**oti_dio_folios;
 	int			oti_dio_pages_used;
 };
 
@@ -612,8 +612,8 @@ static inline struct osd_oi *osd_fid2oi(struct osd_device *osd,
 enum {
 	LPROC_OSD_READ_BYTES = 0,
 	LPROC_OSD_WRITE_BYTES = 1,
-	LPROC_OSD_GET_PAGE = 2,
-	LPROC_OSD_NO_PAGE = 3,
+	LPROC_OSD_GET_FOLIO = 2,
+	LPROC_OSD_NO_FOLIO = 3,
 	LPROC_OSD_CACHE_ACCESS = 4,
 	LPROC_OSD_CACHE_HIT = 5,
 	LPROC_OSD_CACHE_MISS = 6,

lustre/osd-zfs/osd_lproc.c
@@ -82,12 +82,12 @@ static int osd_stats_init(struct osd_device *osd)
 					     osd->od_dt_dev.dd_debugfs_entry,
 					     0);
 	if (osd->od_stats) {
-		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_PAGE,
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_FOLIO,
 				LPROCFS_CNTR_AVGMINMAX | LPROCFS_CNTR_STDDEV |
-				LPROCFS_TYPE_USECS, "get_page");
-		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_PAGE,
+				LPROCFS_TYPE_USECS, "get_folio");
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_FOLIO,
 				LPROCFS_CNTR_AVGMINMAX | LPROCFS_TYPE_REQS,
-				"get_page_failures");
+				"get_folio_failures");
 		lprocfs_counter_init(osd->od_stats, LPROC_OSD_CACHE_ACCESS,
 				LPROCFS_CNTR_AVGMINMAX | LPROCFS_TYPE_PAGES,
 				"cache_access");

lustre/osd-wbcfs/osd_io.c
@@ -43,7 +43,8 @@ static int osd_map_remote_to_local(loff_t offset, ssize_t len, int *nrpages,
 		lnb->lnb_page_offset = poff;
 		lnb->lnb_len = plen;
 		lnb->lnb_flags = 0;
-		lnb->lnb_page = NULL;
+		lnb->lnb_folio = NULL;
+		lnb->lnb_fpgno = 0;
 		lnb->lnb_rc = 0;
 		lnb->lnb_guard_rpc = 0;
 		lnb->lnb_guard_disk = 0;
@@ -61,21 +62,23 @@ static int osd_map_remote_to_local(loff_t offset, ssize_t len, int *nrpages,
 	RETURN(rc);
 }
 
-static int osd_get_page(const struct lu_env *env, struct dt_object *dt,
-			struct niobuf_local *lnb, gfp_t gfp_mask, bool write)
+static int osd_get_folio(const struct lu_env *env, struct dt_object *dt,
+			 struct niobuf_local *lnb, gfp_t gfp_mask, bool write)
 {
 	struct inode *inode = osd_dt_obj(dt)->oo_inode;
-	struct page *page;
+	struct folio *folio;
 	pgoff_t index;
 
 	LASSERT(inode);
 	index = lnb->lnb_file_offset >> PAGE_SHIFT;
 	if (write) {
-		page = find_or_create_page(inode->i_mapping, index, gfp_mask);
-		if (page == NULL)
+		folio = get_folio_create(inode->i_mapping, index,
+					 FGP_LOCK | FGP_ACCESSED | FGP_CREAT,
+					 gfp_mask);
+		if (IS_ERR_OR_NULL(folio))
 			return -ENOMEM;
 
-		LASSERT(!PagePrivate2(page));
+		LASSERT(!folio_test_private_2(folio));
 	} else {
 		/*
 		 * Specially handling for hole in the memory FS during read.
@@ -83,31 +86,34 @@ static int osd_get_page(const struct lu_env *env, struct dt_object *dt,
 		 * free them after reading.
 		 * Otherwise, reading on a large sparse file may hit OOM.
 		 */
-		page = find_lock_page(inode->i_mapping, index);
+		folio = get_folio_lock(inode->i_mapping, index, FGP_LOCK,
+				       gfp_mask);
 		/* fallocated page? */
-		if (page && !PageUptodate(page)) {
-			unlock_page(page);
-			put_page(page);
-			page = NULL;
+		if (!IS_ERR_OR_NULL(folio) && !folio_test_uptodate(folio)) {
+			folio_unlock(folio);
+			folio_put(folio);
+			folio = NULL;
 		}
 
-		if (page == NULL) {
-			page = alloc_page(gfp_mask);
-			if (!page)
+		if (IS_ERR_OR_NULL(folio)) {
+			folio = folio_alloc(gfp_mask, 0);
+			if (!folio)
 				return -ENOMEM;
 
-			SetPagePrivate2(page);
-			lock_page(page);
-			ClearPageUptodate(page);
-			page_folio(page)->index = index;
+			folio_set_private_2(folio);
+			folio_lock(folio);
+			folio_clear_uptodate(folio);
+			folio->index = index;
 			lnb->lnb_hole = 1;
 		}
 	}
 
-	lnb->lnb_page = page;
+	LASSERT(folio_nr_pages(folio) == 1);
+	lnb->lnb_folio = folio;
+	lnb->lnb_fpgno = 0;
 	lnb->lnb_locked = 1;
 	if (!lnb->lnb_hole)
-		mark_page_accessed(page);
+		folio_mark_accessed(folio);
 
 	return 0;
 }
@@ -125,26 +131,27 @@ static int osd_bufs_put(const struct lu_env *env, struct dt_object *dt,
 
 	ll_folio_batch_init(&fbatch);
 	for (i = 0; i < npages; i++) {
-		struct page *page = lnb[i].lnb_page;
+		struct folio *folio = lnb[i].lnb_folio;
 
-		if (page == NULL)
+		if (IS_ERR_OR_NULL(folio))
 			continue;
 
 		/* If the page is not cached in the memory FS, then free it. */
-		if (PagePrivate2(page)) {
+		if (folio_test_private_2(folio)) {
 			LASSERT(lnb[i].lnb_hole);
-			LASSERT(PageLocked(page));
-			ClearPagePrivate2(page);
-			unlock_page(page);
-			__free_page(page);
+			LASSERT(folio_test_locked(folio));
+			folio_clear_private_2(folio);
+			folio_unlock(folio);
+			folio_put(folio);
 		} else {
 			if (lnb[i].lnb_locked)
-				unlock_page(page);
-			if (folio_batch_add_page(&fbatch, page) == 0)
+				folio_unlock(folio);
+			if (folio_batch_add(&fbatch, folio) == 0)
 				folio_batch_release(&fbatch);
 		}
 
-		lnb[i].lnb_page = NULL;
+		lnb[i].lnb_folio = NULL;
+		lnb[i].lnb_fpgno = 0;
 	}
 
 	folio_batch_release(&fbatch);
@@ -191,8 +198,8 @@ static int osd_bufs_get(const struct lu_env *env, struct dt_object *dt,
 	gfp_mask = rw & DT_BUFS_TYPE_LOCAL ? (GFP_NOFS | __GFP_HIGHMEM) :
 					     GFP_HIGHUSER;
 	for (i = 0; i < npages; i++, lnb++) {
-		rc = osd_get_page(env, dt, lnb, gfp_mask,
-				  rw & DT_BUFS_TYPE_WRITE);
+		rc = osd_get_folio(env, dt, lnb, gfp_mask,
+				   rw & DT_BUFS_TYPE_WRITE);
 		if (rc)
 			GOTO(cleanup, rc);
 	}
@@ -287,18 +294,18 @@ static int osd_read_prep(const struct lu_env *env, struct dt_object *dt,
 		if (lnb[i].lnb_hole) {
 			void *kaddr;
 
-			LASSERT(PagePrivate2(lnb[i].lnb_page));
-			kaddr = kmap(lnb[i].lnb_page);
+			LASSERT(folio_test_private_2(lnb[i].lnb_folio));
+			kaddr = lnb_kmap_local(&lnb[i]);
 			memset(kaddr, 0, PAGE_SIZE);
-			kunmap(lnb[i].lnb_page);
-			SetPageUptodate(lnb[i].lnb_page);
+			kunmap_local(kaddr);
+			folio_mark_uptodate(lnb[i].lnb_folio);
 		} else {
 			/*
 			 * The page in cache for MemFS should be always
 			 * in uptodate state.
 			 */
-			LASSERT(PageUptodate(lnb[i].lnb_page));
-			unlock_page(lnb[i].lnb_page);
+			LASSERT(folio_test_uptodate(lnb[i].lnb_folio));
+			folio_unlock(lnb[i].lnb_folio);
 			/*
 			 * No need to unlock in osd_bufs_put(). The sooner page
 			 * is unlocked, the earlier another client can access
@@ -331,15 +338,15 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 		 * we will set it uptodate once bulk is done. Otherwise
 		 * subsequent reads can access non-stable data.
 		 */
-		ClearPageUptodate(lnb[i].lnb_page);
-
+		folio_clear_uptodate(lnb[i].lnb_folio);
 		if (lnb[i].lnb_len == PAGE_SIZE)
 			continue;
 
-		if (maxidx < folio_index_page(lnb[i].lnb_page)) {
+		if (maxidx < lnb[i].lnb_folio->index) {
 			long off;
-			char *p = kmap(lnb[i].lnb_page);
+			char *p;
 
+			p = lnb_kmap_local(&lnb[i]);
 			off = lnb[i].lnb_page_offset;
 			if (off)
 				memset(p, 0, off);
@@ -347,7 +354,7 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 			      ~PAGE_MASK;
 			if (off)
 				memset(p + off, 0, PAGE_SIZE - off);
-			kunmap(lnb[i].lnb_page);
+			kunmap_local(p);
 		}
 	}
 
@@ -370,9 +377,9 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 
 	for (i = 0; i < npages; i++) {
 		if (lnb[i].lnb_rc) { /* ENOSPC, network RPC error, etc. */
-			LASSERT(lnb[i].lnb_page);
+			LASSERT(!IS_ERR_OR_NULL(lnb[i].lnb_folio));
 			generic_error_remove_folio(inode->i_mapping,
-						   page_folio(lnb[i].lnb_page));
+						   lnb[i].lnb_folio);
 			continue;
 		}
 
@@ -384,16 +391,15 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 		if (user_size < lnb[i].lnb_file_offset + lnb[i].lnb_len)
 			user_size = lnb[i].lnb_file_offset + lnb[i].lnb_len;
 
-		LASSERT(PageLocked(lnb[i].lnb_page));
-		LASSERT(!PageWriteback(lnb[i].lnb_page));
-		/* LASSERT(!PageDirty(lnb[i].lnb_page)); */
+		LASSERT(folio_test_locked(lnb[i].lnb_folio));
+		LASSERT(!folio_test_writeback(lnb[i].lnb_folio));
+		/* LASSERT(!folio_test_dirty(lnb[i].lnb_folio)); */
 
-		SetPageUptodate(lnb[i].lnb_page);
+		folio_mark_uptodate(lnb[i].lnb_folio);
 #ifdef HAVE_DIRTY_FOLIO
-		mapping->a_ops->dirty_folio(mapping,
-					    page_folio(lnb[i].lnb_page));
+		mapping->a_ops->dirty_folio(mapping, lnb[i].lnb_folio);
 #else
-		mapping->a_ops->set_page_dirty(lnb[i].lnb_page);
+		mapping->a_ops->set_page_dirty(fpgptr(lnb[i].lnb_folio));
 #endif
 	}
 

microsoft/amlFilesystem-lustre

commit 5bc6b8a4f28886d61075915479215d41b184873d

LU-17916 osd: restore get_page stats for compatibility
The folio conversion renamed the "get_page" and "get_page_failures"
counters in osd-ldiskfs.*.stats and osd-zfs.*.stats to "get_folio"
and "get_folio_failures".  Those names are a user-visible interface:
anything parsing the stats file by counter name simply stops finding
its counter after an upgrade, with no error.

Register the old names again alongside the new ones and account both,
so existing consumers keep working while new ones can follow the folio
naming.  In osd-ldiskfs both counters sample the same events: the time
spent acquiring the buffers of one read/write prep, and the failures to
obtain a folio for the page cache.  Folios there are order 0, so one
failed folio is exactly one failed page; if osd_get_folio() ever asks
for a higher order, get_page_failures has to be scaled by the number of
pages in the requested folio.  In osd-zfs neither counter has ever been
sampled, so both stay absent from the file as before.

The old names are restored without a LUSTRE_VERSION_CODE gate on
purpose: a timebomb would drop them again on a date no consumer
tracks, which is the breakage this patch exists to undo.

Test-Parameters: trivial
Fixes: d9b67be7148d ("LU-17916 osd: prefer folio of order 0")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Change-Id: Iabe35ab8a26a367979eaee8ad5d9fdf6cadc6486
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools

lustre/osd-ldiskfs/osd_internal.h
@@ -512,8 +512,9 @@ struct osd_obj_declare {
 enum {
         LPROC_OSD_READ_BYTES    = 0,
         LPROC_OSD_WRITE_BYTES   = 1,
-        LPROC_OSD_GET_FOLIO     = 2,
-        LPROC_OSD_NO_FOLIO      = 3,
+	/* *_PAGE are the pre-folio names of *_FOLIO, sampled together */
+	LPROC_OSD_GET_PAGE	= 2,
+	LPROC_OSD_NO_PAGE	= 3,
         LPROC_OSD_CACHE_ACCESS  = 4,
         LPROC_OSD_CACHE_HIT     = 5,
         LPROC_OSD_CACHE_MISS    = 6,
@@ -524,6 +525,8 @@ enum {
         LPROC_OSD_THANDLE_CLOSING,
 #endif
 	LPROC_OSD_TOO_MANY_CREDITS,
+	LPROC_OSD_GET_FOLIO,
+	LPROC_OSD_NO_FOLIO,
         LPROC_OSD_LAST,
 };
 #endif

lustre/osd-ldiskfs/osd_io.c
@@ -618,7 +618,9 @@ static struct folio *osd_get_folio(const struct lu_env *env,
 			LASSERT(!folio_test_private_2(folio));
 			folio_wait_writeback(folio);
 		} else {
+			/* order-0 folio: one failed folio is one failed page */
 			lprocfs_counter_add(d->od_stats, LPROC_OSD_NO_FOLIO, 1);
+			lprocfs_counter_add(d->od_stats, LPROC_OSD_NO_PAGE, 1);
 			folio = NULL;
 		}
 		return folio;
@@ -1224,6 +1226,7 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 	end = ktime_get();
 	timediff = ktime_us_delta(end, start);
 	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_FOLIO, timediff);
+	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_PAGE, timediff);
 
 	if (iobuf->dr_npages) {
 		rc = osd_ldiskfs_map_inode_pages(inode, iobuf, osd, 0,
@@ -1626,6 +1629,7 @@ static int osd_read_prep(const struct lu_env *env, struct dt_object *dt,
 	end = ktime_get();
 	timediff = ktime_us_delta(end, start);
 	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_FOLIO, timediff);
+	lprocfs_counter_add(osd->od_stats, LPROC_OSD_GET_PAGE, timediff);
 
 	if (cache_hits != 0)
 		lprocfs_counter_add(osd->od_stats, LPROC_OSD_CACHE_HIT,

lustre/osd-ldiskfs/osd_lproc.c
@@ -94,6 +94,12 @@ static int osd_stats_init(struct osd_device *osd)
 					     osd->od_dt_dev.dd_debugfs_entry,
 					     0);
 	if (osd->od_stats) {
+		/* pre-folio names of the two counters below */
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_PAGE,
+				     LPROCFS_TYPE_LATENCY, "get_page");
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_PAGE,
+				     LPROCFS_CNTR_AVGMINMAX|LPROCFS_TYPE_REQS,
+				     "get_page_failures");
 		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_FOLIO,
 				     LPROCFS_TYPE_LATENCY, "get_folio");
 		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_FOLIO,

lustre/osd-zfs/osd_internal.h
@@ -612,14 +612,17 @@ static inline struct osd_oi *osd_fid2oi(struct osd_device *osd,
 enum {
 	LPROC_OSD_READ_BYTES = 0,
 	LPROC_OSD_WRITE_BYTES = 1,
-	LPROC_OSD_GET_FOLIO = 2,
-	LPROC_OSD_NO_FOLIO = 3,
+	/* *_PAGE are the pre-folio names of *_FOLIO; osd-zfs samples neither */
+	LPROC_OSD_GET_PAGE = 2,
+	LPROC_OSD_NO_PAGE = 3,
 	LPROC_OSD_CACHE_ACCESS = 4,
 	LPROC_OSD_CACHE_HIT = 5,
 	LPROC_OSD_CACHE_MISS = 6,
 	LPROC_OSD_COPY_IO = 7,
 	LPROC_OSD_ZEROCOPY_IO = 8,
 	LPROC_OSD_TAIL_IO = 9,
+	LPROC_OSD_GET_FOLIO = 10,
+	LPROC_OSD_NO_FOLIO = 11,
 	LPROC_OSD_LAST,
 };
 

lustre/osd-zfs/osd_lproc.c
@@ -82,6 +82,13 @@ static int osd_stats_init(struct osd_device *osd)
 					     osd->od_dt_dev.dd_debugfs_entry,
 					     0);
 	if (osd->od_stats) {
+		/* pre-folio names of the two counters below */
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_PAGE,
+				LPROCFS_CNTR_AVGMINMAX | LPROCFS_CNTR_STDDEV |
+				LPROCFS_TYPE_USECS, "get_page");
+		lprocfs_counter_init(osd->od_stats, LPROC_OSD_NO_PAGE,
+				LPROCFS_CNTR_AVGMINMAX | LPROCFS_TYPE_REQS,
+				"get_page_failures");
 		lprocfs_counter_init(osd->od_stats, LPROC_OSD_GET_FOLIO,
 				LPROCFS_CNTR_AVGMINMAX | LPROCFS_CNTR_STDDEV |
 				LPROCFS_TYPE_USECS, "get_folio");