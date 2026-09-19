lustre/lustre-release

commit 1c91e657e35c0535de6c7b470ad0ee9c19d4b9bf

LU-16518 sec: remove unused llcrypt compat functions
... since they are no longer used. llcrypt_release_ctx() and
llcrypt_enqueue_decrypt_work() lost their last (and only)
caller in libcfs/libcfs/crypto/bio.c when that file was
removed. llcrypt_get_ctx(), llcrypt_handle_d_move(),
llcrypt_ioctl_get_policy(), llcrypt_match_name(),
llcrypt_prepare_lookup() and llcrypt_encrypt_symlink() were
imported verbatim from the in-kernel fscrypt code and never
had a caller in Lustre, which uses its own overlays (e.g.
ll_prepare_lookup()) and the __llcrypt_* variants directly.
The corresponding fscrypt_* macro mappings in lustre_crypto.h
are removed as well.

Also remove the machinery those functions relied on, which is
now dead weight: struct llcrypt_ctx and its preallocated pool
(along with the llcrypt_free_ctxs list, llcrypt_ctx_lock,
llcrypt_ctx_cachep and the num_prealloc_crypto_ctxs module
parameter) and the llcrypt_read_workqueue, which was only
ever allocated and destroyed.

Fixes: d328818a456d ("LU-12275 sec: remove bio functions in fscrypt compat")
Fixes: a813e8187009 ("LU-12275 sec: add llcrypt as file encryption library")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I4173ef018becf5fc2b6f3a590e376cb04ac60f57
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67276
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

include/lustre_compat/linux/llcrypt.h
@@ -38,7 +38,6 @@
 	((name) + round_down((len) - LL_CRYPTO_BLOCK_SIZE - 1,	\
 			     LL_CRYPTO_BLOCK_SIZE))
 
-struct llcrypt_ctx;
 struct llcrypt_info;
 
 struct llcrypt_str {
@@ -84,18 +83,6 @@ struct llcrypt_operations {
 	unsigned int max_namelen;
 };
 
-/* Decryption work */
-struct llcrypt_ctx {
-	union {
-		struct {
-			struct bio *bio;
-			struct work_struct work;
-		};
-		struct list_head free_list;	/* Free list */
-	};
-	u8 flags;				/* Flags */
-};
-
 extern bool llcrypt_has_encryption_key(const struct inode *inode);
 
 static inline bool llcrypt_dummy_context_enabled(struct inode *inode)
@@ -107,18 +94,6 @@ static inline bool llcrypt_dummy_context_enabled(struct inode *inode)
 
 	return lsi->lsi_cop->dummy_context &&
 		lsi->lsi_cop->dummy_context(inode);
-}
-
-/*
- * When d_splice_alias() moves a directory's encrypted alias to its decrypted
- * alias as a result of the encryption key being added, DCACHE_ENCRYPTED_NAME
- * must be cleared.  Note that we don't have to support arbitrary moves of this
- * flag because llcrypt doesn't allow encrypted aliases to be the source or
- * target of a rename().
- */
-static inline void llcrypt_handle_d_move(struct dentry *dentry)
-{
-	dentry->d_flags &= ~DCACHE_ENCRYPTED_NAME;
 }
 
 static inline void folio_bounce_private(struct folio *dst, s32 dstpg,
@@ -143,9 +118,6 @@ static inline void folio_bounce_private(struct folio *dst, s32 dstpg,
 /* crypto.c */
 extern int __init llcrypt_init(void);
 extern void llcrypt_exit(void);
-extern void llcrypt_enqueue_decrypt_work(struct work_struct *);
-extern struct llcrypt_ctx *llcrypt_get_ctx(gfp_t);
-extern void llcrypt_release_ctx(struct llcrypt_ctx *);
 
 extern struct folio *llcrypt_encrypt_pagecache_blocks(struct folio *folio,
 						      s32 pgno,
@@ -200,7 +172,6 @@ extern void llcrypt_free_bounce_folio(struct folio *bounce_folio);
 
 /* policy.c */
 extern int llcrypt_ioctl_set_policy(struct file *, const void __user *);
-extern int llcrypt_ioctl_get_policy(struct file *, void __user *);
 extern int llcrypt_ioctl_get_policy_ex(struct file *, void __user *);
 extern int llcrypt_has_permitted_context(struct inode *, struct inode *);
 extern int llcrypt_inherit_context(struct inode *, struct inode *,
@@ -270,39 +241,6 @@ struct llcrypt_digested_name {
 	u8 digest[LLCRYPT_FNAME_DIGEST_SIZE];
 };
 
-/**
- * llcrypt_match_name() - test whether the given name matches a directory entry
- * @fname: the name being searched for
- * @de_name: the name from the directory entry
- * @de_name_len: the length of @de_name in bytes
- *
- * Normally @fname->disk_name will be set, and in that case we simply compare
- * that to the name stored in the directory entry.  The only exception is that
- * if we don't have the key for an encrypted directory and a filename in it is
- * very long, then we won't have the full disk_name and we'll instead need to
- * match against the llcrypt_digested_name.
- *
- * Return: %true if the name matches, otherwise %false.
- */
-static inline bool llcrypt_match_name(const struct llcrypt_name *fname,
-				      const u8 *de_name, u32 de_name_len)
-{
-	if (unlikely(!fname->disk_name.name)) {
-		const struct llcrypt_digested_name *n =
-			(const void *)fname->crypto_buf.name;
-		if (WARN_ON_ONCE(fname->usr_fname->name[0] != '_'))
-			return false;
-		if (de_name_len <= LLCRYPT_FNAME_MAX_UNDIGESTED_SIZE)
-			return false;
-		return !memcmp(LLCRYPT_FNAME_DIGEST(de_name, de_name_len),
-			       n->digest, LLCRYPT_FNAME_DIGEST_SIZE);
-	}
-
-	if (de_name_len != fname->disk_name.len)
-		return false;
-	return !memcmp(de_name, fname->disk_name.name, fname->disk_name.len);
-}
-
 /* hooks.c */
 extern int llcrypt_file_open(struct inode *inode, struct file *filp);
 extern int __llcrypt_prepare_link(struct inode *inode, struct inode *dir,
@@ -350,25 +288,7 @@ static inline bool llcrypt_dummy_context_enabled(struct inode *inode)
 	return false;
 }
 
-static inline void llcrypt_handle_d_move(struct dentry *dentry)
-{
-}
-
 /* crypto.c */
-static inline void llcrypt_enqueue_decrypt_work(struct work_struct *work)
-{
-}
-
-static inline struct llcrypt_ctx *llcrypt_get_ctx(gfp_t gfp_flags)
-{
-	return ERR_PTR(-EOPNOTSUPP);
-}
-
-static inline void llcrypt_release_ctx(struct llcrypt_ctx *ctx)
-{
-	return;
-}
-
 static inline
 struct folio *llcrypt_encrypt_pagecache_blocks(struct folio *folio,
 					       s32 pgno,
@@ -430,11 +350,6 @@ static inline void llcrypt_free_bounce_folio(struct folio *bounce_page)
 /* policy.c */
 static inline int llcrypt_ioctl_set_policy(struct file *filp,
 					   const void __user *arg)
-{
-	return -EOPNOTSUPP;
-}
-
-static inline int llcrypt_ioctl_get_policy(struct file *filp, void __user *arg)
 {
 	return -EOPNOTSUPP;
 }
@@ -547,15 +462,6 @@ static inline int llcrypt_fname_disk_to_usr(struct inode *inode,
 					    struct llcrypt_str *oname)
 {
 	return -EOPNOTSUPP;
-}
-
-static inline bool llcrypt_match_name(const struct llcrypt_name *fname,
-				      const u8 *de_name, u32 de_name_len)
-{
-	/* Encryption support disabled; use standard comparison */
-	if (de_name_len != fname->disk_name.len)
-		return false;
-	return !memcmp(de_name, fname->disk_name.name, fname->disk_name.len);
 }
 
 /* hooks.c */
@@ -702,39 +608,6 @@ static inline int llcrypt_prepare_rename(struct inode *old_dir,
 }
 
 /**
- * llcrypt_prepare_lookup - prepare to lookup a name in a possibly-encrypted directory
- * @dir: directory being searched
- * @dentry: filename being looked up
- * @fname: (output) the name to use to search the on-disk directory
- *
- * Prepare for ->lookup() in a directory which may be encrypted by determining
- * the name that will actually be used to search the directory on-disk.  Lookups
- * can be done with or without the directory's encryption key; without the key,
- * filenames are presented in encrypted form.  Therefore, we'll try to set up
- * the directory's encryption key, but even without it the lookup can continue.
- *
- * This also installs a custom ->d_revalidate() method which will invalidate the
- * dentry if it was created without the key and the key is later added.
- *
- * Return: 0 on success; -ENOENT if key is unavailable but the filename isn't a
- * correctly formed encoded ciphertext name, so a negative dentry should be
- * created; or another -errno code.
- */
-static inline int llcrypt_prepare_lookup(struct inode *dir,
-					 struct dentry *dentry,
-					 struct llcrypt_name *fname)
-{
-	if (IS_ENCRYPTED(dir))
-		return __llcrypt_prepare_lookup(dir, dentry, fname);
-
-	memset(fname, 0, sizeof(*fname));
-	fname->usr_fname = &dentry->d_name;
-	fname->disk_name.name = (unsigned char *)dentry->d_name.name;
-	fname->disk_name.len = dentry->d_name.len;
-	return 0;
-}
-
-/**
  * llcrypt_prepare_setattr - prepare to change a possibly-encrypted inode's attributes
  * @dentry: dentry through which the inode is being changed
  * @attr: attributes to change
@@ -796,31 +669,6 @@ static inline int llcrypt_prepare_symlink(struct inode *dir,
 	disk_link->len = len + 1;
 	if (disk_link->len > max_len)
 		return -ENAMETOOLONG;
-	return 0;
-}
-
-/**
- * llcrypt_encrypt_symlink - encrypt the symlink target if needed
- * @inode: symlink inode
- * @target: plaintext symlink target
- * @len: length of @target excluding null terminator
- * @disk_link: (in/out) the on-disk symlink target being prepared
- *
- * If the symlink target needs to be encrypted, then this function encrypts it
- * into @disk_link->name.  llcrypt_prepare_symlink() must have been called
- * previously to compute @disk_link->len.  If the filesystem did not allocate a
- * buffer for @disk_link->name after calling llcrypt_prepare_link(), then one
- * will be kmalloc()'ed and the filesystem will be responsible for freeing it.
- *
- * Return: 0 on success, -errno on failure
- */
-static inline int llcrypt_encrypt_symlink(struct inode *inode,
-					  const char *target,
-					  unsigned int len,
-					  struct llcrypt_str *disk_link)
-{
-	if (IS_ENCRYPTED(inode))
-		return __llcrypt_encrypt_symlink(inode, target, len, disk_link);
 	return 0;
 }
 

lustre_compat/fs/crypto/crypto.c
@@ -42,14 +42,10 @@ MODULE_IMPORT_NS(CRYPTO_INTERNAL);
 #endif
 
 static unsigned int num_prealloc_crypto_pages = 32;
-static unsigned int num_prealloc_crypto_ctxs = 128;
 
 module_param(num_prealloc_crypto_pages, uint, 0444);
 MODULE_PARM_DESC(num_prealloc_crypto_pages,
 		"Number of crypto pages to preallocate");
-module_param(num_prealloc_crypto_ctxs, uint, 0444);
-MODULE_PARM_DESC(num_prealloc_crypto_ctxs,
-		"Number of crypto contexts to preallocate");
 
 static char *client_encryption_engine = "aes-ni";
 module_param(client_encryption_engine, charp, 0444);
@@ -59,20 +55,9 @@ enum llcrypt_crypto_engine_type llcrypt_crypto_engine = LLCRYPT_ENGINE_AES_NI;
 
 static mempool_t *llcrypt_bounce_pool = NULL;
 
-static LIST_HEAD(llcrypt_free_ctxs);
-static DEFINE_SPINLOCK(llcrypt_ctx_lock);
-
-static struct workqueue_struct *llcrypt_read_workqueue;
 static DEFINE_MUTEX(llcrypt_init_mutex);
 
-static struct kmem_cache *llcrypt_ctx_cachep;
 struct kmem_cache *llcrypt_info_cachep;
-
-void llcrypt_enqueue_decrypt_work(struct work_struct *work)
-{
-	queue_work(llcrypt_read_workqueue, work);
-}
-EXPORT_SYMBOL(llcrypt_enqueue_decrypt_work);
 
 /*
  * A simple mempool-backed page allocator that allocates folios
@@ -97,62 +82,6 @@ static inline mempool_t *llmempool_create_folio_pool(int min_nr, long order)
 	return mempool_create(min_nr, llpool_alloc_folios, llpool_free_folios,
 			      (void *)order);
 }
-
-/**
- * llcrypt_release_ctx() - Release a decryption context
- * @ctx: The decryption context to release.
- *
- * If the decryption context was allocated from the pre-allocated pool, return
- * it to that pool.  Else, free it.
- */
-void llcrypt_release_ctx(struct llcrypt_ctx *ctx)
-{
-	unsigned long flags;
-
-	if (ctx->flags & FS_CTX_REQUIRES_FREE_ENCRYPT_FL) {
-		kmem_cache_free(llcrypt_ctx_cachep, ctx);
-	} else {
-		spin_lock_irqsave(&llcrypt_ctx_lock, flags);
-		list_add(&ctx->free_list, &llcrypt_free_ctxs);
-		spin_unlock_irqrestore(&llcrypt_ctx_lock, flags);
-	}
-}
-EXPORT_SYMBOL(llcrypt_release_ctx);
-
-/**
- * llcrypt_get_ctx() - Get a decryption context
- * @gfp_flags:   The gfp flag for memory allocation
- *
- * Allocate and initialize a decryption context.
- *
- * Return: A new decryption context on success; an ERR_PTR() otherwise.
- */
-struct llcrypt_ctx *llcrypt_get_ctx(gfp_t gfp_flags)
-{
-	struct llcrypt_ctx *ctx;
-	unsigned long flags;
-
-	/*
-	 * First try getting a ctx from the free list so that we don't have to
-	 * call into the slab allocator.
-	 */
-	spin_lock_irqsave(&llcrypt_ctx_lock, flags);
-	ctx = list_first_entry_or_null(&llcrypt_free_ctxs,
-					struct llcrypt_ctx, free_list);
-	if (ctx)
-		list_del(&ctx->free_list);
-	spin_unlock_irqrestore(&llcrypt_ctx_lock, flags);
-	if (!ctx) {
-		ctx = kmem_cache_zalloc(llcrypt_ctx_cachep, gfp_flags);
-		if (!ctx)
-			return ERR_PTR(-ENOMEM);
-		ctx->flags |= FS_CTX_REQUIRES_FREE_ENCRYPT_FL;
-	} else {
-		ctx->flags &= ~FS_CTX_REQUIRES_FREE_ENCRYPT_FL;
-	}
-	return ctx;
-}
-EXPORT_SYMBOL(llcrypt_get_ctx);
 
 struct folio *llcrypt_alloc_bounce(gfp_t gfp_flags)
 {
@@ -472,11 +401,6 @@ const struct dentry_operations llcrypt_d_ops = {
 
 static void llcrypt_destroy(void)
 {
-	struct llcrypt_ctx *pos, *n;
-
-	list_for_each_entry_safe(pos, n, &llcrypt_free_ctxs, free_list)
-		kmem_cache_free(llcrypt_ctx_cachep, pos);
-	INIT_LIST_HEAD(&llcrypt_free_ctxs);
 	mempool_destroy(llcrypt_bounce_pool);
 	llcrypt_bounce_pool = NULL;
 }
@@ -492,7 +416,7 @@ static void llcrypt_destroy(void)
  */
 int llcrypt_initialize(unsigned int cop_flags)
 {
-	int i, res = -ENOMEM;
+	int res = 0;
 
 	/* No need to allocate a bounce page pool if this FS won't use it. */
 	if (cop_flags & LL_CFLG_OWN_PAGES)
@@ -502,25 +426,12 @@ int llcrypt_initialize(unsigned int cop_flags)
 	if (llcrypt_bounce_pool)
 		goto already_initialized;
 
-	for (i = 0; i < num_prealloc_crypto_ctxs; i++) {
-		struct llcrypt_ctx *ctx;
-
-		ctx = kmem_cache_zalloc(llcrypt_ctx_cachep, GFP_NOFS);
-		if (!ctx)
-			goto fail;
-		list_add(&ctx->free_list, &llcrypt_free_ctxs);
-	}
-
 	llcrypt_bounce_pool =
 		llmempool_create_folio_pool(num_prealloc_crypto_pages, 0);
 	if (!llcrypt_bounce_pool)
-		goto fail;
+		res = -ENOMEM;
 
 already_initialized:
-	mutex_unlock(&llcrypt_init_mutex);
-	return 0;
-fail:
-	llcrypt_destroy();
 	mutex_unlock(&llcrypt_init_mutex);
 	return res;
 }
@@ -562,29 +473,11 @@ static inline int set_llcrypt_crypto_engine_type(void)
  */
 int __init llcrypt_init(void)
 {
-	int err = -ENOMEM;
-
-	/*
-	 * Use an unbound workqueue to allow bios to be decrypted in parallel
-	 * even when they happen to complete on the same CPU.  This sacrifices
-	 * locality, but it's worthwhile since decryption is CPU-intensive.
-	 *
-	 * Also use a high-priority workqueue to prioritize decryption work,
-	 * which blocks reads from completing, over regular application tasks.
-	 */
-	llcrypt_read_workqueue = alloc_workqueue("llcrypt_read_queue",
-						 WQ_UNBOUND | WQ_HIGHPRI,
-						 num_online_cpus());
-	if (!llcrypt_read_workqueue)
-		goto fail;
-
-	llcrypt_ctx_cachep = KMEM_CACHE(llcrypt_ctx, SLAB_RECLAIM_ACCOUNT);
-	if (!llcrypt_ctx_cachep)
-		goto fail_free_queue;
+	int err;
 
 	llcrypt_info_cachep = KMEM_CACHE(llcrypt_info, SLAB_RECLAIM_ACCOUNT);
 	if (!llcrypt_info_cachep)
-		goto fail_free_ctx;
+		return -ENOMEM;
 
 	err = set_llcrypt_crypto_engine_type();
 	if (err) {
@@ -601,11 +494,6 @@ int __init llcrypt_init(void)
 
 fail_free_info:
 	kmem_cache_destroy(llcrypt_info_cachep);
-fail_free_ctx:
-	kmem_cache_destroy(llcrypt_ctx_cachep);
-fail_free_queue:
-	destroy_workqueue(llcrypt_read_workqueue);
-fail:
 	return err;
 }
 
@@ -624,6 +512,4 @@ void llcrypt_exit(void)
 	rcu_barrier();
 
 	kmem_cache_destroy(llcrypt_info_cachep);
-	kmem_cache_destroy(llcrypt_ctx_cachep);
-	destroy_workqueue(llcrypt_read_workqueue);
 }

lustre/include/lustre_crypto.h
@@ -98,12 +98,6 @@
 	fscrypt_setup_filename(dir, iname, lookup, fname)
 #define llcrypt_free_filename(fname) \
 	fscrypt_free_filename(fname)
-#define llcrypt_match_name(fname, de_name, name_len)		\
-	fscrypt_match_name(fname, de_name, name_len)
-#define llcrypt_prepare_lookup(dir, dentry, fname) \
-	fscrypt_prepare_lookup(dir, dentry, fname)
-#define llcrypt_encrypt_symlink(inode, target, len, disk_link) \
-	fscrypt_encrypt_symlink(inode, target, len, disk_link)
 #define __llcrypt_encrypt_symlink(inode, target, len, disk_link) \
 	__fscrypt_encrypt_symlink(inode, target, len, disk_link)
 #define llcrypt_prepare_symlink(dir, target, len, max_len, disk_link)	\

lustre_compat/fs/crypto/policy.c
@@ -390,25 +390,6 @@ int llcrypt_ioctl_set_policy(struct file *filp, const void __user *arg)
 }
 EXPORT_SYMBOL(llcrypt_ioctl_set_policy);
 
-/* Original ioctl version; can only get the original policy version */
-int llcrypt_ioctl_get_policy(struct file *filp, void __user *arg)
-{
-	union llcrypt_policy policy;
-	int err;
-
-	err = llcrypt_get_policy(file_inode(filp), &policy);
-	if (err)
-		return err;
-
-	if (policy.version != LLCRYPT_POLICY_V1)
-		return -EINVAL;
-
-	if (copy_to_user(arg, &policy, sizeof(policy.v1)))
-		return -EFAULT;
-	return 0;
-}
-EXPORT_SYMBOL(llcrypt_ioctl_get_policy);
-
 /* Valid filenames_encryption_mode associated with contents_encryption_mode,
  * as imposed by llcrypt_valid_enc_modes()
  */

microsoft/amlFilesystem-lustre

commit efa040f4154c0b2e3ed4080d27f57c679a355be2

LU-16518 sec: fix stale llcrypt function references
Commit 1c91e657e35c ("LU-16518 sec: remove unused llcrypt compat
functions") deleted the unused compat wrappers, but left comments
behind that still name them:

 - the kernel-doc for llcrypt_prepare_symlink() told the filesystem
   to call llcrypt_encrypt_symlink() to create the on-disk target
   later.  The only caller in the tree, ll_new_node_prepare() in
   lustre/llite/namei.c, calls __llcrypt_encrypt_symlink() directly.

 - the kernel-doc for ll_prepare_lookup() described it as an overlay
   to llcrypt_prepare_lookup(), and a comment inside that function
   names the same hook as ll_prepare_lookup(), a self-reference
   mistyped since commit 048e2cc7d2d5 ("LU-15420 sec: handle simple
   fscrypt changes for 5.15 kernels").  Both now name the hook that
   is actually left, __llcrypt_prepare_lookup().  That commit is not
   listed as a second Fixes: tag, because this wording is only
   correct once the compat wrappers are gone.  The in-function
   comment is reflowed while it is being touched, which also drops
   the stray one-space indent it carried.

 - three comments in lustre/llite/crypto.c said a digested name works
   for local file systems that can call llcrypt_match_name().  That
   was only ever a Lustre name -- the compat copy, or an alias for
   fscrypt_match_name() in the native-fscrypt build -- so a local
   file system calls the in-kernel fscrypt_match_name() itself.

Comments only, no functional change.

Fixes: 1c91e657e35c ("LU-16518 sec: remove unused llcrypt compat functions")
Test-Parameters: trivial
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I55b015dfaf8be6c82f240612644ea82eb9f6fd1f

include/lustre_compat/linux/llcrypt.h
@@ -646,7 +646,7 @@ static inline int llcrypt_prepare_setattr(struct dentry *dentry,
  *
  * Additionally, @disk_link->name is set to @target if the symlink will be
  * unencrypted, but left NULL if the symlink will be encrypted.  For encrypted
- * symlinks, the filesystem must call llcrypt_encrypt_symlink() to create the
+ * symlinks, the filesystem must call __llcrypt_encrypt_symlink() to create the
  * on-disk target later.  (The reason for the two-step process is that some
  * filesystems need to know the size of the symlink target before creating the
  * inode, e.g. to determine whether it will be a "fast" or "slow" symlink.)

lustre/llite/crypto.c
@@ -263,7 +263,7 @@ out_free:
 }
 
 /**
- * ll_prepare_lookup() - overlay to llcrypt_prepare_lookup
+ * ll_prepare_lookup() - overlay to __llcrypt_prepare_lookup
  * @dir: the directory that will be searched
  * @de: the dentry contain the user-provided filename being searched for
  * @fname: the filename information to be filled in
@@ -274,7 +274,7 @@ out_free:
  * This overlay function is also necessary to handle the case of operations
  * carried out without the key. Normally llcrypt makes use of digested names in
  * that case. Having a digested name works for local file systems that can call
- * llcrypt_match_name(), but Lustre server side is not aware of encryption.
+ * fscrypt_match_name(), but Lustre server side is not aware of encryption.
  * FID and name hash can then easily be extracted and put into the
  * requests sent to servers.
  *
@@ -316,10 +316,10 @@ int ll_prepare_lookup(struct inode *dir, struct dentry *de,
 		fname->disk_name.len = iname.len;
 		rc = 0;
 	} else {
-		 /* We should use ll_prepare_lookup() but Lustre handles the
-		  * digested form its own way, incompatible with llcrypt's
-		  * digested form.
-		  */
+		/* We should use __llcrypt_prepare_lookup() but Lustre handles
+		 * the digested form its own way, incompatible with llcrypt's
+		 * digested form.
+		 */
 		rc = llcrypt_setup_filename(dir, &iname, 1, fname);
 		if ((rc == 0 || rc == -ENOENT) &&
 #if defined(HAVE_FSCRYPT_NOKEY_NAME) && !defined(CONFIG_LL_ENCRYPTION)
@@ -374,7 +374,7 @@ int ll_prepare_lookup(struct inode *dir, struct dentry *de,
  * This overlay function is also necessary to handle the case of operations
  * carried out without the key. Normally llcrypt makes use of digested names in
  * that case. Having a digested name works for local file systems that can call
- * llcrypt_match_name(), but Lustre server side is not aware of encryption.
+ * fscrypt_match_name(), but Lustre server side is not aware of encryption.
  * So for keyless @lookup operations on long names, for Lustre we choose to
  * present to users the encoded struct ll_digest_filename, instead of a digested
  * name. FID and name hash can then easily be extracted and put into the
@@ -518,7 +518,7 @@ const char *ll_get_symlink(struct inode *inode, const void *caddr,
  * This overlay function is also necessary to handle the case of operations
  * carried out without the key. Normally llcrypt makes use of digested names in
  * that case. Having a digested name works for local file systems that can call
- * llcrypt_match_name(), but Lustre server side is not aware of encryption.
+ * fscrypt_match_name(), but Lustre server side is not aware of encryption.
  * So for keyless @lookup operations on long names, for Lustre we choose to
  * present to users the encoded struct ll_digest_filename, instead of a digested
  * name. FID and name hash can then easily be extracted and put into the