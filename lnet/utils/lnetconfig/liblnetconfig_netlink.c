lustre/lustre-release

commit 985fc4fa2cafc12128f45fe1e6e643a746c3fc72

LU-14391 utils: handle very large YAML data sets.
Some functionality for Lustre and even LNet can return huge
amounts of Netlink data that can overwhelm the internal libyaml
buffers. To resolve this we can create a resizable internal
buffer to collect all the Netlink data that is formated into
YAML. After the message has been completed we can feed this
data in chunk sizes the smaller internal libyaml library can
handle. The libyaml library internal buffer is a rolling buffer
so it will updated when we exceed its internal size. This will
allows collecting every single type of Lustre stat in one go
and for sites that have very large LNet router setups.

Test-Parameters: trivial
Change-Id: I20fdbb19b0f3de3ab52e8ad568c6926f61f627b9
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54132
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/utils/lnetconfig/liblnetconfig_netlink.c
@@ -425,7 +425,10 @@ struct yaml_nl_node {
 
 struct yaml_netlink_input {
 	yaml_parser_t		*parser;
+	void			*start;
+	void			*read;
 	void			*buffer;
+	void			*end;
 	const char		*errmsg;
 	int			error;
 	struct nl_sock		*nl;
@@ -1043,8 +1046,21 @@ static int yaml_netlink_msg_parse(struct nl_msg *msg, void *arg)
 		if (lnet_genlmsg_parse(nlh, 0, attrs, maxtype, policy))
 			return NL_SKIP;
 
-		size = data->parser->raw_buffer.end -
-		       (unsigned char *)data->buffer;
+		size = data->end - data->buffer;
+		if (size < 1024) {
+			size_t len = (data->end - data->start) * 2;
+			size_t off = data->buffer - data->start;
+
+			data->start = realloc(data->start, len);
+			if (!data->start)
+				return NL_STOP;
+			data->end = data->start + len;
+
+			data->buffer = data->start + off;
+			data->read = data->start;
+
+			size = data->end - data->buffer;
+		}
 		yaml_parse_value_list(data, &size, attrs,
 				      &data->cur->keys.lkl_list[1]);
 	}
@@ -1143,34 +1159,36 @@ static int yaml_netlink_read_handler(void *arg, unsigned char *buffer,
 				     size_t size, size_t *size_read)
 {
 	struct yaml_netlink_input *data = arg;
-	struct nl_sock *nl = data->nl;
-	struct nl_cb *cb;
 	int rc = 0;
 
-	if (data->complete) {
-		*size_read = 0;
-		return 1;
+	/* First collect the Netlink data and then transfer it
+	 * into the internal libyaml buffers.
+	 */
+	if (!data->complete) {
+		struct nl_cb *cb = nl_socket_get_cb(data->nl);
+
+		rc = nl_recvmsgs_report(data->nl, cb);
+		if (rc == -NLE_INTR) {
+			*size_read = 0;
+			return 1;
+		} else if (!data->errmsg && rc < 0) {
+			data->errmsg = nl_geterror(rc);
+			return 0;
+		} else if (data->parser->error) {
+			/* data->errmsg is set in NL_CB_FINISH */
+			return 0;
+		}
 	}
-
-	data->buffer = buffer;
-
-	cb = nl_socket_get_cb(nl);
-	rc = nl_recvmsgs_report(nl, cb);
-	if (rc == -NLE_INTR) {
-		*size_read = 0;
-		return 1;
-	} else if (!data->errmsg && rc < 0) {
-		data->errmsg = nl_geterror(rc);
-		return 0;
-	} else if (data->parser->error) {
-		/* data->errmsg is set in NL_CB_FINISH */
-		return 0;
-	}
-
-	rc = (unsigned char *)data->buffer - buffer;
+	rc = data->buffer - data->read;
 	if ((int)size > rc)
 		size = rc;
 
+	if (size) {
+		memcpy(buffer, data->read, size);
+		data->read += size;
+	} else if (data->complete) {
+		free(data->start);
+	}
 	*size_read = size;
 	return 1;
 }
@@ -1229,6 +1247,10 @@ yaml_parser_set_input_netlink(yaml_parser_t *reply, struct nl_sock *nl,
 		goto failed;
 	}
 
+	buf->start = malloc(65536);
+	buf->end = buf->start + 65536;
+	buf->buffer = buf->start;
+	buf->read = buf->start;
 	buf->nl = nl;
 	buf->async = stream;
 	buf->parser = reply;

microsoft/amlFilesystem-lustre

commit ba463a09e2507b29da0fd5453e5fca9557e1dcf7

LU-9680 utils: handle very large netlink message in liblnetconfig
A few scaling patches have exposed a problem in the liblnetconfig
library when receiving message from kernel space. Currently we
examine if the remaining space falls below a certain level, 1024
bytes, then we increase the internal buffer size to receive the
next Netlink message. It is possible that the next Netlink message
is much larger than the remaining 1024 bytes. This patch changes
it to compare the calculated size needed to fit the unpacked
Netlink packet into the internal buffer. Be aware the internal
buffer consumes more space than the size of the received Netlink
packet so we scale it by 4 which covers most cases. The new
buffer size is to ensure it's large enough to cover the next
Netlink packet. A few other issues are addressed as well. Now
data->read is correctly preserved which matters for Netlink
streaming. On error of realloc() we preserve data->start so
it doesn't leak. Lastly set data->error to an errno code
return from a new function nlerr2syserr() which was missed
for yaml_netlink_read_handler(). Unlike most c applications
Lustre utilities treat errno as a negative value. Its still
can be used by strerror().

Test-Parameters: trivial
Fixes: 985fc4fa2caf ("LU-14391 utils: handle very large YAML data sets.")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: I60d81bd21496f814b1e8e4884dc4d4a5334fd21c

lnet/utils/lnetconfig/liblnetconfig_netlink.c
@@ -220,6 +220,7 @@ struct yaml_netlink_input {
 	bool			async;
 	unsigned int		indent;
 	unsigned int		version;
+	unsigned int		key_expansion;
 	struct yaml_nl_node	*cur;
 	struct yaml_nl_node	*root;
 };
@@ -799,9 +800,9 @@ static int yaml_netlink_msg_parse(struct nl_msg *msg, void *arg)
 	yaml_parser_t *parser = arg;
 	struct yaml_netlink_input *data = parser->read_handler_data;
 	struct nlmsghdr *nlh = nlmsg_hdr(msg);
+	struct genlmsghdr *ghdr = genlmsg_hdr(nlh);
 
 	if (nlh->nlmsg_flags & NLM_F_CREATE) {
-		struct genlmsghdr *ghdr = genlmsg_hdr(nlh);
 		struct nlattr *attrs[LN_SCALAR_MAX + 1];
 
 		if (genlmsg_parse(nlh, 0, attrs, LN_SCALAR_MAX,
@@ -841,9 +842,10 @@ static int yaml_netlink_msg_parse(struct nl_msg *msg, void *arg)
 		data->version = ghdr->version;
 	} else {
 		uint16_t maxtype = data->cur->keys.lkl_maxattr;
+		/* unpacked Netlink message is much larger in YAML buffer */
+		int msg_len = genlmsg_len(ghdr), size, i;
 		struct nla_policy policy[maxtype];
 		struct nlattr *attrs[maxtype];
-		int size, i;
 
 		memset(policy, 0, sizeof(struct nla_policy) * maxtype);
 		for (i = 1; i < maxtype; i++)
@@ -852,18 +854,30 @@ static int yaml_netlink_msg_parse(struct nl_msg *msg, void *arg)
 		if (genlmsg_parse(nlh, 0, attrs, maxtype, policy))
 			return NL_SKIP;
 
+		msg_len *= data->key_expansion;
 		size = data->end - data->buffer;
-		if (size < 1024) {
-			size_t len = (data->end - data->start) * 2;
+		if (msg_len > size || size < 1024) {
 			size_t off = data->buffer - data->start;
+			size_t read = data->read - data->start;
+			size_t len = data->end - data->start;
+			char *tmp;
 
-			data->start = realloc(data->start, len);
-			if (!data->start)
+			/* Handle if Netlink message is really big */
+			do {
+				len *= 2;
+			} while (len - off < msg_len);
+
+			tmp = realloc(data->start, len);
+			if (!tmp) {
+				data->errmsg = nl_geterror(NLE_NOMEM);
+				data->error = -ENOMEM;
 				return NL_STOP;
-			data->end = data->start + len;
+			}
 
+			data->start = tmp;
 			data->buffer = data->start + off;
-			data->read = data->start;
+			data->read = data->start + read;
+			data->end = data->start + len;
 
 			size = data->end - data->buffer;
 		}
@@ -886,7 +900,9 @@ static int yaml_netlink_parse_msg_error(struct nlmsgerr *errmsg,
 	if ((nlh->nlmsg_type == NLMSG_ERROR ||
 	     nlh->nlmsg_flags & NLM_F_ACK_TLVS) && errmsg->error) {
 		/* libyaml stomps on the reader error so we need to
-		 * cache the source of the error.
+		 * cache the source of the error. This error is based
+		 * on what libnl3 reports. If the user wants strerror
+		 * a errno is provided.
 		 */
 		const char *errstr = nl_geterror(nl_syserr2nlerr(errmsg->error));
 		struct yaml_netlink_input *data = parser->read_handler_data;
@@ -968,6 +984,77 @@ static int yaml_netlink_msg_complete(struct nl_msg *msg, void *arg)
 	return data->async ? NL_OK : NL_STOP;
 }
 
+static int nlerr2syserr(int error)
+{
+	int rc;
+
+	switch (abs(error)) {
+	case NLE_BAD_SOCK:
+		rc = EBADF;
+		break;
+	case NLE_EXIST:
+		rc = EEXIST;
+		break;
+	case NLE_NOADDR:
+		rc = EADDRNOTAVAIL;
+		break;
+	case NLE_OBJ_NOTFOUND:
+		rc = ENOENT;
+		break;
+	case NLE_INTR:
+		rc = EINTR;
+		break;
+	case NLE_AGAIN:
+		rc = EAGAIN;
+		break;
+	case NLE_NOACCESS:
+		rc = EACCES;
+		break;
+	case NLE_INVAL:
+		rc = EINVAL;
+		break;
+	case NLE_NOMEM:
+		rc = ENOMEM;
+		break;
+	case NLE_AF_NOSUPPORT:
+		rc = EAFNOSUPPORT;
+		break;
+	case NLE_PROTO_MISMATCH:
+		rc = EPROTONOSUPPORT;
+		break;
+	case NLE_OPNOTSUPP:
+		rc = EOPNOTSUPP;
+		break;
+	case NLE_SEQ_MISMATCH:
+		rc = EILSEQ;
+		break;
+	case NLE_PERM:
+		rc = EPERM;
+		break;
+	case NLE_BUSY:
+		rc = EBUSY;
+		break;
+	case NLE_RANGE:
+		rc = ERANGE;
+		break;
+	case NLE_NODEV:
+		rc = ENODEV;
+		break;
+	case NLE_MSG_TRUNC:
+		rc = EMSGSIZE;
+		break;
+	case NLE_MSG_OVERFLOW:
+		rc = ENOBUFS;
+		break;
+	case NLE_FAILURE:
+	default:
+		rc = ENETDOWN;
+		break;
+	}
+
+	return rc;
+}
+
 /**
  * In order for yaml_parser_set_input_netlink() to work we have to
  * register a yaml_read_handler_t callback. This is that call back
@@ -992,6 +1079,7 @@ static int yaml_netlink_read_handler(void *arg, unsigned char *buffer,
 			return 1;
 		} else if (!data->errmsg && rc < 0) {
 			data->errmsg = nl_geterror(rc);
+			data->error = -nlerr2syserr(rc);
 			return 0;
 		} else if (data->parser->error) {
 			/* data->errmsg is set in NL_CB_FINISH */
@@ -1003,6 +1091,7 @@ static int yaml_netlink_read_handler(void *arg, unsigned char *buffer,
 		size = rc;
 
 	if (size) {
+		/* copy from our buffer to libyaml buffer */
 		memcpy(buffer, data->read, size);
 		data->read += size;
 	} else if (data->complete) {
@@ -1040,6 +1129,14 @@ yaml_parser_get_reader_proto_version(yaml_parser_t *parser)
 	return buf->version;
 }
 
+/* When unpacking Netlink messages they get placed into an internal
+ * buffer that includes white spaces created by the indentation levels
+ * which matches the Netlink nested levels. This is a guess based on
+ * profiling of various messages but in the future we can examine the
+ * levels created with the key tables.
+ */
+#define YAML_KEY_EXPANSION	4
+
 /* yaml_parser_set_input_netlink() mirrors the libyaml function
  * yaml_parser_set_input_file(). Internally it does setup of the
  * libnl socket callbacks to parse the Netlink messages received
@@ -1071,6 +1168,7 @@ yaml_parser_set_input_netlink(yaml_parser_t *reply, struct nl_sock *nl,
 	buf->end = buf->start + 65536;
 	buf->buffer = buf->start;
 	buf->read = buf->start;
+	buf->key_expansion = YAML_KEY_EXPANSION;
 	buf->nl = nl;
 	buf->async = stream;
 	buf->parser = reply;