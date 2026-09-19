lustre/lustre-release

commit 23a2db28dcf1422a6a6da575e907fd257106d402

LU-13031 jobstats: store jobid in xattr when files are created
This change stores the jobid of the process that creates a file in an
extended attribute in the file's MDT inode, at file creation time.

The name of the xattr is determined by a new sysfs parameter
"mdt.*.job_xattr" so that the admin can choose a name that does
not conflict with other uses they may have for a given xattr.
The default value is "user.job". A value of "NONE" means that
the jobid will not be stored in the inode.

If the name is in the user namespace "user.", then the name portion
can be up to 7 alphanumeric characters long. The admin can choose
the trusted namespace to prevent users from modifying the value,
but only "trusted.job" is allowed in this namespace.

Allowing users to modify the contents of the xattr is helpful so
that the jobid can be preserved even when files are moved with tools
like `cp` or `rsync`, and when copied from one filesystem to another.

Signed-off-by: Thomas Bertschinger <bertschinger@lanl.gov>
Change-Id: Iad78a5ec6fbc4b761ff481141763bdd0cdcd0128
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50982
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/mdd/mdd_dir.c
@@ -2500,9 +2500,13 @@ static int mdd_create_object(const struct lu_env *env, struct mdd_object *pobj,
 			     struct lu_buf *def_acl_buf,
 			     struct lu_buf *hsm_buf,
 			     struct dt_allocation_hint *hint,
-			     struct thandle *handle, bool initsecctx)
+			     struct thandle *handle, bool initial_create)
 {
+	const struct lu_fid *son_fid = mdd_object_fid(son);
+	const struct lu_ucred *uc = lu_ucred(env);
+	const char *jobid = uc->uc_jobid;
 	const struct lu_buf *buf;
+	size_t jobid_len;
 	int rc;
 
 	mdd_write_lock(env, son, DT_TGT_CHILD);
@@ -2595,7 +2599,7 @@ static int mdd_create_object(const struct lu_env *env, struct mdd_object *pobj,
 			GOTO(err_initlized, rc = -EFAULT);
 	}
 
-	if (initsecctx && spec->sp_cr_file_secctx_name != NULL) {
+	if (initial_create && spec->sp_cr_file_secctx_name != NULL) {
 		buf = mdd_buf_get_const(env, spec->sp_cr_file_secctx,
 					spec->sp_cr_file_secctx_size);
 		rc = mdo_xattr_set(env, son, buf, spec->sp_cr_file_secctx_name,
@@ -2612,6 +2616,25 @@ static int mdd_create_object(const struct lu_env *env, struct mdd_object *pobj,
 				   handle);
 		if (rc < 0)
 			GOTO(err_initlized, rc);
+	}
+
+	if (initial_create &&
+	    spec->sp_cr_job_xattr[0] != '\0' &&
+	    jobid[0] != '\0' &&
+	    (S_ISREG(attr->la_mode) || S_ISDIR(attr->la_mode))) {
+		jobid_len = strnlen(jobid, LUSTRE_JOBID_SIZE);
+		buf = mdd_buf_get_const(env, jobid, jobid_len);
+
+		rc = mdo_xattr_set(env, son, buf, spec->sp_cr_job_xattr,
+				   LU_XATTR_CREATE, handle);
+
+		/* this xattr is nonessential, so ignore errors. */
+		if (rc != 0) {
+			CDEBUG(D_INODE,
+			       DFID" failed to set xattr '%s': rc = %d\n",
+			       PFID(son_fid), spec->sp_cr_job_xattr, rc);
+			rc = 0;
+		}
 	}
 
 err_initlized:

lustre/mdt/mdt_handler.c
@@ -6205,6 +6205,8 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	/* Set this lu_device to obd for error handling purposes. */
 	obd->obd_lu_dev = &m->mdt_lu_dev;
 
+	strncpy(m->mdt_job_xattr, XATTR_NAME_JOB_DEFAULT, XATTR_JOB_MAX_LEN);
+
 	/* init the stack */
 	rc = mdt_stack_init((struct lu_env *)env, m, cfg);
 	if (rc) {

lustre/include/md_object.h
@@ -177,6 +177,9 @@ struct md_op_spec {
 
 	/** to create directory */
 	const struct dt_index_features *sp_feat;
+
+	/* name of xattr used to store jobid in inode, or empty if disabled */
+	char sp_cr_job_xattr[XATTR_JOB_MAX_LEN];
 };
 
 enum md_layout_opc {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1231,6 +1231,10 @@ struct lov_mds_md_v1 {            /* LOV EA mds/wire data (little-endian) */
 #define XATTR_NAME_LFSCK_NAMESPACE "trusted.lfsck_ns"
 #define XATTR_NAME_MAX_LEN	32 /* increase this, if there is longer name. */
 
+#define XATTR_NAME_JOB_DEFAULT	"user.job"
+/* longest allowed jobid xattr name is "user." + 7 chars + null terminator */
+#define XATTR_JOB_MAX_LEN	13
+
 struct lov_mds_md_v3 {            /* LOV EA mds/wire data (little-endian) */
 	__u32 lmm_magic;          /* magic number = LOV_MAGIC_V3 */
 	__u32 lmm_pattern;        /* LOV_PATTERN_RAID0, LOV_PATTERN_RAID1 */

lustre/mdt/mdt_internal.h
@@ -354,6 +354,9 @@ struct mdt_device {
 
 	/* count of old clients that doesn't support DMV implicite inherit */
 	atomic_t		   mdt_dmv_old_client_count;
+
+	/* name of xattr used to store jobid in mdt inode */
+	char			   mdt_job_xattr[XATTR_JOB_MAX_LEN];
 };
 
 #define MDT_SERVICE_WATCHDOG_FACTOR	(2)

lustre/mdt/mdt_lproc.c
@@ -1542,12 +1542,97 @@ static int mdt_checksum_type_seq_show(struct seq_file *m, void *data)
 	return 0;
 }
 
+ssize_t job_xattr_show(struct kobject *kobj, struct attribute *attr, char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
+
+	if (mdt->mdt_job_xattr[0] == '\0')
+		return scnprintf(buf, PAGE_SIZE, "NONE\n");
+
+	return scnprintf(buf, PAGE_SIZE, "%s\n", mdt->mdt_job_xattr);
+}
+
+/**
+ * Read in a name for the jobid xattr and validate it.
+ * The only valid names are "trusted.job" or "user.*" where the name portion
+ * is <= 7 bytes in the user namespace. Only alphanumeric characters are
+ * allowed, aside from the namespace separator '.'.
+ *
+ * "none" is a valid value to turn this feature off.
+ *
+ * @return -EINVAL if the name is invalid, else count
+ */
+ssize_t job_xattr_store(struct kobject *kobj, struct attribute *attr,
+			const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
+	char name[XATTR_JOB_MAX_LEN] = { 0 };
+	char *p;
+
+
+	/* writing "none" turns this off by leaving the name empty */
+	if (!strncmp(buffer, "none", 4) ||
+	    !strncmp(buffer, "NONE", 4)) {
+		memset(mdt->mdt_job_xattr, 0, sizeof(mdt->mdt_job_xattr));
+		return count;
+	}
+
+	/* account for stripping \n before rejecting name for being too long */
+	if (count > XATTR_JOB_MAX_LEN - 1 &&
+	    buffer[XATTR_JOB_MAX_LEN - 1] != '\n')
+		return -EINVAL;
+
+	strncpy(name, buffer, XATTR_JOB_MAX_LEN - 1);
+
+	/* reject if not in namespace.name format */
+	p = strchr(name, '.');
+	if (p == NULL)
+		return -EINVAL;
+
+	p++;
+	for (; *p != '\0'; p++) {
+		/*
+		 * if there are any non-alphanumeric characters, the name is
+		 * invalid unless it's a newline, in which case overwrite it
+		 * with '\0' and that's the end of the name.
+		 */
+		if (!isalnum(*p)) {
+			if (*p != '\n')
+				return -EINVAL;
+			*p = '\0';
+		}
+	}
+
+	/* trusted.job is only valid name in trusted namespace */
+	if (!strncmp(name, "trusted.job", 12)) {
+		strncpy(mdt->mdt_job_xattr, name, XATTR_JOB_MAX_LEN);
+		return count;
+	}
+
+	/* only other valid namespace is user */
+	if (strncmp(name, XATTR_USER_PREFIX, sizeof(XATTR_USER_PREFIX) - 1))
+		return -EINVAL;
+
+	/* ensure that a name was specified */
+	if (name[sizeof(XATTR_USER_PREFIX) - 1] == '\0')
+		return -EINVAL;
+
+	strncpy(mdt->mdt_job_xattr, name, XATTR_JOB_MAX_LEN);
+
+	return count;
+}
+
 LPROC_SEQ_FOPS_RO(mdt_checksum_type);
 
 LPROC_SEQ_FOPS_RO_TYPE(mdt, hash);
 LPROC_SEQ_FOPS_WR_ONLY(mdt, mds_evict_client);
 LPROC_SEQ_FOPS_RW_TYPE(mdt, checksum_dump);
 LUSTRE_RW_ATTR(job_cleanup_interval);
+LUSTRE_RW_ATTR(job_xattr);
 LPROC_SEQ_FOPS_RW_TYPE(mdt, nid_stats_clear);
 LUSTRE_RW_ATTR(hsm_control);
 
@@ -1603,6 +1688,7 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_migrate_hsm_allowed.attr,
 	&lustre_attr_hsm_control.attr,
 	&lustre_attr_job_cleanup_interval.attr,
+	&lustre_attr_job_xattr.attr,
 	&lustre_attr_readonly.attr,
 	&lustre_attr_dir_split_count.attr,
 	&lustre_attr_dir_split_delta.attr,

lustre/mdt/mdt_open.c
@@ -1577,6 +1577,10 @@ again_pw:
 		info->mti_spec.sp_cr_lookup = 0;
 		info->mti_spec.sp_feat = &dt_directory_features;
 
+		/* set jobid xattr name from sysfs parameter */
+		strncpy(info->mti_spec.sp_cr_job_xattr, mdt->mdt_job_xattr,
+			XATTR_JOB_MAX_LEN);
+
 		result = mdo_create(info->mti_env, mdt_object_child(parent),
 				    &rr->rr_name, mdt_object_child(child),
 				    &info->mti_spec, &info->mti_attr);

lustre/mdt/mdt_reint.c
@@ -651,6 +651,10 @@ static int mdt_create(struct mdt_thread_info *info)
 		info->mti_spec.sp_cr_lookup = 1;
 	info->mti_spec.sp_feat = &dt_directory_features;
 
+	/* set jobid xattr name from sysfs parameter */
+	strncpy(info->mti_spec.sp_cr_job_xattr, mdt->mdt_job_xattr,
+		XATTR_JOB_MAX_LEN);
+
 	rc = mdo_create(info->mti_env, mdt_object_child(parent), &rr->rr_name,
 			mdt_object_child(child), &info->mti_spec, ma);
 	if (rc == 0)

microsoft/amlFilesystem-lustre

commit 87c2367f668b3de1f2a0f6f3ee64fc084bd19848

LU-20211 mdd: declare credits for jobid xattr on create
Creating a file stores the jobid in an xattr (LU-13031), but the
create declare path never reserved credits for that set.  Tight
credit accounting can LBUG in transaction execution.

mdt_thread_info is not fully zeroed between requests, so
sp_cr_job_xattr and the create secctx/encctx pointers can leak
from a prior create into a later operation.  The pointers refer
into the previous request pill.  Clear them in
mdt_thread_info_reset().

Fixes: 23a2db28dcf1 ("LU-13031 jobstats: store jobid in xattr when files are created")
Fixes: 4ea24bdabb ("LU-5560 security: send file security context for creates")
Fixes: 40d91eafe2 ("LU-12275 sec: atomicity of encryption context getting/setting")
Signed-off-by: Keguang Xu <kxu@ddn.com>
Change-Id: If697bed9d77c6030a1fa21972592f4af7a035b1b

lustre/mdd/mdd_dir.c
@@ -2754,6 +2754,23 @@ static int mdd_declare_create_object(const struct lu_env *env,
 		if (rc)
 			GOTO(out, rc);
 	}
+
+	/* Declare job xattr credits for mdd_create_object() below. */
+	if (spec->sp_cr_job_xattr[0] != '\0' &&
+	    (S_ISREG(attr->la_mode) || S_ISDIR(attr->la_mode))) {
+		const struct lu_ucred *uc = lu_ucred(env);
+		const struct lu_buf *job_buf;
+
+		if (uc != NULL && uc->uc_jobid[0] != '\0') {
+			job_buf = mdd_buf_get_const(env, uc->uc_jobid,
+						    LUSTRE_JOBID_SIZE);
+			rc = mdo_declare_xattr_set(env, c, job_buf,
+						   spec->sp_cr_job_xattr, 0,
+						   handle);
+			if (rc)
+				GOTO(out, rc);
+		}
+	}
 out:
 	return rc;
 }

lustre/mdt/mdt_handler.c
@@ -4730,12 +4730,8 @@ void mdt_thread_info_reset(struct mdt_thread_info *info)
 	info->mti_som_strict = 0;
 	info->mti_intent_lock = 0;
 
-	info->mti_spec.no_create = 0;
-	info->mti_spec.sp_rm_entry = 0;
-	info->mti_spec.sp_permitted = 0;
-
-	info->mti_spec.u.sp_ea.eadata = NULL;
-	info->mti_spec.u.sp_ea.eadatalen = 0;
+	/* spec pointers may alias the previous request pill */
+	memset(&info->mti_spec, 0, sizeof(info->mti_spec));
 
 	if (info->mti_batch_env && info->mti_object != NULL) {
 		mdt_object_put(info->mti_env, info->mti_object);