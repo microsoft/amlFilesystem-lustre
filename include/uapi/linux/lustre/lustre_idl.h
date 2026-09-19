lustre/lustre-release

commit f172b116885753d0f316549a2fb9d451e9b4bd2e

LU-10092 llite: Add persistent cache on client
PCC is a new framework which provides a group of local cache
on Lustre client side. No global namespace will be provided
by PCC. Each client uses its own local storage as a cache for
itself. Local file system is used to manage the data on local
caches. Cached I/O is directed to local filesystem while
normal I/O is directed to OSTs.

PCC uses HSM for data synchronization. It uses HSM copytool
to restore file from local caches to Lustre OSTs. Each PCC
has a copytool instance running with unique archive number.
Any remote access from another Lustre client would trigger
the data synchronization. If a client with PCC goes offline,
the cached data becomes inaccessible for other client
temporarilly. And after the PCC client reboots and the copytool
restarts, the data will be accessible again.

ToDo:
1) Make PCC exclusive with HSM.
2) Strong size consistence for PCC cached file among clients.
3) Support to cache partial content of a file.

Change-Id: I188ed36c48aae223380739f607cc6caf2f789298
Test-Parameters: clientcount=3 testlist=sanity-pcc,sanity-pcc,sanity-pcc
Signed-off-by: Li Xi <lixi@ddn.com>
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Signed-off-by: Qian Yingjin <qian@ddn.com>
Reviewed-on: https://review.whamcloud.com/32963
Tested-by: Jenkins
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd.h
@@ -917,6 +917,8 @@ struct md_op_data {
 	bool			op_post_migrate;
 	/* used to access dir with bash hash */
 	__u32			op_stripe_index;
+	/* Archive ID for PCC attach */
+	__u32			op_archive_id;
 };
 
 struct md_callback {

microsoft/amlFilesystem-lustre

commit 069bbc4463bb853fb5f315138e973ff550d40527

LU-18746 flr: allow merge stale mirror
Let a mirror be merged into a file without copying data into it
first, so that mirrors can be appended cheaply and resynced later,
by a data mover or by an EC parity extend that computes the mirror
content in place.

The client asks for it with LL_LEASE_ALLOW_STALE, a modifier on
LL_LEASE_LAYOUT_MERGE. It travels to the MDT as CD_MERGE_STALE in
the new close_data field cd_merge_flags and becomes the
LU_XATTR_MERGE_STALE modifier on LU_XATTR_MERGE, on which
lod_declare_layout_merge() marks every merged component
LCME_FL_STALE. The merge also reloads the striping with
LVF_ALL_STALE now, so a merge that would leave every mirror stale
is refused with -EPERM, as split already does. An MDT without this
change ignores cd_merge_flags and would merge the mirror as up to
date, so the client refuses the request with -EOPNOTSUPP unless
the MDT advertises OBD_CONNECT2_FLR_EC. That flag is only
negotiated with EC enabled on both the client and the MDT, so a
stale merge needs that too.

lfs still merges in sync. The stale path gets its callers in
"LU-19548 lfs: mirror extend support for EC" and "LU-17531 lfs:
add --no-resync option in mirror extend".

Also return the error from the LL_IOC_LEASE unlock argument checks
instead of overwriting it with the lease type from the close.

Fixes: f172b116885 ("LU-10092 llite: Add persistent cache on client")
Test-Parameters: testlist=sanity serverversion=2.17 env=SKIP27D="-s 37"
Test-Parameters: testlist=sanity-flr serverversion=2.17 env=SANITY_FLR_EXCEPT=0a
Test-Parameters: testlist=sanity clientversion=2.17
Test-Parameters: testlist=sanity-flr clientversion=2.17
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Signed-off-by: Maximilian Dilger <mdilger@whamcloud.com>
Assisted-by: ClaudeCode:Fable-5.1 llm_code_and_review_tools
Change-Id: I0d730566ea9b238aeac5d2e27e77c719a1c73308

include/uapi/linux/lustre/lustre_idl.h
@@ -3842,6 +3842,12 @@ struct close_data_resync_done {
 	__u32	resync_ids_inline[INLINE_RESYNC_ARRAY_SIZE];
 };
 
+enum cd_merge_flags {
+	CD_MERGE_STALE		= 0x0001,
+	/* Sentinel to ensure merge flags fit into 16-bit cd_merge_flags */
+	CD_MERGE_FLAG_MAX
+};
+
 struct close_data {
 	struct lustre_handle	cd_handle;
 	struct lu_fid		cd_fid;
@@ -3851,6 +3857,8 @@ struct close_data {
 		struct close_data_resync_done	cd_resync;
 		/* split close */
 		__u16				cd_mirror_id;
+		/* merge */
+		__u16				cd_merge_flags;
 		/* PCC release */
 		__u32				cd_archive_id;
 		/* migrate swap layout */

include/uapi/linux/lustre/lustre_user.h
@@ -614,6 +614,7 @@ enum ll_lease_flags {
 	LL_LEASE_LAYOUT_MERGE	= 0x4,
 	LL_LEASE_LAYOUT_SPLIT	= 0x8,
 	LL_LEASE_PCC_ATTACH	= 0x10,
+	LL_LEASE_ALLOW_STALE	= 0x20,
 };
 
 #define IOC_IDS_MAX	4096

lustre/include/lu_object.h
@@ -898,6 +898,8 @@ enum lu_xattr_flags {
 	LU_XATTR_MERGE   = BIT(2),
 	LU_XATTR_SPLIT   = BIT(3),
 	LU_XATTR_PURGE   = BIT(4),
+	/* modifier for LU_XATTR_MERGE: mark the merged mirror stale */
+	LU_XATTR_MERGE_STALE = BIT(5),
 };
 
 /* For lu_context health-checks */

lustre/include/lustre_mds.h
@@ -30,7 +30,10 @@
 
 struct md_rejig_data {
 	struct md_object	*mrd_obj;
+	/* mirror split */
 	__u16			mrd_mirror_id;
+	/* mirror merge flags, matches close_data::cd_merge_flags */
+	__u16			mrd_merge_flags;
 };
 
 #define MDD_OBD_NAME     "mdd_obd"

lustre/include/obd.h
@@ -1087,6 +1087,8 @@ struct md_op_data {
 	 * order there may be a race with creation by others.
 	 */
 	bool			op_new_layout;
+	/* merge flags, matches close_data::cd_merge_flags */
+	__u16			op_merge_flags;
 	/* used to access dir with bash hash */
 	__u32			op_stripe_index;
 	/* Archive ID for PCC attach */

lustre/llite/file.c
@@ -40,9 +40,10 @@
 #include "llite_internal.h"
 #include "vvp_internal.h"
 
-struct split_param {
-	struct inode	*sp_inode;
-	__u16		sp_mirror_id;
+struct mirror_rejig_param {
+	struct inode	*mrp_inode;
+	__u16		 mrp_mirror_id; /* mirror split */
+	__u16		 mrp_merge_flags; /* match close_data::cd_merge_flags */
 };
 
 struct pcc_param {
@@ -165,23 +166,23 @@ static int ll_close_inode_openhandle(struct inode *inode,
 	ll_prepare_close(inode, op_data, och);
 	switch (bias) {
 	case MDS_CLOSE_LAYOUT_MERGE:
-		/* merge blocks from the victim inode */
-		op_data->op_attr_blocks += ((struct inode *)data)->i_blocks;
-		op_data->op_attr.ia_valid |= ATTR_SIZE;
-		op_data->op_xvalid |= OP_XVALID_BLOCKS;
-		fallthrough;
-	case MDS_CLOSE_LAYOUT_SPLIT: {
-		struct split_param *sp = data;
+	case MDS_CLOSE_LAYOUT_SPLIT:
+	{
+		struct mirror_rejig_param *mrp = data;
 
 		LASSERT(data != NULL);
 		op_data->op_bias |= bias;
 		op_data->op_data_version = 0;
 		op_data->op_lease_handle = och->och_lease_handle;
-		if (bias == MDS_CLOSE_LAYOUT_SPLIT) {
-			op_data->op_fid2 = *ll_inode2fid(sp->sp_inode);
-			op_data->op_mirror_id = sp->sp_mirror_id;
-		} else { /* MDS_CLOSE_LAYOUT_MERGE */
-			op_data->op_fid2 = *ll_inode2fid(data);
+		op_data->op_fid2 = *ll_inode2fid(mrp->mrp_inode);
+		if (bias == MDS_CLOSE_LAYOUT_MERGE) {
+			/* merge blocks from the victim inode */
+			op_data->op_attr_blocks += mrp->mrp_inode->i_blocks;
+			op_data->op_attr.ia_valid |= ATTR_SIZE;
+			op_data->op_xvalid |= OP_XVALID_BLOCKS;
+			op_data->op_merge_flags = mrp->mrp_merge_flags;
+		} else /* if (bias == MDS_CLOSE_LAYOUT_SPLIT) */ {
+			op_data->op_mirror_id = mrp->mrp_mirror_id;
 		}
 		break;
 	}
@@ -4434,7 +4435,7 @@ static long ll_file_unlock_lease(struct file *file, struct ll_ioc_lease *ioc,
 	struct ll_file_data *lfd = file->private_data;
 	struct ll_inode_info *lli = ll_i2info(inode);
 	struct obd_client_handle *och = NULL;
-	struct split_param sp;
+	struct mirror_rejig_param mrp;
 	struct pcc_param param;
 	bool lease_broken = false;
 	enum mds_open_flags open_flags = MDS_FMODE_CLOSED;
@@ -4459,74 +4460,91 @@ static long ll_file_unlock_lease(struct file *file, struct ll_ioc_lease *ioc,
 
 	open_flags = och->och_flags;
 
-	switch (ioc->lil_flags) {
+	if (ioc->lil_flags & LL_LEASE_ALLOW_STALE) {
+		/* a modifier for layout merge only */
+		if ((ioc->lil_flags & ~LL_LEASE_ALLOW_STALE) !=
+		    LL_LEASE_LAYOUT_MERGE)
+			GOTO(out_lease_close, rc2 = -EINVAL);
+
+		/* an older MDT ignores cd_merge_flags and would merge the
+		 * mirror as up to date
+		 */
+		if (!(exp_connect_flags2(ll_i2mdexp(inode)) &
+		      OBD_CONNECT2_FLR_EC))
+			GOTO(out_lease_close, rc2 = -EOPNOTSUPP);
+	}
+
+	switch (ioc->lil_flags & ~LL_LEASE_ALLOW_STALE) {
 	case LL_LEASE_RESYNC_DONE:
 		if (ioc->lil_count > IOC_IDS_MAX)
-			GOTO(out_lease_close, rc = -EINVAL);
+			GOTO(out_lease_close, rc2 = -EINVAL);
 
 		data_size = offsetof(typeof(*ioc), lil_ids[ioc->lil_count]);
 		OBD_ALLOC(data, data_size);
 		if (!data)
-			GOTO(out_lease_close, rc = -ENOMEM);
+			GOTO(out_lease_close, rc2 = -ENOMEM);
 
 		if (copy_from_user(data, uarg, data_size))
-			GOTO(out_lease_close, rc = -EFAULT);
+			GOTO(out_lease_close, rc2 = -EFAULT);
 
 		bias = MDS_CLOSE_RESYNC_DONE;
 		break;
 	case LL_LEASE_LAYOUT_MERGE:
 		if (ioc->lil_count != 1)
-			GOTO(out_lease_close, rc = -EINVAL);
+			GOTO(out_lease_close, rc2 = -EINVAL);
 
 		uarg += sizeof(*ioc);
 		if (copy_from_user(&fdv, uarg, sizeof(fdv)))
-			GOTO(out_lease_close, rc = -EFAULT);
+			GOTO(out_lease_close, rc2 = -EFAULT);
 
 		layout_file = fget(fdv);
 		if (!layout_file)
-			GOTO(out_lease_close, rc = -EBADF);
+			GOTO(out_lease_close, rc2 = -EBADF);
 
 		if ((file->f_flags & O_ACCMODE) == O_RDONLY ||
 				(layout_file->f_flags & O_ACCMODE) == O_RDONLY)
-			GOTO(out_lease_close, rc = -EPERM);
+			GOTO(out_lease_close, rc2 = -EPERM);
 
-		data = file_inode(layout_file);
+		mrp.mrp_inode = file_inode(layout_file);
+		mrp.mrp_merge_flags = ioc->lil_flags & LL_LEASE_ALLOW_STALE ?
+				      CD_MERGE_STALE : 0;
+		data = &mrp;
 		bias = MDS_CLOSE_LAYOUT_MERGE;
 		break;
 	case LL_LEASE_LAYOUT_SPLIT: {
 		__u32 mirror_id;
 
 		if (ioc->lil_count != 2)
-			GOTO(out_lease_close, rc = -EINVAL);
+			GOTO(out_lease_close, rc2 = -EINVAL);
 
 		uarg += sizeof(*ioc);
 		if (copy_from_user(&fdv, uarg, sizeof(fdv)))
-			GOTO(out_lease_close, rc = -EFAULT);
+			GOTO(out_lease_close, rc2 = -EFAULT);
 
 		uarg += sizeof(fdv);
 		if (copy_from_user(&mirror_id, uarg, sizeof(mirror_id)))
-			GOTO(out_lease_close, rc = -EFAULT);
+			GOTO(out_lease_close, rc2 = -EFAULT);
 		if (mirror_id >= MIRROR_ID_NEG)
-			GOTO(out_lease_close, rc = -EINVAL);
+			GOTO(out_lease_close, rc2 = -EINVAL);
 
 		layout_file = fget(fdv);
 		if (!layout_file)
-			GOTO(out_lease_close, rc = -EBADF);
+			GOTO(out_lease_close, rc2 = -EBADF);
 
 		/* if layout_file == file, it means to destroy the mirror */
-		sp.sp_inode = file_inode(layout_file);
-		sp.sp_mirror_id = (__u16)mirror_id;
-		data = &sp;
+		mrp.mrp_inode = file_inode(layout_file);
+		mrp.mrp_mirror_id = (__u16)mirror_id;
+		data = &mrp;
 		bias = MDS_CLOSE_LAYOUT_SPLIT;
 		break;
 	}
 	case LL_LEASE_PCC_ATTACH:
 		if (ioc->lil_count != 1)
-			RETURN(-EINVAL);
+			GOTO(out_lease_close, rc2 = -EINVAL);
 
 		/* PCC-RW is not supported for encrypted files. */
 		if (IS_ENCRYPTED(inode))
-			RETURN(-EOPNOTSUPP);
+			GOTO(out_lease_close, rc2 = -EOPNOTSUPP);
 
 		uarg += sizeof(*ioc);
 		if (copy_from_user(&param.pa_archive_id, uarg, sizeof(__u32)))
@@ -4562,6 +4580,9 @@ out_lease_close:
 
 	if (lease_broken)
 		open_flags = MDS_FMODE_CLOSED;
+
+	if (!rc)
+		rc = rc2;
 	EXIT;
 
 out:
@@ -4572,8 +4593,6 @@ out:
 		fput(layout_file);
 
 	if (ioc->lil_flags == LL_LEASE_PCC_ATTACH) {
-		if (!rc)
-			rc = rc2;
 		rc = pcc_readwrite_attach_fini(file, inode,
 					       param.pa_layout_gen,
 					       lease_broken, rc,

lustre/lod/lod_object.c
@@ -3399,7 +3399,7 @@ out:
  */
 static int lod_declare_layout_merge(const struct lu_env *env,
 				    struct dt_object *dt,
-				    const struct lu_buf *mbuf,
+				    const struct lu_buf *mbuf, bool stale,
 				    struct thandle *th)
 {
 	struct lod_thread_info *info = lod_env_info(env);
@@ -3523,7 +3523,7 @@ static int lod_declare_layout_merge(const struct lu_env *env,
 
 		*lcme = *merge_lcme;
 		lcme->lcme_offset = cpu_to_le32(offset);
-		if (merge_has_dom && i == 0)
+		if ((merge_has_dom && i == 0) || stale)
 			lcme->lcme_flags |= cpu_to_le32(LCME_FL_STALE);
 
 		id = pflr_id(mirror_id, i + 1);
@@ -3545,7 +3545,7 @@ static int lod_declare_layout_merge(const struct lu_env *env,
 					      ~LCM_FL_FLR_MASK) |
 					     LCM_FL_RDONLY);
 
-	rc = lod_striping_reload(env, lo, buf, 0);
+	rc = lod_striping_reload(env, lo, buf, LVF_ALL_STALE);
 	if (rc)
 		GOTO(out, rc);
 
@@ -3832,7 +3832,9 @@ static int lod_declare_xattr_set(const struct lu_env *env,
 	} else if (fl & LU_XATTR_MERGE) {
 		LASSERT(strcmp(name, XATTR_NAME_LOV) == 0 ||
 			strcmp(name, XATTR_LUSTRE_LOV) == 0);
-		rc = lod_declare_layout_merge(env, dt, buf, th);
+		rc = lod_declare_layout_merge(env, dt, buf,
+					      !!(fl & LU_XATTR_MERGE_STALE),
+					      th);
 	} else if (fl & LU_XATTR_SPLIT) {
 		LASSERT(strcmp(name, XATTR_NAME_LOV) == 0 ||
 			strcmp(name, XATTR_LUSTRE_LOV) == 0);

lustre/mdc/mdc_lib.c
@@ -544,7 +544,9 @@ static void mdc_close_intent_pack(struct req_capsule *pill,
 	data->cd_data_version = op_data->op_data_version;
 	data->cd_fid = op_data->op_fid2;
 
-	if (bias & MDS_CLOSE_LAYOUT_SPLIT) {
+	if (bias & MDS_CLOSE_LAYOUT_MERGE) {
+		data->cd_merge_flags = op_data->op_merge_flags;
+	} else if (bias & MDS_CLOSE_LAYOUT_SPLIT) {
 		data->cd_mirror_id = op_data->op_mirror_id;
 	} else if (bias & MDS_CLOSE_RESYNC_DONE) {
 		struct close_data_resync_done *sync = &data->cd_resync;

lustre/mdd/mdd_object.c
@@ -1681,7 +1681,7 @@ static int mdd_xattr_del(const struct lu_env *env, struct md_object *obj,
 			 const char *name);
 
 static int mdd_xattr_merge(const struct lu_env *env, struct md_object *md_obj,
-			   struct md_object *md_vic)
+			   struct md_object *md_vic, __u16 merge_flags)
 {
 	struct mdd_device *mdd = mdo2mdd(md_obj);
 	struct mdd_object *obj = md2mdd_obj(md_obj);
@@ -1693,6 +1693,8 @@ static int mdd_xattr_merge(const struct lu_env *env, struct md_object *md_obj,
 	struct lu_attr *cattr = MDD_ENV_VAR(env, cattr);
 	struct lu_attr *tattr = MDD_ENV_VAR(env, tattr);
 	bool is_same_projid;
+	int fl = LU_XATTR_MERGE |
+		 (merge_flags & CD_MERGE_STALE ? LU_XATTR_MERGE_STALE : 0);
 	int rc;
 	int retried = 0;
 
@@ -1750,7 +1752,7 @@ retry:
 		GOTO(stop, rc);
 
 	rc = mdd_declare_xattr_set(env, mdd, obj, buf_vic, XATTR_NAME_LOV,
-				   LU_XATTR_MERGE, handle);
+				   fl, handle);
 	if (rc)
 		GOTO(stop, rc);
 
@@ -1769,8 +1771,7 @@ retry:
 			GOTO(out, rc);
 	}
 
-	rc = mdo_xattr_set(env, obj, buf_vic, XATTR_NAME_LOV, LU_XATTR_MERGE,
-			   handle);
+	rc = mdo_xattr_set(env, obj, buf_vic, XATTR_NAME_LOV, fl, handle);
 	if (rc)
 		GOTO(out, rc);
 
@@ -2335,7 +2336,8 @@ retry:
 			if (rc)
 				RETURN(rc);
 			/* merge layout of victim as a mirror of obj's. */
-			rc = mdd_xattr_merge(env, obj, victim);
+			rc = mdd_xattr_merge(env, obj, victim,
+					     mrd->mrd_merge_flags);
 		} else {
 			rc = mdd_xattr_split(env, obj, mrd);
 		}

lustre/mdt/mdt_open.c
@@ -2375,7 +2375,9 @@ static int mdt_close_handle_layouts(struct mdt_thread_info *info,
 			mrd.mrd_obj = NULL;
 		}
 
-		if (ma->ma_attr_flags & MDS_CLOSE_LAYOUT_SPLIT) {
+		if (ma->ma_attr_flags & MDS_CLOSE_LAYOUT_MERGE) {
+			mrd.mrd_merge_flags = data->cd_merge_flags;
+		} else if (ma->ma_attr_flags & MDS_CLOSE_LAYOUT_SPLIT) {
 			mrd.mrd_mirror_id = data->cd_mirror_id;
 			/* set a small enough blocks in the SoM */
 			ma->ma_attr.la_blocks >>= 1;

lustre/ptlrpc/pack_generic.c
@@ -3149,6 +3149,8 @@ void lustre_swab_close_data_special(struct close_data *cd, enum mds_op_bias b)
 		lustre_swab_close_data_resync_done(&cd->cd_resync);
 	else if (b & MDS_CLOSE_LAYOUT_SPLIT)
 		__swab16s(&cd->cd_mirror_id);
+	else if (b & MDS_CLOSE_LAYOUT_MERGE)
+		__swab16s(&cd->cd_merge_flags);
 	else if (b & MDS_PCC_ATTACH)
 		swab32s(&cd->cd_archive_id);
 	else if (b & MDS_CLOSE_LAYOUT_SWAP)

lustre/ptlrpc/wiretest.c
@@ -5721,6 +5721,50 @@ void lustre_assert_wire_constants(void)
 	LASSERTF((int)sizeof(((struct hsm_user_import *)0)->hui_archive_id) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct hsm_user_import *)0)->hui_archive_id));
 
+	/* Checks for struct close_data */
+	LASSERTF((int)sizeof(struct close_data) == 96, "found %lld\n",
+		 (long long)(int)sizeof(struct close_data));
+	LASSERTF((int)offsetof(struct close_data, cd_handle) == 0, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_handle));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_handle) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_handle));
+	LASSERTF((int)offsetof(struct close_data, cd_fid) == 8, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_fid));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_fid) == 16, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_fid));
+	LASSERTF((int)offsetof(struct close_data, cd_data_version) == 24, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_data_version));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_data_version) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_data_version));
+	LASSERTF((int)offsetof(struct close_data, cd_reserved) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_reserved));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_reserved) == 64, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_reserved));
+	LASSERTF((int)offsetof(struct close_data, cd_resync) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_resync));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_resync) == 64, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_resync));
+	LASSERTF((int)offsetof(struct close_data, cd_mirror_id) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_mirror_id));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_mirror_id) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_mirror_id));
+	LASSERTF((int)offsetof(struct close_data, cd_merge_flags) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_merge_flags));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_merge_flags) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_merge_flags));
+	LASSERTF((int)offsetof(struct close_data, cd_archive_id) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_archive_id));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_archive_id) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_archive_id));
+	LASSERTF((int)offsetof(struct close_data, cd_data_version2) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_data_version2));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_data_version2) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_data_version2));
+	LASSERTF(CD_MERGE_STALE == 1, "found %lld\n",
+		 (long long)CD_MERGE_STALE);
+	/* all merge flags must fit into the 16-bit cd_merge_flags */
+	BUILD_BUG_ON(CD_MERGE_FLAG_MAX > 0xffff);
+
 #ifndef HAVE_NATIVE_LINUX_CLIENT
 
 	/* Checks for struct netobj_s */

lustre/utils/lfs.c
@@ -2266,7 +2266,7 @@ out:
 }
 
 static int mirror_extend_layout(char *name, struct llapi_layout *m_layout,
-				bool inherit, uint32_t comp_flags,
+				bool inherit, bool resync, uint32_t comp_flags,
 				enum llapi_migration_flags migration_flags,
 				unsigned long long bandwidth_bytes_sec,
 				long stats_interval_sec)
@@ -2320,11 +2320,13 @@ static int mirror_extend_layout(char *name, struct llapi_layout *m_layout,
 	if (stats_interval_sec)
 		printf("%s:\n", name);
 
-	rc = migrate_nonblock(fd_src, fd_dst, bandwidth_bytes_sec,
-			      stats_interval_sec, NULL, &err_str);
-	if (rc < 0) {
-		llapi_lease_release(fd_src);
-		goto out;
+	if (resync) {
+		rc = migrate_nonblock(fd_src, fd_dst, bandwidth_bytes_sec,
+				      stats_interval_sec, NULL, &err_str);
+		if (rc < 0) {
+			llapi_lease_release(fd_src);
+			goto out;
+		}
 	}
 
 	rc = migrate_set_timestamps(fd_src, &st);
@@ -2341,6 +2343,9 @@ static int mirror_extend_layout(char *name, struct llapi_layout *m_layout,
 	}
 	data->lil_mode = LL_LEASE_UNLCK;
 	data->lil_flags = LL_LEASE_LAYOUT_MERGE;
+	/* whether to merge a stale mirror */
+	if (!resync)
+		data->lil_flags |= LL_LEASE_ALLOW_STALE;
 	data->lil_count = 1;
 	data->lil_ids[0] = fd_dst;
 	rc = llapi_lease_set(fd_src, data);
@@ -2386,6 +2391,7 @@ static int mirror_extend(char *fname, struct mirror_args *mirror_list,
 				rc = mirror_extend_layout(fname,
 							mirror_list->m_layout,
 							mirror_list->m_inherit,
+							true,
 							mirror_list->m_flags,
 							migration_flags,
 							bandwidth_bytes_sec,

lustre/utils/wirecheck.c
@@ -2671,6 +2671,26 @@ static void check_hsm_user_import(void)
 	CHECK_MEMBER(hsm_user_import, hui_archive_id);
 }
 
+static void check_close_data(void)
+{
+	BLANK_LINE();
+	CHECK_STRUCT(close_data);
+	CHECK_MEMBER(close_data, cd_handle);
+	CHECK_MEMBER(close_data, cd_fid);
+	CHECK_MEMBER(close_data, cd_data_version);
+	CHECK_MEMBER(close_data, cd_reserved);
+	CHECK_MEMBER(close_data, cd_resync);
+	CHECK_MEMBER(close_data, cd_mirror_id);
+	CHECK_MEMBER(close_data, cd_merge_flags);
+	CHECK_MEMBER(close_data, cd_archive_id);
+	CHECK_MEMBER(close_data, cd_data_version2);
+
+	CHECK_VALUE(CD_MERGE_STALE);
+
+	COMMENT("all merge flags must fit into the 16-bit cd_merge_flags");
+	CHECK_BUILD_TEST(CD_MERGE_FLAG_MAX > 0xffff);
+}
+
 static void check_netobj_s(void)
 {
 	BLANK_LINE();
@@ -3910,6 +3930,7 @@ main(int argc, char **argv)
 	check_hsm_request();
 	check_hsm_user_request();
 	check_hsm_user_import();
+	check_close_data();
 
 	CHECK_COND_STARTN(HAVE_NATIVE_LINUX_CLIENT);
 	check_netobj_s();

lustre/utils/wiretest.c
@@ -5740,6 +5740,50 @@ void lustre_assert_wire_constants(void)
 	LASSERTF((int)sizeof(((struct hsm_user_import *)0)->hui_archive_id) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct hsm_user_import *)0)->hui_archive_id));
 
+	/* Checks for struct close_data */
+	LASSERTF((int)sizeof(struct close_data) == 96, "found %lld\n",
+		 (long long)(int)sizeof(struct close_data));
+	LASSERTF((int)offsetof(struct close_data, cd_handle) == 0, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_handle));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_handle) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_handle));
+	LASSERTF((int)offsetof(struct close_data, cd_fid) == 8, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_fid));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_fid) == 16, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_fid));
+	LASSERTF((int)offsetof(struct close_data, cd_data_version) == 24, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_data_version));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_data_version) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_data_version));
+	LASSERTF((int)offsetof(struct close_data, cd_reserved) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_reserved));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_reserved) == 64, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_reserved));
+	LASSERTF((int)offsetof(struct close_data, cd_resync) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_resync));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_resync) == 64, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_resync));
+	LASSERTF((int)offsetof(struct close_data, cd_mirror_id) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_mirror_id));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_mirror_id) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_mirror_id));
+	LASSERTF((int)offsetof(struct close_data, cd_merge_flags) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_merge_flags));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_merge_flags) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_merge_flags));
+	LASSERTF((int)offsetof(struct close_data, cd_archive_id) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_archive_id));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_archive_id) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_archive_id));
+	LASSERTF((int)offsetof(struct close_data, cd_data_version2) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct close_data, cd_data_version2));
+	LASSERTF((int)sizeof(((struct close_data *)0)->cd_data_version2) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct close_data *)0)->cd_data_version2));
+	LASSERTF(CD_MERGE_STALE == 1, "found %lld\n",
+		 (long long)CD_MERGE_STALE);
+	/* all merge flags must fit into the 16-bit cd_merge_flags */
+	BUILD_BUG_ON(CD_MERGE_FLAG_MAX > 0xffff);
+
 #ifndef HAVE_NATIVE_LINUX_CLIENT
 
 	/* Checks for struct netobj_s */