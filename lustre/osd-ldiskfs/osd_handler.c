lustre/lustre-release

commit 5744762a5a38da37f16319e36e007c8d819356fe

LU-19142 osd-ldiskfs: journal commit callback
With the removal of t_private_list from transaction_t
the ldiskfs commit callback scheme is not available.

Provide and osd-ldiskfs / ldiskfs specific callback scheme

Introduces ext4-journal-commit-transaction-callback.patch
which can be back-ported to older kernels to maintain
an independent transaction commit handler.

Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ie26be875d3c8f62e64b2cff6ee4ef32a22f673de
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59973
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -1860,10 +1860,8 @@ static int osd_param_is_not_sane(const struct osd_device *dev,
 /*
  * Concurrency: shouldn't matter.
  */
-static void osd_trans_commit_cb(struct super_block *sb,
-				struct ldiskfs_journal_cb_entry *jcb, int error)
+static void osd_trans_commit_complete(struct osd_thandle *oh, int error)
 {
-	struct osd_thandle *oh = container_of(jcb, struct osd_thandle, ot_jcb);
 	struct thandle *th = &oh->ot_super;
 	struct lu_device *lud = &th->th_dev->dd_lu_dev;
 	struct osd_device *osd = osd_dev(lud);
@@ -1891,11 +1889,128 @@ static void osd_trans_commit_cb(struct super_block *sb,
 	OBD_FREE_PTR(oh);
 }
 
+#ifdef HAVE_S_TXN_CB_MAP
+static inline void osd_trans_commit_cb(struct osd_thandle *oh,
+				       journal_t *journal,
+				       transaction_t *transaction, int error)
+{
+	osd_trans_commit_complete(oh, error);
+}
+
+static int cmp_key_txn(const void *transaction, const struct rb_node *node)
+{
+	struct osd_thandle *oh = container_of(node, typeof(*oh), ot_node);
+
+	return (transaction - (void *)oh->ot_transaction);
+}
+
+static int cmp_node_txn(struct rb_node *left, const struct rb_node *node)
+{
+	struct osd_thandle *oh;
+
+	oh = container_of(left, struct osd_thandle, ot_node);
+	return cmp_key_txn(oh->ot_transaction, node);
+}
+
+static void osd_trans_txn_cb(struct ldiskfs_sb_info *sbi, journal_t *journal,
+			     transaction_t *transaction)
+{
+	struct rb_node *node;
+	struct osd_thandle *top = NULL;
+	struct osd_thandle *oh;
+	int error = is_journal_aborted(journal);
+
+	spin_lock(&sbi->s_txn_cb_lock);
+	node = rb_find(transaction, &sbi->s_txn_cb_map, cmp_key_txn);
+	if (!node)
+		goto out;
+	rb_erase(node, &sbi->s_txn_cb_map);
+	top = container_of(node, struct osd_thandle, ot_node);
+	top->ot_transaction = NULL;
+	while ((oh = list_first_entry_or_null(&top->ot_cblist,
+					      struct osd_thandle,
+					      ot_cblist)) != NULL) {
+		list_del_init(&oh->ot_cblist);
+		oh->ot_transaction = NULL;
+		spin_unlock(&sbi->s_txn_cb_lock);
+		/* a callback could sleep */
+		osd_trans_commit_cb(oh, journal, transaction, error);
+		spin_lock(&sbi->s_txn_cb_lock);
+	}
+out:
+	spin_unlock(&sbi->s_txn_cb_lock);
+	if (top)
+		osd_trans_commit_cb(top, journal, transaction, error);
+}
+
+static void osd_trans_register_callback(struct osd_device *osd,
+					struct osd_thandle *oh)
+{
+	struct ldiskfs_sb_info *sbi = LDISKFS_SB(osd_sb(osd));
+	struct rb_node *node;
+	struct osd_thandle *top = NULL;
+	transaction_t *transaction = NULL;
+
+	if (oh && oh->ot_handle)
+		transaction = oh->ot_handle->h_transaction;
+
+	spin_lock(&sbi->s_txn_cb_lock);
+	oh->ot_transaction = transaction;
+	node = rb_find_add(&oh->ot_node, &sbi->s_txn_cb_map, cmp_node_txn);
+	if (node) {
+		/* found existing: add additional osd to be notified */
+		top = container_of(node, struct osd_thandle, ot_node);
+		list_add_tail(&oh->ot_cblist, &top->ot_cblist);
+	}
+	spin_unlock(&sbi->s_txn_cb_lock);
+}
+
+static inline void osd_trans_txn_cb_handler(struct super_block *sb)
+{
+	struct ldiskfs_sb_info *sbi = LDISKFS_SB(sb);
+
+	if (!sbi->s_txn_cb)
+		sbi->s_txn_cb = osd_trans_txn_cb;
+}
+
+static inline void osd_trans_txn_cb_init(struct osd_thandle *oh)
+{
+	INIT_LIST_HEAD(&oh->ot_cblist);
+}
+
+#else /* !HAVE_S_TXN_CB_MAP */
+
+static inline void osd_trans_commit_cb(struct super_block *sb,
+				       struct ldiskfs_journal_cb_entry *jcb,
+				       int error)
+{
+	struct osd_thandle *oh = container_of(jcb, struct osd_thandle, ot_jcb);
+
+	osd_trans_commit_complete(oh, error);
+}
+
+static inline void osd_trans_register_callback(struct osd_device *osd,
+					       struct osd_thandle *oh)
+{
+	ldiskfs_journal_callback_add(oh->ot_handle, osd_trans_commit_cb,
+				     &oh->ot_jcb);
+}
+
+static inline void osd_trans_txn_cb_handler(struct super_block *sb)
+{
+}
+
+static inline void osd_trans_txn_cb_init(struct osd_thandle *oh)
+{
+}
+#endif /* HAVE_S_TXN_CB_MAP */
+
 static struct thandle *osd_trans_create(const struct lu_env *env,
 					struct dt_device *d)
 {
 	struct osd_thread_info *oti = osd_oti_get(env);
 	struct osd_iobuf *iobuf = &oti->oti_iobuf;
+	struct super_block *sb = osd_sb(osd_dt_dev(d));
 	struct osd_thandle *oh;
 	struct thandle *th;
 
@@ -1912,21 +2027,23 @@ static struct thandle *osd_trans_create(const struct lu_env *env,
 	/* on pending IO in this thread should left from prev. request */
 	LASSERT(atomic_read(&iobuf->dr_numreqs) == 0);
 
-	sb_start_write(osd_sb(osd_dt_dev(d)));
+	sb_start_write(sb);
 
 	OBD_ALLOC_GFP(oh, sizeof(*oh), GFP_NOFS);
 	if (!oh) {
-		sb_end_write(osd_sb(osd_dt_dev(d)));
+		sb_end_write(sb);
 		RETURN(ERR_PTR(-ENOMEM));
 	}
 
 	oh->ot_quota_trans = &oti->oti_quota_trans;
 	memset(oh->ot_quota_trans, 0, sizeof(*oh->ot_quota_trans));
+	osd_trans_txn_cb_handler(sb);
 	th = &oh->ot_super;
 	th->th_dev = d;
 	th->th_result = 0;
 	oh->ot_credits = 0;
 	oh->oh_declared_ext = 0;
+	osd_trans_txn_cb_init(oh);
 	INIT_LIST_HEAD(&oh->ot_commit_dcb_list);
 	INIT_LIST_HEAD(&oh->ot_stop_dcb_list);
 	INIT_LIST_HEAD(&oh->ot_trunc_locks);
@@ -2188,17 +2305,14 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 
 	if (oh->ot_handle != NULL) {
 		int rc2;
-
-		handle_t *hdl = oh->ot_handle;
+		handle_t *handle = oh->ot_handle;
 
 		/*
 		 * add commit callback
 		 * notice we don't do this in osd_trans_start()
 		 * as underlying transaction can change during truncate
 		 */
-		ldiskfs_journal_callback_add(hdl, osd_trans_commit_cb,
-					     &oh->ot_jcb);
-
+		osd_trans_register_callback(osd, oh);
 		LASSERT(oti->oti_txns == 1);
 		oti->oti_txns--;
 
@@ -2209,10 +2323,10 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 
 		osd_trans_stop_cb(oh, rc);
 		/* hook functions might modify th_sync */
-		hdl->h_sync = th->th_sync;
+		handle->h_sync = th->th_sync;
 
 		oh->ot_handle = NULL;
-		OSD_CHECK_SLOW_TH(oh, osd, rc2 = ldiskfs_journal_stop(hdl));
+		OSD_CHECK_SLOW_TH(oh, osd, rc2 = ldiskfs_journal_stop(handle));
 		if (rc2 != 0)
 			CERROR("%s: failed to stop transaction: rc = %d\n",
 			       osd_name(osd), rc2);

ldiskfs/kernel_patches/patches/linux-6.15/ext4-journal-commit-transaction-callback.patch
@@ -1,0 +1,77 @@
+From b7dec37b4052a43d8dd66c56bbf31bda8788f44c Mon Sep 17 00:00:00 2001
+From: Shaun Tancheff <shaun.tancheff@hpe.com>
+Date: Sun, 20 Jul 2025 09:52:51 +0700
+Subject: [PATCH] LU-19142 osd-ldiskfs: journal commit callback
+
+With the removal of t_private_list from transaction_t
+the ldiskfs commit callback scheme is not available.
+
+Provide and osd-ldiskfs / ldiskfs specific callback scheme
+
+Introduces ext4-journal-commit-transaction-callback.patch
+which can be back-ported to older kernels to maintain
+an independent transaction commit handler.
+
+Test-Parameters: trivial
+Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
+Change-Id: Ie26be875d3c8f62e64b2cff6ee4ef32a22f673de
+---
+ fs/ext4/ext4.h  | 5 +++++
+ fs/ext4/super.c | 6 ++++++
+ 2 files changed, 11 insertions(+)
+
+diff --git a/fs/ext4/ext4.h b/fs/ext4/ext4.h
+index 9e9f7fb7..d0daf7ab 100644
+--- a/fs/ext4/ext4.h
++++ b/fs/ext4/ext4.h
+@@ -1515,6 +1515,8 @@ struct ext4_orphan_info {
+ 						 * file blocks */
+ };
+ 
++#define HAVE_S_TXN_CB_MAP	1
++
+ /*
+  * fourth extended-fs super-block data in memory
+  */
+@@ -1751,6 +1753,9 @@ struct ext4_sb_info {
+ 	__u64 s_last_error_block;
+ 	const char *s_last_error_func;
+ 	time64_t s_last_error_time;
++	struct rb_root s_txn_cb_map;
++	void (*s_txn_cb)(struct ext4_sb_info *, journal_t *, transaction_t *);
++	spinlock_t s_txn_cb_lock;
+ 	/*
+ 	 * If we are in a context where we cannot update the on-disk
+ 	 * superblock, we queue the work here.  This is used to update
+diff --git a/fs/ext4/super.c b/fs/ext4/super.c
+index 68bb4145..26ca0229 100644
+--- a/fs/ext4/super.c
++++ b/fs/ext4/super.c
+@@ -502,11 +502,14 @@ static void ext4_maybe_update_superblock(struct super_block *sb)
+ static void ext4_journal_commit_callback(journal_t *journal, transaction_t *txn)
+ {
+ 	struct super_block		*sb = journal->j_private;
++	struct ext4_sb_info		*sbi = EXT4_SB(sb);
+ 
+ 	BUG_ON(txn->t_state == T_FINISHED);
+ 
+ 	ext4_process_freed_data(sb, txn->t_tid);
+ 	ext4_maybe_update_superblock(sb);
++	if (sbi->s_txn_cb)
++		sbi->s_txn_cb(sbi, journal, txn);
+ }
+ 
+ /*
+@@ -5404,6 +5407,9 @@ static int __ext4_fill_super(struct fs_context *fc, struct super_block *sb)
+ 	ext4_atomic_write_init(sb);
+ 	ext4_fast_commit_init(sb);
+ 
++	sbi->s_txn_cb_map = RB_ROOT;
++	sbi->s_txn_cb = NULL;
++	spin_lock_init(&sbi->s_txn_cb_lock);
+ 	sb->s_root = NULL;
+ 
+ 	needs_recovery = (es->s_last_orphan != 0 ||
+-- 
+2.45.2
+

ldiskfs/kernel_patches/series/ldiskfs-6.15-ml.series
@@ -35,3 +35,4 @@ linux-6.12/ext4-filename-encode.patch
 rhel9.1/ext4-enc-flag.patch
 linux-6.15/ext4-encdata.patch
 linux-6.15/ext4-add-IGET_NO_CHECKS-flag.patch
+linux-6.15/ext4-journal-commit-transaction-callback.patch

lustre/osd-ldiskfs/osd_internal.h
@@ -402,11 +402,17 @@ struct osd_access_lock {
 };
 
 struct osd_thandle {
-        struct thandle          ot_super;
-        handle_t               *ot_handle;
-        struct ldiskfs_journal_cb_entry ot_jcb;
-	struct list_head       ot_commit_dcb_list;
-	struct list_head       ot_stop_dcb_list;
+	struct thandle		ot_super;
+	handle_t	       *ot_handle;
+#ifdef HAVE_S_TXN_CB_MAP
+	transaction_t	       *ot_transaction;
+	struct rb_node		ot_node;
+	struct list_head	ot_cblist;
+#else
+	struct ldiskfs_journal_cb_entry ot_jcb;
+#endif
+	struct list_head	ot_commit_dcb_list;
+	struct list_head	ot_stop_dcb_list;
 	unsigned int		ot_credits;
 	/*
 	 * For iterative operation within one transaction,

microsoft/amlFilesystem-lustre

commit e4152398630e6bd5656941a31b92bd8c369e7a81

LU-20725 osd-ldiskfs: run commit callbacks newest first
An MDS soft lockup panics the jbd2 commit thread in
ptlrpc_commit_replies() under a metadata create load.

osd_trans_txn_cb() runs the commit's callbacks in registration order,
so tgt_cb_last_committed() raises exp_last_committed one transno at a
time and ptlrpc_commit_replies() rescans the whole export uncommitted
reply list each time.  With sync lock cancel that list held 8069
replies in the crash dump, and 6930 callbacks were still queued.
ext4-jcb-optimization.patch reversed the order for older ldiskfs.

Queue the extra handles at the head so the callbacks run newest
registration first, which normally commits the highest transno first
and lets the rest take the tgt_cb_last_committed() fast path.
100k creates on 2 MDTs: 2.2e9 list steps scanned before, 1.0e5 after.

Test-Parameters: trivial serverdistro=el10.2 testlist=performance-sanity
Fixes: 5744762a5a38 ("LU-19142 osd-ldiskfs: journal commit callback")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Idf1578be0fa2434ebae511118ec2217acab3ddfb

lustre/osd-ldiskfs/osd_handler.c
@@ -1968,9 +1968,14 @@ static void osd_trans_register_callback(struct osd_device *osd,
 	oh->ot_transaction = transaction;
 	node = rb_find_add(&oh->ot_node, &sbi->s_txn_cb_map, cmp_node_txn);
 	if (node) {
-		/* found existing: add additional osd to be notified */
+		/* found existing: add this handle to be notified too.
+		 * newest registration first, so osd_trans_txn_cb() (which
+		 * runs the list from the head, holder last) sees the highest
+		 * transno first -- only the common case, as the transno is
+		 * assigned later, in dt_txn_hook_stop()
+		 */
 		top = container_of(node, struct osd_thandle, ot_node);
-		list_add_tail(&oh->ot_cblist, &top->ot_cblist);
+		list_add(&oh->ot_cblist, &top->ot_cblist);
 	}
 	spin_unlock(&sbi->s_txn_cb_lock);
 }