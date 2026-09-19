lustre/lustre-release

commit 15d44e787e17ff57fc1fb5a6c9ae568cdaab6e34

LU-12682 llite: fake symlink type of foreign file/dir
This patch implements a "fake symlink" specific usage of
"foreign" LOV/LMV format. It basically allows these
particular type of foreign files/dirs to behave as a
symlink from VFS point of view, by allowing to construct
a relative path from the LOV/LMV foreign content, to
complement it with a prefix, and then to expose it to
the VFS as a symlink destination. The default/internal
mechanism simply takes the full foreign free string as
the relative path, and for more complex internal formats
an upcall has been implemented to provide format's
details (presently just in terms of constant strings
and substrings positions in EA, but this can be enhanced)
to llite layer.
Using this feature, instead of real symlinks or user EA,
will permit to benefit from the special features (lock,
prefetch, caches) already implemented to handle both
LOV/LMV EAs.

Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Change-Id: Id3c262e3b042447aa09aad25f682ff02787b350d
Reviewed-on: https://review.whamcloud.com/35856
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Ben Evans <beevans@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -129,6 +129,25 @@ static struct ll_sb_info *ll_init_sbi(void)
 	if (sbi->ll_cache == NULL)
 		GOTO(out_destroy_ra, rc = -ENOMEM);
 
+	/* initialize foreign symlink prefix path */
+	OBD_ALLOC(sbi->ll_foreign_symlink_prefix, sizeof("/mnt/"));
+	if (sbi->ll_foreign_symlink_prefix == NULL)
+		GOTO(out_destroy_ra, rc = -ENOMEM);
+	memcpy(sbi->ll_foreign_symlink_prefix, "/mnt/", sizeof("/mnt/"));
+	sbi->ll_foreign_symlink_prefix_size = sizeof("/mnt/");
+
+	/* initialize foreign symlink upcall path, none by default */
+	OBD_ALLOC(sbi->ll_foreign_symlink_upcall, sizeof("none"));
+	if (sbi->ll_foreign_symlink_upcall == NULL)
+		GOTO(out_destroy_ra, rc = -ENOMEM);
+	memcpy(sbi->ll_foreign_symlink_upcall, "none", sizeof("none"));
+	sbi->ll_foreign_symlink_upcall_items = NULL;
+	sbi->ll_foreign_symlink_upcall_nb_items = 0;
+	init_rwsem(&sbi->ll_foreign_symlink_sem);
+	/* foreign symlink support (LL_SBI_FOREIGN_SYMLINK in ll_flags)
+	 * not enabled by default
+	 */
+
 	sbi->ll_ra_info.ra_max_pages =
 		min(pages / 32, SBI_DEFAULT_READ_AHEAD_MAX);
 	sbi->ll_ra_info.ra_max_pages_per_file =
@@ -183,6 +202,12 @@ static struct ll_sb_info *ll_init_sbi(void)
 	sbi->ll_heat_period_second = SBI_DEFAULT_HEAT_PERIOD_SECOND;
 	RETURN(sbi);
 out_destroy_ra:
+	if (sbi->ll_foreign_symlink_prefix)
+		OBD_FREE(sbi->ll_foreign_symlink_prefix, sizeof("/mnt/"));
+	if (sbi->ll_cache) {
+		cl_cache_decref(sbi->ll_cache);
+		sbi->ll_cache = NULL;
+	}
 	destroy_workqueue(sbi->ll_ra_info.ll_readahead_wq);
 out_pcc:
 	pcc_super_fini(&sbi->ll_pcc_super);
@@ -204,6 +229,32 @@ static void ll_free_sbi(struct super_block *sb)
 		if (sbi->ll_cache != NULL) {
 			cl_cache_decref(sbi->ll_cache);
 			sbi->ll_cache = NULL;
+		}
+		if (sbi->ll_foreign_symlink_prefix) {
+			OBD_FREE(sbi->ll_foreign_symlink_prefix,
+				 sbi->ll_foreign_symlink_prefix_size);
+			sbi->ll_foreign_symlink_prefix = NULL;
+		}
+		if (sbi->ll_foreign_symlink_upcall) {
+			OBD_FREE(sbi->ll_foreign_symlink_upcall,
+				 strlen(sbi->ll_foreign_symlink_upcall) +
+				       1);
+			sbi->ll_foreign_symlink_upcall = NULL;
+		}
+		if (sbi->ll_foreign_symlink_upcall_items) {
+			int i;
+			int nb_items = sbi->ll_foreign_symlink_upcall_nb_items;
+			struct ll_foreign_symlink_upcall_item *items =
+				sbi->ll_foreign_symlink_upcall_items;
+
+			for (i = 0 ; i < nb_items; i++)
+				if (items[i].type == STRING_TYPE)
+					OBD_FREE(items[i].string,
+						       items[i].size);
+
+			OBD_FREE_LARGE(items, nb_items *
+				sizeof(struct ll_foreign_symlink_upcall_item));
+			sbi->ll_foreign_symlink_upcall_items = NULL;
 		}
 		pcc_super_fini(&sbi->ll_pcc_super);
 		OBD_FREE(sbi, sizeof(*sbi));
@@ -986,6 +1037,58 @@ static int ll_options(char *options, struct ll_sb_info *sbi)
 #else
 			LCONSOLE_WARN("noencrypt mount option ignored: encryption not supported\n");
 #endif
+			goto next;
+		}
+		tmp = ll_set_opt("foreign_symlink", s1, LL_SBI_FOREIGN_SYMLINK);
+		if (tmp) {
+			int prefix_pos = sizeof("foreign_symlink=") - 1;
+			int equal_pos = sizeof("foreign_symlink=") - 2;
+
+			/* non-default prefix provided ? */
+			if (strlen(s1) >= sizeof("foreign_symlink=") &&
+			    *(s1 + equal_pos) == '=') {
+				char *old = sbi->ll_foreign_symlink_prefix;
+				size_t old_len =
+					sbi->ll_foreign_symlink_prefix_size;
+
+				/* path must be absolute */
+				if (*(s1 + sizeof("foreign_symlink=")
+				      - 1) != '/') {
+					LCONSOLE_ERROR_MSG(0x152,
+						"foreign prefix '%s' must be an absolute path\n",
+						s1 + prefix_pos);
+					RETURN(-EINVAL);
+				}
+				/* last option ? */
+				s2 = strchrnul(s1 + prefix_pos, ',');
+
+				if (sbi->ll_foreign_symlink_prefix) {
+					sbi->ll_foreign_symlink_prefix = NULL;
+					sbi->ll_foreign_symlink_prefix_size = 0;
+				}
+				/* alloc for path length and '\0' */
+				OBD_ALLOC(sbi->ll_foreign_symlink_prefix,
+						s2 - (s1 + prefix_pos) + 1);
+				if (!sbi->ll_foreign_symlink_prefix) {
+					/* restore previous */
+					sbi->ll_foreign_symlink_prefix = old;
+					sbi->ll_foreign_symlink_prefix_size =
+						old_len;
+					RETURN(-ENOMEM);
+				}
+				if (old)
+					OBD_FREE(old, old_len);
+				strncpy(sbi->ll_foreign_symlink_prefix,
+					s1 + prefix_pos,
+					s2 - (s1 + prefix_pos));
+				sbi->ll_foreign_symlink_prefix_size =
+					s2 - (s1 + prefix_pos) + 1;
+			} else {
+				LCONSOLE_ERROR_MSG(0x152,
+						   "invalid %s option\n", s1);
+			}
+			/* enable foreign symlink support */
+			*flags |= tmp;
 			goto next;
 		}
                 LCONSOLE_ERROR_MSG(0x152, "Unknown option '%s', won't mount.\n",
@@ -2870,6 +2973,13 @@ int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
 	if (default_lmv_deleted)
 		ll_update_default_lsm_md(*inode, &md);
 
+	/* we may want to apply some policy for foreign file/dir */
+	if (ll_sbi_has_foreign_symlink(sbi)) {
+		rc = ll_manage_foreign(*inode, &md);
+		if (rc < 0)
+			GOTO(out, rc);
+	}
+
 	GOTO(out, rc = 0);
 
 out:
@@ -3073,6 +3183,11 @@ int ll_show_options(struct seq_file *seq, struct dentry *dentry)
 		seq_puts(seq, ",encrypt");
 	else
 		seq_puts(seq, ",noencrypt");
+
+	if (sbi->ll_flags & LL_SBI_FOREIGN_SYMLINK) {
+		seq_puts(seq, ",foreign_symlink=");
+		seq_puts(seq, sbi->ll_foreign_symlink_prefix);
+	}
 
 	RETURN(0);
 }

lustre/doc/Makefile.am
@@ -178,7 +178,8 @@ LIBMAN = 					\
 	llapi_rmfid.3				\
 	llapi_search_mdt.3			\
 	llapi_search_ost.3			\
-	llapi_search_tgt.3
+	llapi_search_tgt.3			\
+	llapi_unlink_foreign.3
 
 SERVER_MANFILES =				\
 	lctl-barrier.8				\

lustre/doc/lfs-find.1
@@ -119,7 +119,7 @@ File has a foreign (non-Lustre/free format) layout and is of the given
 if specified.  Presently only
 .B none
 or
-.B daos
+.B symlink
 are defined types, though 32-bit numeric types can also be used.
 .TP
 .BR --gid | -g
@@ -365,9 +365,9 @@ Recursively list all mirrored files that have more than 2 mirrors.
 .B $ lfs find ! --mirror-state=ro /mnt/lustre
 Recursively list all out-of-sync mirrored files.
 .TP
-.B $ lfs find ! --foreign=daos /mnt/lustre
+.B $ lfs find ! --foreign=symlink /mnt/lustre
 Recursively list all but foreign files/dirs of
-.B daos
+.B symlink
 type.
 .SH BUGS
 The

lustre/doc/lfs-setdirstripe.1
@@ -94,9 +94,10 @@ Create a directory with a foreign (non-Lustre/free format, see
 option) striping. Where
 .BR type
 specifies a known foreign type (like
-.BR none ,
-.BR daos )
-or a 32-bit numeric type.
+.BR none
+,
+.BR symlink
+, ...) or a 32-bit numeric type.
 .TP
 .BR \-\-flags =\fI<hex>\fR
 Specify a numeric bitmask of type-specific layout flags for the foreign layout.
@@ -149,12 +150,12 @@ This creates a directory striped on two MDTs, whose first stripe is on
 (MDT index 1), and whose hash type is
 .BR all_char .
 .TP
-.B $ lfs mkdir --foreign=daos --xattr PUUID:CUUID /mnt/lustre/dir1
+.B $ lfs mkdir --foreign=symlink --xattr PUUID/CUUID /mnt/lustre/dir1
 This creates
 .B dir1
 with foreign (non-lustre/free format)
-.B PUUID:CUUID
-striping/LMV EA.
+.B PUUID/CUUID
+striping/LMV EA value (symlink type).
 .SH AVAILABILITY
 The
 .B lfs setdirstripe

lustre/doc/lfs-setstripe.1
@@ -133,7 +133,7 @@ file.
 Create a new
 .I file
 with a foreign/non-lustre layout of type
-.I foreign_type \fR(\fBnone\fR, \fBdaos\fR, ...)
+.I foreign_type \fR(\fBnone\fR, \fBsymlink\fR, ...)
 with flags
 .I hex
 and a free-format layout value of
@@ -264,7 +264,7 @@ options).
 Current known types are
 .BR none
 and
-.BR daos
+.BR symlink
 , default is
 .BR none
 .
@@ -533,12 +533,12 @@ which can be created with the
 .B lfs getstripe --yaml
 command.
 .TP
-.B lfs setstripe --foreign=daos --flags=0xda08 \
+.B lfs setstripe --foreign=symlink --flags=0xda08 \
 	--xattr=PUUID:CUUID /mnt/lustre/file1
 This creates foreign
 .BR file1
 of type
-.BR daos
+.BR symlink
 with non-lustre/free-format
 .BR PUUID:CUUID
 layout/LOV EA and flags

lustre/doc/lfs.1
@@ -127,6 +127,9 @@ lfs \- client utility for Lustre-specific file layout and other attributes
 .br
 .B lfs setstripe --yaml=\fR<\fIyaml_template_file\fR> <\fIfilename\fR>
 .br
+.B lfs unlink_foreign
+.IR path \fR[\fIpath\fR ...]
+.br
 .B lfs --version
 .br
 .B lfs --list-commands
@@ -219,6 +222,9 @@ See lfs-migrate(1).
 .TP
 .B setstripe
 See lfs-setstripe(1).
+.TP
+.B unlink_foreign
+Remove the foreign files/dirs that are prevented to be using regular unlink/rmdir commands/syscalls. Works also for regular files/dirs.
 .TP
 .B --version
 Output the build version of the lfs utility. Use "lctl lustre_build_version" to get the version of the Lustre kernel modules

lustre/doc/llapi_file_create_foreign.3
@@ -73,13 +73,13 @@ int main(int argc, char *argv[])
         if (argc != 2)
                 return -1;
 
-        rc = llapi_file_create_foreign(argv[1], 0600, LOV_FOREIGN_TYPE_DAOS,
-				       0xda08, "PUUID@CUUID");
+        rc = llapi_file_create_foreign(argv[1], 0600, LOV_FOREIGN_TYPE_SYMLINK,
+				       0xda05, "PUUID/CUUID");
         if (rc < 0) {
                 fprintf(stderr, "file creation has failed, %s\\n", strerror(-rc));
                 return -1;
         }
-        printf("foreign file %s has been created with 'PUUID@CUUID' layout!\\n",
+        printf("foreign file %s has been created with 'PUUID/CUUID' layout!\\n",
                argv[1]);
         return 0;
 }

lustre/doc/llapi_unlink_foreign.3
@@ -1,0 +1,80 @@
+.TH lustreapi 3 "2009 Jul 10" The Lustre user application interface library
+.SH NAME
+llapi_unlink_foreign \- unlink file/dir with foreign layout on a Lustre filesystem
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_unlink_foreign(char *" name ");"
+.sp
+.fi
+.SH DESCRIPTION
+.LP
+.B llapi_unlink_foreign(\|)
+will allow to unlink a file/dir of
+.I name
+with foreign LOV/LMV, that would be prevented to be using regular unlink/rmdir
+command/syscall.
+It also works for regular file/dir.
+.SH RETURN VALUES
+.LP
+.B llapi_unlink_foreign(\|)
+return:
+.TP
+=0
+on success.
+.TP
+<0
+on failure, the absolute value is an error code.
+.SH ERRORS
+.TP 15
+.SM ENOENT
+.I name
+does not exist.
+.TP
+.SM ENOTTY
+.I name
+may not point to a Lustre filesystem.
+.SH "EXAMPLE"
+.nf
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <lustre/lustreapi.h>
+
+int main(int argc, char **argv)
+{
+	char *foreign = NULL;
+	int c, rc;
+
+	while ((c = getopt(argc, argv, "hf:")) != -1) {
+		switch (c) {
+		case 'f':
+			foreign = optarg;
+			break;
+		case 'h':
+		default:
+			fprintf(stderr,
+				"Usage: %s [-f <foreign file/dir pathname>]\n",
+				argv[0]);
+			exit(0);
+			break;
+		}
+	}
+
+	if (foreign == NULL) {
+		fprintf(stderr, "a foreign file/dir pathname must be provided\n");
+		exit(0);
+	}
+
+	rc = llapi_unlink_foreign(foreign);
+	if (rc < 0)
+		fprintf(stderr, "llapi_unlink_foreign() error: %d\n", rc);
+
+	return rc;
+}
+.fi
+.SH "SEE ALSO"
+.BR lustre (7),
+.BR lustreapi (7)

microsoft/amlFilesystem-lustre

commit 3ddb7ca2d5a48596dcac23ba3c2171ba750ac4f3

LU-20714 llite: read foreign symlink prefix under lock
ll_show_options() prints sbi->ll_foreign_symlink_prefix without
holding ll_foreign_symlink_sem, while foreign_symlink_prefix_store()
swaps in a new prefix under the write lock and frees the old buffer.
Nothing keeps the pointer this reader sampled alive, so any
unprivileged read of /proc/mounts or /proc/self/mountinfo racing a
root write of the foreign_symlink_prefix parameter can print freed
memory.

Take the semaphore for read around the access, as
foreign_symlink_prefix_show() already does. ll_options() swaps the
same field unlocked, but runs before the super block is in a mount
namespace or the sysfs files exist, so it has no concurrent reader.

Test-Parameters: trivial
Fixes: 15d44e787e17 ("LU-12682 llite: fake symlink type of foreign file/dir")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I7d201ed1f86d8b9df597cc37989c48bd425717f3

lustre/llite/llite_lib.c
@@ -4218,8 +4218,10 @@ int ll_show_options(struct seq_file *seq, struct dentry *dentry)
 		    strncmp(ll_sbi_flags_name[i].pattern, "no", 2)) {
 			if (ll_sbi_flags_name[i].token ==
 			    LL_SBI_FOREIGN_SYMLINK) {
+				down_read(&sbi->ll_foreign_symlink_sem);
 				seq_show_option(seq, "foreign_symlink",
 						sbi->ll_foreign_symlink_prefix);
+				up_read(&sbi->ll_foreign_symlink_sem);
 			} else if (ll_sbi_flags_name[i].token ==
 				   LL_SBI_USER_PRINCIPAL) {
 				seq_show_option(seq, "user_principal",