lustre/lustre-release

commit 6a20bdcc608bc2b933774b9f34ec25395e920a54

LU-11376 lov: new foreign LOV format
This patch introduces a new layout/LOV format in order to
allow to specify an arbitrary external reference for a file
in Lustre namespace.
The new LOV format is made of {newmagic, length, type, flags,
string[length]} to be as flexible as possible.
Foreign file can be created by using the open(O_LOV_DELAY_CREATE) +
ioctl(LL_IOC_LOV_SETSTRIPE) operations and it can only be and remain
an empty file until removed.
A new API method llapi_file_create_foreign() has been introduced
and "lfs [[get,set]stripe,find" modified to understand new layout.
The idea behind this is to provide Lustre namespace support and
layout prefetch/caching under layout protection, for user/external
usage.

Code has been added for lfsck to handle foreign files, and
a new sub-test has been added in sanity-lfsck in order to verify
if does not break foreign file and that reverse is also true.

Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Change-Id: I5d9c0642fe8e7009c30918bfa946cac7c00c9af8
Reviewed-on: https://review.whamcloud.com/33755
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>

lustre/lov/lov_ea.c
@@ -132,10 +132,16 @@ void lsm_free(struct lov_stripe_md *lsm)
 	unsigned int i;
 	size_t lsm_size;
 
-	for (i = 0; i < entry_count; i++)
-		lsme_free(lsm->lsm_entries[i]);
+	if (lsm->lsm_magic == LOV_MAGIC_FOREIGN) {
+		OBD_FREE_LARGE(lsm_foreign(lsm), lsm->lsm_foreign_size);
+	} else {
+		for (i = 0; i < entry_count; i++)
+			lsme_free(lsm->lsm_entries[i]);
+	}
 
-	lsm_size = offsetof(typeof(*lsm), lsm_entries[entry_count]);
+	lsm_size = lsm->lsm_magic == LOV_MAGIC_FOREIGN ?
+		   offsetof(typeof(*lsm), lsm_entries[1]) :
+		   offsetof(typeof(*lsm), lsm_entries[entry_count]);
 	OBD_FREE(lsm, lsm_size);
 }
 
@@ -510,15 +516,63 @@ const struct lsm_operations lsm_comp_md_v1_ops = {
 	.lsm_unpackmd         = lsm_unpackmd_comp_md_v1,
 };
 
+static struct
+lov_stripe_md *lsm_unpackmd_foreign(struct lov_obd *lov, void *buf,
+				    size_t buf_size)
+{
+	struct lov_foreign_md *lfm = buf;
+	struct lov_stripe_md *lsm;
+	size_t lsm_size;
+	struct lov_stripe_md_entry *lsme;
+
+	lsm_size = offsetof(typeof(*lsm), lsm_entries[1]);
+	OBD_ALLOC(lsm, lsm_size);
+	if (lsm == NULL)
+		RETURN(ERR_PTR(-ENOMEM));
+
+	atomic_set(&lsm->lsm_refc, 1);
+	spin_lock_init(&lsm->lsm_lock);
+	lsm->lsm_magic = le32_to_cpu(lfm->lfm_magic);
+	lsm->lsm_foreign_size = foreign_size_le(lfm);
+
+	/* alloc for full foreign EA including format fields */
+	OBD_ALLOC_LARGE(lsme, lsm->lsm_foreign_size);
+	if (lsme == NULL) {
+		OBD_FREE(lsm, lsm_size);
+		RETURN(ERR_PTR(-ENOMEM));
+	}
+
+	/* copy full foreign EA including format fields */
+	memcpy(lsme, buf, lsm->lsm_foreign_size);
+
+	lsm_foreign(lsm) = lsme;
+
+	return lsm;
+}
+
+const struct lsm_operations lsm_foreign_ops = {
+	.lsm_unpackmd         = lsm_unpackmd_foreign,
+};
+
 void dump_lsm(unsigned int level, const struct lov_stripe_md *lsm)
 {
 	int i, j;
 
-	CDEBUG(level, "lsm %p, objid "DOSTID", maxbytes %#llx, magic 0x%08X, "
-	       "refc: %d, entry: %u, layout_gen %u\n",
+	CDEBUG_LIMIT(level,
+		     "lsm %p, objid "DOSTID", maxbytes %#llx, magic 0x%08X, refc: %d, entry: %u, layout_gen %u\n",
 	       lsm, POSTID(&lsm->lsm_oi), lsm->lsm_maxbytes, lsm->lsm_magic,
 	       atomic_read(&lsm->lsm_refc), lsm->lsm_entry_count,
 	       lsm->lsm_layout_gen);
+
+	if (lsm->lsm_magic == LOV_MAGIC_FOREIGN) {
+		struct lov_foreign_md *lfm = (void *)lsm_foreign(lsm);
+
+		CDEBUG_LIMIT(level,
+			     "foreign LOV EA, magic %x, length %u, type %x, flags %x, value '%.*s'\n",
+		       lfm->lfm_magic, lfm->lfm_length, lfm->lfm_type,
+		       lfm->lfm_flags, lfm->lfm_length, lfm->lfm_value);
+		return;
+	}
 
 	for (i = 0; i < lsm->lsm_entry_count; i++) {
 		struct lov_stripe_md_entry *lse = lsm->lsm_entries[i];

lustre/doc/Makefile.am
@@ -63,6 +63,7 @@ MANFILES =					\
 	llapi_create_volatile_param.3		\
 	llapi_fd2parent.3			\
 	llapi_file_create.3			\
+	llapi_file_create_foreign.3		\
 	llapi_file_get_stripe.3			\
 	llapi_file_open.3			\
 	llapi_group_lock.3			\

lustre/doc/lfs-find.1
@@ -12,6 +12,7 @@ lfs-find \- Lustre client utility to list files with specific attributes
       [[\fB!\fR] \fB--component-start|\fB--comp-start\fR [\fB+-\fR]\fIn\fR[\fBKMGTPE\fR]]
       [[\fB!\fR] \fB--gid\fR|\fB-g\fR|\fB--group\fR|\fB-G\fR <\fIgname\fR>|<\fIgid\fR>]
       [[\fB!\fR] \fB--layout\fR|\fB-L mdt\fR,\fBraid0\fR,\fBreleased\fR]
+      [[\fB!\fR] \fB--foreign\fR [<\fItype\fR>]]
 [\fB--maxdepth\fR|\fB-D\fI n\fR]
       [[\fB!\fR] \fB--mdt\fR|\fB--mdt-index\fR|\fB-m\fR <\fIuuid\fR|\fIindex\fR,...>]
       [[\fB!\fR] \fB--mdt-count\fR|\fB-T\fR [\fB+-\fR]\fIn\fR]
@@ -107,7 +108,7 @@ File has specified numeric group ID.
 .BR --group | -G
 File belongs to specified group, numeric group ID allowed.
 .TP
-.BR --layout
+.BR --layout | -L
 File has a layout of the given type, one of:
 .RS 1.2i
 .TP
@@ -119,6 +120,11 @@ HSM-archived files that are not resident in the filesystem.
 .TP
 .B mdt
 Files that have the first data component on an MDT.
+.RE
+.TP
+.BR --foreign[=<type>]
+File is foreign (non-Lustre layout) and is of type if specified.
+Presently only none or daos are defined types.
 .RE
 .TP
 .BR --maxdepth
@@ -290,6 +296,9 @@ Recursively list all mirrored files that have more than 2 mirrors.
 .TP
 .B $ lfs find ! --mirror-state=ro /mnt/lustre
 Recursively list all out-of-sync mirrored files.
+.TP
+.B $ lfs find ! --foreign=daos /mnt/lustre
+Recursively list all but foreign files of daos type.
 .SH BUGS
 The
 .B lfs find

lustre/doc/lfs-setstripe.1
@@ -23,6 +23,9 @@ lfs setstripe \- set striping pattern of a file or directory default
 .B lfs setstripe --yaml=\fR<\fIyaml_template_file.lyl\fR> <\fIfile\fR>
 .br
 .B lfs setstripe --copy=\fR<\fIsource_template_file\fR> <\fIfile\fR>
+.br
+.B lfs setstripe --foreign[=\fR<\fIforeign_type\fR>\fB] \
+[--flags=\fR<\fIhex\fR>\fB] --xattr=\fR<\fIlayout_string\fR> <\fIfile\fR>
 .SH DESCRIPTION
 The
 .B lfs setstripe
@@ -123,6 +126,18 @@ This is similar to the
 option but avoids the need for the intermediate
 .B .lyl
 file.
+.TP
+.B lfs setstripe --foreign[=\fR<\fIforeign_type\fR>\fB] \
+[--flags=\fR<\fIhex\fR>\fB] --xattr=\fR<\fIlayout_string\fR> <\fIfile\fR>
+.br
+Create a new
+.I file
+with a foreign/non-lustre layout of type
+.I foreign_type \fR(\fBnone\fR, \fBdaos\fR, ...)
+with flags
+.I hex
+and a free-format layout value of
+.I layout_string.
 .SH STRIPE_OPTIONS
 The various OST stripe related options are listed and explained below:
 .TP
@@ -231,6 +246,29 @@ or
 .BR pool_name=none
 (since Lustre 2.11) to force a component to inherit the pool from the parent
 or root directory instead of the previous component.
+.TP
+.B --foreign \fR[<\fIforeign_type\fR>]
+file layout is non-lustre/free-format and of type <
+.IR foreign_type
+>, if specified (see also
+.IR --xattr
+and
+.IR --flags
+options).
+Current known types are
+.BR none
+and
+.BR daos
+, default is
+.BR none
+.
+.B -x\fR, \fB--xattr \fR<\fIlayout_string\fR>
+Mandatory non-lustre/free-format layout/LOV EA content for
+.I foreign
+file.
+.TP
+.B -F\fR, \fB--flags \fR<\fIhex\fR>
+Optional bitmap of flags for foreign type.
 .SH COMPONENT_OPTIONS
 The various component related options are listed and explained below.  The
 .B --component-*
@@ -436,6 +474,18 @@ file
 which can be created with the
 .B lfs getstripe --yaml
 command.
+.TP
+.B lfs setstripe --foreign=daos --flags=0xda08 \
+	--xattr=PUUID:CUUID /mnt/lustre/file1
+This creates foreign
+.BR file1
+of type
+.BR daos
+with non-lustre/free-format
+.BR PUUID:CUUID
+layout/LOV EA and flags
+.BR 0xda08
+.
 .SH SEE ALSO
 .BR lctl (1),
 .BR lfs (1),

lustre/doc/llapi_file_create_foreign.3
@@ -1,0 +1,91 @@
+.TH lustreapi 3 "2009 Jul 10" The Lustre user application interface library
+.SH NAME
+llapi_file_create_foreign \- create a file with foreign layout on a Lustre filesystem
+.SH SYNOPSIS
+.nf
+.B #include <sys/types.h>
+.B #include <sys/stat.h>
+.B #include <fcntl.h>
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_file_create_foreign(const char *" name ", mode_t " mode ","
+.BI "                    __u32 " type ", __u32 " flags ", char *" foreign_lov );
+.sp
+.fi
+.SH DESCRIPTION
+.LP
+.B llapi_file_create_foreign(\|)
+creates a file with
+.I foreign_lov
+free format layout and
+.I mode
+and
+.I name
+on a Lustre filesystem, followed by file close.
+.TP 15
+.I mode
+specifies the permission bits.
+Refer to
+.BR open(2)
+man page for a detailed description.
+.TP 15
+.I type
+specifies a type of foreign layout.
+.TP 15
+.I flags
+specifies any flags for foreign layout.
+.TP 15
+.I foreign_lov
+free-format string to be set as file layout/LOV EA.
+.SH RETURN VALUES
+.LP
+.B llapi_file_create_foreign(\|)
+return:
+.TP
+>=0
+on success, for
+.B llapi_file_open
+the return value is a file descriptor.
+.TP
+<0
+on failure, the absolute value is an error code.
+.SH ERRORS
+.TP 15
+.SM EEXIST
+.I name
+already exists.
+.TP
+.SM ENOTTY
+.I name
+may not point to a Lustre filesystem.
+.SH "EXAMPLE"
+.nf
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <lustre/lustreapi.h>
+int main(int argc, char *argv[])
+{
+        int rc;
+
+        if (argc != 2)
+                return -1;
+
+        rc = llapi_file_create_foreign(argv[1], 0600, LOV_FOREIGN_TYPE_DAOS,
+				       0xda08, "PUUID@CUUID");
+        if (rc < 0) {
+                fprintf(stderr, "file creation has failed, %s\\n", strerror(-rc));
+                return -1;
+        }
+        printf("foreign file %s has been created with 'PUUID@CUUID' layout!\\n",
+               argv[1]);
+        return 0;
+}
+.fi
+.SH "SEE ALSO"
+.BR llapi_file_open (3)
+.BR lustre (7),
+.BR lustreapi (7)
+.BR open (2),

lustre/include/lustre/lustreapi.h
@@ -128,6 +128,8 @@ struct llapi_stripe_param {
 
 int llapi_file_open_param(const char *name, int flags, mode_t mode,
 			  const struct llapi_stripe_param *param);
+int llapi_file_create_foreign(const char *name, mode_t mode, __u32 type,
+			      __u32 flags, char *foreign_lov);
 int llapi_file_create(const char *name, unsigned long long stripe_size,
 		      int stripe_offset, int stripe_count, int stripe_pattern);
 int llapi_file_open(const char *name, int flags, int mode,
@@ -263,7 +265,9 @@ struct find_param {
 				 fp_exclude_hash_type:1,
 				 fp_yaml:1,	/* output layout in YAML */
 				 fp_check_blocks:1,
-				 fp_exclude_blocks:1;
+				 fp_exclude_blocks:1,
+				 fp_check_foreign:1,
+				 fp_exclude_foreign:1;
 
 	enum llapi_layout_verbose fp_verbose;
 	int			 fp_quiet;
@@ -321,6 +325,7 @@ struct find_param {
 	unsigned int		 fp_depth;
 	unsigned int		 fp_hash_type;
 	unsigned int		 fp_time_margin; /* time margin in seconds */
+	__u32			 fp_foreign_type;
 };
 
 int llapi_ostlist(char *path, struct find_param *param);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1112,6 +1112,7 @@ enum obdo_flags {
 #define LOV_MAGIC_SPECIFIC	(0x0BD50000 | LOV_MAGIC_MAGIC)
 #define LOV_MAGIC		LOV_MAGIC_V1
 #define LOV_MAGIC_COMP_V1	(0x0BD60000 | LOV_MAGIC_MAGIC)
+#define LOV_MAGIC_FOREIGN	(0x0BD70000 | LOV_MAGIC_MAGIC)
 
 /*
  * magic for fully defined striping

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -55,6 +55,7 @@
 # include <limits.h>
 # include <stdbool.h>
 # include <stdio.h> /* snprintf() */
+# include <stdint.h>
 # include <string.h>
 # define NEED_QUOTA_DEFS
 /* # include <sys/quota.h> - this causes complaints about caddr_t */
@@ -540,6 +541,7 @@ struct fsxattr {
 /* 0x0BD40BD0 is occupied by LOV_MAGIC_MIGRATE */
 #define LOV_USER_MAGIC_SPECIFIC 0x0BD50BD0	/* for specific OSTs */
 #define LOV_USER_MAGIC_COMP_V1	0x0BD60BD0
+#define LOV_USER_MAGIC_FOREIGN	0x0BD70BD0
 
 #define LMV_USER_MAGIC		0x0CD30CD0    /* default lmv magic */
 #define LMV_USER_MAGIC_V0	0x0CD20CD0    /* old default lmv magic*/
@@ -624,6 +626,21 @@ struct lov_user_md_v3 {           /* LOV EA user data (host-endian) */
 	char  lmm_pool_name[LOV_MAXPOOLNAME + 1]; /* pool name */
 	struct lov_user_ost_data_v1 lmm_objects[0]; /* per-stripe data */
 } __attribute__((packed));
+
+struct lov_foreign_md {
+	__u32 lfm_magic;	/* magic number = LOV_MAGIC_FOREIGN */
+	__u32 lfm_length;	/* length of lfm_value */
+	__u32 lfm_type;		/* type, see LOV_FOREIGN_TYPE_ */
+	__u32 lfm_flags;	/* flags, type specific */
+	char lfm_value[];
+};
+
+#define foreign_size(lfm) (((struct lov_foreign_md *)lfm)->lfm_length + \
+			   offsetof(struct lov_foreign_md, lfm_value))
+
+#define foreign_size_le(lfm) \
+	(le32_to_cpu(((struct lov_foreign_md *)lfm)->lfm_length) + \
+	offsetof(struct lov_foreign_md, lfm_value))
 
 struct lu_extent {
 	__u64	e_start;
@@ -781,6 +798,20 @@ enum lmv_hash_type {
 #define LMV_HASH_NAME_FNV_1A_64	"fnv_1a_64"
 
 extern char *mdt_hash_name[LMV_HASH_TYPE_MAX];
+
+/**
+ * LOV foreign types
+ **/
+#define LOV_FOREIGN_TYPE_NONE 0
+#define LOV_FOREIGN_TYPE_DAOS 0xda05
+#define LOV_FOREIGN_TYPE_UNKNOWN UINT32_MAX
+
+struct lustre_foreign_type {
+	uint32_t lft_type;
+	const char *lft_name;
+};
+
+extern struct lustre_foreign_type lov_foreign_type[];
 
 /* Got this according to how get LOV_MAX_STRIPE_COUNT, see above,
  * (max buffer size - lmv+rpc header) / sizeof(struct lmv_user_mds_data) */

microsoft/amlFilesystem-lustre

commit b77099f275c2d4c0a159b924839e7f5b387b4c88

LU-20714 lov: bound foreign LOV EA reads by buffer size
The foreign layout EA reaches the client as a reply buffer of
mbo_eadatasize bytes, and its readers trust it. ll_manage_foreign()
checks only that the length is non-zero before reading lfm_magic and
lfm_type, and lsme_unpack_foreign() reads both before it bounds
lfm_length, so a buffer shorter than the 16-byte lov_foreign_md
header is read past its end. lsm_unpackmd_foreign() never compares
lfm_length with buf_size at all, then memcpy()s that many bytes out,
and readlink() or lfs getstripe hands the result to userspace.

Bound all three reads by the buffer the EA arrived in.
lod_verify_striping() bounds a foreign layout when it is set, so
reaching any of them needs a corrupt on-disk EA or a hostile MDS.

Test-Parameters: trivial
Fixes: 6a20bdcc608b ("LU-11376 lov: new foreign LOV format")
Fixes: 15d44e787e17 ("LU-12682 llite: fake symlink type of foreign file/dir")
Fixes: 61a002cd8631 ("LU-13602 flr: skip unknown FLR component types")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Id7acfb24dd190da63cf9ced7ae22d28da1874e37

lustre/llite/llite_foreign.c
@@ -65,7 +65,9 @@ int ll_manage_foreign(struct inode *inode, struct lustre_md *lmd)
 		if (lmd->layout.lb_buf != NULL && lmd->layout.lb_len != 0) {
 			struct lov_foreign_md *lfm = lmd->layout.lb_buf;
 
-			if (lfm->lfm_magic == LOV_MAGIC_FOREIGN)
+			if (lmd->layout.lb_len >=
+			    offsetof(struct lov_foreign_md, lfm_value) &&
+			    lfm->lfm_magic == LOV_MAGIC_FOREIGN)
 				ll_manage_foreign_file(inode, lfm);
 			GOTO(out, rc);
 		}

lustre/lov/lov_ea.c
@@ -485,6 +485,13 @@ lsme_unpack_foreign(struct lov_obd *lov, void *buf, size_t buf_size,
 
 	ENTRY;
 
+	if (buf_size < offsetof(struct lov_foreign_md, lfm_value)) {
+		CDEBUG(D_LAYOUT,
+		       "LOV EA foreign header does not fit in %zu bytes\n",
+		       buf_size);
+		RETURN(ERR_PTR(-EINVAL));
+	}
+
 	magic = le32_to_cpu(lfm->lfm_magic);
 	if (magic != LOV_MAGIC_FOREIGN)
 		RETURN(ERR_PTR(-EINVAL));
@@ -693,10 +700,25 @@ static struct
 lov_stripe_md *lsm_unpackmd_foreign(struct lov_obd *lov, void *buf,
 				    size_t buf_size)
 {
+	size_t hdr_size = offsetof(struct lov_foreign_md, lfm_value);
 	struct lov_foreign_md *lfm = buf;
 	struct lov_stripe_md *lsm;
 	size_t lsm_size;
 	struct lov_stripe_md_entry *lsme;
+
+	if (buf_size < hdr_size) {
+		CDEBUG(D_LAYOUT,
+		       "LOV EA foreign header of %zu bytes does not fit in %zu bytes\n",
+		       hdr_size, buf_size);
+		RETURN(ERR_PTR(-EINVAL));
+	}
+
+	if (le32_to_cpu(lfm->lfm_length) > buf_size - hdr_size) {
+		CDEBUG(D_LAYOUT,
+		       "LOV EA foreign value of %u bytes does not fit in %zu bytes\n",
+		       le32_to_cpu(lfm->lfm_length), buf_size - hdr_size);
+		RETURN(ERR_PTR(-EINVAL));
+	}
 
 	lsm_size = offsetof(typeof(*lsm), lsm_entries[1]);
 	OBD_ALLOC(lsm, lsm_size);