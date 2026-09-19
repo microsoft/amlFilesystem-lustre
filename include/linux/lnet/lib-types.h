lustre/lustre-release

commit 3c39dac19aaf7f3f4fdee104ce6da92dd1962776

LU-9680 utils: add netlink infrastructure
Netlink was designed as a successor to ioctl as defined under
RFC 3549. There are several advantages to using netlink over
ioctls or virtual file system interfaces like proc. Collecting
proc doesn't scale well which was seen with power drain on Android
phones. A netlink implementation was developed to remove this
performance hit. Details can be read at:

https://lwn.net/Articles/406975

Besides the scaling gains the other benefit is the flexiblity
with API changes. Adding or removing information to be transmitted
doesn't require creating a new interface like ioctl do. Instead
you add new code to handle the stream of attributes read from the
socket. Lastly you can multiplex data to N listeners with groups
using one request.

This patch adds netlink handling in a generic way that can be
used by the libyaml library. This greatly lowers the barrier by
only requiring the implementor to understand the libyaml API.

Change-Id: Idcdac653a1f9cc9931238e869c3beadaefcf3410
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/34230
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Ben Evans <beevans@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -2650,6 +2650,85 @@ failed:
 	return rc;
 }
 
+static int lnet_genl_parse_list(struct sk_buff *msg,
+				const struct ln_key_list *data[], u16 idx)
+{
+	const struct ln_key_list *list = data[idx];
+	const struct ln_key_props *props;
+	struct nlattr *node;
+	u16 count;
+
+	if (!list)
+		return 0;
+
+	if (!list->lkl_maxattr)
+		return -ERANGE;
+
+	props = list->lkl_list;
+	if (!props)
+		return -EINVAL;
+
+	node = nla_nest_start(msg, LN_SCALAR_ATTR_LIST);
+	if (!node)
+		return -ENOBUFS;
+
+	for (count = 1; count <= list->lkl_maxattr; count++) {
+		struct nlattr *key = nla_nest_start(msg, count);
+
+		if (count == 1)
+			nla_put_u16(msg, LN_SCALAR_ATTR_LIST_SIZE,
+				    list->lkl_maxattr);
+
+		nla_put_u16(msg, LN_SCALAR_ATTR_INDEX, count);
+		if (props[count].lkp_values)
+			nla_put_string(msg, LN_SCALAR_ATTR_VALUE,
+				       props[count].lkp_values);
+		if (props[count].lkp_key_format)
+			nla_put_u16(msg, LN_SCALAR_ATTR_KEY_FORMAT,
+				    props[count].lkp_key_format);
+		nla_put_u16(msg, LN_SCALAR_ATTR_NLA_TYPE,
+			    props[count].lkp_data_type);
+		if (props[count].lkp_data_type == NLA_NESTED) {
+			int rc;
+
+			rc = lnet_genl_parse_list(msg, data, ++idx);
+			if (rc < 0)
+				return rc;
+		}
+
+		nla_nest_end(msg, key);
+	}
+
+	nla_nest_end(msg, node);
+	return 0;
+}
+
+int lnet_genl_send_scalar_list(struct sk_buff *msg, u32 portid, u32 seq,
+			       const struct genl_family *family, int flags,
+			       u8 cmd, const struct ln_key_list *data[])
+{
+	int rc = 0;
+	void *hdr;
+
+	if (!data[0])
+		return -EINVAL;
+
+	hdr = genlmsg_put(msg, portid, seq, family, flags, cmd);
+	if (!hdr)
+		GOTO(canceled, rc = -EMSGSIZE);
+
+	rc = lnet_genl_parse_list(msg, data, 0);
+	if (rc < 0)
+		GOTO(canceled, rc);
+
+	genlmsg_end(msg, hdr);
+canceled:
+	if (rc < 0)
+		genlmsg_cancel(msg, hdr);
+	return rc;
+}
+EXPORT_SYMBOL(lnet_genl_send_scalar_list);
+
 /**
  * Initialize LNet library.
  *

lnet/include/lnet/lib-types.h
@@ -46,7 +46,9 @@
 #include <linux/semaphore.h>
 #include <linux/types.h>
 #include <linux/kref.h>
+#include <net/genetlink.h>
 
+#include <uapi/linux/lnet/lnet-nl.h>
 #include <uapi/linux/lnet/lnet-dlc.h>
 #include <uapi/linux/lnet/lnetctl.h>
 #include <uapi/linux/lnet/nidstr.h>
@@ -1253,5 +1255,45 @@ struct lnet {
 	/* UDSP list */
 	struct list_head		ln_udsp_list;
 };
+
+static const struct nla_policy scalar_attr_policy[LN_SCALAR_CNT + 1] = {
+	[LN_SCALAR_ATTR_LIST]		= { .type = NLA_NESTED },
+	[LN_SCALAR_ATTR_LIST_SIZE]	= { .type = NLA_U16 },
+	[LN_SCALAR_ATTR_INDEX]		= { .type = NLA_U16 },
+	[LN_SCALAR_ATTR_NLA_TYPE]	= { .type = NLA_U16 },
+	[LN_SCALAR_ATTR_VALUE]		= { .type = NLA_STRING },
+	[LN_SCALAR_ATTR_KEY_FORMAT]	= { .type = NLA_U16 },
+};
+
+int lnet_genl_send_scalar_list(struct sk_buff *msg, u32 portid, u32 seq,
+			       const struct genl_family *family, int flags,
+			       u8 cmd, const struct ln_key_list *data[]);
+
+/* Special workaround for pre-4.19 kernels to send error messages
+ * from dumpit routines. Newer kernels will send message with
+ * NL_SET_ERR_MSG information by default if NETLINK_EXT_ACK is set.
+ */
+static inline int lnet_nl_send_error(struct sk_buff *msg, int portid, int seq,
+				     int error)
+{
+#ifndef HAVE_NL_DUMP_WITH_EXT_ACK
+	struct nlmsghdr *nlh;
+
+	if (!error)
+		return 0;
+
+	nlh = nlmsg_put(msg, portid, seq, NLMSG_ERROR, sizeof(error), 0);
+	if (!nlh)
+		return -ENOMEM;
+#ifdef HAVE_NL_PARSE_WITH_EXT_ACK
+	netlink_ack(msg, nlh, error, NULL);
+#else
+	netlink_ack(msg, nlh, error);
+#endif
+	return nlmsg_len(nlh);
+#else
+	return error;
+#endif
+}
 
 #endif

debian/control
@@ -4,7 +4,7 @@ Priority: optional
 Maintainer: Brian J. Murrell <brian.murrell@intel.com>
 Uploaders: Brian J. Murrell <brian.murrell@intel.com>
 Standards-Version: 3.8.3
-Build-Depends: module-assistant, libreadline-dev, debhelper (>=9.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, pkg-config, libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync, libssl-dev, libpython3-dev, swig
+Build-Depends: module-assistant, libreadline-dev, debhelper (>=9.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, pkg-config, libtool, libyaml-dev, libnl-genl-3-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync, libssl-dev, libpython3-dev, swig
 Homepage: https://wiki.whamcloud.com/
 Vcs-Git: git://git.whamcloud.com/fs/lustre-release.git
 
@@ -12,7 +12,7 @@ Package: lustre-source
 Section: admin
 Architecture: all
 Priority: optional
-Depends: module-assistant, bzip2, debhelper (>= 9.0.0), libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch, pkg-config
+Depends: module-assistant, bzip2, debhelper (>= 9.0.0), libtool, libyaml-dev, libnl-genl-3-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch, pkg-config
 Description: source for Lustre filesystem client kernel modules
  Lustre is a scalable, secure, robust, highly-available cluster file system.
  This release is maintained by Whamcloud and available from
@@ -26,7 +26,7 @@ Package: lustre-client-utils
 Section: utils
 Architecture: i386 armhf powerpc ppc64el amd64 ia64 arm64
 Priority: optional
-Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, perl
+Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, libnl-genl-3-200, perl
 Description: Userspace utilities for the Lustre filesystem (client)
  Lustre is a scalable, secure, robust, highly-available cluster file system.
  This release is maintained by Whamcloud and available from
@@ -40,7 +40,7 @@ Package: lustre-server-utils
 Section: utils
 Architecture: i386 armhf powerpc ppc64el amd64 ia64 arm64
 Priority: optional
-Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, perl
+Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, libnl-genl-3-200, perl
 Provides: lustre-server-utils, lustre-client-utils (= ${binary:Version})
 Conflicts: lustre-client-utils
 Replaces: lustre-client-utils

debian/control.main
@@ -4,7 +4,7 @@ Priority: optional
 Maintainer: Brian J. Murrell <brian.murrell@intel.com>
 Uploaders: Brian J. Murrell <brian.murrell@intel.com>
 Standards-Version: 3.8.3
-Build-Depends: module-assistant, libreadline-dev, debhelper (>=9.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, pkg-config, libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync, libssl-dev, libpython3-dev, swig
+Build-Depends: module-assistant, libreadline-dev, debhelper (>=9.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, pkg-config, libtool, libyaml-dev, libnl-genl-3-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync, libssl-dev, libpython3-dev, swig
 Homepage: https://wiki.whamcloud.com/
 Vcs-Git: git://git.whamcloud.com/fs/lustre-release.git
 
@@ -12,7 +12,7 @@ Package: lustre-source
 Section: admin
 Architecture: all
 Priority: optional
-Depends: module-assistant, bzip2, debhelper (>= 9.0.0), libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch, pkg-config
+Depends: module-assistant, bzip2, debhelper (>= 9.0.0), libtool, libyaml-dev, libnl-genl-3-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch, pkg-config
 Description: source for Lustre filesystem client kernel modules
  Lustre is a scalable, secure, robust, highly-available cluster file system.
  This release is maintained by Whamcloud and available from
@@ -26,7 +26,7 @@ Package: lustre-client-utils
 Section: utils
 Architecture: i386 armhf powerpc ppc64el amd64 ia64 arm64
 Priority: optional
-Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, perl
+Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, libnl-genl-3-200, perl
 Description: Userspace utilities for the Lustre filesystem (client)
  Lustre is a scalable, secure, robust, highly-available cluster file system.
  This release is maintained by Whamcloud and available from
@@ -40,7 +40,7 @@ Package: lustre-server-utils
 Section: utils
 Architecture: i386 armhf powerpc ppc64el amd64 ia64 arm64
 Priority: optional
-Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, perl
+Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp-dev, zlib1g, libnl-genl-3-200, perl
 Provides: lustre-server-utils, lustre-client-utils (= ${binary:Version})
 Conflicts: lustre-client-utils
 Replaces: lustre-client-utils

libcfs/autoconf/lustre-libcfs.m4
@@ -488,6 +488,26 @@ kstrtobool_from_user, [
 ]) # LIBCFS_KSTRTOBOOL_FROM_USER
 
 #
+# LIBCFS_NETLINK_CALLBACK_START
+#
+# Kernel version 4.4-rc3 commit fc9e50f5a5a4e1fa9ba2756f745a13e693cf6a06
+# added a start function callback for struct netlink_callback
+#
+AC_DEFUN([LIBCFS_NETLINK_CALLBACK_START], [
+LB_CHECK_COMPILE([if struct genl_ops has start callback],
+cb_start, [
+	#include <net/genetlink.h>
+],[
+	struct genl_ops ops;
+
+	ops.start = NULL;
+],[
+	AC_DEFINE(HAVE_NETLINK_CALLBACK_START, 1,
+		[struct genl_ops has 'start' callback])
+])
+]) # LIBCFS_NETLINK_CALLBACK_START
+
+#
 # Kernel version 4.5-rc1 commit d12481bc58fba89427565f8592e88446ec084a24
 # added crypto hash helpers
 #
@@ -824,6 +844,26 @@ rht_bucket_var, [
 ]) # LIBCFS_RHT_BUCKET_VAR
 
 #
+# Kernel version 4.11-rc5 commit fceb6435e85298f747fee938415057af837f5a8a
+# began the enhanchement of Netlink with extended ACK struct for advanced
+# error handling. By commit 7ab606d1609dd6dfeae9c8ad0a8a4e051d831e46 we
+# had full support for this new feature.
+#
+AC_DEFUN([LIBCFS_NL_EXT_ACK], [
+LB_CHECK_COMPILE([if Netlink supports netlink_ext_ack],
+netlink_ext_ack, [
+	#include <net/genetlink.h>
+],[
+	struct genl_info info;
+
+	info.extack = NULL;
+],[
+	AC_DEFINE(HAVE_NL_PARSE_WITH_EXT_ACK, 1,
+		[netlink_ext_ack is an argument to nla_parse type function])
+])
+]) # LIBCFS_NL_EXT_ACK
+
+#
 # Kernel version 4.11 commit f9fe1c12d126f9887441fa5bb165046f30ddd4b5
 # introduced rhashtable_lookup_get_insert_fast
 #
@@ -918,6 +958,25 @@ wait_queue_task_list, [
 		[linux wait_queue_head_t list_head is name head])
 ])
 ]) # LIBCFS_WAIT_QUEUE_TASK_LIST_RENAME
+
+#
+# LIBCFS_NLA_STRDUP
+#
+# Kernel version 4.13-rc1 commit 2cf0c8b3e6942ecafe6ebb1a6d0328a81641bf39
+# created nla_strdup(). This is needed since push strings can be
+# any size.
+#
+AC_DEFUN([LIBCFS_NLA_STRDUP], [
+LB_CHECK_COMPILE([if 'nla_strdup()' exists],
+nla_strdup, [
+	#include <net/netlink.h>
+],[
+	char *tmp = nla_strdup(NULL, GFP_KERNEL);
+],[
+	AC_DEFINE(HAVE_NLA_STRDUP, 1,
+		['nla_strdup' is available])
+])
+]) # LIBCFS_NLA_STRDUP
 
 #
 # LIBCFS_WAIT_QUEUE_ENTRY
@@ -1209,6 +1268,23 @@ xarray_support, [
 ])
 EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_XARRAY_SUPPORT
+
+#
+# Kernel version 4.19-rc6 commit 4a19edb60d0203cd5bf95a8b46ea8f63fd41194c
+# added extended ACK handling to Netlink dump handlers
+#
+AC_DEFUN([LIBCFS_NL_DUMP_EXT_ACK], [
+LB_CHECK_COMPILE([if Netlink dump handlers support ext_ack],
+netlink_dump_ext_ack, [
+	#include <net/netlink.h>
+],[
+	struct netlink_callback *cb = NULL;
+	cb->extack = NULL;
+],[
+	AC_DEFINE(HAVE_NL_DUMP_WITH_EXT_ACK, 1,
+		[netlink_ext_ack is handled for Netlink dump handlers])
+])
+]) # LIBCFS_NL_DUMP_EXT_ACK
 
 #
 # LIBCFS_HAVE_IOV_ITER_TYPE
@@ -1535,6 +1611,7 @@ LIBCFS_KERNEL_PARAM_LOCK
 LIBCFS_HAVE_TOPOLOGY_SIBLING_CPUMASK
 # 4.4
 LIBCFS_KSTRTOBOOL_FROM_USER
+LIBCFS_NETLINK_CALLBACK_START
 # 4.5
 LIBCFS_CRYPTO_HASH_HELPERS
 LIBCFS_EXPORT_KSET_FIND_OBJ
@@ -1566,10 +1643,12 @@ LIBCFS_RHT_BUCKET_VAR
 LIBCFS_HAVE_PROCESSOR_HEADER
 LIBCFS_HAVE_WAIT_BIT_HEADER
 LIBCFS_MEMALLOC_NORECLAIM
+LIBCFS_NL_EXT_ACK
 LIBCFS_WAIT_QUEUE_TASK_LIST_RENAME
 LIBCFS_CPUS_READ_LOCK
 LIBCFS_UUID_T
 # 4.13
+LIBCFS_NLA_STRDUP
 LIBCFS_WAIT_QUEUE_ENTRY
 # 4.14
 LIBCFS_DEFINE_TIMER
@@ -1589,6 +1668,7 @@ LIBCFS_TCP_SOCK_SET_NODELAY
 LIBCFS_TCP_SOCK_SET_KEEPIDLE
 # 4.19
 LIBCFS_XARRAY_SUPPORT
+LIBCFS_NL_DUMP_EXT_ACK
 # 4.20
 LIBCFS_HAVE_IOV_ITER_TYPE
 # 5.0

libcfs/include/libcfs/linux/linux-net.h
@@ -23,6 +23,59 @@
 #ifndef __LIBCFS_LINUX_NET_H__
 #define __LIBCFS_LINUX_NET_H__
 
+#include <net/netlink.h>
+
+#ifndef HAVE_NLA_STRDUP
+char *nla_strdup(const struct nlattr *nla, gfp_t flags);
+#endif /* !HAVE_NLA_STRDUP */
+
+#ifndef HAVE_NL_PARSE_WITH_EXT_ACK
+
+#define NL_SET_BAD_ATTR(extack, attr)
+
+/* this can be increased when necessary - don't expose to userland */
+#define NETLINK_MAX_COOKIE_LEN  20
+
+/**
+ * struct netlink_ext_ack - netlink extended ACK report struct
+ * @_msg: message string to report - don't access directly, use
+ *      %NL_SET_ERR_MSG
+ * @bad_attr: attribute with error
+ * @cookie: cookie data to return to userspace (for success)
+ * @cookie_len: actual cookie data length
+ */
+struct netlink_ext_ack {
+	const char *_msg;
+	const struct nlattr *bad_attr;
+	u8 cookie[NETLINK_MAX_COOKIE_LEN];
+	u8 cookie_len;
+};
+
+#define GENL_SET_ERR_MSG(info, msg) NL_SET_ERR_MSG(NULL, msg)
+
+static inline int cfs_nla_parse(struct nlattr **tb, int maxtype,
+				const struct nlattr *head, int len,
+				const struct nla_policy *policy,
+				struct netlink_ext_ack *extack)
+{
+	return nla_parse(tb, maxtype, head, len, policy);
+}
+
+static inline int cfs_nla_parse_nested(struct nlattr *tb[], int maxtype,
+				       const struct nlattr *nla,
+				       const struct nla_policy *policy,
+				       struct netlink_ext_ack *extack)
+{
+	return nla_parse_nested(tb, maxtype, nla, policy);
+}
+
+#else /* !HAVE_NL_PARSE_WITH_EXT_ACK */
+
+#define cfs_nla_parse_nested    nla_parse_nested
+#define cfs_nla_parse           nla_parse
+
+#endif
+
 #ifdef HAVE_KERNEL_SETSOCKOPT
 
 #include <net/tcp.h>

libcfs/libcfs/linux/linux-prim.c
@@ -38,7 +38,9 @@
 #ifdef HAVE_SCHED_HEADERS
 #include <linux/sched/mm.h>
 #endif
+#include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <net/netlink.h>
 
 #if defined(CONFIG_KGDB)
 #include <asm/kgdb.h>
@@ -199,3 +201,22 @@ int kstrtobool_from_user(const char __user *s, size_t count, bool *res)
 }
 EXPORT_SYMBOL(kstrtobool_from_user);
 #endif /* !HAVE_KSTRTOBOOL_FROM_USER */
+
+#ifndef HAVE_NLA_STRDUP
+char *nla_strdup(const struct nlattr *nla, gfp_t flags)
+{
+	size_t srclen = nla_len(nla);
+	char *src = nla_data(nla), *dst;
+
+	if (srclen > 0 && src[srclen - 1] == '\0')
+		srclen--;
+
+	dst = kmalloc(srclen + 1, flags);
+	if (dst != NULL) {
+		memcpy(dst, src, srclen);
+		dst[srclen] = '\0';
+	}
+	return dst;
+}
+EXPORT_SYMBOL(nla_strdup);
+#endif /* !HAVE_NLA_STRDUP */

lnet/autoconf/lustre-lnet.m4
@@ -894,6 +894,41 @@ AC_DEFUN([LN_CONFIGURE], [
 AC_MSG_NOTICE([LNet core checks
 ==============================================================================])
 
+# lnet/utils/lnetconfig/liblnetconfig_netlink.c
+AS_IF([test "x$enable_dist" = xno], [
+	PKG_CHECK_MODULES(LIBNL3, [libnl-genl-3.0 >= 3.1])
+])
+
+AC_CHECK_LIB([nl-3], [nla_get_s32], [
+	AC_DEFINE(HAVE_NLA_GET_S32, 1,
+		[libnl3 supports nla_get_s32])
+	], [
+])
+
+AC_CHECK_LIB([nl-3], [nla_get_s64], [
+	AC_DEFINE(HAVE_NLA_GET_S64, 1,
+		[libnl3 supports nla_get_s64])
+	], [
+])
+
+#
+# LN_USR_NLMSGERR
+#
+AC_DEFUN([LN_USR_NLMSGERR], [
+AC_MSG_CHECKING([if 'enum nlmsgerr_attrs' exists])
+AC_COMPILE_IFELSE([AC_LANG_SOURCE([
+	#include <linux/netlink.h>
+
+	int main(void) {
+		int x = (int)NLMSGERR_ATTR_MAX;
+		return x;
+	}
+])],[
+	AC_DEFINE(HAVE_USRSPC_NLMSGERR, 1,
+		['enum nlmsgerr_attrs' exists])
+])
+]) # LN_USR_NLMGSERR
+
 # lnet/utils/portals.c
 AC_CHECK_HEADERS([netdb.h])
 AC_CHECK_FUNCS([gethostbyname])
@@ -920,6 +955,7 @@ AC_SUBST(LIBEFENCE)
 
 LN_CONFIG_DLC
 LN_USR_RDMA
+LN_USR_NLMSGERR
 ]) # LN_CONFIGURE
 
 #

microsoft/amlFilesystem-lustre

commit 3ea20fb445ef35e9c0c817a870e33abff5d1a79a

LU-9680 lnet: pass version to lnet_genl_send_scalar_list
By default genlmsg_put() sets the message version to the default
genl_family version. That is the latest version. Much of our
Netlink handling involves requesting that a specific version
of a message be sent back to user land and user land can query
the received version. This change ensures the correct version
is sent with the key table message.

Test-Parameters: trivial
Fixes: 3c39dac19aa ("LU-9680 utils: add netlink infrastructure")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: I16bac147b69b0761b96aa9f5c01222b8353355c4

include/linux/lnet/lib-types.h
@@ -2317,7 +2317,8 @@ static const struct nla_policy scalar_attr_policy[LN_SCALAR_MAX + 1] = {
 };
 
 int lnet_genl_send_scalar_list(struct sk_buff *msg, u32 portid, u32 seq,
-			       const struct genl_family *family, int flags,
-			       u8 cmd, const struct ln_key_list *data[]);
+			       const struct genl_family *family,
+			       u8 version, int flags, u8 cmd,
+			       const struct ln_key_list *data[]);
 
 #endif

lnet/lnet/api-ni.c
@@ -3121,9 +3121,11 @@ static int lnet_genl_parse_list(struct sk_buff *msg,
 }
 
 int lnet_genl_send_scalar_list(struct sk_buff *msg, u32 portid, u32 seq,
-			       const struct genl_family *family, int flags,
-			       u8 cmd, const struct ln_key_list *data[])
+			       const struct genl_family *family,
+			       u8 version, int flags, u8 cmd,
+			       const struct ln_key_list *data[])
 {
+	struct genlmsghdr *gnlh;
 	int rc = 0;
 	void *hdr;
 
@@ -3133,6 +3135,9 @@ int lnet_genl_send_scalar_list(struct sk_buff *msg, u32 portid, u32 seq,
 	hdr = genlmsg_put(msg, portid, seq, family, flags, cmd);
 	if (!hdr)
 		GOTO(canceled, rc = -EMSGSIZE);
+
+	gnlh = (struct genlmsghdr *)((char *)hdr - GENL_HDRLEN);
+	gnlh->version = min_t(u8, version, family->version);
 
 	rc = lnet_genl_parse_list(msg, data, 0);
 	if (rc < 0)
@@ -5443,6 +5448,7 @@ static const struct ln_key_list cpt_of_nid_props_list = {
 static int lnet_cpt_of_nid_show_dump(struct sk_buff *msg,
 				     struct netlink_callback *cb)
 {
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct lnet_genl_nid_cpt_list *lgncl;
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
@@ -5466,6 +5472,7 @@ static int lnet_cpt_of_nid_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq, &lnet_family,
+						gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_CPT_OF_NID, all);
 		if (rc < 0) {
@@ -6145,7 +6152,8 @@ static int lnet_net_show_dump(struct sk_buff *msg,
 			}
 
 			rc = lnet_genl_send_scalar_list(msg, portid, seq,
-							&lnet_family, flags,
+							&lnet_family,
+							gnlh->version, flags,
 							LNET_CMD_NETS, all);
 			if (rc < 0) {
 				NL_SET_ERR_MSG(extack, "failed to send key table");
@@ -7616,7 +7624,7 @@ static int lnet_route_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lnet_family,
+						&lnet_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_ROUTES, all);
 		if (rc < 0) {
@@ -8062,7 +8070,7 @@ static int lnet_peer_ni_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lnet_family,
+						&lnet_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_PEERS, all);
 		if (rc < 0) {
@@ -8773,6 +8781,7 @@ static int lnet_ping_show_dump(struct sk_buff *msg,
 			       struct netlink_callback *cb)
 {
 	struct lnet_genl_ping_list *plist = lnet_ping_dump_ctx(cb);
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
 	int seq = cb->nlh->nlmsg_seq;
@@ -8785,7 +8794,7 @@ static int lnet_ping_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lnet_family,
+						&lnet_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_PING, all);
 		if (rc < 0) {
@@ -8890,7 +8899,8 @@ cant_reach:
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq, &lnet_family,
-						flags, LNET_CMD_PING, fail);
+						gnlh->version, flags,
+						LNET_CMD_PING, fail);
 		if (rc < 0) {
 			NL_SET_ERR_MSG(extack,
 				       "failed to send new key table");
@@ -9022,6 +9032,7 @@ static int lnet_ping_cmd(struct sk_buff *skb, struct genl_info *info)
 
 	rc = lnet_genl_send_scalar_list(reply, info->snd_portid,
 					info->snd_seq, &lnet_family,
+					gnlh->version,
 					NLM_F_CREATE | NLM_F_MULTI,
 					LNET_CMD_PING, all);
 	if (rc < 0) {
@@ -9157,7 +9168,8 @@ static int lnet_ping_cmd(struct sk_buff *skb, struct genl_info *info)
 
 		rc = lnet_genl_send_scalar_list(reply, info->snd_portid,
 						info->snd_seq, &lnet_family,
-						flags, LNET_CMD_PING, fail);
+						gnlh->version, flags,
+						LNET_CMD_PING, fail);
 		if (rc < 0) {
 			GENL_SET_ERR_MSG(info,
 					 "failed to send new key table");
@@ -9323,6 +9335,7 @@ static int lnet_peer_dist_show_dump(struct sk_buff *msg,
 				    struct netlink_callback *cb)
 {
 	struct lnet_genl_processid_list *plist = lnet_peer_dump_ctx(cb);
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
 	int seq = cb->nlh->nlmsg_seq;
@@ -9335,7 +9348,7 @@ static int lnet_peer_dist_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lnet_family,
+						&lnet_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_PEER_DIST, all);
 		if (rc < 0) {
@@ -9622,6 +9635,7 @@ static const struct ln_key_list debug_recovery_attr_list = {
 static int lnet_debug_recovery_show_dump(struct sk_buff *msg,
 					 struct netlink_callback *cb)
 {
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct lnet_genl_debug_recovery_list *drlist;
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
@@ -9676,7 +9690,7 @@ static int lnet_debug_recovery_show_dump(struct sk_buff *msg,
 
 		all[0] = keys;
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lnet_family,
+						&lnet_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_DBG_RECOV, all);
 		if (rc < 0) {
@@ -9880,6 +9894,7 @@ static int lnet_fault_show_dump(struct sk_buff *msg,
 				struct netlink_callback *cb)
 {
 	struct lnet_genl_fault_rule_list *rlist = lnet_fault_dump_ctx(cb);
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
 	int seq = cb->nlh->nlmsg_seq;
@@ -9899,7 +9914,7 @@ static int lnet_fault_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lnet_family,
+						&lnet_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_CMD_FAULT, all);
 		if (rc < 0) {
@@ -10420,6 +10435,7 @@ static int lnet_stats_show_dump(struct sk_buff *msg,
 				struct netlink_callback *cb)
 {
 	const struct ln_key_list *all[] = { &lnet_stats_list, NULL };
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
 	int seq = cb->nlh->nlmsg_seq;
@@ -10443,6 +10459,7 @@ static int lnet_stats_show_dump(struct sk_buff *msg,
 	health = &counters.lct_health;
 
 	rc = lnet_genl_send_scalar_list(msg, portid, seq, &lnet_family,
+					gnlh->version,
 					NLM_F_CREATE | NLM_F_MULTI,
 					LNET_CMD_STATS, all);
 	if (rc < 0) {

lnet/selftest/conctl.c
@@ -791,6 +791,7 @@ static int lst_sessions_show_dump(struct sk_buff *msg,
 	const struct ln_key_list *all[] = {
 		&lst_session_keys, NULL
 	};
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
 	int seq = cb->nlh->nlmsg_seq;
@@ -809,6 +810,7 @@ static int lst_sessions_show_dump(struct sk_buff *msg,
 		node_count++;
 
 	rc = lnet_genl_send_scalar_list(msg, portid, seq, &lst_family,
+					gnlh->version,
 					NLM_F_CREATE | NLM_F_MULTI,
 					LNET_SELFTEST_CMD_SESSIONS, all);
 	if (rc < 0) {
@@ -940,6 +942,7 @@ err_conf:
 
 		rc = lnet_genl_send_scalar_list(msg, info->snd_portid,
 						info->snd_seq, &lst_family,
+						gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_SELFTEST_CMD_SESSIONS,
 						all);
@@ -1228,6 +1231,7 @@ static int lst_groups_show_dump(struct sk_buff *msg,
 				struct netlink_callback *cb)
 {
 	struct lst_genl_group_list *glist = lst_group_dump_ctx(cb);
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
 	int seq = cb->nlh->nlmsg_seq;
@@ -1239,6 +1243,7 @@ static int lst_groups_show_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq, &lst_family,
+						gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LNET_SELFTEST_CMD_GROUPS, all);
 		if (rc < 0) {

lustre/obdclass/kernelcomm.c
@@ -169,6 +169,7 @@ static int lustre_device_list_dump(struct sk_buff *msg,
 				   struct netlink_callback *cb)
 {
 	struct genl_dev_list *glist = device_dump_ctx(cb);
+	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct obd_device *filter = glist->gdl_target;
 	struct obd_device *obd = NULL;
 	struct netlink_ext_ack *extack = cb->extack;
@@ -183,7 +184,7 @@ static int lustre_device_list_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lustre_family,
+						&lustre_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LUSTRE_CMD_DEVICES, all);
 		if (rc < 0) {
@@ -496,7 +497,7 @@ static int lustre_targets_dump(struct sk_buff *msg,
 		};
 
 		rc = lnet_genl_send_scalar_list(msg, portid, seq,
-						&lustre_family,
+						&lustre_family, gnlh->version,
 						NLM_F_CREATE | NLM_F_MULTI,
 						LUSTRE_CMD_TARGETS, all);
 		if (rc < 0) {
@@ -849,7 +850,8 @@ int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb)
 			if (idx)
 				flags |= NLM_F_REPLACE;
 			rc = lnet_genl_send_scalar_list(msg, portid, seq,
-							info->family, flags,
+							info->family,
+							gnlh->version, flags,
 							gnlh->cmd, all);
 			OBD_FREE_PTR_ARRAY(all, stats->ls_num + 2);
 			OBD_FREE(start, len);
@@ -866,6 +868,7 @@ int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb)
 
 			rc = lnet_genl_send_scalar_list(msg, portid, seq,
 							info->family,
+							gnlh->version,
 							NLM_F_CREATE | NLM_F_MULTI,
 							gnlh->cmd, all);
 			if (rc < 0) {