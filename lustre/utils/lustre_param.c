lustre/lustre-release

commit 908117b0328df1d4c1d7f413fab792c70830b01a

LU-17231 doc: updated man page script
Updated the generate-manpage.sh script to generate better parameter
man pages.

Now uses --param to create a parameter man page, and they are no
longer headed by lctl-param as it could be confused with
lctl-{get,list,set}_param, and also makes looking for parameters
easier.

Added lctl help_param to open the man file for a parameter which
includes tab completion for intermediate directories to make it
easier to find groups of parameter man pages.

The man pages cpu_partition_table(4) and cpu_partition_distance(4)
have been added as an example for formatting.

Signed-off-by: Frederick Dilger <fdilger@whamcloud.com>
Change-Id: I2a3b64331c1d423caa1d1ebfd6f9a8316b0da489
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61318
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/utils/lustre_param.c
@@ -1246,6 +1246,114 @@ int jt_lctl_findparam(int argc, char **argv)
 	return rc;
 }
 
+/**
+ * Modifies param to remove any device name.
+ * Ex. NNN.lustre-OST0000.MMM -> NNN.MMM
+ * \param[in] param	parameter name
+ *
+ * \retval 0		success
+ * \retval -errno	error
+ */
+static int man_path(char *param)
+{
+	char *tmp;
+	char *dot;
+	char *dot2;
+
+	tmp = strstr(param, "llite");
+	if (tmp) {
+		dot = strchr(tmp, '.');
+		if (!dot) {
+			*tmp = '\0';
+			return 0;
+		}
+		if (tmp == param)
+			param = dot + 1;
+	}
+
+	dot = strchr(param, '.');
+	while (dot) {
+		dot2 = strchr(dot + 1, '.');
+		tmp = strchr(dot, '-');
+		if (tmp) {
+			if (!dot2) {
+				*tmp = '\0';
+				break;
+			}
+			if (tmp < dot2) {
+				memmove(dot+1, dot2+1, strlen(dot2+1) + 1);
+				dot = dot2;
+			}
+		}
+		tmp = strchr(dot, '@');
+		if (tmp) {
+			if (!dot2) {
+				*tmp = '\0';
+				break;
+			}
+			if (tmp < dot2) {
+				memmove(dot+1, dot2+1, strlen(dot2+1) + 1);
+				dot = dot2;
+			}
+		}
+		dot = dot2;
+	}
+
+	return 0;
+}
+
+static int helpparam_cmdline(int argc, char **argv, struct param_opts *popt)
+{
+	popt->po_show_name = 0;
+	popt->po_only_name = 1;
+	popt->po_follow_symlinks = 1;
+	popt->po_merge = 1;
+	popt->po_dshbak = 1;
+
+	return param_out_cmdline(argc, argv, popt, "");
+}
+
+int jt_lctl_helpparam(int argc, char **argv)
+{
+	int rc = 0, index;
+	struct param_opts popt;
+	char *path;
+
+	memset(&popt, 0, sizeof(popt));
+	index = helpparam_cmdline(argc, argv, &popt);
+	if (index != argc - 1)
+		return CMD_HELP;
+
+	path = strdup(argv[index]);
+
+	rc = jt_clean_path(&popt, path);
+	if (rc < 0) {
+		fprintf(stderr, "error: %s: cleaning '%s': %s\n",
+			jt_cmdname(argv[0]), path, strerror(-rc));
+		goto out;
+	}
+
+	rc = do_param_op(&popt, path, NULL, LIST_PARAM, NULL);
+	if (rc < 0) {
+		if (rc == -ENOENT && getuid() != 0) {
+			rc = llapi_param_display_value(path, 0,
+						       PARAM_FLAGS_SHOW_SOURCE,
+						       stdout);
+		}
+		if (rc < 0)
+			rc = execlp("man", "man", argv[index], NULL);
+		goto out;
+	}
+
+	man_path(argv[index]);
+
+	rc = execlp("man", "man", argv[index], NULL);
+
+out:
+	free(path);
+	return rc;
+}
+
 static int getparam_cmdline(int argc, char **argv, struct param_opts *popt)
 {
 	popt->po_show_name = 1;

Documentation/man4/cpu_npartitions.4
@@ -1,0 +1,1 @@
+.so man4/cpu_pattern.4

Documentation/man4/cpu_partition_distance.4
@@ -1,0 +1,61 @@
+.TH CPU_PARTITION_DISTANCE 4 2025-09-15 Lustre "Lustre Kernel Interfaces"
+.SH NAME
+cpu_partition_distance \-
+.SH SYNOPSIS
+.SS PROPERTIES
+.TP
+.B Perms
+.BR 444 " | " -r--r--r--.
+.TP
+.B Scope
+Global
+.TP
+.B Config
+always present
+.SH DESCRIPTION
+.B cpu_partition_distance
+shows the maximum NUMA distance between all source nodes to all destination
+nodes in each partition. The distance values are used to determine the best
+network interface for each thread to use when sending messages,
+in order to minimize cross-NUMA memory access.
+The NUMA distance reported between two nodes the ratio of the latency to access
+memory on the destination node from the source node compared to that of
+accessing local memory, the values are normalized to 10 (1.0x) meaning that if
+the distance between two nodes is
+.IR DIST ,
+then there will be
+.RI ( DIST /10)x
+more latency to access remote memory.
+These values are taken from `/sys/devices/system/node/*/distance`
+parameters provided by the kernel.
+.PP
+The distance output is formatted as follows:
+.EX
+CPT0	: CPT0:DIST00 CPT1:DIST01 ...
+CPT1	: CPT0:DIST10 CPT1:DIST11 ...
+\&...
+.EE
+.SH MODULES
+This parameter is in the following modules:
+.EX
+.B cpu_partition_distance
+.EE
+.SH EXAMPLES
+For a system with 4 partitions:
+.RS
+.EX
+cpu_partition_distance=
+0	: 0:10 1:12 2:10 3:11
+1	: 0:12 1:10 2:14 3:10
+2	: 0:10 1:14 2:10 3:11
+3	: 0:11 1:10 2:11 3:10
+.EE
+.RE
+.SH AVAILABILITY
+.B cpu_partition_distance
+is part of the
+.BR lustre (7)
+filesystem package.
+.\" commit #
+.SH SEE ALSO
+.BR cpu_partition_table (4)

Documentation/man4/cpu_partition_table.4
@@ -1,0 +1,49 @@
+.TH CPU_PARTITION_TABLE 4 2025-09-15 Lustre "Lustre Kernel Interfaces"
+.SH NAME
+cpu_partition_table \- Display of the CPU format
+.SH SYNOPSIS
+.SS PROPERTIES
+.TP
+.B Perms
+.BR 444 " | " -r--r--r--.
+.TP
+.B Scope
+Global
+.TP
+.B Config
+always present
+.SH DESCRIPTION
+.B cpu_partition_table
+is a representation of how the CPU cores are distributed across mutliple
+partitions. The table is formatted as follows:
+.EX
+PART0	: CPU_CORE [CPU_CORE ...]
+PART1	: CPU_CORE [CPU_CORE ...]
+\&...
+.EE
+where
+.IR PART0 ", " PART1 ,
+etc. are the partition numbers and CPU_CORE is the
+id of the CPU core.
+.SH MODULES
+This parameter is in the following modules:
+.EX
+.B cpu_partition_table
+.EE
+.SH EXAMPLES
+The default CPT layout puts 4 cores in each partition, for a system with 16
+cores:
+.EX
+.B cpu_partition_table=0	: 0 1 2 3
+1	: 4 5 6 7
+2	: 8 9 10 11
+3	: 12 13 14 15
+.EE
+.SH AVAILABILITY
+.B cpu_partition_table
+is part of the
+.BR lustre (7)
+filesystem package.
+.\" commit #
+.SH SEE ALSO
+.BR cpu_partition_distance (4)

Documentation/man4/cpu_pattern.4
@@ -1,0 +1,160 @@
+.TH CPU_PATTERN 4 2025-10-18 Lustre "Lustre Kernel Interfaces"
+.SH NAME
+cpu_pattern \- CPU partitions pattern
+cpu_npartitions \- number of CPU partitions
+.SH SYNOPSIS
+.SS PROPERTIES
+.TP
+.B Perms
+.BR 444 " | " -r--r--r--.
+.BR 444 " | " -r--r--r--.
+.TP
+.B Scope
+Glabal
+.TP
+.B Config
+always present
+.SH DESCRIPTION
+.B cpu_pattern
+and
+.B cpu_npartitions
+are used to configure the CPU partition table.
+If no pattern is specifified then there will be a partition for every 4 cores
+in the system.
+This is a module option that can only be set during module initialization using
+the
+.B cpu_pattern
+and
+.B cpu_npartitions
+options in
+.BR /etc/modprobe.d/lustre.conf .
+.PP
+A user can specify CPU partitions by string pattern with the following
+behaviors:
+.RS
+.EX
+cpu_pattern="0[CORE0, CORE1, ...] 1[CORE2, CORE3, ...] ..."
+.EE
+.RE
+This pattern allows you to specify a set of CPU cores for each partition. The
+cores do not have to be contiguous and do not need to contain all available
+cores. However, cores in a CPT should be in the same NUMA node, according to
+.BR numactl (8) " -s" .
+.RS
+.EX
+cpu_npartitions=N
+.EE
+.RE
+This pattern allows you to specify the number of CPU partitions. The cores
+will be evenly split into the specified number of partitions. This is overridden
+by the
+.B cpu_pattern="N"
+option, but can otherwise be combined with other patterns.
+.RS
+.EX
+cpu_pattern="N 0[NODE0, NODE1, ...] 1[NODE2, NODE3, ...] ..."
+.EE
+.RE
+This pattern allows you to specify a set of NUMA nodes for each partition. The
+nodes do not have to be contiguous and do not need to contain all available
+nodes.
+.RS
+.EX
+cpu_pattern="C[INDEX0, INDEX1, ...]"
+.EE
+.RE
+This pattern allows you to exclude a set of cores from each partition.
+The CPT is first initialized, using npartitions, NUMA
+nodes, or the default layout.
+Then cores are removed from each partition that match the specified indeces.
+.RS
+.EX
+cpu_pattern="X[CORE0, CORE1, ...]"
+.EE
+.RE
+This pattern allows you to exclude a set of cores from the CPT that they
+belong to. The CPT is first initialized, using npartitions, NUMA nodes,
+or the default layout.
+.SS CPU partition
+The CPU partition is virtual processing unit. It can present 1-N cores,
+or 1-N NUMA nodes, in other words, CPU partition is a pool of processors.
+The ID of a CPU partition always starts from 0.
+.SS CPU Partition Table (CPT)
+A CPT is a set of CPU partitions. There are two modes for CPT:
+.B CFS_CPU_MODE_NUMA
+and
+.BR CFS_CPU_MODE_SMP.
+.SH MODULES
+This parameter is in the following modules:
+.EX
+.B /etc/modprobe.d/lustre.conf
+.EE
+.SH EXAMPLES
+The default pattern for the CPT will put 4 cores in each partitions:
+.RS
+.EX
+options libcfs cpu_pattern=
+cpu_partition_table=0	: 0 1 2 3
+1	: 4 5 6 7
+2	: 8 9 10 11
+3	: 12 13 14 15
+.EE
+.RE
+The modifying the npartitions for the CPT will split the cores evenly:
+.RS
+.EX
+options libcfs cpu_npartitions=3
+cpu_partition_table=0	: 0 1 2 3 4 5
+1	: 6 7 8 9 10
+2	: 11 12 13 14 15
+.EE
+.RE
+Use a default mapping of the CPT to the NUMA node layout that has 4 nodes:
+.RS
+.EX
+options libcfs cpu_pattern=N
+cpu_partition_table=0	: 0 1 2 3
+1	: 4 5 6 7
+2	: 8 9 10 11
+3	: 12 13 14 15
+.EE
+.RE
+Put several NUMA nodes into one partition:
+.RS
+.EX
+options libcfs cpu_pattern=N 0[0,1] 1[2-3]
+cpu_partition_table=0	: 0 1 2 3 4 5 6 7
+1	: 8 9 10 11 12 13 14 15
+.EE
+.RE
+Exclude the first two cores from each partition:
+.RS
+.EX
+options libcfs cpu_pattern=C[0,1]
+cpu_partition_table=0	: 2 3
+1	: 6 7
+2	: 10 11
+3	: 14 15
+.EE
+.RE
+Exclude cores 0 and 10 to reseerve them for non-Lustre tasks:
+.RS
+.EX
+options libcfs cpu_pattern=X[0,10]
+options libcfs cpu_npartitions=3
+cpu_partition_table=0	: 1 2 3 4 5
+1	: 6 7 8 9
+2	: 11 12 13 14 15
+.EE
+.RE
+.SH AVAILABILITY
+.B cpu_pattern
+is part of the
+.BR lustre (7)
+filesystem package.
+.\" commit #
+.SH SEE ALSO
+.BR cpu_npartitions (4),
+.BR cpu_partition_distance (4),
+.BR cpu_partition_table (4),
+.BR numa (7)

Documentation/man8/lctl-find_param.8
@@ -3,6 +3,7 @@
 lctl-find_param \- search for parameters matching patterns
 .SH SYNOPSIS
 .SY "lctl find_param"
+.RB [ --dshbak | -b ]
 .RB [ --color | "-c auto" | always | never ]
 .RB [ --links | -l ]
 .RB [ --no-links | -L ]
@@ -22,6 +23,13 @@ This command is equivalent to
 The various options supported by
 .B lctl find_param
 are listed and explained below:
+.TP
+.BR -b ", " --dshbak
+Aggregate parameters with wildcards whenever a device number appears in the
+parameter name.
+By default --merge is enabled with this option to merge the simplified names.
+Specify --no-merge if you want the parameter names with wildcards but do not
+want them to be merged.
 .TP
 .BR -c ", " --color=auto|always|never
 Use one of auto|always|never as the rule of when to apply color. The default is

Documentation/man8/lctl-get_param.8
@@ -185,5 +185,6 @@ filesystem package since release 1.7.0
 .BR lustre (7),
 .BR lctl (8),
 .BR lctl-find_param (8),
+.BR lctl-help_param (8),
 .BR lctl-list_param (8),
 .BR lctl-set_param (8)

Documentation/man8/lctl-help_param.8
@@ -1,0 +1,40 @@
+.TH LCTL-HELP_PARAM 8 "2025-09-17" Lustre "Lustre Configuration Utilities"
+.SH NAME
+lctl-help_param \- show documentation for Lustre parameters
+.SH SYNOPSIS
+.SY "lctl help_param"
+.I PARAM
+.YS
+.SH DESCRIPTION
+Opens the man page documentation for
+.IR PARAM .
+This is helpful because the man page files do not always match the parameter
+name since the OBD-device-specific part of the parameter is excluded from the
+man page title. Additionally, the tab completion support allows users to look
+through each subset of parameters if they are not sure of the exact name.
+.SH OPTIONS
+.B lctl help_param
+does not currently support any options.
+.SH EXAMPLES
+Find which params are available for fld.cli-*:
+.EX
+.RS
+.B # lctl help_param fld.cli-lustre-clilmv-ffff907f78edc000.
+.I fld.cli-lustre-clilmv-ffff907f78edc000.cache_flush
+.I fld.cli-lustre-clilmv-ffff907f78edc000.hash
+.I fld.cli-lustre-clilmv-ffff907f78edc000.targets
+.RE
+.EE
+.SH AVAILABILITY
+.B lctl help_param
+is part of the
+.BR lustre (7)
+filesystem package since release 2.18.0
+.\" Added in commit v2_17_5x
+.SH SEE ALSO
+.BR lfs (1),
+.BR lustre (7),
+.BR lctl-find_param (8),
+.BR lctl-get_param (8),
+.BR lctl-list_param (8),
+.BR lctl-set_param (8)

microsoft/amlFilesystem-lustre

commit e8e71632d7cc73976d9a009ddf49c9b6b99d6d42

LU-930 utils: reach the llite pages from "lctl help_param"
"lctl help_param" strips the OBD device name out of a parameter name
with man_path() and then execs man(1) on it, but it never found a page
for an llite parameter:

  # lctl help_param llite.lustre-ffff8ca6f63e5000.sync_on_close
  No manual entry for llite.lustre-ffff8ca6f63e5000.sync_on_close

man_path() special-cased llite by advancing its own copy of the "param"
pointer past the "llite." prefix.  The loop below only collapses a
component that sits between two dots, so starting it after "llite."
left the device as the first component instead of a middle one: nothing
was rewritten at all and the caller's argv[] reached man(1) untouched.

The special case is not needed either.  The generic loop already turns
"llite.lustre-ffff8ca6f63e5000.sync_on_close" into
"llite.sync_on_close", which is what the page is called.  Drop it.

Test-Parameters: trivial
Fixes: 908117b0328d ("LU-17231 doc: updated man page script")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I96ba6350ea6556b698f498be0e7744c8dd2f43ca

lustre/utils/lustre_param.c
@@ -1271,17 +1271,6 @@ static int man_path(char *param)
 	char *dot;
 	char *dot2;
 
-	tmp = strstr(param, "llite");
-	if (tmp) {
-		dot = strchr(tmp, '.');
-		if (!dot) {
-			*tmp = '\0';
-			return 0;
-		}
-		if (tmp == param)
-			param = dot + 1;
-	}
-
 	dot = strchr(param, '.');
 	while (dot) {
 		dot2 = strchr(dot + 1, '.');